15b4440f95387fd4bdc0cd6ef4aaaa88f45b6915
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-07-11 17:00:54 +0500

    BC-3565

--- src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
+++ src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
@@ -1,14 +1,22 @@
 package school.faang.user_service.service;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.UserFilterDto;
+import school.faang.user_service.entity.Country;
+import school.faang.user_service.entity.Skill;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.contact.Contact;
 import school.faang.user_service.exceptions.DataValidationException;
 import school.faang.user_service.repository.SubscriptionRepository;
 import school.faang.user_service.service.SubscriptionService;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class SubscriptionServiceTest {
@@ -61,5 +69,76 @@ class SubscriptionServiceTest {
         int idUser = 11;
         Assert.assertThrows(DataValidationException.class,
                 ()-> subscriptionService.unfollowUser(idUser, idUser));
+    }
+
+    @Test
+    void getFollowersThrowIllegalException(){
+        int idUser = -10;
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> subscriptionService.getFollowers(idUser, new UserFilterDto()));
+    }
+
+    @Test
+    void filterUserReturnTrue(){
+        User user = createUser();
+        UserFilterDto filter = createUserFilterDto();
+        Assertions.assertEquals(true, subscriptionService.filterUser(user, filter));
+    }
+
+    @Test
+    void filterUserReturnFalse(){
+        User user = createUser();
+        user.setCity("notCity");
+        UserFilterDto filter = createUserFilterDto();
+        Assertions.assertEquals(false, subscriptionService.filterUser(user, filter));
+    }
+
+    private User createUser(){
+        User user = new User();
+        user.setUsername("Username");
+        user.setEmail("user@mail.com");
+        user.setPhone("89991324567");
+        user.setAboutMe("I'm interesting person");
+        user.setCountry(createCountry());
+        user.setCity("UserCity");
+        user.setExperience(4);
+        user.setContacts(createContacts());
+        user.setSkills(createSkills());
+        return user;
+    }
+
+    private Country createCountry(){
+        Country country = new Country();
+        country.setTitle("UserCountry");
+        return country;
+    }
+    private List<Contact> createContacts(){
+        Contact contact = new Contact();
+        Contact contact2 = new Contact();
+        contact.setContact("UserContact");
+        contact2.setContact("UserContact");
+        return List.of(contact, contact2);
+    }
+    private List<Skill> createSkills(){
+        Skill skill = new Skill();
+        Skill skill2 = new Skill();
+        skill.setTitle("userSkill");
+        skill2.setTitle("userSkill");
+        return List.of(skill, skill2);
+    }
+
+    private UserFilterDto createUserFilterDto(){
+        UserFilterDto userFilterDto = new UserFilterDto();
+        userFilterDto.setNamePattern("Username");
+        userFilterDto.setAboutPattern("I'm interesting person");
+        userFilterDto.setEmailPattern("user@mail.com");
+        userFilterDto.setContactPattern("UserContact");
+        userFilterDto.setCountryPattern("UserCountry");
+        userFilterDto.setCityPattern("UserCity");
+        userFilterDto.setPhonePattern("89991324567");
+        userFilterDto.setSkillPattern("userSkill");
+        userFilterDto.setExperienceMin(1);
+        userFilterDto.setExperienceMax(7);
+        return userFilterDto;
     }
 }

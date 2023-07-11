0ea1f3c9920360989778ff1f617bb946dd1fd3cb
aliceklim <50102971+aliceklim@users.noreply.github.com>
2023-07-11 22:15:44 +0200

    validation added

--- src/main/java/school/faang/user_service/dto/skill/SkillDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -1,5 +1,7 @@
 package school.faang.user_service.dto.skill;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,5 +9,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class SkillDto {
     private Long id;
+    @NotEmpty(message = "Title can't be empty")
+    @Size(max = 100, message = "Title should be at least 3 symbols short")
     private String title;
 }

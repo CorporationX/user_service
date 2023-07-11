a1252cd44041a2cc2c26df0c627be61740cad3e7
DiegaLoL <alex.eey@mail.ru>
2023-07-11 18:15:25 +0900

    bc-2528 implement MentorshipService

---
+++ src/main/java/school/faang/user_service/controller/MentorshipController.java
@@ -0,0 +1,18 @@
+package school.faang.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.service.MentorshipService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MentorshipController {
+    private final MentorshipService mentorshipService;
+
+    public List<User> getMentees(Long userId) {
+        return mentorshipService.getMentees(userId);
+    }
+}
---
+++ src/main/java/school/faang/user_service/service/MentorshipService.java
@@ -0,0 +1,28 @@
+package school.faang.user_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MentorshipService {
+    private final UserRepository userRepository;
+
+    public List<User> getMentees(Long userId) {
+        List<User> mentees = new ArrayList<>();
+        if (userRepository.findById(userId).isEmpty()) {
+            throw new NoSuchElementException("There is no user with such ID");
+        } else if (userRepository.findById(userId).get().getMentees().isEmpty()) {
+            return mentees;
+        }
+        return userRepository.findById(userId).get().getMentees();
+    }
+}
+

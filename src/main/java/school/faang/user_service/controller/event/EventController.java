c3de7e5b002b8f23c3b58ba9fd1e19fe5a2f3c8b
zazozhik <ya.940@mail.ru>
2023-07-12 22:53:17 +0300

    SkillDto3

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -3,6 +3,7 @@ package school.faang.user_service.controller.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.event.EventDto;
+import school.faang.user_service.dto.event.EventFilterDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.event.EventService;
 
@@ -20,5 +21,9 @@ public class EventController {
     }
     public EventDto getEvent(long id){
         return eventService.create(eventService.getEvent(id));
+    }
+    public void getEventsByFilter(EventFilterDto filter){
+
+        eventService.getEventsByFilter(filter);
     }
 }
--- src/main/java/school/faang/user_service/service/event/EventService.java
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -3,6 +3,7 @@ package school.faang.user_service.service.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.event.EventDto;
+import school.faang.user_service.dto.event.EventFilterDto;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.entity.Skill;
 import school.faang.user_service.entity.User;
@@ -10,6 +11,10 @@ import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.mapper.EventMapper;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.event.EventRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component("eventService")
 @RequiredArgsConstructor
@@ -46,5 +51,74 @@ public class EventService {
     public EventDto getEvent(long eventId) {
         return EventMapper.INSTANCE.toEventDto(eventRepository.findById(eventId)
                 .orElseThrow(() -> new DataValidationException("Ошибка")));
+    }
+
+    public List<EventDto> getEventsByFilter(EventFilterDto filter) {
+        List<EventDto> events = new ArrayList<>();
+        eventRepository.findAll().forEach(event -> {
+            events.add(EventMapper
+                    .INSTANCE.toEventDto(event));
+        });
+        return events.stream()
+                .filter(event -> {
+                    if (event.getStartDate() == null) {
+                        event.setStartDate(LocalDateTime.MIN);
+                    }
+                    if (event.getEndDate() == null) {
+                        event.setEndDate(LocalDateTime.MAX);
+                    }
+                    return event.getStartDate().isAfter(filter.getStartDate())
+                            && event.getStartDate().isBefore(filter.getEndDate());
+                })
+                .filter(event -> {
+                    if (event.getOwnerId() == null) {
+                        return false;
+                    } else {
+                        return event.getOwnerId().equals(filter.getOwnerId());
+                    }
+                })
+                .filter(event -> {
+                    if (event.getRelatedSkills() == null) {
+                        return false;
+                    } else {
+                        return event.getRelatedSkills().containsAll(filter.getRelatedSkills());
+                    }
+                })
+                .filter(event -> {
+                    if (event.getLocation() == null) {
+                        return false;
+                    } else {
+                        return event.getLocation().equals(filter.getLocation());
+                    }
+                })
+                .filter(event -> {
+                    if (event.getTitle() == null) {
+                        return false;
+                    } else {
+                        return event.getTitle().equals(filter.getTitle());
+                    }
+                })
+                .filter(event -> {
+                    if (event.getDescription() == null) {
+                        return false;
+                    } else {
+                        return event.getDescription().equals(filter.getDescription());
+                    }
+                })
+                .filter(event -> {
+                    if (event.getMaxAttendees() == 0) {
+                        return false;
+                    } else {
+                        return event.getMaxAttendees() == (filter.getMaxAttendees());
+                    }
+                })
+                .filter(event -> {
+                    if (event.getId() == null) {
+                        return false;
+                    } else {
+                        return event.getId().equals(filter.getId());
+                    }
+                })
+                .toList();
     }
 }

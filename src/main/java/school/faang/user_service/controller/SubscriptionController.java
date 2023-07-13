0bf07fbc6a0317067e99b748b62c310d4c2fff3f
Alex Tarasov <tarasof.av@gmail.com>
2023-07-13 16:28:56 +0300

    fix task BC-3445

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,23 +1,23 @@
 package school.faang.user_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.SubscriptionService;
 
 @Controller
+@RequiredArgsConstructor
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
-    @Autowired
-    public SubscriptionController(SubscriptionService subscriptionService) {
-        this.subscriptionService = subscriptionService;
+    void followUser(long followerId, long followeeId) {
+        validate(followerId, followeeId);
+        subscriptionService.followUser(followerId, followeeId);
     }
 
-    void followUser(long followerId, long followeeId) {
-        if (followerId == followeeId) {
+    private void validate(long followerId, long followeeId) {
+        if(followerId == followeeId){
             throw new DataValidationException("You can't subscribe to yourself");
         }
-        subscriptionService.followUser(followerId, followeeId);
     }
 }
--- src/test/java/school/faang/user_service/controller/SubscriptionControllerTest.java
+++ src/test/java/school/faang/user_service/controller/SubscriptionControllerTest.java
@@ -1,17 +1,19 @@
 package school.faang.user_service.controller;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.SubscriptionService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class SubscriptionControllerTest {
@@ -30,10 +32,8 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    public void testAssertThrow() {
-
-        // Verify that a DataValidationException is thrown
-        Assert.assertThrows(DataValidationException.class, () -> subscriptionController.followUser(followerId, followeeId));
+    public void testAssertThrowsDataValidationExceptionForMethodFollowUser() {
+        assertThrows(DataValidationException.class, () -> subscriptionController.followUser(followerId, followeeId));
     }
 
     @Test
@@ -41,24 +41,18 @@ class SubscriptionControllerTest {
         followerId = 1;
         followeeId = 2;
 
-        // Test case where followerId is not equal to followeeId
         subscriptionController.followUser(followerId, followeeId);
 
-        // Verify that the followUser method in the subscriptionService is called with the correct arguments
-        Mockito.verify(subscriptionService, Mockito.times(1)).followUser(followerId, followeeId);
+        verify(subscriptionService, times(1)).followUser(followerId, followeeId);
     }
 
     @Test
-    public void testMessageThrow() {
-        // Test case where followerId is equal to followeeId
+    public void testMessageThrowForMethodFollowUser() {
         try {
             subscriptionController.followUser(followerId, followerId);
         } catch (DataValidationException e) {
-            // Verify that a DataValidationException is thrown
             assertEquals("You can't subscribe to yourself", e.getMessage());
         }
-
-        // Verify that the followUser method in the subscriptionService is not called
-        Mockito.verifyNoInteractions(subscriptionService);
+        verifyNoInteractions(subscriptionService);
     }
 }

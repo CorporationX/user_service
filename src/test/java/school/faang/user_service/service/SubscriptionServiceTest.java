4cdacda9809f8a72a5c3af0f67a2169d86634913
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-07-13 12:53:39 +0500

    BC-3568 - fixed all bugs

--- src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
+++ src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
@@ -34,7 +34,6 @@ class SubscriptionServiceTest {
     void followUserCallRepositoryMethod(){
         int followerId = 11;
         int followeeId = 15;
-        doNothing().when(subscriptionRepository).unfollowUser(followerId, followeeId);
         subscriptionService.followUser(followerId, followeeId);
         Mockito.verify(subscriptionRepository, Mockito.times(1))
                 .followUser(followerId, followeeId);
@@ -43,7 +42,6 @@ class SubscriptionServiceTest {
     void unfollowUserCallRepositoryMethod(){
         int followerId = 11;
         int followeeId = 15;
-        doNothing().when(subscriptionRepository).unfollowUser(followerId, followeeId);
         subscriptionService.unfollowUser(followerId, followeeId);
         Mockito.verify(subscriptionRepository, Mockito.times(1))
                 .unfollowUser(followerId, followeeId);
@@ -54,10 +52,10 @@ class SubscriptionServiceTest {
         long followeeId = 15;
         UserFilterDto filterDto = createUserFilterDto();
         Mockito.when(subscriptionRepository.findByFolloweeId(followeeId))
-                .thenReturn(Stream.of(new User()));
+                .thenReturn(Stream.of(createUser()));
         subscriptionService.getFollowers(followeeId, filterDto);
         Mockito.verify(subscriptionRepository, Mockito.times(1))
-                .findByFollowerId(followeeId);
+                .findByFolloweeId(followeeId);
     }
 
     @Test
@@ -66,7 +64,7 @@ class SubscriptionServiceTest {
         int amountOfFollowers = 50;
         Mockito.when(subscriptionRepository.findFollowersAmountByFolloweeId(userID))
                         .thenReturn(amountOfFollowers);
-        subscriptionRepository.findFollowersAmountByFolloweeId(userID);
+        subscriptionService.getFollowersCount(userID);
         Mockito.verify(subscriptionRepository, Mockito.times(1))
                 .findFollowersAmountByFolloweeId(userID);
     }
@@ -76,10 +74,10 @@ class SubscriptionServiceTest {
         long followeeId = 15;
         UserFilterDto filterDto = createUserFilterDto();
         Mockito.when(subscriptionRepository.findByFolloweeId(followeeId))
-                .thenReturn(Stream.of(new User()));
+                .thenReturn(Stream.of(createUser()));
         subscriptionService.getFollowers(followeeId, filterDto);
         Mockito.verify(subscriptionRepository, Mockito.times(1))
-                .findByFollowerId(followeeId);
+                .findByFolloweeId(followeeId);
     }
 
     @Test
@@ -88,7 +86,7 @@ class SubscriptionServiceTest {
         int amountOfFollowers = 50;
         Mockito.when(subscriptionRepository.findFolloweesAmountByFollowerId(followeeId))
                 .thenReturn(amountOfFollowers);
-        subscriptionService.getFollowingCount(amountOfFollowers);
+        subscriptionService.getFollowingCount(followeeId);
         Mockito.verify(subscriptionRepository, Mockito.times(1))
                 .findFolloweesAmountByFollowerId(followeeId);
     }
@@ -130,5 +128,38 @@ class SubscriptionServiceTest {
         userFilterDto.setExperienceMin(1);
         userFilterDto.setExperienceMax(7);
         return userFilterDto;
+    }
+    private User createUser(){
+        User user = new User();
+        user.setUsername("Username");
+        user.setEmail("user@mail.com");
+        user.setPhone("89991324567");
+        user.setAboutMe("I'm interesting person");
+        user.setCountry(createCountry());
+        user.setCity("UserCity");
+        user.setExperience(4);
+        user.setContacts(createContacts());
+        user.setSkills(createSkills());
+        return user;
+    }
+
+    private Country createCountry(){
+        Country country = new Country();
+        country.setTitle("UserCountry");
+        return country;
+    }
+    private List<Contact> createContacts(){
+        Contact contact = new Contact();
+        Contact contact2 = new Contact();
+        contact.setContact("UserContact");
+        contact2.setContact("UserContact");
+        return List.of(contact, contact2);
+    }
+    private List<Skill> createSkills(){
+        Skill skill = new Skill();
+        Skill skill2 = new Skill();
+        skill.setTitle("userSkill");
+        skill2.setTitle("userSkill");
+        return List.of(skill, skill2);
     }
 }

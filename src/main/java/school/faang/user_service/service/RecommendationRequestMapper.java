ae49b916c2d60da80772d3f7a457ca75223c1b3c
Viktor Ivanov <avacode78@gmail.com>
2023-07-13 13:18:58 +0700

    add component annotation but it does not work with it

--- src/main/java/school/faang/user_service/service/RecommendationRequestMapper.java
+++ src/main/java/school/faang/user_service/service/RecommendationRequestMapper.java
@@ -1,9 +1,11 @@
 package school.faang.user_service.service;
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.RecommendationRequestDto;
 import school.faang.user_service.entity.recommendation.RecommendationRequest;
 
+@Component
 @Mapper
 public interface RecommendationRequestMapper {
     RecommendationRequest toEntity(RecommendationRequestDto dto);

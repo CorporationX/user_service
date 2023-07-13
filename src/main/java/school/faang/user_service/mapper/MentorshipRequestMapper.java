2e46a30719f15466a61069993561cffe5240cddb
Andrey <khalyavinandrey@gmail.com>
2023-07-13 16:51:12 +0500

    updated toEntity method (with handling NullPointerException when filtering) added toDto method to transform entity to dto when filtering

--- src/main/java/school/faang/user_service/mapper/MentorshipRequestMapper.java
+++ src/main/java/school/faang/user_service/mapper/MentorshipRequestMapper.java
@@ -4,8 +4,10 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.MentorshipRequestDto;
+import school.faang.user_service.dto.RequestFilterDto;
 import school.faang.user_service.entity.MentorshipRequest;
 import school.faang.user_service.service.MentorshipRequestService;
+import school.faang.user_service.util.validator.FilterRequestStatusValidator;
 
 @Component
 @Mapper(unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE,
@@ -18,4 +20,14 @@ public interface MentorshipRequestMapper {
     @Mapping(target = "requester", expression = "java(mentorshipRequestService.findUserById(dto.getRequesterId()))")
     @Mapping(target = "receiver", expression = "java(mentorshipRequestService.findUserById(dto.getReceiverId()))")
     MentorshipRequest toEntity(MentorshipRequestDto dto, MentorshipRequestService mentorshipRequestService); // поэтому вынес сервис в аргументы
+
+    @Mapping(target = "requester", expression = "java(validator.validateRequester(dto, mentorshipRequestService))")
+    @Mapping(target = "receiver", expression = "java(validator.validateReceiver(dto, mentorshipRequestService))")
+    @Mapping(target = "status", expression = "java(validator.validateStatus(dto))")
+    MentorshipRequest toEntity(RequestFilterDto dto, MentorshipRequestService mentorshipRequestService,
+                               FilterRequestStatusValidator validator); // работает даже если все поля = null
+
+    @Mapping(target = "requesterId", expression = "java(mentorshipRequest.getRequester().getId())")
+    @Mapping(target = "receiverId", expression = "java(mentorshipRequest.getReceiver().getId())")
+    MentorshipRequestDto toDto(MentorshipRequest mentorshipRequest);
 }

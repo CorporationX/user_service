da2a3639bd0ea227c7ff89312b4eaebf73ac2943
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-07-14 00:22:17 +0100

    Pre PR some issue with Mapper and Spring

--- src/main/java/school/faang/user_service/service/recommendation/RecommendationService.java
+++ src/main/java/school/faang/user_service/service/recommendation/RecommendationService.java
@@ -6,7 +6,6 @@ import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.dto.recommendation.SkillOfferDto;
 import school.faang.user_service.entity.Skill;
 import school.faang.user_service.entity.recommendation.Recommendation;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.mapper.recommendation.RecommendationMapper;
 import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.UserSkillGuaranteeRepository;
@@ -14,7 +13,6 @@ import school.faang.user_service.repository.recommendation.RecommendationReposit
 import school.faang.user_service.repository.recommendation.SkillOfferRepository;
 import school.faang.user_service.validator.RecommendationValidator;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,21 +31,15 @@ public class RecommendationService {
     public RecommendationDto create(RecommendationDto recommendationDto) {
         validate(recommendationDto);
 
-        long entityId = recommendationRepository.create(
+        long recommendationId = recommendationRepository.create(
                 recommendationDto.getAuthorId(), recommendationDto.getReceiverId(), recommendationDto.getContent());
         saveSkillOffers(recommendationDto);
-        Recommendation recommendation = recommendationRepository.findById(entityId)
+        Recommendation recommendation = recommendationRepository.findById(recommendationId)
                 .orElseThrow();
 
         return recommendationMapper.toDto(recommendation);
     }
 
-    // В случае успешной валидации входных данных в методе create сервиса recommendationService,
-    // создается новая рекомендация в базе данных с помощью вызова recommendationRepository.create,
-    // сохраняются предлагаемые навыки с помощью метода saveSkillOffers, и возвращается объект RecommendationDto с данными созданной рекомендации.
-    //Сохранить предложенные в рекомендации скиллы в репозиторий SkillOfferRepository используя его метод create.
-    // Если у пользователя, которому дают рекомендацию, такой скилл уже есть,
-    // то добавить автора рекомендации гарантом к скиллу, который он предлагает, если этот автор еще не стоит там гарантом.
 
     private void saveSkillOffers(RecommendationDto recommendationDto) {
         long recommendationId = recommendationDto.getId();

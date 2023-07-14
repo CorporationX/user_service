bab625d2da341c84f24a273ff01f412da84621a4
Matvey <matveynikishaev@gmail.com>
2023-07-14 19:48:51 +0300

    Added paths for all methods in controller

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -1,10 +1,9 @@
 package school.faang.user_service.controller.event;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.event.EventFilterDto;
 import school.faang.user_service.exception.DataValidationException;
@@ -13,16 +12,24 @@ import school.faang.user_service.service.event.EventService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/event")
 public class EventController {
     private final EventService eventService;
 
-    public EventDto create(EventDto eventDto) {
-        validateEvent(eventDto);
-        return eventService.create(eventDto);
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody EventDto eventDto) {
+        try {
+            validateEvent(eventDto);
+            return ResponseEntity.ok(eventService.create(eventDto));
+        } catch (DataValidationException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
-
+    @GetMapping("/get/{eventId}")
     public ResponseEntity<?> getEvent(long eventId) {
         if (eventId < 0) {
             return ResponseEntity.badRequest().body("Event ID cannot be less than 0");
@@ -35,7 +42,7 @@ public class EventController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-
+    @GetMapping("/filter")
     public ResponseEntity<List<?>> getEventsByFilter(@RequestBody EventFilterDto filter) {
         try {
             List<EventDto> filteredEvents = eventService.getEventsByFilter(filter);
@@ -46,7 +53,7 @@ public class EventController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteEvent(@RequestBody long id) {
         try {
             if (id < 0) {
@@ -63,16 +70,11 @@ public class EventController {
         if (event.getTitle() == null || event.getTitle().isBlank()) {
             throw new DataValidationException("Event title cannot be empty");
         }
-        if (event.getTitle().isBlank()) {
-            throw new DataValidationException("Event title cannot be empty");
-        }
-
         if (event.getStartDate() == null) {
             throw new DataValidationException("Event start date cannot be null");
         }
-
         if (event.getOwnerId() == null || event.getOwnerId() < 0) {
-            throw new DataValidationException("Event owner ID cannot be null");
+            throw new DataValidationException("Event owner ID cannot be null or negative");
         }
     }
 }

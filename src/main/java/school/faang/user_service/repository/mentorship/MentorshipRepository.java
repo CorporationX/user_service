3a4a79f38c9e87bb910edb7261db0da5ebe3a00c
”Ramazan” <mansurov.r13@gmail.com>
2023-07-14 16:47:15 +0300

    3597: new fixes after review

--- src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
+++ src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
@@ -9,10 +9,4 @@ import school.faang.user_service.entity.User;
 @Repository
 public interface MentorshipRepository extends CrudRepository<User, Long> {
 
-    @Query(nativeQuery = true, value = """
-            SELECT * FROM users u
-            JOIN mentorship m  ON u.id = m.mentee_id
-            WHERE m.mentor_id = ?
-            """)
-    List<User> findMenteesByMentorId(long mentorId);
 }

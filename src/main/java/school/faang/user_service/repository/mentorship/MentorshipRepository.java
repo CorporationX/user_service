e3f1c066cdb6b65ba92e2f8fa133624fc77c9312
”Ramazan” <mansurov.r13@gmail.com>
2023-07-14 16:53:25 +0300

    3597: new fixes after review

--- src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
+++ src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
@@ -1,7 +1,5 @@
 package school.faang.user_service.repository.mentorship;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.User;

35be028480873610ebbd99f7f94c1ab16d4c0e70
Sergey Ivahnenko <ivahnenkose.2003@gmail.com>
2023-07-14 13:02:10 +0700

    fixed

--- src/main/java/school/faang/user_service/controller/MenteeMapper.java
+++
@@ -1,9 +0,0 @@
-package school.faang.user_service.controller;
-
-import org.mapstruct.Mapper;
-import school.faang.user_service.entity.User;
-
-@Mapper(componentModel = "spring")
-public interface MenteeMapper {
-    MenteeDto toUser (User user);
-}

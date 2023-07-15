06d9a03079938f8c0eb2d026a041db4d777e3ba7
DiegaLoL <alex.eey@mail.ru>
2023-07-16 07:13:20 +0900

    bc-3808 Add tests for MentorshipService

--- src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.service;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,62 +8,62 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(MockitoExtension.class)
 class MentorshipServiceTest {
-
     @Mock
     private MentorshipRepository mentorshipRepository;
-
-    @Mock
-    private UserRepository userRepository;
 
     @InjectMocks
     private MentorshipService mentorshipService;
 
     private final Long userId = 2L;
-    private final Long mentor = 1L;
-    private final Long mentee = 3L;
-    private final List<User> userList = Collections.emptyList();
-
-    @Test
-    void shouldReturnMentees(){
-        //Mockito.when(userRepository.findById(userId).isPresent()).thenReturn(Boolean.TRUE);
-        Mockito.when(mentorshipService.getMentees(userId)).thenReturn(new ArrayList<>()).getMock();
-        assertEquals(userList, mentorshipService.getMentees(userId));
-        Mockito.verify(mentorshipService, Mockito.times(1)).getMentees(userId);
-    }
+    private final Long user1 = 3L;
+    private final Long user2 = 4L;
+    private final Long user3 = 5L;
+    private final List<User> userList = List.of(
+            User.builder().id(user1).build(),
+            User.builder().id(user2).build(),
+            User.builder().id(user3).build());
+    private final User user = User.builder()
+            .id(2L)
+            .mentees(userList)
+            .build();
+    private final User user4 = User.builder()
+            .id(2L)
+            .mentors(userList)
+            .build();
 
     @Test
     void shouldThrowEntityNotFoundExceptionForMentor() {
-        Mockito.when(mentorshipRepository.findById(userId)).thenReturn(Optional.empty());
+        Mockito.when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.empty());
         assertThrows(EntityNotFoundException.class, ()->mentorshipService.getMentees(userId));
         Mockito.verify(mentorshipRepository).findById(userId);
     }
 
-
-
     @Test
     void shouldThrowEntityNotFoundExceptionForMentee() {
-        Mockito.when(mentorshipRepository.findById(userId)).thenReturn(Optional.empty());
+        Mockito.when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.empty());
         assertThrows(EntityNotFoundException.class, ()->mentorshipService.getMentors(userId));
         Mockito.verify(mentorshipRepository).findById(userId);
     }
 
     @Test
-    void shouldReturnMentors(){
-
+    void shouldReturnMentees(){
+        Mockito.when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        assertEquals(userList, mentorshipService.getMentees(userId));
+        Mockito.verify(mentorshipRepository, Mockito.times(2)).findById(user.getId());
     }
 
-
+    @Test
+    void shouldReturnMentors(){
+        Mockito.when(mentorshipRepository.findById(user4.getId())).thenReturn(Optional.of(user4));
+        assertEquals(userList, mentorshipService.getMentors(userId));
+        Mockito.verify(mentorshipRepository, Mockito.times(2)).findById(user4.getId());
+    }
 }

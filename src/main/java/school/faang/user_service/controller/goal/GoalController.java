a90a7f664ac3375b40bb75b6a39e30f3709d5047
Egor Selivanov <egorselivanov0@gmail.com>
2023-07-15 18:59:28 +0300

    Добавил реализацию метода getGoalsByUser(Long userId, GoalFilterDto filter)

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -1,11 +1,35 @@
 package school.faang.user_service.controller.goal;
 
+import com.amazonaws.services.kms.model.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import school.faang.user_service.dto.goal.GoalDto;
+import school.faang.user_service.dto.goal.GoalFilterDto;
 import school.faang.user_service.service.goal.GoalService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/goal")
 public class GoalController {
     private final GoalService goalService;
+
+    @GetMapping("/get/{userId}")
+    public ResponseEntity<List<GoalDto>> getGoalsByUser(@PathVariable Long userId,
+                                                        @RequestBody(required = false) GoalFilterDto filter) {
+        try {
+            return ResponseEntity.ok(goalService.getGoalsByUser(userId, filter));
+        } catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- src/main/java/school/faang/user_service/service/goal/GoalService.java
+++ src/main/java/school/faang/user_service/service/goal/GoalService.java
@@ -2,10 +2,48 @@ package school.faang.user_service.service.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.goal.GoalDto;
+import school.faang.user_service.dto.goal.GoalFilterDto;
+import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.filter.goal.GoalFilter;
+import school.faang.user_service.mapper.GoalMapper;
 import school.faang.user_service.repository.goal.GoalRepository;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class GoalService {
     private final GoalRepository goalRepository;
+    private final GoalMapper goalMapper;
+    private final List<GoalFilter> goalFilters;
+
+    public List<GoalDto> getGoalsByUser(Long userId, GoalFilterDto filter) {
+        validateId(userId);
+
+        Stream<Goal> goalStream = goalRepository.findGoalsByUserId(userId);
+
+        if (filter == null) {
+            return goalStream.map(goalMapper::toDto).toList();
+        }
+
+        for (GoalFilter goalFilter : goalFilters) {
+            if (goalFilter.isApplicable(filter)) {
+                goalStream = goalFilter.apply(goalStream, filter);
+            }
+        }
+
+        return goalStream.map(goalMapper::toDto).toList();
+    }
+
+    private void validateId(Long userId) {
+        if (userId == null) {
+            throw new DataValidationException("User id cannot be null!");
+        }
+        if (userId <= 0) {
+            throw new DataValidationException("User id cannot be less than 1!");
+        }
+    }
 }

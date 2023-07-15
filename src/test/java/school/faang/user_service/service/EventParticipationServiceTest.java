ed4a4dbcdde0fd14a069765a12110059139519f7
M1khail <mishalion888@gmail.com>
2023-07-15 19:33:38 +0200

    Cancel register for the event - fixed tests

--- src/test/java/school/faang/user_service/service/EventParticipationServiceTest.java
+++ src/test/java/school/faang/user_service/service/EventParticipationServiceTest.java
@@ -4,10 +4,13 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.event.EventParticipationRepository;
 import school.faang.user_service.service.event.EventParticipationService;
 
-import java.util.List;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EventParticipationServiceTest {
     @Mock
@@ -18,15 +21,27 @@ public class EventParticipationServiceTest {
 
     @Test
     public void registerParticipantTest() {
-        Mockito.when(eventParticipationRepository.findAllParticipantsByEventId(1L)).thenReturn(List.of());
+        Mockito.when(eventParticipationRepository.findAllParticipantsByEventId(1L)).thenReturn(Collections.emptyList());
         eventParticipationService.registerParticipant(1L, 10L);
         Mockito.verify(eventParticipationRepository).register(1L, 10L);
     }
 
     @Test
+    public void registerParticipantThrowExceptionTest() {
+        assertThrows(DataValidationException.class,
+                () -> eventParticipationService.registerParticipant(1L, 10L));
+    }
+
+    @Test
     public void unregisterParticipantTest() {
-        Mockito.when(eventParticipationRepository.findAllParticipantsByEventId(1L)).thenReturn(List.of());
+        Mockito.when(eventParticipationRepository.findAllParticipantsByEventId(1L)).thenReturn(Collections.emptyList());
         eventParticipationService.unregisterParticipant(1L, 10L);
-        Mockito.verify(eventParticipationRepository).unregister(1L, 10L);
+        Mockito.verify(eventParticipationRepository).unregister(1L, 5L);
+    }
+
+    @Test
+    public void unregisterParticipantThrowExceptionTest() {
+        assertThrows(DataValidationException.class,
+                () -> eventParticipationService.registerParticipant(1L, 10L));
     }
 }

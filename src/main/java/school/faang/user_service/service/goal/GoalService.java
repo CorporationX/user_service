4d6f54dccc245b7dc627fdbfab6a26caf24c8201
Egor Selivanov <egorselivanov0@gmail.com>
2023-07-15 15:47:03 +0300

    Создал классы контроллера и сервиса для целей

---
+++ src/main/java/school/faang/user_service/service/goal/GoalService.java
@@ -0,0 +1,11 @@
+package school.faang.user_service.service.goal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.repository.goal.GoalRepository;
+
+@Service
+@RequiredArgsConstructor
+public class GoalService {
+    private final GoalRepository goalRepository;
+}

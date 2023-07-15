8e6efa0ea9da9e588476a14414922a8f40bd8613
Egor Selivanov <egorselivanov0@gmail.com>
2023-07-15 15:54:27 +0300

    Создал классы dto для целей и для фильтров

---
+++ src/main/java/school/faang/user_service/dto/goal/GoalFilterDto.java
@@ -0,0 +1,14 @@
+package school.faang.user_service.dto.goal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import school.faang.user_service.entity.goal.GoalStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GoalFilterDto {
+    private String titleFilter;
+    private GoalStatus goalStatusFilter;
+}

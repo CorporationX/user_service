13d5f075d3f937abc8f98d79019328dde91101f3
Abramov Nikita <mr.abramovn@gmail.com>
2023-07-16 16:47:08 +0300

    fix bugs

--- src/test/java/school/faang/user_service/service/SkillServiceTest.java
+++ src/test/java/school/faang/user_service/service/SkillServiceTest.java
@@ -1,30 +1,42 @@
 package school.faang.user_service.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.SkillDto;
 import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.mapper.SkillMapperImpl;
 import school.faang.user_service.repository.SkillRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class SkillServiceTest {
     @InjectMocks
     private SkillService skillService;
+    @Spy
+    private SkillMapperImpl skillMapper;
     @Mock
     private SkillRepository skillRepository;
-    SkillDto skillDto = new SkillDto(1L, "flexibility");
+    SkillDto skillDto;
+
+    @BeforeEach
+    public void setUp() {
+        skillDto = new SkillDto(1L, "flexibility");
+
+    }
 
     @Test
-    void testCreateExistingSkill() {
-        Mockito.when(skillRepository.existsByTitle(Mockito.anyString()))
+    void testCreateExistingSkillThrowsException() {
+        when(skillRepository.existsByTitle(anyString()))
                 .thenReturn(true);
-        assertTrue(skillRepository.existsByTitle(Mockito.anyString()));
+        assertTrue(skillRepository.existsByTitle(anyString()));
         assertThrows(DataValidationException.class,
                 () -> skillService.create(new SkillDto(1L, "flexibility")));
     }
@@ -32,6 +44,6 @@ class SkillServiceTest {
     @Test
     void testCreateSkill() {
         skillService.create(skillDto);
-        Mockito.verify(skillRepository, Mockito.times(1)).save(Mockito.any());
+        verify(skillRepository, times(1)).save(any());
     }
 }

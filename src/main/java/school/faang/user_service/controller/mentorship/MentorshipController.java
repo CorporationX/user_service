c73ab9d0dfce9782c131bad69be0e898b2da647a
”Ramazan” <mansurov.r13@gmail.com>
2023-07-16 15:43:35 +0300

    3597: fixes after review

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
@@ -22,7 +23,7 @@ public class MentorshipController {
     public ResponseEntity<?> getMentees(@PathVariable long mentorId) {
         log.debug("Got new request to get mentees for mentor with id:{}", mentorId);
         try {
-            List<User> mentees = mentorshipService.getMentees(mentorId);
+            List<UserDto> mentees = mentorshipService.getMentees(mentorId);
             log.debug("Successfully got mentees for mentor with id:{}", mentorId);
             return ResponseEntity.ok(mentees);
         } catch (RuntimeException e) {
--- src/main/java/school/faang/user_service/service/mentorship/MentorshipService.java
+++ src/main/java/school/faang/user_service/service/mentorship/MentorshipService.java
@@ -3,23 +3,28 @@ package school.faang.user_service.service.mentorship;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.exception.UserNotFoundException;
+import school.faang.user_service.exception.EntityNotFoundException;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MentorshipService {
     private final MentorshipRepository mentorshipRepository;
+    private final UserMapper userMapper;
 
-    public List<User> getMentees(long mentorId) {
+    public List<UserDto> getMentees(long mentorId) {
         User user = validateUserId(mentorId);
-        return user.getMentees();
+        return user.getMentees().stream()
+                .map(userMapper::userToUserDto)
+                .toList();
     }
 
     private User validateUserId(long mentorId) {
         return mentorshipRepository.findUserById(mentorId)
-                .orElseThrow(() -> new UserNotFoundException("Invalid mentor id"));
+                .orElseThrow(() -> new EntityNotFoundException("Invalid user id"));
     }
 }
 

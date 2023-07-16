ccacb7cf32a71abed397ad7c23c9baf19413d884
Abramov Nikita <mr.abramovn@gmail.com>
2023-07-16 16:47:08 +0300

    fix bugs

--- src/main/java/school/faang/user_service/service/SkillService.java
+++ src/main/java/school/faang/user_service/service/SkillService.java
@@ -1,6 +1,6 @@
 package school.faang.user_service.service;
 
-import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.SkillDto;
 import school.faang.user_service.entity.Skill;
@@ -9,12 +9,15 @@ import school.faang.user_service.mapper.SkillMapper;
 import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.UserRepository;
 
+
 import java.util.List;
 
 @Service
-@Data
+@RequiredArgsConstructor
+
 public class SkillService {
     private final SkillRepository skillRepository;
+    private final SkillMapper skillMapper;
     private final UserRepository userRepository;
 
     public List<SkillDto> getUserSkills(long userId) {
@@ -22,14 +25,15 @@ public class SkillService {
             throw new RuntimeException("user is not found");
         }
         List<Skill> skillsOfUsers = skillRepository.findAllByUserId(userId);
-        return skillsOfUsers.stream().map(SkillMapper.INSTANCE::toDTO).toList();
+        return skillsOfUsers.stream().map(skillMapper::toDTO).toList();
     }
 
     public SkillDto create(SkillDto skillDto) {
-        Skill skillFromDto = SkillMapper.INSTANCE.toEntity(skillDto);
-        if (skillRepository.existsByTitle(skillFromDto.getTitle())) {
+        Skill skill = skillMapper.toEntity(skillDto);
+        if (skillRepository.existsByTitle(skill.getTitle())) {
             throw new DataValidationException("The skill already exists");
         }
-        return SkillMapper.INSTANCE.toDTO(skillRepository.save(skillFromDto));
+        SkillDto skillDtoResult = skillMapper.toDTO(skillRepository.save(skill));
+        return skillDtoResult;
     }
 }

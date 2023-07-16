4c5467b48b2f60f69bd843c541cbbf99623a8f84
46 <grisha.chay@gmail.com>
2023-07-16 13:15:36 +0300

    super s шо це?

--- src/main/java/school/faang/user_service/exeption/DataValidationException.java
+++ src/main/java/school/faang/user_service/exeption/DataValidationException.java
@@ -2,6 +2,7 @@ package school.faang.user_service.exeption;
 
 public class DataValidationException extends RuntimeException {
     public DataValidationException(String s) {
+        super(s);
     }
     public DataValidationException(long followerId, long followeeId) {
         super(String.format("User with id %d already follow user with id %d", followerId, followeeId));

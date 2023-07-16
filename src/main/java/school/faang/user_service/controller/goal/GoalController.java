a7fe861be833be066c79d42fcb88e0a2c8119afb
Egor Selivanov <egorselivanov0@gmail.com>
2023-07-16 18:47:53 +0300

    Добавил обработчик на DataValidationException с возвратом текста ошибки

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.dto.goal.GoalFilterDto;
+import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.goal.GoalService;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,16 +26,20 @@ public class GoalController {
                                         @RequestBody GoalDto goalDto) {
         try {
             return ResponseEntity.ok(goalService.createGoal(userId, goalDto));
+        } catch (DataValidationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
     @GetMapping("/get/{userId}")
-    public ResponseEntity<List<GoalDto>> getGoalsByUser(@PathVariable Long userId,
-                                                        @RequestBody(required = false) GoalFilterDto filter) {
+    public ResponseEntity<?> getGoalsByUser(@PathVariable Long userId,
+                                            @RequestBody(required = false) GoalFilterDto filter) {
         try {
             return ResponseEntity.ok(goalService.getGoalsByUser(userId, filter));
+        } catch (DataValidationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (NotFoundException e) {
             return ResponseEntity.notFound().build();
         } catch (Exception e) {

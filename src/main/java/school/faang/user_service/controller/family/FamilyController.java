67457c14604e48d2774d7bd5379fe629368a0b25
InnMish <inn.mishustina@gmail.com>
2023-07-16 20:16:19 +0300

    getfamily added

--- src/main/java/school/faang/user_service/controller/family/FamilyController.java
+++ src/main/java/school/faang/user_service/controller/family/FamilyController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.mapper.UserMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -15,5 +16,19 @@ public class FamilyController {
 
     public List<User> create(User user) {
         return List.of(user);
+    }
+
+    private boolean validateFamily(User user) {
+        if (user.getCountry().getTitle().equals("USA")) {
+            return false;
+        }
+        if (user.getCountry().getTitle().equals("Russia")) {
+            return false;
+        }
+        return true;
+    }
+
+    public List<User> getFamily(long familyId) {
+        return new ArrayList<>();
     }
 }

a610095a4e4983488ca84953385efd2a38be9fea
Doppad <swsdoppad@gmail.com>
2023-07-16 15:05:31 +0300

    Запрос рекоммендации

---
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -0,0 +1,21 @@
+package school.faang.user_service.controller.recommendation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.dto.RecommendationRequestDto;
+import school.faang.user_service.service.RecommendationRequestService;
+
+@Controller
+@RequiredArgsConstructor
+public class RecommendationRequestController {
+    private final RecommendationRequestService recommendationRequestService;
+
+    public RecommendationRequestDto requestRecommendation(RecommendationRequestDto recommendationRequest) {
+        if (!recommendationRequest.getMessage().isEmpty() && recommendationRequest.getMessage() != null) {
+            recommendationRequestService.create(recommendationRequest);
+            return recommendationRequest;
+        } else {
+            throw new IllegalArgumentException("Empty recommendation request!");
+        }
+    }
+}

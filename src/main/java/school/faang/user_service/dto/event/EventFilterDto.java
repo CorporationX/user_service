669f86d2a147d4ee4f2dc9d271bcf27991eb06dd
Devilserk <demonserker@gmail.com>
2023-07-17 17:54:50 +0600

    Убрал @Component у EventFilterDto

--- src/main/java/school/faang/user_service/dto/event/EventFilterDto.java
+++ src/main/java/school/faang/user_service/dto/event/EventFilterDto.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Component
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter

3fa3ba110e455fcec14d1465ecbd650e445fc786
”Ramazan” <mansurov.r13@gmail.com>
2023-07-17 19:53:23 +0300

    3597: fixes after review, hope final

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,27 +1,29 @@
 package school.faang.user_service.controller.mentorship;
 
+import jakarta.validation.constraints.Min;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
-import school.faang.user_service.entity.User;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
 @RestController
 @RequestMapping("/mentorship")
 @RequiredArgsConstructor
 @Slf4j
+@Validated
 public class MentorshipController {
     private final MentorshipService mentorshipService;
 
     @GetMapping("/{mentorId}/mentees")
-    public ResponseEntity<?> getMentees(@PathVariable long mentorId) {
-        log.debug("Got new request to get mentees for mentor with id:{}", mentorId);
+    public ResponseEntity<?> getMentees(@PathVariable @Min(1L) long mentorId) {
+        log.debug("Received new request to get mentees for mentor with id:{}", mentorId);
         try {
             List<UserDto> mentees = mentorshipService.getMentees(mentorId);
             log.debug("Successfully got mentees for mentor with id:{}", mentorId);
--- src/test/java/school/faang/user_service/mapper/UserMapperTest.java
+++ src/test/java/school/faang/user_service/mapper/UserMapperTest.java
@@ -1,14 +1,10 @@
 package school.faang.user_service.mapper;
 
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.Country;
-import school.faang.user_service.entity.Skill;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.entity.goal.Goal;
-import school.faang.user_service.entity.recommendation.Recommendation;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,14 +27,6 @@ class UserMapperTest {
                 .country(Country.builder().title("country").build())
                 .city("city")
                 .experience(10)
-                .followers(List.of(User.builder().id(2L).build()))
-                .followees(List.of(User.builder().id(3L).build()))
-                .mentees(List.of(User.builder().id(4L).build()))
-                .mentors(List.of(User.builder().id(5L).build()))
-                .goals(List.of(Goal.builder().id(1L).build()))
-                .skills(List.of(Skill.builder().id(1L).build()))
-                .recommendationsGiven(List.of(Recommendation.builder().id(1L).build()))
-                .recommendationsReceived(List.of(Recommendation.builder().id(2L).build()))
                 .build();
 
         userDto = UserDto.builder()
@@ -64,14 +52,6 @@ class UserMapperTest {
             assertEquals("country", result.getCountry());
             assertEquals("city", result.getCity());
             assertEquals(10, result.getExperience());
-            assertEquals(List.of(2L), result.getFollowerIds());
-            assertEquals(List.of(3L), result.getFolloweeIds());
-            assertEquals(List.of(4L), result.getMenteeIds());
-            assertEquals(List.of(5L), result.getMentorIds());
-            assertEquals(List.of(1L), result.getGoalIds());
-            assertEquals(List.of(1L), result.getSkillIds());
-            assertEquals(List.of(1L), result.getRecommendationGivenIds());
-            assertEquals(List.of(2L), result.getRecommendationReceivedIds());
         });
     }
 

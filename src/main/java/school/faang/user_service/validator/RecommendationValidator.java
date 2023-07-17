4ad8ced9a7280cda691b27795c44c97c7b6a6d06
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-07-18 00:12:43 +0100

    Resolved som collapse of branches

--- src/main/java/school/faang/user_service/validator/RecommendationValidator.java
+++ src/main/java/school/faang/user_service/validator/RecommendationValidator.java
@@ -3,14 +3,11 @@ package school.faang.user_service.validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
-import school.faang.user_service.dto.recommendation.SkillOfferDto;
 import school.faang.user_service.entity.recommendation.Recommendation;
 import school.faang.user_service.exception.DataValidationException;
-import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.recommendation.RecommendationRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -20,7 +17,6 @@ public class RecommendationValidator {
     private static final int RECOMMENDATION_INTERVAL_MONTHS = 6;
 
     private final RecommendationRepository recommendationRepository;
-    private final SkillRepository skillRepository;
 
     public void validateRecommendationContent(RecommendationDto recommendation) {
         String content = recommendation.getContent();
@@ -41,26 +37,9 @@ public class RecommendationValidator {
             LocalDateTime currentDate = LocalDateTime.now();
 
             if (lastUpdate.plusMonths(RECOMMENDATION_INTERVAL_MONTHS).isAfter(currentDate)) {
-                throw new DataValidationException("You've already recommended this user in the last 6 months");
+                throw new DataValidationException(
+                        "You've already recommended this user in the last " + RECOMMENDATION_INTERVAL_MONTHS + " months");
             }
         }
-    }
-
-    public void validateSkills(RecommendationDto recommendation) {
-        List<SkillOfferDto> skills = recommendation.getSkillOffers();
-
-        List<Long> skillIds = getUniqueSkillIds(skills);
-        int countedSkills = skillRepository.countExisting(skillIds);
-
-        if (skills.size() != countedSkills) {
-            throw new DataValidationException("Invalid skills offered within the recommendation");
-        }
-    }
-
-    private List<Long> getUniqueSkillIds(List<SkillOfferDto> skills) {
-        return skills.stream()
-                .map(SkillOfferDto::getSkillId)
-                .distinct()
-                .toList();
     }
 }

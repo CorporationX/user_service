a2c630ae43a71cbe139d71a9fa655b7ab2f5a1d8
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-07-17 23:20:53 +0100

    For PR New implementation for "Give recommendation" feature. Project is not depending on any SQL syntax anymore, so we can change databases freely in the future

--- src/test/java/school/faang/user_service/validator/RecommendationValidatorTest.java
+++ src/test/java/school/faang/user_service/validator/RecommendationValidatorTest.java
@@ -1,19 +1,21 @@
 package school.faang.user_service.validator;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
-import school.faang.user_service.dto.recommendation.SkillOfferDto;
-import school.faang.user_service.repository.SkillRepository;
+import school.faang.user_service.entity.recommendation.Recommendation;
+import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.recommendation.RecommendationRepository;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-
 
 @ExtendWith(MockitoExtension.class)
 public class RecommendationValidatorTest {
@@ -21,27 +23,81 @@ public class RecommendationValidatorTest {
     private RecommendationValidator recommendationValidator;
     @Mock
     private RecommendationRepository recommendationRepository;
-    @Mock
-    private SkillRepository skillRepository;
-
-
-//    @Test
-//    public void testCountExisting(){
-//        List<Long> skillIds = List.of(1L,2L);
-//        verify(skillRepository,times(1)).countExisting(skillIds);
-//    }
 
     @Test
-    public void testValidateSkills() {
-        List<SkillOfferDto> skills = List.of(new SkillOfferDto(1L, 1L), new SkillOfferDto(2L, 2L));
+    public void testValidateRecommendationContent_PassTheValidation() {
         RecommendationDto recommendationDto = new RecommendationDto();
-        recommendationDto.setSkillOffers(skills);
-        List<Long> skillIds = List.of(1L,2L);
+        recommendationDto.setContent("Sample content");
 
+        assertDoesNotThrow(() -> recommendationValidator.validateRecommendationContent(recommendationDto));
+    }
 
+    @Test
+    public void testValidateRecommendationContent_EmptyContent() {
+        RecommendationDto recommendationDto = new RecommendationDto();
+        recommendationDto.setContent("");
+        String message = "Content can't be empty";
 
-//        when(skillRepository.countExisting(skillIds)).thenReturn(2);
-        recommendationValidator.validateSkills(recommendationDto);
-        verify(skillRepository,times(1)).countExisting(skillIds);
+        assertThrows(DataValidationException.class,
+                () -> recommendationValidator.validateRecommendationContent(recommendationDto), message);
+    }
+
+    @Test
+    public void testValidateRecommendationContent_NullContent() {
+        RecommendationDto recommendationDto = new RecommendationDto();
+        recommendationDto.setContent(null);
+        String message = "Content can't be empty";
+
+        assertThrows(DataValidationException.class,
+                () -> recommendationValidator.validateRecommendationContent(recommendationDto), message);
+    }
+
+    @Test
+    public void testValidateLastUpdate_PassTheValidation() {
+        RecommendationDto recommendationDto = new RecommendationDto();
+        recommendationDto.setAuthorId(1L);
+        recommendationDto.setReceiverId(2L);
+
+        when(recommendationRepository.findFirstByAuthorIdAndReceiverIdOrderByCreatedAtDesc(1L, 2L))
+                .thenReturn(Optional.empty());
+        assertDoesNotThrow(() -> recommendationValidator.validateLastUpdate(recommendationDto));
+        verify(recommendationRepository, times(1))
+                .findFirstByAuthorIdAndReceiverIdOrderByCreatedAtDesc(1L, 2L);
+    }
+
+    @Test
+    public void testValidateLastUpdate_LastRecommendationWithinInterval() {
+        RecommendationDto recommendationDto = new RecommendationDto();
+        recommendationDto.setAuthorId(1L);
+        recommendationDto.setReceiverId(2L);
+
+        Recommendation lastRecommendation = new Recommendation();
+        lastRecommendation.setUpdatedAt(
+                LocalDateTime.now().minusMonths(6 / 2));
+        String message = "You've already recommended this user in the last 6 months";
+
+        when(recommendationRepository.findFirstByAuthorIdAndReceiverIdOrderByCreatedAtDesc(1L, 2L))
+                .thenReturn(Optional.of(lastRecommendation));
+        assertThrows(DataValidationException.class,
+                () -> recommendationValidator.validateLastUpdate(recommendationDto), message);
+        verify(recommendationRepository, times(1))
+                .findFirstByAuthorIdAndReceiverIdOrderByCreatedAtDesc(1L, 2L);
+    }
+
+    @Test
+    public void testValidateLastUpdate_LastRecommendationOutsideInterval() {
+        RecommendationDto recommendationDto = new RecommendationDto();
+        recommendationDto.setAuthorId(1L);
+        recommendationDto.setReceiverId(2L);
+
+        Recommendation lastRecommendation = new Recommendation();
+        lastRecommendation.setUpdatedAt(
+                LocalDateTime.now().minusMonths(6 * 2));
+
+        when(recommendationRepository.findFirstByAuthorIdAndReceiverIdOrderByCreatedAtDesc(1L, 2L))
+                .thenReturn(Optional.of(lastRecommendation));
+        assertDoesNotThrow(() -> recommendationValidator.validateLastUpdate(recommendationDto));
+        verify(recommendationRepository, times(1))
+                .findFirstByAuthorIdAndReceiverIdOrderByCreatedAtDesc(1L, 2L);
     }
 }

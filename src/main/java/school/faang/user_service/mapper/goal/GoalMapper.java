590ab5df12bc6563475c39d22d9a1c47f4d99315
Ivan Averkov <averkovivan@gmail.com>
2023-07-18 19:50:31 +0300

    update without tests

--- src/main/java/school/faang/user_service/mapper/goal/GoalMapper.java
+++ src/main/java/school/faang/user_service/mapper/goal/GoalMapper.java
@@ -4,19 +4,25 @@ import org.mapstruct.*;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.entity.Skill;
 import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.repository.SkillRepository;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface GoalMapper {
     @Named("skillsToIds")
-    default List<Long> map(List<Skill> value) {
+    default List<Long> map1(List<Skill> value) {
         return value.stream().map(Skill::getId).toList();
     }
 
     @Mapping(target = "skillIds", source = "skillsToAchieve", qualifiedByName = "skillsToIds")
     @Mapping(target = "parentId", source = "parent.id")
     GoalDto toDto(Goal goal);
+
+
+//    @Mapping(target = "skillsToAchieve", source = "skillIds", qualifiedByName = "IdsToskills")
+//    @Mapping(target = "parent.id", source = "parentId")
+//    Goal toEntity(GoalDto goal);
 
     List<GoalDto> goalListToDto(List<Goal> list);
 }
--- src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
+++ src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
@@ -6,6 +6,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.goal.GoalDto;
+import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.entity.goal.GoalStatus;
 import school.faang.user_service.repository.goal.GoalRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,4 +22,12 @@ class GoalServiceTest {
     @InjectMocks
     GoalService service;
 
+    @Test
+    public void updateGoalComletedValidation() {
+        Goal goal = new Goal().builder().status(GoalStatus.COMPLETED).id(0L).build();
+        GoalDto newGoal = new GoalDto().builder().title("a").status(GoalStatus.COMPLETED).build();
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.updateGoal(goal.getId(), newGoal);
+        });
+    }
 }

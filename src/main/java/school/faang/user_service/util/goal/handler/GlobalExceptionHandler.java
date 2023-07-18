0ec92d7b723f95f719064610c76c9a6761706c0c
Andrey <khalyavinandrey@gmail.com>
2023-07-18 14:44:55 +0500

    Создал кастомные exceptions и внедрил их обработку в exceptionHandler

--- src/main/java/school/faang/user_service/util/goal/handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/util/goal/handler/GlobalExceptionHandler.java
@@ -4,8 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import school.faang.user_service.dto.goal.ErrorResponse;
+import school.faang.user_service.util.goal.exception.AcceptingGoalInvitationException;
 import school.faang.user_service.util.goal.exception.CreateInvitationException;
+import school.faang.user_service.util.goal.exception.GoalInvitationNotFoundException;
 import school.faang.user_service.util.goal.exception.GoalNotFoundException;
+import school.faang.user_service.util.goal.exception.IncorrectIdException;
 import school.faang.user_service.util.goal.exception.MappingGoalInvitationDtoException;
 import school.faang.user_service.util.goal.exception.UserNotFoundException;
 
@@ -29,6 +32,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(GoalNotFoundException.class)
     public ResponseEntity<ErrorResponse> handle(GoalNotFoundException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), System.currentTimeMillis()));
+    }
+
+    @ExceptionHandler(IncorrectIdException.class)
+    public ResponseEntity<ErrorResponse> handle(IncorrectIdException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), System.currentTimeMillis()));
+    }
+
+    @ExceptionHandler(GoalInvitationNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handle(GoalInvitationNotFoundException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), System.currentTimeMillis()));
+    }
+
+    @ExceptionHandler(AcceptingGoalInvitationException.class)
+    public ResponseEntity<ErrorResponse> handle(AcceptingGoalInvitationException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), System.currentTimeMillis()));
     }
 }

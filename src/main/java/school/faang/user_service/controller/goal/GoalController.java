37d2f9422bd7616de512db1970146f4cd94802d8
Ivan Averkov <averkovivan@gmail.com>
2023-07-19 23:05:03 +0300

    dataalidationxception extends runtimexception

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -2,12 +2,9 @@ package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import school.faang.user_service.Exeptions.DataValidationException;
-import school.faang.user_service.dto.goal.GoalFilterDto;
+import school.faang.user_service.exeptions.DataValidationException;
 import school.faang.user_service.entity.goal.Goal;
 import school.faang.user_service.service.goal.GoalService;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor

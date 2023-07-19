39e016617854f043cf8da2594740ac324aa67bb7
Andrey <khalyavinandrey@gmail.com>
2023-07-19 17:12:53 +0500

    Добавлены кастомные Exceptions в exceptionHandler

---
+++ src/main/java/school/faang/user_service/util/goal/exception/IncorrectStatusException.java
@@ -0,0 +1,7 @@
+package school.faang.user_service.util.goal.exception;
+
+public class IncorrectStatusException extends RuntimeException{
+    public IncorrectStatusException(String message) {
+        super(message);
+    }
+}

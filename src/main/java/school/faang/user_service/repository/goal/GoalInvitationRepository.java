181affd7af6eedef2d42eaa2a524a96848b80884
Sveta Zalogina <zalogina.sveta@gmail.com>
2023-07-19 22:13:24 -0400

    Initial setup for goal invitations

--- src/main/java/school/faang/user_service/repository/goal/GoalInvitationRepository.java
+++ src/main/java/school/faang/user_service/repository/goal/GoalInvitationRepository.java
@@ -1,9 +1,15 @@
 package school.faang.user_service.repository.goal;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.goal.GoalInvitation;
 
 @Repository
 public interface GoalInvitationRepository extends CrudRepository<GoalInvitation, Long> {
+    @Query(nativeQuery = true, value = """
+            INSERT INTO goal_invitation (goal_id, inviter_id, invited_id, status, created_at, updated_at)
+            VALUES (?1, ?2, ?3, 0, NOW(), NOW())
+            """)
+    GoalInvitation create(long goal_id, long inviter_id, long invited_id);
 }

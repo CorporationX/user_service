7bc59159d6e046c0e7c5b3e5e667a563a4760690
”Ramazan” <mansurov.r13@gmail.com>
2023-07-21 14:41:49 +0300

    3593: filtration was added

---
+++ src/main/java/school/faang/user_service/filter/mentorship/MentorshipRequestStatusFilter.java
@@ -0,0 +1,22 @@
+package school.faang.user_service.filter.mentorship;
+
+import java.util.List;
+import java.util.Objects;
+import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.mentorship.MentorshipRequestFilterDto;
+import school.faang.user_service.entity.MentorshipRequest;
+
+@Component
+public class MentorshipRequestStatusFilter implements MentorshipRequestFilter {
+    @Override
+    public boolean isApplicable(MentorshipRequestFilterDto filter) {
+        return Objects.nonNull(filter.getStatus()) && !filter.getStatus().isBlank();
+    }
+
+    @Override
+    public void apply(List<MentorshipRequest> requests, MentorshipRequestFilterDto filter) {
+        requests.removeIf(request -> !Objects.equals(
+                request.getStatus().toString(),
+                filter.getStatus().toUpperCase()));
+    }
+}

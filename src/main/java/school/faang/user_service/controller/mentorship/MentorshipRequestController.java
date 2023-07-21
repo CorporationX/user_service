9a0ba78bdd87c8a707c7c6730901057310174e49
”Ramazan” <mansurov.r13@gmail.com>
2023-07-21 19:52:09 +0300

    3593: service and controller were added, mappers were fixed, tests for service were added

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
@@ -5,11 +5,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
+import school.faang.user_service.dto.mentorship.MentorshipRequestFilterDto;
 import school.faang.user_service.exception.EntityNotFoundException;
 import school.faang.user_service.exception.mentorship.MentorshipRequestValidationException;
 import school.faang.user_service.service.mentorship.MentorshipRequestService;
@@ -32,6 +34,22 @@ public class MentorshipRequestController {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             log.error("Failed to save mentorship request: {}\nException: {}", requestDto, e.getMessage());
+            return ResponseEntity.internalServerError().body("Server error");
+        }
+    }
+
+    @GetMapping("/requests")
+    public ResponseEntity<?> getRequests(@Valid @RequestBody MentorshipRequestFilterDto requestFilter) {
+        log.debug("Received request to get mentorship requests: {}", requestFilter);
+        try {
+            return ResponseEntity.ok(requestService.getRequests(requestFilter));
+        } catch (EntityNotFoundException e) {
+            log.warn("Failed to get mentorship requests with filter: {}\nException: {}",
+                    requestFilter, e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            log.error("Failed to get mentorship requests with filter: {}\nException: {}",
+                    requestFilter, e.getMessage());
             return ResponseEntity.internalServerError().body("Server error");
         }
     }
--- src/main/java/school/faang/user_service/dto/mentorship/MentorshipRequestFilterDto.java
+++ src/main/java/school/faang/user_service/dto/mentorship/MentorshipRequestFilterDto.java
@@ -1,5 +1,7 @@
 package school.faang.user_service.dto.mentorship;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MentorshipRequestFilterDto {
     private String description;
+
+    @Min(message = "Requester ID must be greater than zero", value = 1)
     private Long requesterId;
+
+    @Min(message = "Receiver ID must be greater than zero", value = 1)
     private Long receiverId;
+
+    @Pattern(message = "Status must be one of PENDING, ACCEPTED, REJECTED", regexp = "PENDING|ACCEPTED|REJECTED|^$")
     private String status;
 }

cc5c4434b8c29e7c08bbd1d7c574873da93f4172
Dregid <ufl852@gmail.com>
2023-07-22 12:13:04 +0600

    Partially implemented the filter tests - Revamped the filtering method

---
+++ src/test/java/school/faang/user_service/filter/UserFilterTest.java
@@ -0,0 +1,50 @@
+package school.faang.user_service.filter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.mapper.UserMapperImpl;
+import school.faang.user_service.repository.SubscriptionRepository;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.service.subscription.SubscriptionService;
+import school.faang.user_service.service.user.filter.UserFilter;
+import school.faang.user_service.service.user.filter.UserNameFilter;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class UserFilterTest {
+    @Mock
+    private SubscriptionRepository subscriptionRepository;
+    @Mock
+    private List<UserFilter> userFilters;
+    @Spy
+    private UserMapperImpl userMapper;
+    @InjectMocks
+    private SubscriptionService subscriptionService;
+    private final long followerId = 2;
+    private final long followeeId = 1;
+
+    @Test
+    public void shouldFilterFollowersByNameFilter() {
+        List<User> usersStream = List.of(
+                User.builder()
+                        .username("MichaelJohnson")
+                        .build(),
+                User.builder()
+                        .username("JaneSmith")
+                        .build()
+        );
+        UserNameFilter nameFilter = Mockito.mock(UserNameFilter.class);
+
+        Mockito.when(subscriptionRepository.findByFolloweeId(followeeId))
+                .thenReturn(usersStream.stream());
+        List<User> filteredUsers = usersStream.stream()
+                .filter()
+    }
+}

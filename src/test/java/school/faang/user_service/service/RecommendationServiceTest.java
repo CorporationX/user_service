4d1251d02e869799bf2c02c17b6e15a09d2a3ffb
Azat <azat.takhautdinov@gmail.com>
2023-07-22 22:54:02 +0500

    Создан тест

--- src/test/java/school/faang/user_service/service/RecommendationServiceTest.java
+++ src/test/java/school/faang/user_service/service/RecommendationServiceTest.java
@@ -1,11 +1,23 @@
 package school.faang.user_service.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.repository.SkillRepository;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.repository.recommendation.RecommendationRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class RecommendationServiceTest {
-
+    @Mock
+    private RecommendationRepository recommendationRepository;
+    @Mock
+    private SkillRepository skillRepository;
+    @Mock
+    private UserRepository userRepository;
     @Test
     void create() {
     }

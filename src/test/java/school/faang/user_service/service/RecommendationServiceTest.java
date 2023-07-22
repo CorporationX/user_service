bbc50a5cc5a02d66b4bddaef676ebf92da48a406
Azat <azat.takhautdinov@gmail.com>
2023-07-22 22:04:42 +0500

    Начало теста

---
+++ src/test/java/school/faang/user_service/service/RecommendationServiceTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecommendationServiceTest {
+
+    @Test
+    void create() {
+    }
+}

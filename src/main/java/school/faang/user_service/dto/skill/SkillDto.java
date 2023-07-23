691984a7e0127d55443448d67bec7ac4230d6fca
Dobrovolsky Artem <artdobrov@mail.ru>
2023-07-23 21:33:30 +0300

    BC-3675: Подправил аннотации в DTO

--- src/main/java/school/faang/user_service/dto/skill/SkillDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -3,12 +3,10 @@ package school.faang.user_service.dto.skill;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @Builder
-@Component
 public class SkillDto {
 
     private long id;

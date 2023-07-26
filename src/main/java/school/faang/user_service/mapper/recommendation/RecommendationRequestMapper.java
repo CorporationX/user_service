806106dcd93eb369cfc787de4c05bde35f747328
Basimov Rustam <sombra1nf@yandex.ru>
2023-07-26 22:32:55 +0500

    Добавил мапер для листов

---
+++ src/main/java/school/faang/user_service/mapper/recommendation/RecommendationRequestMapper.java
@@ -0,0 +1,24 @@
+package school.faang.user_service.mapper.recommendation;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
+import school.faang.user_service.entity.recommendation.RecommendationRequest;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
+public interface RecommendationRequestMapper {
+
+    @Mapping(target = "requesterId", source = "requester.id")
+    @Mapping(target = "receiverId", source = "receiver.id")
+    RecommendationRequestDto toDto(RecommendationRequest request);
+
+    @Mapping(target = "requester", ignore = true)
+    @Mapping(target = "receiver", ignore = true)
+    RecommendationRequest toEntity(RecommendationRequestDto request);
+
+    List<RecommendationRequest> toEntityList(List<RecommendationRequestDto> request);
+
+    List<RecommendationRequestDto> toDtoList(List<RecommendationRequest> request);
+}

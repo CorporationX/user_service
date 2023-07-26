da19d27b238b80f9486a11d881f8368dd2bb1573
Kirill Soshnikov <soshnikov17@gmail.com>
2023-07-26 21:28:39 +0300

    Tasks BC-3519, BC-3520, BC-3521, BC-3522 fixed

--- src/main/java/school/faang/user_service/service/recommendation/RecommendationRequestService.java
+++ src/main/java/school/faang/user_service/service/recommendation/RecommendationRequestService.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.service.recommendation;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.filter.RecommendationRequestFilterDto;
@@ -14,6 +15,7 @@ import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.recommendation.RecommendationRequestRepository;
 
+import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +30,7 @@ public class RecommendationRequestService {
     private final SkillRepository skillRepository;
     private final List<RecommendationRequestFilter> recommendationRequestFilters;
 
+    @Transactional
     public RecommendationRequestDto create(RecommendationRequestDto recommendationRequestDto) {
         validate(recommendationRequestDto);
         RecommendationRequest entity = recommendationRequestMapper.toEntity(recommendationRequestDto);
@@ -37,14 +40,14 @@ public class RecommendationRequestService {
     public RecommendationRequestDto getRequest(long id) {
         Optional<RecommendationRequest> optionalRecommendationRequest = recommendationRequestRepository.findById(id);
         RecommendationRequest entity = optionalRecommendationRequest
-                .orElseThrow(() -> new DataValidationException("Recommendation with id = " + id + " does not exist!"));
+                .orElseThrow(() -> new DataValidationException(MessageFormat.format("Recommendation with id = {0} does not exist!", id)));
         return recommendationRequestMapper.toDto(entity);
     }
 
     public RecommendationRequestDto rejectRequest(long id, RejectionDto rejection) {
         Optional<RecommendationRequest> optionalRecommendationRequest = recommendationRequestRepository.findById(id);
         RecommendationRequest entity = optionalRecommendationRequest
-                .orElseThrow(() -> new DataValidationException("Recommendation with id = " + id + " does not exist!"));
+                .orElseThrow(() -> new DataValidationException(MessageFormat.format("Recommendation with id = {0} does not exist!", id)));
         validatePendingStatus(entity);
         entity.setStatus(RequestStatus.REJECTED);
         entity.setRejectionReason(rejection.getReason());
@@ -60,17 +63,16 @@ public class RecommendationRequestService {
                 .toList();
     }
 
-
     private void validate(Long userId) {
         if (!userRepository.existsById(userId)) {
-            throw new DataValidationException("User " + userId + " does not exist!");
+            throw new DataValidationException(MessageFormat.format("User {0} does not exist!", userId));
         }
     }
 
     private void validate(List<Long> skillIds) {
         for (Long skillId : skillIds) {
             if (!skillRepository.existsById(skillId)) {
-                throw new DataValidationException("Skill " + skillId + " does not exist!");
+                throw new DataValidationException(MessageFormat.format("Skill {0}} does not exist!", skillId));
             }
         }
     }
@@ -92,7 +94,8 @@ public class RecommendationRequestService {
     private void validatePendingStatus(RecommendationRequest entity) {
         if (entity.getStatus().equals(RequestStatus.REJECTED)) {
             throw new DataValidationException("Recommendation request was rejected earlier!");
-        } else if (entity.getStatus().equals(RequestStatus.ACCEPTED)) {
+        }
+        if (entity.getStatus().equals(RequestStatus.ACCEPTED)) {
             throw new DataValidationException("Recommendation request was accepted earlier!");
         }
     }

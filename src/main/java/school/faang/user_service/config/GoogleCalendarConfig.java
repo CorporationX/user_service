27ba8a5a3410a855da6548467b73170536100b71
Pashkevich <ppavel012100@gmail.com>
2023-07-26 16:01:47 +0200

    Rework GoogleCalendarConfig

--- src/main/java/school/faang/user_service/config/GoogleCalendarConfig.java
+++ src/main/java/school/faang/user_service/config/GoogleCalendarConfig.java
@@ -9,6 +9,7 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +18,6 @@ import org.springframework.context.annotation.Configuration;
 import java.io.FileReader;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-
 import java.util.Collections;
 
 @Configuration
@@ -26,6 +26,7 @@ public class GoogleCalendarConfig {
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final String APPLICATION_NAME = "Google Calendar API Java";
     private static final String CREDENTIALS_FILE_PATH = "src/main/resources/credentials.json";
+    private static final String TOKENS_DIRECTORY_PATH = "src/main/resources/tokens";
 
     @Bean
     public Calendar getCalendarService() throws IOException, GeneralSecurityException {
@@ -39,12 +40,15 @@ public class GoogleCalendarConfig {
                 JSON_FACTORY,
                 clientSecrets,
                 Collections.singleton(CalendarScopes.CALENDAR))
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .build();
+
         LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder().setPort(8888).build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, localServerReceiver)
                 .authorize("user");
 
         return new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME).build();
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 }

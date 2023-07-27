d5e4bd3c747918fa16a65d533bf92152d29c7843
Devilserk <demonserker@gmail.com>
2023-07-27 22:58:04 +0600

    cherry pick 3749

---
+++ src/main/java/school/faang/user_service/service/GoalInvitationService.java
@@ -0,0 +1,44 @@
+package school.faang.user_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.entity.goal.GoalInvitation;
+import school.faang.user_service.exception.DataValidException;
+import school.faang.user_service.mapper.goal.GoalInvitationMapper;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.repository.goal.GoalInvitationRepository;
+import school.faang.user_service.repository.goal.GoalRepository;
+
+@Service
+@RequiredArgsConstructor
+public class GoalInvitationService {
+    private final UserRepository userRepository;
+    private final GoalRepository goalRepository;
+    private final GoalInvitationRepository goalInvitationRepository;
+    private final GoalInvitationMapper goalInvitationMapper;
+
+    public GoalInvitationDto createInvitation(GoalInvitationDto invitationDto) {
+        validateInvitation(invitationDto);
+        GoalInvitation invitation = goalInvitationRepository.save(goalInvitationMapper.toEntity(invitationDto));
+        return goalInvitationMapper.toDto(invitation);
+    }
+
+    private void validateInvitation(GoalInvitationDto invitation) {
+        if (invitation.getId() == null || invitation.getId() < 1) {
+            throw new DataValidException("Invitation illegal id");
+        }
+        if (!goalRepository.existsById(invitation.getGoalId())) {
+            throw new DataValidException("Goal does not exist. Invitation Id: " + invitation.getId());
+        }
+        if (!userRepository.existsById(invitation.getInviterId())) {
+            throw new DataValidException("Inviter does not exist, Id: " + invitation.getInviterId());
+        }
+        if (!userRepository.existsById(invitation.getInvitedUserId())) {
+            throw new DataValidException("Invited does not exist, Id: " + invitation.getInvitedUserId());
+        }
+        if (invitation.getInviterId().equals(invitation.getInvitedUserId())) {
+            throw new DataValidException("Inviter and invited are equal. Invitation Id: " + invitation.getId());
+        }
+    }
+}
---
+++ src/test/java/school/faang/user_service/service/GoalInvitationServiceTest.java
@@ -0,0 +1,105 @@
+package school.faang.user_service.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.entity.goal.GoalInvitation;
+import school.faang.user_service.exception.DataValidException;
+import school.faang.user_service.mapper.goal.GoalInvitationMapper;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.repository.goal.GoalInvitationRepository;
+import school.faang.user_service.repository.goal.GoalRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GoalInvitationServiceTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private GoalRepository goalRepository;
+    @Mock
+    private GoalInvitationRepository goalInvitationRepository;
+    @Spy
+    private GoalInvitationMapper goalInvitationMapper = GoalInvitationMapper.INSTANCE;
+    @InjectMocks
+    private GoalInvitationService goalInvitationService;
+
+    @Test
+    void createTestIllegalId() {
+        GoalInvitationDto goalInvitationDto = createInvitationDto();
+        goalInvitationDto.setId(0L);
+
+        Exception ex = assertThrows(DataValidException.class, () -> goalInvitationService.createInvitation(goalInvitationDto));
+        assertEquals("Invitation illegal id", ex.getMessage());
+    }
+
+    @Test
+    void createTestIllegalEventId() {
+        GoalInvitationDto goalInvitationDto = createInvitationDto();
+
+        when(goalRepository.existsById(anyLong())).thenReturn(false);
+        Exception ex = assertThrows(DataValidException.class, () -> goalInvitationService.createInvitation(goalInvitationDto));
+        assertTrue(ex.getMessage().contains("Goal does not exist"));
+    }
+
+    @Test
+    void createTestEqualInviterInvited() {
+        GoalInvitationDto goalInvitationDto = createInvitationDto();
+        goalInvitationDto.setInvitedUserId(1L);
+
+        when(goalRepository.existsById(anyLong())).thenReturn(true);
+        when(userRepository.existsById(anyLong())).thenReturn(true);
+
+        Exception ex = assertThrows(DataValidException.class, () -> goalInvitationService.createInvitation(goalInvitationDto));
+        assertTrue(ex.getMessage().contains("Inviter and invited are equal"));
+
+    }
+
+    @Test
+    void createSuccessful() {
+        GoalInvitationDto goalInvitationDto = createInvitationDto();
+
+        when(userRepository.existsById(anyLong())).thenReturn(true);
+        when(goalRepository.existsById(anyLong())).thenReturn(true);
+        when(goalInvitationRepository.save(any(GoalInvitation.class))).thenReturn(createGoalInvitation());
+
+        GoalInvitationDto result = goalInvitationService.createInvitation(goalInvitationDto);
+        assertEquals(goalInvitationDto, result);
+    }
+
+    private GoalInvitationDto createInvitationDto() {
+        return GoalInvitationDto.builder().id(1L).inviterId(1L).invitedUserId(2L).goalId(1L).build();
+    }
+
+    private GoalInvitation createGoalInvitation() {
+        GoalInvitation goalInvitation = new GoalInvitation();
+        goalInvitation.setId(1L);
+
+        User user1 = new User();
+        user1.setId(1L);
+        goalInvitation.setInviter(user1);
+
+        User user2 = new User();
+        user2.setId(2L);
+        goalInvitation.setInvited(user2);
+
+        Goal goal = new Goal();
+        goal.setId(1L);
+        goalInvitation.setGoal(goal);
+
+        return goalInvitation;
+    }
+}

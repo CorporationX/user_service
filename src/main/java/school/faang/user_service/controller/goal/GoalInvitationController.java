d103eb59e472b4bebf58ec5c8b8d29fe0ee8e386
Devilserk <demonserker@gmail.com>
2023-07-27 22:14:03 +0600

    cherry-pick 3749 2

---
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -0,0 +1,20 @@
+package school.faang.user_service.controller.goal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.service.GoalInvitationService;
+
+@RestController
+@RequestMapping("/goals")
+@RequiredArgsConstructor
+public class GoalInvitationController {
+    private final GoalInvitationService goalInvitationService;
+
+    @PostMapping
+    public GoalInvitationDto createInvitation(GoalInvitationDto invitationDto) {
+        return goalInvitationService.createInvitation(invitationDto);
+    }
+}
--- src/main/java/school/faang/user_service/service/GoalInvitationService.java
+++ src/main/java/school/faang/user_service/service/GoalInvitationService.java
@@ -3,20 +3,19 @@ package school.faang.user_service.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.goal.GoalInvitation;
 import school.faang.user_service.exception.DataValidException;
 import school.faang.user_service.mapper.goal.GoalInvitationMapper;
 import school.faang.user_service.repository.UserRepository;
-import school.faang.user_service.repository.goal.GoalInvitationRepository;
-import school.faang.user_service.repository.goal.GoalRepository;
 
 @Service
 @RequiredArgsConstructor
 public class GoalInvitationService {
-    private final UserRepository userRepository;
-    private final GoalRepository goalRepository;
-    private final GoalInvitationRepository goalInvitationRepository;
-    private final GoalInvitationMapper goalInvitationMapper;
+    private UserRepository userRepository;
+    private GoalRepository goalRepository;
+    private GoalInvitationRepository goalInvitationRepository;
+    private GoalInvitationMapper goalInvitationMapper;
 
     public GoalInvitationDto createInvitation(GoalInvitationDto invitationDto) {
         validateInvitation(invitationDto);
@@ -31,13 +30,11 @@ public class GoalInvitationService {
         if (!goalRepository.existsById(invitation.getGoalId())) {
             throw new DataValidException("Goal does not exist. Invitation Id: " + invitation.getId());
         }
-        if (!userRepository.existsById(invitation.getInviterId())) {
-            throw new DataValidException("Inviter does not exist, Id: " + invitation.getInviterId());
-        }
-        if (!userRepository.existsById(invitation.getInvitedUserId())) {
-            throw new DataValidException("Invited does not exist, Id: " + invitation.getInvitedUserId());
-        }
-        if (invitation.getInviterId().equals(invitation.getInvitedUserId())) {
+        User inviter = userRepository.findById(invitation.getInviterId())
+                .orElseThrow(() -> new DataValidException("Inviter does not exist, Id: " + invitation.getInviterId()));
+        User invited = userRepository.findById(invitation.getInvitedUserId())
+                .orElseThrow(() -> new DataValidException("Invited does not exist, Id: " + invitation.getInvitedUserId()));
+        if (inviter.equals(invited)) {
             throw new DataValidException("Inviter and invited are equal. Invitation Id: " + invitation.getId());
         }
     }

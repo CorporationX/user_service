ea6a5a07b819994879a470fac9697f78758c0762
Andrey <khalyavinandrey@gmail.com>
2023-07-28 12:20:56 +0500

    перенес binding result в exception handler

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -3,7 +3,6 @@ package school.faang.user_service.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.request.UserIdsRequest;
 import school.faang.user_service.service.UserService;
-import school.faang.user_service.util.exception.DataValidationException;
 import school.faang.user_service.util.validator.UserControllerValidator;
 
 import java.util.List;
@@ -34,16 +32,7 @@ public class UserController {
 
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
-    List<UserDto> getUsersByIds(@Valid @RequestBody UserIdsRequest request, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            StringBuilder message = new StringBuilder();
-            bindingResult.getAllErrors().forEach(
-                    error -> message.append(error.getDefaultMessage())
-            );
-
-            throw new DataValidationException(message.toString());
-        }
-
+    List<UserDto> getUsersByIds(@Valid @RequestBody UserIdsRequest request) {
         return service.getUsersByIds(request.ids());
     }
 }
--- src/main/java/school/faang/user_service/util/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/util/exceptionhandler/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package school.faang.user_service.util.exceptionhandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import school.faang.user_service.dto.response.ErrorResponse;
@@ -21,6 +23,21 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
                 e.getMessage(), LocalDateTime.now()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException e){
+        BindingResult bindingResult = e.getBindingResult();
+        StringBuilder message = new StringBuilder();
+
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(
+                    error -> message.append(error.getDefaultMessage())
+            );
+        }
+
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                message.toString(), LocalDateTime.now()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)

93b72ecf33707c4b43e4cc924aec1b43611f1973
Andrey <khalyavinandrey@gmail.com>
2023-07-28 12:07:46 +0500

    добавил binding result

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -3,6 +3,7 @@ package school.faang.user_service.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.request.UserIdsRequest;
 import school.faang.user_service.service.UserService;
+import school.faang.user_service.util.exception.DataValidationException;
 import school.faang.user_service.util.validator.UserControllerValidator;
 
 import java.util.List;
@@ -32,7 +34,16 @@ public class UserController {
 
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
-    List<UserDto> getUsersByIds(@Valid @RequestBody UserIdsRequest request) {
+    List<UserDto> getUsersByIds(@Valid @RequestBody UserIdsRequest request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder message = new StringBuilder();
+            bindingResult.getAllErrors().forEach(
+                    error -> message.append(error.getDefaultMessage())
+            );
+
+            throw new DataValidationException(message.toString());
+        }
+
         return service.getUsersByIds(request.ids());
     }
 }
--- src/main/java/school/faang/user_service/dto/request/UserIdsRequest.java
+++ src/main/java/school/faang/user_service/dto/request/UserIdsRequest.java
@@ -2,9 +2,11 @@ package school.faang.user_service.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
+@Validated
 public record UserIdsRequest(
 
         @NotNull

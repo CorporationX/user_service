016a39d1d43bfcc9000ebdbe54217e5dcabe59ac
Basimov Rustam <sombra1nf@yandex.ru>
2023-07-29 23:09:39 +0500

    Добавил валидацию входных параметров на контроллере

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -17,7 +17,24 @@ public class RecommendationRequestController {
     private final RecommendationRequestService recommendationRequestService;
 
     @PostMapping("/rejectRequest/{id}")
-    public RecommendationRequestDto rejectRequest(@PathVariable long id, RejectionDto rejection) {
+    public RecommendationRequestDto rejectRequest(@PathVariable Long id, RejectionDto rejection) {
+        validateId(id);
+        validateRejection(rejection);
         return recommendationRequestService.rejectRequest(id, rejection);
+    }
+
+    private void validateRejection(RejectionDto rejection) {
+        if (rejection.getReason() == null || rejection.getReason().isEmpty()) {
+            throw new IllegalArgumentException("Rejection reason is null or empty");
+        }
+    }
+
+    private void validateId(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Recommendation request id is null");
+        }
+        if (id < 0) {
+            throw new IllegalArgumentException("Recommendation request id is negative");
+        }
     }
 }

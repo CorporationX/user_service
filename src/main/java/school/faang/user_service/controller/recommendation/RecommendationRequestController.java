3b2c90a5581157ed4c5ed25db59f01f4a0ad69d1
Basimov Rustam <sombra1nf@yandex.ru>
2023-07-29 23:05:17 +0500

    Добавил руты для контроллеров и rejectRequest теперь принимает айдишник из запроса

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -2,16 +2,22 @@ package school.faang.user_service.controller.recommendation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.dto.recommendation.RejectionDto;
 import school.faang.user_service.service.RecommendationRequestService;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/recommendation")
 public class RecommendationRequestController {
 
     private final RecommendationRequestService recommendationRequestService;
-    public RecommendationRequestDto rejectRequest(long id, RejectionDto rejection) {
+
+    @PostMapping("/rejectRequest/{id}")
+    public RecommendationRequestDto rejectRequest(@PathVariable long id, RejectionDto rejection) {
         return recommendationRequestService.rejectRequest(id, rejection);
     }
 }

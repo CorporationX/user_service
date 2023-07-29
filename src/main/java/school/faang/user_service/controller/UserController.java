93d1024e4f5026de96a87ba3e7a76bbf3e74081c
pavel <p.grischaev@yandex.ru>
2023-07-29 15:11:43 +0700

    доаботал метод деактивации

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -3,8 +3,8 @@ package school.faang.user_service.controller;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.DtoDeactiv;
@@ -18,7 +18,7 @@ public class UserController {
 
     private final DeactivatingService deactivatingService;
 
-    @GetMapping("/activation/{userId}")
+    @PostMapping("/activation/{userId}")
     public DtoDeactiv deactivating(@PathVariable @Min(0) long userId) {
         return deactivatingService.deactivatingTheUser(userId);
     }

f767364ebe18b0573ce71112323c47b04c2a72b1
Eggur <eggurin2033@gmail.com>
2023-07-29 19:55:31 +0300

    added test classes to the right directory

---
+++ src/test/java/school/faang/user_service/service/recommendation/RecommendationRequestServiceTest.java
@@ -0,0 +1,46 @@
+package school.faang.user_service.service.recommendation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import school.faang.user_service.dto.RecommendationRequestDto;
+import school.faang.user_service.repository.recommendation.RecommendationRequestRepository;
+import school.faang.user_service.repository.recommendation.SkillRequestRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecommendationRequestServiceTest {
+
+    @InjectMocks
+    private RecommendationRequestService recommendationRequestService;
+
+    @Mock
+    private RecommendationRequestRepository recommendationRequestRepository;
+
+    @Mock
+    private SkillRequestRepository skillRequestRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void create_EmptyMessage_ThrowsIllegalArgumentException() {
+        RecommendationRequestDto recommendationRequestDto = new RecommendationRequestDto();
+        recommendationRequestDto.setMessage("");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            recommendationRequestService.create(recommendationRequestDto);
+        });
+
+        Mockito.verify(recommendationRequestRepository, Mockito.never())
+                .create(Mockito.anyLong(), Mockito.anyLong(), Mockito.anyString());
+        Mockito.verify(skillRequestRepository, Mockito.never())
+                .create(Mockito.anyLong(), Mockito.anyLong());
+    }
+}

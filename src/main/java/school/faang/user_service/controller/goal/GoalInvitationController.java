3981b9751d0ec2be9f520276726dc9c2035f790f
Devilserk <demonserker@gmail.com>
2023-07-29 15:01:42 +0600

    добавил @RequestBody

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -2,6 +2,7 @@ package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
@@ -14,7 +15,7 @@ public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
 
     @PostMapping("/createInvitation/{id}")
-    public GoalInvitationDto createInvitation(GoalInvitationDto invitationDto) {
+    public GoalInvitationDto createInvitation(@RequestBody GoalInvitationDto invitationDto) {
         return goalInvitationService.createInvitation(invitationDto);
     }
 

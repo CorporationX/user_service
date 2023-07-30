16d20378fc6ddc9a95f371440dae98566c3d3e60
Andrey <khalyavinandrey@gmail.com>
2023-07-30 12:29:32 +0500

    обновил дто

--- src/main/java/school/faang/user_service/dto/mydto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/mydto/UserDto.java
@@ -5,9 +5,19 @@ import lombok.Builder;
 import java.util.List;
 
 @Builder
-public record UserDto(Long id, String username, String email,
-                      String phone, String aboutMe, boolean active, String city,
-                      Integer experience, List<Long> followers, List<Long> followees,
-                      List<Long> mentors, List<Long> mentees, CountryDto country,
-                      List<GoalDto> goals, List<SkillDto> skills) {
+public record UserDto(Long id,
+                      String username,
+                      String email,
+                      String phone,
+                      String aboutMe,
+                      boolean active,
+                      String city,
+                      Integer experience,
+                      List<Long> followers,
+                      List<Long> followees,
+                      List<Long> mentors,
+                      List<Long> mentees,
+                      CountryDto country,
+                      List<GoalDto> goals,
+                      List<SkillDto> skills) {
 }

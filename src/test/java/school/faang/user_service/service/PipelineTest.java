0e23e52c67d187561c4d7092788381feed89e653
Basimov Rustam <sombra1nf@yandex.ru>
2023-07-30 19:41:23 +0500

    Create test which will fail

---
+++ src/test/java/school/faang/user_service/service/PipelineTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PipelineTest {
+
+    @Test
+    void test() {
+        Assertions.fail();
+    }
+}

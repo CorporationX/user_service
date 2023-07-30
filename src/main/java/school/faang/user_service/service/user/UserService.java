62732a53adbde327fa5575bc6367f1112727debd
Kirill Soshnikov <soshnikov17@gmail.com>
2023-07-30 11:54:58 +0300

    Add user id validation

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -5,16 +5,21 @@ import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.goal.GoalRepository;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final GoalRepository goalRepository;
+    private final UserRepository userRepository;
 
     public UserDto deactivateUser(UserDto userDto) {
+        validate(userDto.getId());
         deleteGoals(userDto);
         return userDto;
     }
@@ -26,6 +31,12 @@ public class UserService {
             if (users.size() == 1 && users.get(0).getId() == userDto.getId()) {
                 goalRepository.deleteById(goal.getId());
             }
+        }
+    }
+
+    private void validate(Long userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new DataValidationException(MessageFormat.format("User {0} does not exist!", userId));
         }
     }
 }
--- src/test/java/school/faang/user_service/service/user/UserDeactivateTest.java
+++ src/test/java/school/faang/user_service/service/user/UserDeactivateTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.goal.GoalRepository;
 
 import java.util.List;
@@ -22,6 +23,8 @@ public class UserDeactivateTest {
     private UserDto userDto;
     @Mock
     GoalRepository goalRepository;
+    @Mock
+    UserRepository userRepository;
     @InjectMocks
     UserService userService;
 
@@ -41,6 +44,7 @@ public class UserDeactivateTest {
         goal.setUsers(List.of(user1));
         List<Goal> goals = List.of(goal);
         user1.setGoals(goals);
+        Mockito.when(userRepository.existsById(userDto.getId())).thenReturn(true);
         Mockito.when(goalRepository.findGoalsByUserId(userDto.getId())).thenReturn(goals.stream());
         userService.deactivateUser(userDto);
         Mockito.verify((goalRepository), Mockito.times(1)).deleteById(userDto.getId());
@@ -52,6 +56,7 @@ public class UserDeactivateTest {
         List<Goal> goals = List.of(goal);
         user1.setGoals(goals);
         user2.setGoals(goals);
+        Mockito.when(userRepository.existsById(userDto.getId())).thenReturn(true);
         Mockito.when(goalRepository.findGoalsByUserId(userDto.getId())).thenReturn(goals.stream());
         userService.deactivateUser(userDto);
         Mockito.verify((goalRepository), Mockito.times(0)).deleteById(userDto.getId());

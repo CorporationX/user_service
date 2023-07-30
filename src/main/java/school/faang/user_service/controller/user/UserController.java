d8c1fa7574776abeae3d7622a80c2b1a1d23a6c6
Viktor Ivanov <avacode78@gmail.com>
2023-07-30 13:52:24 +0700

    small fix

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -2,7 +2,7 @@ package school.faang.user_service.controller.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.mentor.UserDto;
 import school.faang.user_service.dto.user.UserFilterDto;
@@ -12,11 +12,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
 
     @GetMapping("/premium-users")
-    public List<UserDto> getPremiumUsers(@RequestBody UserFilterDto userFilterDto) {
+    public List<UserDto> getPremiumUsers(UserFilterDto userFilterDto) {
         return userService.getPremiumUsers(userFilterDto);
     }
 }

5692de32d3c927d33593fdf79c8eef2a94c9af7d
Devilserk <demonserker@gmail.com>
2023-07-30 14:30:55 +0600

    change controller and service

--- src/main/java/school/faang/user_service/service/GoalInvitationService.java
+++ src/main/java/school/faang/user_service/service/GoalInvitationService.java
@@ -23,6 +23,7 @@ public class GoalInvitationService {
     @Transactional
     public GoalInvitationDto createInvitation(GoalInvitationDto invitationDto) {
         validateInvitation(invitationDto);
+
         GoalInvitation invitation = goalInvitationRepository.save(goalInvitationMapper.toEntity(invitationDto));
         return goalInvitationMapper.toDto(invitation);
     }
@@ -45,10 +46,7 @@ public class GoalInvitationService {
 
     @Transactional(readOnly = true)
     private void validateInvitation(GoalInvitationDto invitation) {
-        if (invitation.getId() == null || invitation.getId() < 1) {
-            throw new DataValidException("Invitation illegal id");
-        }
-        if (goalInvitationRepository.existsById(invitation.getId())) {
+        if (invitation.getId() != null && goalInvitationRepository.existsById(invitation.getId())) {
             throw new DataValidException("Invitation already exist. Id: " + invitation.getId());
         }
         if (!goalRepository.existsById(invitation.getGoalId())) {
@@ -63,5 +61,6 @@ public class GoalInvitationService {
         if (invitation.getInviterId().equals(invitation.getInvitedUserId())) {
             throw new DataValidException("Inviter and invited are equal. Invitation Id: " + invitation.getId());
         }
+        invitation.setId(null);
     }
 }

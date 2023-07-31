1370cabbd61137e751a9d8b6d3a63e5822a96a8a
Basimov Rustam <sombra1nf@yandex.ru>
2023-07-31 22:38:54 +0500

    Доработал тесты, изменил валидацию

--- src/main/java/school/faang/user_service/contrtoller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/contrtoller/recommendation/RecommendationRequestController.java
@@ -1,26 +1,29 @@
 package school.faang.user_service.contrtoller.recommendation;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.service.RecommendationRequestService;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/recommendation")
 public class RecommendationRequestController {
 
     private final RecommendationRequestService recommendationRequestService;
 
-    public static final String INVALID_ID = "Recommendation request id must be greater than 0";
+    public static final String INVALID_ID = "Id is required";
 
-    public RecommendationRequestDto getRecommendationRequest(long id) {
+    @GetMapping("/{id}")
+    public RecommendationRequestDto getRecommendationRequest(@PathVariable Long id) {
         validateId(id);
         return recommendationRequestService.getRequest(id);
     }
 
-    private void validateId(long id) {
-        if (id <= 0) {
+    private void validateId(Long id) {
+        if (id == null) {
             throw new IllegalArgumentException(INVALID_ID);
         }
     }
+
 }
--- src/test/java/school/faang/user_service/service/RecommendationRequestServiceTest.java
+++ src/test/java/school/faang/user_service/service/RecommendationRequestServiceTest.java
@@ -1,20 +1,27 @@
 package school.faang.user_service.service;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.entity.recommendation.RecommendationRequest;
 import school.faang.user_service.mapper.recommendation.RecommendationRequestMapperImpl;
 import school.faang.user_service.repository.recommendation.RecommendationRequestRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class RecommendationRequestServiceTest {
 
@@ -25,33 +32,37 @@ class RecommendationRequestServiceTest {
     @InjectMocks
     RecommendationRequestService service;
 
-
-    @Test
-    @DisplayName("Positive test for getRecommendationRequest")
-    void getRecommendationRequest_positiveTest() {
-        RecommendationRequest recommendationRequest = new RecommendationRequest();
-        recommendationRequest.setId(1L);
-        Mockito.when(
-                repository.findById(1L))
-                .thenReturn(Optional.of(recommendationRequest));
-        assertEquals(
-                service.getRequest(1L),
-                mapper.toDto(recommendationRequest)
-        );
+    @ParameterizedTest
+    @MethodSource("getId")
+    @DisplayName("Get recommendation request by id")
+    void getRecommendationRequestById(long id) {
+        RecommendationRequest request = new RecommendationRequest();
+        request.setId(id);
+        when(repository.findById(id))
+                .thenReturn(Optional.of(request));
+        RecommendationRequestDto requestDto = service.getRequest(id);
+        assertEquals(id, requestDto.getId());
     }
 
-    @Test
-    @DisplayName("Negative test with non-existent Id for getRecommendationRequest")
-    void getRecommendationRequest_negativeTest_nonExistentId() {
-        Mockito.when(
-                repository.findById(Mockito.anyLong())
-        ).thenReturn(Optional.empty());
+    @ParameterizedTest
+    @MethodSource("getId")
+    @DisplayName("Recommendation request not found")
+    void getRecommendationRequestByIdNotFound(long id) {
+        when(repository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> service.getRequest(id));
+        assertEquals("Recommendation request not found", exception.getMessage());
+    }
 
-        IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class, () -> service.getRequest(20L)
+    private static Stream<Arguments> getId() {
+        return Stream.of(
+                Arguments.of(2L),
+                Arguments.of(19L),
+                Arguments.of(11231L),
+                Arguments.of(1L),
+                Arguments.of(123L),
+                Arguments.of(1547L)
         );
-
-        assertEquals(RecommendationRequestService.NOT_FOUND, exception.getMessage());
     }
 
 }

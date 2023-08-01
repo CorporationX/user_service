76d939aebf0a4ca6340cca2f44e199376a249f47
Sergey Gerasimov <czar997@yandex.ru>
2023-08-01 15:38:25 +0300

    перенес валидацию данных с контроллера в класс валидатор

--- src/main/java/school/faang/user_service/controller/recomendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recomendation/RecommendationRequestController.java
@@ -13,6 +13,7 @@ import school.faang.user_service.dto.RejectionDto;
 import school.faang.user_service.dto.RequestFilterDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.RecommendationRequestService;
+import school.faang.user_service.validator.RecommendationRequestValidator;
 
 import java.util.List;
 
@@ -23,27 +24,27 @@ public class RecommendationRequestController {
 
     private final RecommendationRequestService recommendationRequestService;
 
-    @PostMapping()
+    private final RecommendationRequestValidator recommendationRequestValidator;
+
+    @PostMapping
     public RecommendationRequestDto requestRecommendation(@RequestBody RecommendationRequestDto recommendationRequest) {
-        validationDto(recommendationRequest);
+        recommendationRequestValidator.validationDto(recommendationRequest);
         return recommendationRequestService.create(recommendationRequest);
     }
 
     @GetMapping("/filter")
     public List<RecommendationRequestDto> getRecommendationRequests(@RequestBody RequestFilterDto filter) {
-        validationFilter(filter);
+        recommendationRequestValidator.validationFilter(filter);
         return recommendationRequestService.getRequests(filter);
     }
 
     @GetMapping("/{id}")
     public RecommendationRequestDto getRecommendationRequest(@PathVariable long id) {
-        validationId(id);
         return recommendationRequestService.getRecommendationRequest(id);
     }
 
     @PutMapping("/rejection/{id}")
     public RecommendationRequestDto rejectRequest(@PathVariable long id, RejectionDto rejection) {
-        validationId(id);
         validationRejection(rejection);
         return recommendationRequestService.rejectRequest(id, rejection);
     }
@@ -54,27 +55,6 @@ public class RecommendationRequestController {
         }
         if (rejection.getReason().isBlank()) {
             throw new DataValidationException("Rejection reson cannot be blank");
-        }
-    }
-
-    private void validationDto(RecommendationRequestDto recommendationRequest) {
-        if (recommendationRequest == null) {
-            throw new DataValidationException("RecommendationRequestDto cannot be null");
-        }
-        if (recommendationRequest.getMessage().isBlank()) {
-            throw new DataValidationException("RecommendationRequestDto message cannot be blank");
-        }
-    }
-
-    private void validationFilter(RequestFilterDto filter) {
-        if (filter == null) {
-            throw new DataValidationException("Request filter is null!");
-        }
-    }
-
-    private void validationId(long id) {
-        if (id < 1) {
-            throw new DataValidationException("Id cannot be less than 1");
         }
     }
 }
--- src/main/java/school/faang/user_service/validator/RecommendationRequestValidator.java
+++ src/main/java/school/faang/user_service/validator/RecommendationRequestValidator.java
@@ -3,6 +3,7 @@ package school.faang.user_service.validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.RecommendationRequestDto;
+import school.faang.user_service.dto.RequestFilterDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.UserRepository;
 
@@ -26,6 +27,21 @@ public class RecommendationRequestValidator {
     public void validationExistById(Long id) {
         if (!userRepository.existsById(id)) {
             throw new DataValidationException("User with id " + id + " does not exist");
+        }
+    }
+
+    public void validationDto(RecommendationRequestDto recommendationRequest) {
+        if (recommendationRequest == null) {
+            throw new DataValidationException("RecommendationRequestDto cannot be null");
+        }
+        if (recommendationRequest.getMessage().isBlank()) {
+            throw new DataValidationException("RecommendationRequestDto message cannot be blank");
+        }
+    }
+
+    public void validationFilter(RequestFilterDto filter) {
+        if (filter == null) {
+            throw new DataValidationException("Request filter is null!");
         }
     }
 }

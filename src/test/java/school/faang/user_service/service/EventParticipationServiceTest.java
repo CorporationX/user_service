0b33cae91379df470cf55d33cd1635a2ede40b35
M1khail <mishalion888@gmail.com>
2023-08-02 12:13:50 +0200

    Register for the event - Ready

--- src/test/java/school/faang/user_service/service/EventParticipationServiceTest.java
+++ src/test/java/school/faang/user_service/service/EventParticipationServiceTest.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;

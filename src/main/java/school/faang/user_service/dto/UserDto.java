326b1c957fcb6dd4d2f4017559ad5bf2abde556d
46 <grisha.chay@gmail.com>
2023-08-02 14:44:18 +0300

    вынесла общее UserDto

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -3,11 +3,15 @@ package school.faang.user_service.dto;
 import lombok.Builder;
 import lombok.Data;
 
-
 @Data
 @Builder
 public class UserDto {
-    private Long id;
+
+    private long id;
     private String username;
     private String email;
+    private String phone;
+    private String aboutMe;
+    private String city;
+    private Integer experience;
 }

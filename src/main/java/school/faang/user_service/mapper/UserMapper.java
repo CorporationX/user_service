8cae8a4476393272cfa501c1ea0ff711cbf42653
Sergey Gerasimov <czar997@yandex.ru>
2023-08-03 17:19:16 +0300

    Исправлен:  1. Неправильно написаны url-адресса в MentorshipController  2. В MentorshipRequestMapper не создается бин  3. 2 класса UserDto  4. 2 маппера UserMapper  5. Нет аннотации @Component на классе MentorshipValidator

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -6,10 +6,14 @@ import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     User toEntity(UserDto userDto);
 
     UserDto toDto(User user);
+
+    List<UserDto> toDto(List<User> users);
 }

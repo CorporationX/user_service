9563fbc4a1f9510bb0f0bf3a27c9b1691d2821f3
Basimov Rustam <sombra1nf@yandex.ru>
2023-08-04 19:04:50 +0500

    Move service to package for this logic

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.dto.recommendation.RejectionDto;
 import school.faang.user_service.dto.recommendation.filter.RequestFilterDto;
-import school.faang.user_service.service.RecommendationRequestService;
+import school.faang.user_service.service.recommendation.RecommendationRequestService;
 
 import java.util.List;
 

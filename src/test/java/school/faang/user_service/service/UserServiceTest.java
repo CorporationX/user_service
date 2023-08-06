81108bfd90c6515e47f438b18fe69393bf05ab50
Viktor Ivanov <avacode78@gmail.com>
2023-08-06 18:33:32 +0700

    add tests for negative cases

--- src/test/java/school/faang/user_service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,11 +9,16 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.mentor.UserDto;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.UserNotFoundException;
 import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -38,6 +44,18 @@ class UserServiceTest {
     }
 
     @Test
+    void getUser_UserNotFound_Test() {
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class,
+                () -> userService.getUser(1L));
+
+        assertEquals("User with id 1 not found", exception.getMessage());
+
+        Mockito.verify(userRepository).findById(1L);
+    }
+
+    @Test
     void getUsersByIds_Test() {
         User user1 = User.builder()
                 .id(1)
@@ -49,11 +67,22 @@ class UserServiceTest {
 
         List<User> users = List.of(user1, user2);
 
-        Mockito.when(userRepository.findAllById(List.of(1L,2L))).thenReturn(users);
+        Mockito.when(userRepository.findAllById(List.of(1L, 2L))).thenReturn(users);
 
         List<UserDto> usersByIds = userService.getUsersByIds(List.of(1L, 2L));
 
         Mockito.verify(userRepository).findAllById(List.of(1L, 2L));
-        Mockito.verify(userMapper).toUserListDto(users);
+    }
+
+    @Test
+    void getUsersByIds_ReturnEmptyList_Test() {
+
+        Mockito.when(userRepository.findAllById(List.of(1L, 2L))).thenReturn(new ArrayList<>());
+
+        List<UserDto> usersByIds = userService.getUsersByIds(List.of(1L, 2L));
+
+        Mockito.verify(userRepository).findAllById(List.of(1L, 2L));
+
+        Assertions.assertEquals(0, usersByIds.size());
     }
 }

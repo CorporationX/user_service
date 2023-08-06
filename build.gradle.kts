941b2152425af0547f5218277e2fa3a3bbfb82d9
Паша <petrov.pashapet@yandex.ru>
2023-08-06 03:45:00 +0300

    Swagger для user_service

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,11 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
 	testImplementation("org.assertj:assertj-core:3.24.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	/**
+	 * Swagger
+	 */
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 }
 
 jsonSchema2Pojo {

b6ca78487688f7c871c9d0222c6d4e441448d3a9
Dobrovolsky Artem <artdobrov@mail.ru>
2023-08-06 15:02:48 +0300

    BC-4820: Создать класс GlobalExceptionHandler с методом handleValidationExceptions

---
+++ src/main/java/school/faang/user_service/exception_handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package school.faang.user_service.exception_handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+}

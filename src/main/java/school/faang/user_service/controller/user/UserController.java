4c8e232b895ebf4fd047a85a50bf884690a4a3e6
Viktor Ivanov <avacode78@gmail.com>
2023-08-06 18:09:53 +0700

    fix according to the comments

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -5,32 +5,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.mentor.UserDto;
-import school.faang.user_service.exception.DataValidException;
 import school.faang.user_service.service.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     UserDto getUser(@PathVariable long userId) {
         return userService.getUser(userId);
     }
 
-    @PostMapping("/users")
+    @PostMapping("/get-by-ids")
     List<UserDto> getUsersByIds(@RequestBody List<Long> ids) {
         validateUsersIds(ids);
         return userService.getUsersByIds(ids);
     }
 
-    private static void validateUsersIds(List<Long> ids) {
+    private static List<Long> validateUsersIds(List<Long> ids) {
         if (ids.isEmpty()) {
-            throw new DataValidException("List of user ids is empty");
+            return new ArrayList<>();
         }
+        return ids;
     }
 }
---
+++ src/main/java/school/faang/user_service/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package school.faang.user_service.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponse {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private String url;
+    private HttpStatus status;
+    private String message;
+}

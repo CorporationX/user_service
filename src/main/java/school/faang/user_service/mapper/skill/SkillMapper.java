8d4279594026c5f90e7f752d4c796f7eb887ec2a
svetlana.obrancova <obrancova.s@gmail.com>
2023-08-07 16:22:07 +0200

    BC-3649-4

--- src/main/java/school/faang/user_service/mapper/skill/SkillMapper.java
+++ src/main/java/school/faang/user_service/mapper/skill/SkillMapper.java
@@ -5,10 +5,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
-import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.entity.Skill;
-import school.faang.user_service.entity.event.Event;
 
 import java.util.List;
 

3aff1d79a9f4dcfa7ad4e8cf129438e80ceaa97a
Dobrovolsky Artem <artdobrov@mail.ru>
2023-08-07 18:25:16 +0300

    BC-4721: Добавить логирование ошибок

--- src/main/java/school/faang/user_service/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception_handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.exception_handler;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +12,7 @@ import school.faang.user_service.exception.DataValidationException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler /* extends ResponseEntityExceptionHandler */ {
 
@@ -22,18 +24,21 @@ public class GlobalExceptionHandler /* extends ResponseEntityExceptionHandler */
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+        log.error(exception.getMessage() + exception.getStackTrace().toString(), exception);
         return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(DataValidationException.class)
     public ResponseEntity<Object> handleDataValidationException(DataValidationException exception) {
         Map<String, String> body = Map.of("message", exception.getMessage());
+        log.error(exception.getMessage() + exception.getStackTrace().toString(), exception);
         return ResponseEntity.badRequest().body(body);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException exception) {
         Map<String, String> body = Map.of("message", exception.getMessage());
+        log.error(exception.getMessage() + exception.getStackTrace().toString(), exception);
         return ResponseEntity.status(404).body(body);
     }
 }

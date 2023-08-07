cd77e4469438887987f09b91144920fd6d298b6d
MalinaInk <nataleshen.ko@yandex.ru>
2023-08-08 00:16:57 +0700

    some edits

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -4,16 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.service.RecommendationService;
-import school.faang.user_service.validator.RecommendationValidator;
 
     @RestController
+    @RequestMapping("/api/v1/recommendations")
     public class RecommendationController {
         @Autowired
         private RecommendationService recommendationService;
-        @Autowired
-        private RecommendationValidator recommendationValidator;
 
         @DeleteMapping("/delete/{id}")
         public ResponseEntity deleteRecommendation(@PathVariable Long id) {

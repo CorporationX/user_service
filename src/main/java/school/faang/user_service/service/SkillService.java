f6b7bf98298cca3be3d7061b70b5d6a7d9d9df61
Dobrovolsky Artem <artdobrov@mail.ru>
2023-08-09 19:10:57 +0300

    BC-4721: Заменил DataValidationException в валидации SkillService на EntityNotFoundException

--- src/main/java/school/faang/user_service/service/SkillService.java
+++ src/main/java/school/faang/user_service/service/SkillService.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
@@ -76,7 +77,7 @@ public class SkillService {
 
     private void verifyUserExist(long userId) {
         if (!(userRepository.existsById(userId))) {
-            throw new DataValidationException("User doesn't exist");
+            throw new EntityNotFoundException("User doesn't exist");
         }
     }
 

58b4a73c9330e5e3676d492308765d73dba51ceb
Pashkevich <ppavel012100@gmail.com>
2023-08-09 18:00:08 +0200

    Resolve get User task

--- src/test/java/school/faang/user_service/service/user/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/user/UserServiceTest.java
@@ -5,15 +5,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.mapper.UserMapperImpl;
 import school.faang.user_service.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+    @Spy
+    private UserMapperImpl userMapper;
 
     @Mock
     private UserRepository userRepository;
@@ -29,5 +38,19 @@ class UserServiceTest {
     void areOwnedSkillsFalse() {
         Mockito.when(userRepository.countOwnedSkills(1L, List.of(2L))).thenReturn(3);
         assertFalse(userService.areOwnedSkills(1L, List.of(2L)));
+    }
+
+    @Test
+    void getUser() {
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(new User()));
+        userService.getUser(1L);
+        Mockito.verify(userRepository, Mockito.times(1)).findById(1L);
+    }
+
+    @Test
+    void getUsersByIds() {
+        Mockito.when(userRepository.findAllById(List.of(1L, 2L))).thenReturn(List.of(User.builder().id(1L).build(), User.builder().id(2L).build()));
+        List<UserDto> users = userService.getUsersByIds(List.of(1L, 2L));
+        assertEquals(2, users.size());
     }
 }

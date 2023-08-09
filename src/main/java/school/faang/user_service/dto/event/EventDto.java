bdddde31fd38c2278bc75854490430d40f11313f
Dobrovolsky Artem <artdobrov@mail.ru>
2023-08-09 20:23:03 +0300

    BC-4858: Добавлена аннотация @JsonFormat для полей LocalDateTime

--- src/main/java/school/faang/user_service/dto/event/EventDto.java
+++ src/main/java/school/faang/user_service/dto/event/EventDto.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +17,9 @@ import java.util.List;
 public class EventDto {
     private Long id;
     private String title;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endDate;
     private Long ownerId;
     private String description;
--- src/main/java/school/faang/user_service/dto/event/EventFilterDto.java
+++ src/main/java/school/faang/user_service/dto/event/EventFilterDto.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 public class EventFilterDto {
     private String titlePattern;
     private Long ownerId;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDate;
     private Long eventId;
 }

e2ea253352bed92f9a554e9dce3798fa76a1e2c8
Devilserk <demonserker@gmail.com>
2023-08-10 16:51:00 +0600

    fix EventFilterDto

--- src/main/java/school/faang/user_service/dto/event/EventFilterDto.java
+++ src/main/java/school/faang/user_service/dto/event/EventFilterDto.java
@@ -1,7 +1,5 @@
 package school.faang.user_service.dto.event;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;

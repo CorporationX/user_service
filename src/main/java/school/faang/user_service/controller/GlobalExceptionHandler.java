66dd1c34ca7bae84230e5d623a87d7a781bffc78
An Dima <an.dima@slexn.com>
2023-08-10 11:47:06 +0900

    fixed

--- src/main/java/school/faang/user_service/controller/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/controller/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package school.faang.user_service.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,13 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import school.faang.user_service.exception.DataValidException;
 import school.faang.user_service.exception.ErrorResponse;
 import school.faang.user_service.exception.UserAlreadyRegisteredAtEvent;
+import school.faang.user_service.exception.UserNotFoundException;
 import school.faang.user_service.exception.UserNotRegisteredAtEvent;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataValidException.class)
@@ -47,9 +52,25 @@ public class GlobalExceptionHandler {
         return new ErrorResponse(unrae.getMessage());
     }
 
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUserNotFoundException(UserNotFoundException exception, HttpServletRequest request) {
+        log.error("Cannot find particular user", exception.getMessage(), exception);
+        return buildErrorResponse(exception, request, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleRuntimeException(RuntimeException re){
         return new ErrorResponse(re.getMessage());
+    }
+
+    private ErrorResponse buildErrorResponse(UserNotFoundException exception, HttpServletRequest request, HttpStatus status) {
+        return ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .url(request.getRequestURI())
+                .status(status)
+                .message(exception.getMessage())
+                .build();
     }
 }
--- src/main/java/school/faang/user_service/exception/ErrorResponse.java
+++ src/main/java/school/faang/user_service/exception/ErrorResponse.java
@@ -12,8 +12,11 @@ import java.time.LocalDateTime;
 public class ErrorResponse {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
-
     private String url;
     private HttpStatus status;
     private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
 }

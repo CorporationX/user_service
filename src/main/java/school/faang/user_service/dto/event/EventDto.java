41306a08d4aeb134de34fc5286cfb1002d00d9fd
Tamerlan <100015209+Devilserk@users.noreply.github.com>
2023-08-11 20:26:23 +0600

    Feature/bc 4862 validation (#474)
    * set up validation for the EventController
    
    * fix EventFilterDto
    
    * fix EventFilterDto
    
    * fix EventController

--- src/main/java/school/faang/user_service/dto/event/EventDto.java
+++ src/main/java/school/faang/user_service/dto/event/EventDto.java
@@ -1,8 +1,12 @@
 package school.faang.user_service.dto.event;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import school.faang.user_service.dto.skill.SkillDto;
+import school.faang.user_service.entity.event.EventType;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,13 +14,23 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class EventDto {
+    @Min(value = 0, message = "Id should be a positive value")
     private Long id;
+    @NotBlank(message = "Title cannot be blank")
     private String title;
+    @NotNull(message = "Start Date cannot be null")
     private LocalDateTime startDate;
+    @NotNull(message = "End Date cannot be null")
     private LocalDateTime endDate;
+    @NotNull(message = "Owner cannot be null")
     private Long ownerId;
+    @NotBlank(message = "Description cannot be blank")
     private String description;
     private List<SkillDto> relatedSkills;
+    @NotNull(message = "Location cannot be null")
     private String location;
+    @Min(value = 0, message = "Max Attendees should be a positive value")
     private int maxAttendees;
+    @NotNull(message = "Type cannot be null")
+    private EventType type;
 }

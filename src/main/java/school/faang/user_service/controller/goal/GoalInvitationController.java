a8bf09b281b894a04469b0e4db171f2ae7e6831b
ПК_Home <wrofdt@ya.ru>
2023-08-11 08:50:58 +0300

    Solution task 3737

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -1,16 +1,23 @@
 package school.faang.user_service.controller.goal;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
 import school.faang.user_service.service.goal.GoalInvitationService;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/invitation")
 public class GoalInvitationController {
     private final GoalInvitationService service;
 
+    @PostMapping("/create")
+    @ResponseBody
     public GoalInvitationDto createInvitation(GoalInvitationDto invitation) {
         return service.createInvitation(invitation);
     }

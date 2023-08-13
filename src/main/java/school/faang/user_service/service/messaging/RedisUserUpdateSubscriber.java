c77f56386dc8b120870c6d75f15f23ca104475a8
Basimov Rustam <sombra1nf@yandex.ru>
2023-08-13 18:02:26 +0500

    Create RedisUserUpdateSubscriber

---
+++ src/main/java/school/faang/user_service/service/messaging/RedisUserUpdateSubscriber.java
@@ -0,0 +1,33 @@
+package school.faang.user_service.service.messaging;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.user.RedisUserDto;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class RedisUserUpdateSubscriber implements MessageListener {
+    public List<RedisUserDto> userDtos = new ArrayList<>();
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        log.info("Received message: {}", new String(message.getBody()));
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            RedisUserDto user = mapper.readValue(message.getBody(), RedisUserDto.class);
+            userDtos.add(user);
+
+            log.info("Received user: {}", user);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

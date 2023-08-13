c723fbe46bc1d38fe935e2d94ee17990c87ce137
M1khail <mishalion888@gmail.com>
2023-08-13 20:10:25 +0200

    Validation in the User's service №2

--- src/main/java/school/faang/user_service/controller/SkillController.java
+++ src/main/java/school/faang/user_service/controller/SkillController.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,6 @@ import school.faang.user_service.service.SkillService;
 
 import java.util.List;
 
-import school.faang.user_service.exception.DataValidationException;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/skill")
@@ -23,8 +22,7 @@ public class SkillController {
     private final SkillService skillService;
 
     @PostMapping
-    public SkillDto create(@RequestBody SkillDto skill) {
-        validateSkill(skill);
+    public SkillDto create(@Valid @RequestBody SkillDto skill) {
         return skillService.create(skill);
     }
 
@@ -41,15 +39,5 @@ public class SkillController {
     @PutMapping("/acquire/{skillId}/user/{userId}")
     public SkillDto acquireSkillFromOffers(@PathVariable long skillId, @PathVariable long userId) {
         return skillService.acquireSkillFromOffers(skillId, userId);
-    }
-
-    private void validateSkill(SkillDto skill) {
-        String title = skill.getTitle();
-        if (title == null || title.isBlank()) {
-            throw new DataValidationException("Skill can't be created with empty name");
-        }
-        if (title.length() > 64) {
-            throw new DataValidationException("Skill's title length can't be more than 64 symbols");
-        }
     }
 }

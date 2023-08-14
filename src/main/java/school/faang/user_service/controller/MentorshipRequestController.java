19bca95337bb405723ccf121aa3340415e94ef1f
Zhargal Budaev <budaev17@mail.ru>
2023-08-14 20:04:03 +0700

    Исправил замечания техлидов

--- src/main/java/school/faang/user_service/controller/MentorshipRequestController.java
+++ src/main/java/school/faang/user_service/controller/MentorshipRequestController.java
@@ -1,20 +1,19 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.MentorshipRequestDto;
-import school.faang.user_service.entity.MentorshipRequest;
 import school.faang.user_service.service.MentorshipRequestService;
 
-import java.util.Objects;
-
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 public class MentorshipRequestController {
     private final MentorshipRequestService mentorshipRequestService;
 
-    public MentorshipRequestDto requestMentorship(MentorshipRequestDto mentorshipRequestDto) {
+    @PostMapping("/mentorship_request")
+    public MentorshipRequestDto requestMentorship(@RequestBody MentorshipRequestDto mentorshipRequestDto) {
         return mentorshipRequestService.requestMentorship(mentorshipRequestDto);
     }
 }
--- src/test/java/school/faang/user_service/service/MentorshipRequestServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipRequestServiceTest.java
@@ -3,10 +3,8 @@ package school.faang.user_service.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mapstruct.control.MappingControl;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.MentorshipRequestDto;
@@ -16,27 +14,30 @@ import school.faang.user_service.mapper.MentorshipRequestMapper;
 import school.faang.user_service.mapper.MentorshipRequestMapperImpl;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.mentorship.MentorshipRequestRepository;
-import school.faang.user_service.validation.MentorshipRequestValidator;
 
-import javax.xml.validation.Validator;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MentorshipRequestServiceTest {
     @Spy
     MentorshipRequestMapper mapper = new MentorshipRequestMapperImpl();
     @Mock
-    MentorshipRequestValidator validator;
-    @Mock
     MentorshipRequestRepository mentorshipRequestRepository;
+    @Mock
+    UserRepository userRepository;
     @InjectMocks
     MentorshipRequestService mentorshipRequestService;
 
     private final long REQUESTER_ID = 1L;
     private final long RECEIVER_ID = 2L;
     private final String DESCRIPTION = "description";
+    private final LocalDateTime THREE_MONTH_AGO = LocalDateTime.now().minusMonths(3);
     private MentorshipRequest request;
     private MentorshipRequestDto requestDto;
 
@@ -52,7 +53,7 @@ class MentorshipRequestServiceTest {
         request.setReceiver(receiver);
         request.setDescription(DESCRIPTION);
 
-        requestDto  = MentorshipRequestDto.builder()
+        requestDto = MentorshipRequestDto.builder()
                 .requesterId(REQUESTER_ID)
                 .receiverId(RECEIVER_ID)
                 .description(DESCRIPTION)
@@ -73,15 +74,57 @@ class MentorshipRequestServiceTest {
 
     @Test
     void testRequestMentorshipInvokeCreate() {
+        when(userRepository.existsById(REQUESTER_ID)).thenReturn(true);
+        when(userRepository.existsById(RECEIVER_ID)).thenReturn(true);
+
         mentorshipRequestService.requestMentorship(requestDto);
         verify(mentorshipRequestRepository)
-            .create(REQUESTER_ID, RECEIVER_ID, DESCRIPTION);
+                .create(REQUESTER_ID, RECEIVER_ID, DESCRIPTION);
     }
 
     @Test
-    void testRequestMentorshipInvokeValidate() {
-        mentorshipRequestService.requestMentorship(requestDto);
-        verify(validator)
-            .validate(request);
+    void testCorrectRequest() {
+        when(userRepository.existsById(REQUESTER_ID)).thenReturn(true);
+        when(userRepository.existsById(RECEIVER_ID)).thenReturn(true);
+        assertDoesNotThrow(() -> mentorshipRequestService.requestMentorship(requestDto));
+    }
+
+    @Test
+    void testRequestWithoutDescription() {
+        requestDto.setDescription("");
+        assertThrows(Exception.class, () -> mentorshipRequestService.requestMentorship(requestDto));
+    }
+
+    @Test
+    void testRequesterDoesNotExists() {
+        when(userRepository.existsById(REQUESTER_ID)).thenReturn(false);
+        assertThrows(IndexOutOfBoundsException.class, () -> mentorshipRequestService.requestMentorship(requestDto));
+    }
+
+    @Test
+    void testReceiverDoesNotExists() {
+        when(userRepository.existsById(REQUESTER_ID)).thenReturn(true);
+        when(userRepository.existsById(RECEIVER_ID)).thenReturn(false);
+        assertThrows(IndexOutOfBoundsException.class, () -> mentorshipRequestService.requestMentorship(requestDto));
+    }
+
+    @Test
+    void testReceiverIsRequester() {
+        requestDto.setReceiverId(REQUESTER_ID);
+
+        when(userRepository.existsById(REQUESTER_ID)).thenReturn(true);
+        when(userRepository.existsById(requestDto.getReceiverId())).thenReturn(true);
+        assertThrows(IllegalArgumentException.class, () -> mentorshipRequestService.requestMentorship(requestDto));
+    }
+
+    @Test
+    void testOneRequestAt3Months() {
+        requestDto.setUpdatedAt(THREE_MONTH_AGO.plusDays(1));
+
+        when(userRepository.existsById(REQUESTER_ID)).thenReturn(true);
+        when(userRepository.existsById(RECEIVER_ID)).thenReturn(true);
+        when(mentorshipRequestRepository.findLatestRequest(REQUESTER_ID, RECEIVER_ID))
+                .thenReturn(Optional.of(request));
+        assertThrows(RuntimeException.class, () -> mentorshipRequestService.requestMentorship(requestDto));
     }
 }

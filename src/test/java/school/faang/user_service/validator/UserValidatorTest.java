a0e922526140056d2e25b21faec8da55723da4ee
Sveta Zalogina <zalogina.sveta@gmail.com>
2023-08-15 20:49:30 -0400

    Fix UserValidatorTest

--- src/test/java/school/faang/user_service/validator/UserValidatorTest.java
+++ src/test/java/school/faang/user_service/validator/UserValidatorTest.java
@@ -3,13 +3,18 @@ package school.faang.user_service.validator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.exception.EntityNotFoundException;
+import school.faang.user_service.repository.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class UserValidatorTest {
+    @Mock
+    private UserRepository userRepository;
+
     @InjectMocks
     private UserValidator userValidator;
 

3c3f4706174a0bf6c056208cb68b87ba7a78d63d
Паша <petrov.pashapet@yandex.ru>
2023-08-16 12:58:26 +0300

    -

--- src/test/java/school/faang/user_service/service/EventCleanupSchedulerServiceTest.java
+++ src/test/java/school/faang/user_service/service/EventCleanupSchedulerServiceTest.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.service.scheduler;
+package school.faang.user_service.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;

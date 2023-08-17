e0e884b6bbda3e7b02efeda87933ca2708d7a1d4
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-08-17 14:59:48 -0500

    Added user_ratings

---
+++ src/main/resources/db/changelog/changeset/user_V009__insert_user_rating.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_ratings (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    user_id bigint NOT NULL,
+    rating smallint NOT NULL,
+
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);

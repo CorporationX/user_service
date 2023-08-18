19c1860ef788df03c782be18f463193ce29c8e6c
Ramazan <mansurov.r13@gmail.com>
2023-08-18 19:53:47 +0300

    BC-4494: resolved conflict in AsyncConfig

--- src/main/java/school/faang/user_service/config/AsyncConfig.java
+++ src/main/java/school/faang/user_service/config/AsyncConfig.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

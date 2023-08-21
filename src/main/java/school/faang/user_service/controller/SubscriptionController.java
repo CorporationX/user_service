704e9b8da11e7ee72feac1f82d1b14c557c7b616
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-08-20 22:01:18 -0700

    feature: BC-5480 small changes

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.exception.DataValidationException;
@@ -9,11 +8,9 @@ import school.faang.user_service.service.SubscriptionService;
 
 import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
-
 
     public void followUser(long followerId, long followeeId) {
         if (!isValid(followerId, followeeId)) {

9e9e400eb017417debb5ed0f7696dcff82f2c4b9
pavel <p.grischaev@yandex.ru>
2023-08-22 11:02:50 +0700

    исправил тесты

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
 	/**
 	 * Database
 	 */

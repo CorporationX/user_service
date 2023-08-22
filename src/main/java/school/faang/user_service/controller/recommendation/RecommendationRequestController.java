6c39e773ceebef82ef47805c2bb93c78c7d4daa7
chaykinks <chaykinks@gmail.com>
2023-08-22 11:04:53 +0500

    добавил аннотации на эндпоинты

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -1,7 +1,7 @@
 package school.faang.user_service.controller.recommendation;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.RecommendationRequestDto;
 import school.faang.user_service.dto.RequestFilterDto;
 import school.faang.user_service.dto.RejectionDto;
@@ -9,28 +9,33 @@ import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.RecommendationRequestService;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/recommendation")
 public class RecommendationRequestController {
     private final RecommendationRequestService recommendationRequestService;
 
-    public RecommendationRequestDto requestRecommendation(RecommendationRequestDto recommendationRequest) {
+    @PostMapping("/request/create")
+    public RecommendationRequestDto requestRecommendation(@RequestBody RecommendationRequestDto recommendationRequest) {
         if (recommendationRequest.getMessage() == null || recommendationRequest.getMessage().isBlank()) {
             throw new DataValidationException("Recommendation request message should not be empty");
         } else {
             return recommendationRequestService.create(recommendationRequest);
         }
     }
-  
-    public List<RecommendationRequestDto> getRecommendationRequests(RequestFilterDto filter) {
+
+    @GetMapping("/request")
+    public List<RecommendationRequestDto> getRecommendationRequests(@RequestBody RequestFilterDto filter) {
         return recommendationRequestService.getRequests(filter);
     }
-    
-    public RecommendationRequestDto getRecommendationRequest(long id) {
+
+    @GetMapping("/request/{id}")
+    public RecommendationRequestDto getRecommendationRequest(@PathVariable long id) {
         return recommendationRequestService.getRequest(id);
     }
-  
-    public RecommendationRequestDto rejectRequest(long id, RejectionDto rejection) {
+
+    @PostMapping("/request/{id}/reject")
+    public RecommendationRequestDto rejectRequest(@PathVariable long id, @RequestBody RejectionDto rejection) {
         if (rejection.getReason() == null || rejection.getReason().isBlank()) {
             throw new DataValidationException("Recommendation request rejection reason should not be empty");
         } else {

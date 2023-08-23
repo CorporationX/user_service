930ef5050820c47d337d1aa66b6d77c59e193e11
alexandr <matcnbev2001@mail.ru>
2023-08-23 16:54:02 +0300

    MentorshipRequestControllerTest

--- src/test/java/school/faang/user_service/controller/mentorship/MentorshipRequestControllerTest.java
+++ src/test/java/school/faang/user_service/controller/mentorship/MentorshipRequestControllerTest.java
@@ -3,31 +3,21 @@ package school.faang.user_service.controller.mentorship;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
 import school.faang.user_service.service.mentorship.MentorshipRequestService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 class MentorshipRequestControllerTest {
     @InjectMocks
     private MentorshipRequestController mentorshipRequestController;
@@ -36,9 +26,6 @@ class MentorshipRequestControllerTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     private MockMvc mockMvc;
-    @Container
-    public static PostgreSQLContainer<?> POSTGRESQL_CONTAINER =
-            new PostgreSQLContainer<>("postgres:13.6");
 
     @BeforeEach
     void setUp() {

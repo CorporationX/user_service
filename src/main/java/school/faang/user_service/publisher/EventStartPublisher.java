76e98057f9aa3646697297434bee4ecd9f3fb3b2
Devilserk <demonserker@gmail.com>
2023-08-23 22:12:41 +0600

    changed EventStartPublisher

--- src/main/java/school/faang/user_service/publisher/EventStartPublisher.java
+++ src/main/java/school/faang/user_service/publisher/EventStartPublisher.java
@@ -1,28 +1,24 @@
 package school.faang.user_service.publisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import school.faang.user_service.dto.redis.EventStartDto;
+import school.faang.user_service.entity.event.Event;
 
 @Component
-@RequiredArgsConstructor
-public class EventStartPublisher {
-    private final RedisTemplate<String, String> redisTemplate;
+public class EventStartPublisher extends EventPublisher<Event> {
     @Value("${spring.data.redis.channels.event_start_channel.name}")
     private String channel;
-    private final ObjectMapper objectMapper;
 
-    public void publishMessage(EventStartDto event) {
-        String jsonEvent;
-        try {
-            jsonEvent = objectMapper.writeValueAsString(event);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        redisTemplate.convertAndSend(channel, jsonEvent);
+    @Autowired
+    public EventStartPublisher(RedisTemplate<String, String> redisTemplate, ObjectMapper objectMapper) {
+        super(redisTemplate, objectMapper);
+    }
+
+    @Override
+    protected String getChannel() {
+        return channel;
     }
 }

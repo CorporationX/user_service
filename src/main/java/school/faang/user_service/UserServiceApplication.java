620a415035df4925bfe64d61a542fe1adee30769
RasulMurtuzaliev <ras8705@mail.ru>
2023-08-23 20:30:12 +0500

    updates UserServiceApplication

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableRetry

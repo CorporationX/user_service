df141d64860795e5bff4b8efd9a34851759ca35c
Basimov Rustam <sombra1nf@yandex.ru>
2023-08-23 18:22:13 +0500

    Add ThreadPoolExecutor

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -2,16 +2,24 @@ package school.faang.user_service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
 @EnableScheduling
 public class UserServiceApplication {
+
+    @Value("${multithreading.batch-size}")
+    private Integer batchSize;
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
@@ -24,4 +32,8 @@ public class UserServiceApplication {
         return objectMapper;
     }
 
+    @Bean
+    public ThreadPoolExecutor executor() {
+        return new ThreadPoolExecutor(batchSize, batchSize, 0L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(batchSize));
+    }
 }

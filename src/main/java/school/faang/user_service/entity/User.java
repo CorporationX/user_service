198bff9745221f9232eedcb992dc7bf017712b7f
Ramazan <mansurov.r13@gmail.com>
2023-08-25 13:44:32 +0300

    BC-5607: simple changes for notification_service work

--- src/main/java/school/faang/user_service/entity/User.java
+++ src/main/java/school/faang/user_service/entity/User.java
@@ -5,8 +5,6 @@ import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;

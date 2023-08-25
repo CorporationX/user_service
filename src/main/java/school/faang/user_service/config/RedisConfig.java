d5c75c7b7b54c8cc943465af5c649bf09d6ca446
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-08-25 16:29:45 -0700

    feature: BC-5648

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration

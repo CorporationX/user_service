ab88a1ca35aad3161996dc3ed0210dfeba9a7087
Alex Tarasov <tarasof.av@gmail.com>
2023-08-28 20:06:41 +0300

    task bc-5630 after review, create AbstractPublisher and rewrite test

--- src/main/java/school/faang/user_service/publisher/SearchAppearanceEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/SearchAppearanceEventPublisher.java
@@ -1,36 +1,17 @@
 package school.faang.user_service.publisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.analytics.SearchAppearanceEventDto;
 
 @Component
-@Data
-@RequiredArgsConstructor
-@Slf4j
-public class SearchAppearanceEventPublisher {
-    private final RedisTemplate<String, Object> redisTemplate;
-    private final ObjectMapper objectMapper;
-    @Value("${spring.data.redis.channels.search-appearance-channel.name}")
-    private String searchAppearanceTopic;
+public class SearchAppearanceEventPublisher extends AbstractPublisher<SearchAppearanceEventDto> {
 
-    public void publish(SearchAppearanceEventDto searchAppearanceEventDto) {
-        String json = toJson(searchAppearanceEventDto);
-        redisTemplate.convertAndSend(searchAppearanceTopic, json);
-    }
-
-    public String toJson(Object object) {
-        try {
-            return objectMapper.writeValueAsString(object);
-        } catch (JsonProcessingException e) {
-            log.error("Error while converting object to string", e);
-        }
-        return null;
+    public SearchAppearanceEventPublisher(ObjectMapper objectMapper,
+                                          RedisTemplate<String, Object> redisTemplate,
+                                          @Value("${spring.data.redis.channels.search-appearance-channel.name}") String searchAppearanceTopic) {
+        super(objectMapper, redisTemplate, searchAppearanceTopic);
     }
 }
--- src/test/java/school/faang/user_service/publisher/SearchAppearanceEventPublisherTest.java
+++ src/test/java/school/faang/user_service/publisher/SearchAppearanceEventPublisherTest.java
@@ -10,8 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.core.RedisTemplate;
 import school.faang.user_service.dto.analytics.SearchAppearanceEventDto;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -28,43 +27,33 @@ class SearchAppearanceEventPublisherTest {
     @Mock
     private ObjectMapper objectMapper;
     private String json;
-    private Object object;
 
     @BeforeEach
     void setUp() {
-        searchAppearanceEventPublisher = new SearchAppearanceEventPublisher(redisTemplate, objectMapper);
-        searchAppearanceEventPublisher.setSearchAppearanceTopic("search-appearance-channel");
+        String topic = "search-appearance-channel";
+        searchAppearanceEventPublisher = new SearchAppearanceEventPublisher(objectMapper, redisTemplate, topic);
         json = "EXPECTED_JSON";
-        object = new Object();
+
     }
 
     @Test
-    void testMethodPublish() {
-        when(searchAppearanceEventPublisher.toJson(searchAppearanceEventDto)).thenReturn(json);
+    void testMethodPublish() throws JsonProcessingException {
+        when(objectMapper.writeValueAsString(searchAppearanceEventDto)).thenReturn(json);
 
         searchAppearanceEventPublisher.publish(searchAppearanceEventDto);
 
         verify(redisTemplate).convertAndSend(anyString(), eq(json));
-    }
-
-    @Test
-    void testToJson_SuccessfulSerialization() throws JsonProcessingException {
-        when(objectMapper.writeValueAsString(object)).thenReturn(json);
-
-        String resultJson = searchAppearanceEventPublisher.toJson(object);
-
-        verify(objectMapper).writeValueAsString(object);
-        assertEquals(json, resultJson);
+        verify(objectMapper).writeValueAsString(searchAppearanceEventDto);
     }
 
     @Test
     void testToJson_FailedSerialization() throws JsonProcessingException {
 
-        when(objectMapper.writeValueAsString(object)).thenThrow(JsonProcessingException.class);
+        when(objectMapper.writeValueAsString(searchAppearanceEventDto)).thenThrow(JsonProcessingException.class);
 
-        String resultJson = searchAppearanceEventPublisher.toJson(object);
+        assertThrows(RuntimeException.class, () -> searchAppearanceEventPublisher.publish(searchAppearanceEventDto));
 
-        verify(objectMapper).writeValueAsString(object);
-        assertNull(resultJson);
+        verify(objectMapper).writeValueAsString(searchAppearanceEventDto);
+
     }
 }

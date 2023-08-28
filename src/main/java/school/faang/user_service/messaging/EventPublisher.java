2524ba73f145d179303e3050da60def12038f9d9
Basimov Rustam <sombra1nf@yandex.ru>
2023-08-28 19:03:19 +0500

    Add abstract class and SkillOfferedEventPublisher

---
+++ src/main/java/school/faang/user_service/messaging/EventPublisher.java
@@ -0,0 +1,27 @@
+package school.faang.user_service.messaging;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+
+@RequiredArgsConstructor
+public abstract class EventPublisher<T> {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ChannelTopic topic;
+    private final JsonMapper mapper;
+
+    public void publish(T event) {
+        String json = getJson(event);
+        redisTemplate.convertAndSend(topic.getTopic(), json);
+    }
+
+    private String getJson(T event) {
+        try {
+            return mapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

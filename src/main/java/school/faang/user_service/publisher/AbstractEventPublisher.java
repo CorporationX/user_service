47a9e620e10bfca08eba8439e45946535660e2a6
Sergey Gerasimov <czar997@yandex.ru>
2023-08-30 19:55:45 +0300

    1. Десериализация json вынесена в абстрактный класс 2. Исправлена названия полей в yaml 3. удален sout в конфиге

---
+++ src/main/java/school/faang/user_service/publisher/AbstractEventPublisher.java
@@ -0,0 +1,27 @@
+package school.faang.user_service.publisher;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Component;
+import school.faang.user_service.exception.SerializeJsonException;
+
+@Component
+@RequiredArgsConstructor
+public abstract class AbstractEventPublisher<T> {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    protected void publishInTopic(ChannelTopic topic, T event) {
+        String json;
+        try {
+            json = objectMapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            throw new SerializeJsonException("Cannot serialize event to json");
+        }
+        redisTemplate.convertAndSend(topic.getTopic(), json);
+    }
+}
--- src/main/java/school/faang/user_service/publisher/SkillOfferedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/SkillOfferedEventPublisher.java
@@ -1,29 +1,24 @@
 package school.faang.user_service.publisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.redis.SkillOfferEventDto;
-import school.faang.user_service.exception.SerializeJsonException;
 
 @Component
-@RequiredArgsConstructor
-public class SkillOfferedEventPublisher {
+public class SkillOfferedEventPublisher extends AbstractEventPublisher<SkillOfferEventDto> {
 
-    public final RedisTemplate<String, Object> redisTemplate;
-    public final ObjectMapper objectMapper;
     public final ChannelTopic topicSkillOffer;
 
+    @Autowired
+    public SkillOfferedEventPublisher(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper, ChannelTopic topicSkillOffer) {
+        super(redisTemplate, objectMapper);
+        this.topicSkillOffer = topicSkillOffer;
+    }
+
     public void publish(SkillOfferEventDto event) {
-        String json;
-        try {
-            json = objectMapper.writeValueAsString(event);
-        } catch (JsonProcessingException e) {
-            throw new SerializeJsonException("Failed to serialize event");
-        }
-        redisTemplate.convertAndSend(topicSkillOffer.getTopic(), json);
+        publishInTopic(topicSkillOffer, event);
     }
 }

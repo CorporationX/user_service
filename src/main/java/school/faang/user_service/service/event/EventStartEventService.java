a94a9519d49b84058625ebdb5c77f6278dc0e46f
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-08-30 21:50:30 -0700

    feature: BC-5648 small changes

---
+++ src/main/java/school/faang/user_service/service/event/EventStartEventService.java
@@ -0,0 +1,40 @@
+package school.faang.user_service.service.event;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.event.EventStartEventDto;
+import school.faang.user_service.entity.event.Event;
+import school.faang.user_service.mapper.event.EventStartEventMapper;
+import school.faang.user_service.publisher.JsonObjectMapper;
+import school.faang.user_service.repository.event.EventRepository;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Setter
+public class EventStartEventService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final JsonObjectMapper jsonObjectMapper;
+    private final EventStartEventMapper eventStartEventMapper;
+    private final EventRepository eventRepository;
+
+    @Value("${spring.data.redis.channels.event_start_channel.name}")
+    private String eventStartEventTopicName;
+
+    public void sendScheduledEvent(long eventId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Event with id %d has already been canceled", eventId)));
+        EventStartEventDto eventToSend = eventStartEventMapper.toDto(event);
+        String json = jsonObjectMapper.writeValueAsString(eventToSend);
+        redisTemplate.convertAndSend(eventStartEventTopicName, json);
+
+        log.info("Event start event sending with event id: {}, participants number: {}, has been sent",
+                eventToSend.getId(), eventToSend.getUserIds().size());
+    }
+}
---
+++ src/test/java/school/faang/user_service/service/event/EventStartEventServiceTest.java
@@ -0,0 +1,70 @@
+package school.faang.user_service.service.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import school.faang.user_service.dto.event.EventStartEventDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.event.Event;
+import school.faang.user_service.mapper.event.EventStartEventMapper;
+import school.faang.user_service.mapper.event.EventStartEventMapperImpl;
+import school.faang.user_service.publisher.JsonObjectMapper;
+import school.faang.user_service.repository.event.EventRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EventStartEventServiceTest {
+
+    @Mock
+    private RedisTemplate<String, Object> redisTemplate;
+    @Mock
+    private JsonObjectMapper jsonObjectMapper;
+    @Spy
+    private EventStartEventMapper eventStartEventMapper = new EventStartEventMapperImpl();
+    @Mock
+    private EventRepository eventRepository;
+
+    @InjectMocks
+    private EventStartEventService eventStartEventService;
+
+    @Test
+    void sendScheduledEventTest() {
+        eventStartEventService.setEventStartEventTopicName("event_start_channel");
+        EventStartEventDto eventStartEventDto = EventStartEventDto.builder()
+                .id(1)
+                .title("Halloween")
+                .userIds(List.of(1L, 2L))
+                .build();
+        User first = User.builder()
+                .id(1)
+                .build();
+        User second = User.builder()
+                .id(2)
+                .build();
+        Event event = Event.builder()
+                .id(1)
+                .title("Halloween")
+                .attendees(List.of(first, second))
+                .build();
+        String json = "{\"id\": 1, \"title\": \"Halloween\", \"userIds\": [1, 2]}";
+
+        when(eventRepository.findById(1L)).thenReturn(Optional.of(event));
+        when(jsonObjectMapper.writeValueAsString(eventStartEventDto)).thenReturn(json);
+
+        eventStartEventService.sendScheduledEvent(1);
+
+        verify(eventRepository).findById(1L);
+        verify(jsonObjectMapper).writeValueAsString(eventStartEventDto);
+        verify(redisTemplate).convertAndSend("event_start_channel", json);
+    }
+
+}

31bb6d065c8a8e521ac92dd410f3239a606033f9
Angie <angie@>
2023-09-07 00:36:47 +0300

    added edits

--- src/main/java/school/faang/user_service/exception/DeserializeJSONException.java
+++
@@ -1,7 +0,0 @@
-package school.faang.user_service.exception;
-
-public class DeserializeJSONException extends RuntimeException{
-    public DeserializeJSONException(String message){
-        super(message);
-    }
-}

3c58aa100dadc5ecf09f3821655d798c5ab98672
Basimov Rustam <sombra1nf@yandex.ru>
2023-09-08 17:14:13 +0500

    Исправил DTO

--- src/main/java/school/faang/user_service/dto/fundraising/FundRaisedEvent.java
+++ src/main/java/school/faang/user_service/dto/fundraising/FundRaisedEvent.java
@@ -1,15 +1,14 @@
 package school.faang.user_service.dto.fundraising;
 
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 public class FundRaisedEvent {
     private long donorId;
     private long projectId;
-    @Pattern(regexp = "[0-9]+")
-    private String amount;
+    private BigDecimal amount;
     private LocalDateTime timestamp;
 }

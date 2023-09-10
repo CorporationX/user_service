ce34990b379cfbef8a5743f9c90d76fe1a5f0546
adil <adil.200191@gmail.com>
2023-09-10 13:18:45 +0600

    Добавил публикатор события загрузки аватара пользователя

--- src/main/java/school/faang/user_service/service/UserProfilePicService.java
+++ src/main/java/school/faang/user_service/service/UserProfilePicService.java
@@ -2,14 +2,17 @@ package school.faang.user_service.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import school.faang.user_service.dto.redis.ProfilePicEventDto;
 import school.faang.user_service.dto.userProfilePic.AvatarFromAwsDto;
 import school.faang.user_service.dto.userProfilePic.UserProfilePicDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.UserProfilePic;
 import school.faang.user_service.exception.DataValidException;
+import school.faang.user_service.publisher.ProfilePicPublisher;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.util.FileStorageService;
 import school.faang.user_service.util.ImageService;
@@ -23,9 +26,16 @@ public class UserProfilePicService {
     private final UserRepository userRepository;
     private final FileStorageService fileStorageService;
     private final ImageService imageService;
+    private final ProfilePicPublisher profilePicPublisher;
+
+    @Value("${urls.base_dev}")
+    private String base_url;
+
+    @Value("${urls.avatar_url}")
+    private String avatar_url;
 
     @Transactional
-    public UserProfilePicDto upload(MultipartFile file, long userId) {
+    public UserProfilePicDto uploadWithPublishProfilePicEvent(MultipartFile file, long userId) {
         User user = userRepository.findById(userId).orElseThrow(() ->
                 new EntityNotFoundException("User with id " + userId + " is not found"));
 
@@ -41,6 +51,9 @@ public class UserProfilePicService {
 
         user.setUserProfilePic(userProfilePic);
         userRepository.save(user);
+
+        String link = base_url + avatar_url + userId;
+        profilePicPublisher.publishMessage(new ProfilePicEventDto(userId, link));
 
         return new UserProfilePicDto(userId, bigKey, smallKey);
     }

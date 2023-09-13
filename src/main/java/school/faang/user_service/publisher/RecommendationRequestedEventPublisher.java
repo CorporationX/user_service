82328c7b5978e5de43af7120cc37ee8fe9b79b98
Pashkevich <ppavel012100@gmail.com>
2023-09-13 17:37:21 +0200

    rework

--- src/main/java/school/faang/user_service/publisher/RecommendationRequestedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/RecommendationRequestedEventPublisher.java
@@ -2,23 +2,21 @@ package school.faang.user_service.publisher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.redis.EventRecommendationRequestDto;
 
 @Component
 public class RecommendationRequestedEventPublisher extends AbstractEventPublisher<EventRecommendationRequestDto> {
 
-    private final ChannelTopic topicRecommendationRequestedEvent;
-
     @Autowired
-    public RecommendationRequestedEventPublisher(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper, ChannelTopic topicRecommendationRequestedEvent) {
-        super(redisTemplate, objectMapper);
-        this.topicRecommendationRequestedEvent = topicRecommendationRequestedEvent;
+    public RecommendationRequestedEventPublisher(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper,
+                                                 @Value("${spring.data.redis.channels.recommendation-requested-event-channel}") String topicRecommendationRequestedEvent) {
+        super(redisTemplate, objectMapper, topicRecommendationRequestedEvent);
     }
 
     public void publish(EventRecommendationRequestDto event) {
-        publishInTopic(topicRecommendationRequestedEvent, event);
+        publishInTopic(event);
     }
 }

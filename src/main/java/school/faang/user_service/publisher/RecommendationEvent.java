92a03113abc8178225e89e1e95bead47c2d9b134
Pashkevich <ppavel012100@gmail.com>
2023-09-13 17:29:57 +0200

    rework

--- src/main/java/school/faang/user_service/publisher/RecommendationEvent.java
+++ src/main/java/school/faang/user_service/publisher/RecommendationEvent.java
@@ -1,8 +1,8 @@
 package school.faang.user_service.publisher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.redis.RecommendationEventDto;
 import school.faang.user_service.entity.recommendation.Recommendation;
@@ -10,17 +10,16 @@ import school.faang.user_service.mapper.RecommendationMapper;
 
 @Component
 public class RecommendationEvent extends AbstractEventPublisher<RecommendationEventDto> {
-    private final ChannelTopic topicRecommendation;
     private final RecommendationMapper recommendationMapper;
 
-    public RecommendationEvent(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper, ChannelTopic topicRecommendation, RecommendationMapper recommendationMapper) {
-        super(redisTemplate, objectMapper);
-        this.topicRecommendation = topicRecommendation;
+    public RecommendationEvent(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper,
+                               @Value("${spring.data.redis.channels.recommendation_channel}") String topicRecommendation, RecommendationMapper recommendationMapper) {
+        super(redisTemplate, objectMapper, topicRecommendation);
         this.recommendationMapper = recommendationMapper;
     }
 
     public void publish(Recommendation recommendation) {
         RecommendationEventDto event = recommendationMapper.toRecommendationEventDto(recommendation);
-        publishInTopic(topicRecommendation, event);
+        publishInTopic(event);
     }
 }

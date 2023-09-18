48764e448618b3cbcbf4efd80343674d67c6707c
Pashkevich <ppavel012100@gmail.com>
2023-09-18 17:22:30 +0200

    add content for task 4189

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -3,10 +3,13 @@ package school.faang.user_service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class UserDto {
     private long id;
     private String username;
     private String email;
+    private List<Long> followedUserIds;
 }
--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package school.faang.user_service.mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
@@ -13,7 +15,16 @@ public interface UserMapper {
 
     User toEntity(UserDto userDto);
 
+    @Mapping(target = "followedUserIds", source = "followers", qualifiedByName = "usersToIds")
     UserDto toDto(User user);
 
     List<UserDto> toDto(List<User> users);
+
+    @Named("usersToIds")
+    default List<Long> mapUsersToIds(List<User> value) {
+        if (value == null) {
+            return null;
+        }
+        return value.stream().map(User::getId).toList();
+    }
 }

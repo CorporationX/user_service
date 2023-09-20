32b71896e8ad3f72e99c66886718dc1760db4e8b
Stepan Palkov <step.palkov@gmail.com>
2023-09-20 14:51:52 +0300

    complete

--- src/main/java/school/faang/user_service/publisher/MentorshipRequestedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/MentorshipRequestedEventPublisher.java
@@ -3,7 +3,6 @@ package school.faang.user_service.publisher;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.mentorship.MentorshipRequestEvent;
 

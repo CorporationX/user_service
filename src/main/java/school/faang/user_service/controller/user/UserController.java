a6f13e8c2332db9e51e5348c478183f190165c80
auslandeer <b.mukhtarkhanov>
2023-10-01 17:18:38 +0600

    решил конфликты

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -3,10 +3,11 @@ package school.faang.user_service.controller.user;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.service.user.UserService;
+
+import java.io.IOException;
 
 @Tag(name = "Управление пользователями")
 @RestController
@@ -24,6 +25,7 @@ public class UserController {
     public void createUser(@RequestBody UserDto userDto) throws IOException {
         userService.createUser(userDto);
     }
+
     @Operation(summary = "Найти пользователя по идентификатору")
     @GetMapping("/users/{id}")
     public UserDto getById(@PathVariable Long id) {

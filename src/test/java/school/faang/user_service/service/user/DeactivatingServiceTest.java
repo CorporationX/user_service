7af2c7a9d50c9462f3c8dbf68eb3b44f25114f69
PavelBel <ilpavel94@gmail.com>
2023-10-04 13:33:57 +0200

    Task 3825: Added tests

--- src/test/java/school/faang/user_service/service/user/DeactivatingServiceTest.java
+++ src/test/java/school/faang/user_service/service/user/DeactivatingServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.ResponseDeactivateDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.exception.EntityNotFoundException;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.event.EventRepository;
 import school.faang.user_service.repository.goal.GoalRepository;
@@ -17,10 +18,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DeactivatingServiceTest {
@@ -31,16 +30,15 @@ public class DeactivatingServiceTest {
     @Mock
     private GoalRepository goalRepository;
     @Mock
-    private MentorshipService mentorsService;
+    private MentorshipService mentorshipService;
     @Mock
     private EventRepository eventRepository;
 
 
     @Test
     void testDeactivateUserWhenUserIsActive() {
-        long userId = 23L;
-
-        User user = User.builder().id(23L).username("Sasha").active(true).build();
+        Long userId = 23L;
+        User user = User.builder().id(userId).username("Sasha").active(true).build();
         User user2 = User.builder().id(32L).username("Pavel").active(false).build();
 
         List<User> users = List.of(user);
@@ -51,7 +49,6 @@ public class DeactivatingServiceTest {
 
         Stream<Goal> goalStream = Stream.of(goal1, goal2);
 
-
         when(userRepository.findById(userId)).thenReturn(Optional.of(user));
         when(goalRepository.findGoalsByUserId(userId)).thenReturn(goalStream);
 
@@ -59,6 +56,31 @@ public class DeactivatingServiceTest {
 
         assertFalse(user.isActive());
         verify(userRepository).save(user);
+        verify(eventRepository).deleteByOwnerId(userId);
         assertEquals("User was successfully deactivated", response.getMessage());
+    }
+
+    @Test
+    void testDeactivateUserWhenUserNotFound() {
+        Long userId = 1L;
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> userService.deactivateUser(userId));
+        verifyNoInteractions(goalRepository, eventRepository, mentorshipService);
+    }
+
+    @Test
+    void testDeactivateUserWhenUserIsAlreadyDeactivated() {
+        Long userId = 1L;
+        User user = User.builder().id(userId).username("Pavel").active(false).build();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        ResponseDeactivateDto response = userService.deactivateUser(userId);
+
+        assertFalse(user.isActive());
+        verifyNoInteractions(goalRepository, eventRepository, mentorshipService);
+        assertEquals("User is already deactivated", response.getMessage());
     }
 }

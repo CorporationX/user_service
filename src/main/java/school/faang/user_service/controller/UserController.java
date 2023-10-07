5dda2012f70b8cdddf5586aec506ad7d480f755d
InnMish <inn.mishustina@gmail.com>
2023-10-07 13:34:19 +0300

    conflicts

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.config.context.UserContext;
+import school.faang.user_service.dto.ResponseDeactivateDto;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserProfilePicDto;
 import school.faang.user_service.service.user.UserService;

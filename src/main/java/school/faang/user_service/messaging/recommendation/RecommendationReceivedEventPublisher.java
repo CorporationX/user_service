59b49980bca6174a893973c3f3572c8b20a2662b
Azat <azat.takhautdinov@gmail.com>
2023-10-14 16:32:48 +0500

    Маленькие поправки

--- src/main/java/school/faang/user_service/messaging/recommendation/RecommendationReceivedEventPublisher.java
+++ src/main/java/school/faang/user_service/messaging/recommendation/RecommendationReceivedEventPublisher.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.messaging.recommendation;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import school.faang.user_service.util.JsonMapper;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class RecommendationReceivedEventPublisher {
     private final ChannelTopic recommendationReceivedChannel;
     private final RedisTemplate<String, Object> redisTemplate;
@@ -17,5 +19,6 @@ public class RecommendationReceivedEventPublisher {
     public void publish(RecommendationReceivedEvent event) {
         jsonMapper.toJson(event)
                 .ifPresent(json -> redisTemplate.convertAndSend(recommendationReceivedChannel.getTopic(), json));
+        log.info(event.getClass() + " " + "send");
     }
 }

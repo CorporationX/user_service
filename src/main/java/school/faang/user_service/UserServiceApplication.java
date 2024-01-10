8b6995f7bdf6bfc9f1d6710790dea235ecc9dcf5
Vlad Meniailov <vlad.meniailov@yandex.ru>
2024-01-11 00:27:44 +0300

    First commit BJS2-1435

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -20,6 +20,7 @@ public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+        System.out.println("First commit");
     }
 
     @Bean

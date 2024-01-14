80dcd223d33c5eed5da486e1d9e49ad997a72c38
Leilorr <148541216+Leilorr@users.noreply.github.com>
2024-01-14 23:49:59 +0300

    Получить все рекомендации пользователя Условия задачи Реализовать возможность получения всех рекомендаций, которые были даны одному пользователю.

---
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -0,0 +1,21 @@
+package school.faang.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.recommendation.RecommendationDto;
+import school.faang.user_service.service.RecommendationService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class RecommendationController {
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/recommendations/user/{receiverId}")
+    public List<RecommendationDto> getAllUserRecommendations(@PathVariable long receiverId){
+        return recommendationService.getAllUserRecommendations(receiverId);
+    }
+}

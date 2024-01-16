b2f626ea4b6f7eae46b791c332327e08a9fcb30b
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-01-16 07:58:45 +0300

    added swagger and fix test

--- src/main/java/school/faang/user_service/controller/PremiumController.java
+++ src/main/java/school/faang/user_service/controller/PremiumController.java
@@ -1,5 +1,9 @@
 package school.faang.user_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.config.context.UserContext;
@@ -7,6 +11,7 @@ import school.faang.user_service.dto.premium.PremiumDto;
 import school.faang.user_service.dto.premium.PremiumPeriod;
 import school.faang.user_service.service.PremiumService;
 
+@Tag(name = "Премиум контролер")
 @RestController
 @RequestMapping("/api/premium")
 @RequiredArgsConstructor
@@ -14,6 +19,11 @@ public class PremiumController {
     private final PremiumService premiumService;
     private final UserContext userContext;
 
+    @Operation(
+            summary = "Покупка премиума",
+            description = "Позволяет купить премиум по тарифному плану",
+            parameters = {@Parameter(in = ParameterIn.HEADER, name = "x-user-id", description = "id пользователя", required = true)}
+    )
     @PostMapping("/buy")
     public PremiumDto buyPremium(@RequestParam("day") int day) {
         PremiumPeriod period = PremiumPeriod.fromDays(day);
--- src/test/java/school/faang/user_service/controller/PremiumControllerTest.java
+++ src/test/java/school/faang/user_service/controller/PremiumControllerTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.dto.premium.PremiumDto;
 import school.faang.user_service.dto.premium.PremiumPeriod;
 import school.faang.user_service.service.PremiumService;
@@ -21,6 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 class PremiumControllerTest {
     private MockMvc mockMvc;
+
+    @Mock
+    private UserContext userContext;
 
     @Mock
     private PremiumService premiumService;
@@ -37,8 +42,10 @@ class PremiumControllerTest {
     public void buyPremium_whenValidBuyPremium_thenReturnsPremiumDto() throws Exception {
         PremiumDto premiumDto = new PremiumDto(1L, 1L);
         when(premiumService.buyPremium(1L, PremiumPeriod.ONE_MONTH)).thenReturn(premiumDto);
+        when(userContext.getUserId()).thenReturn(1L);
 
-        mockMvc.perform(post("/api/premium/buy/{id}", 1L)
+        mockMvc.perform(post("/api/premium/buy")
+                        .header("id", 1)
                         .param("day", "30"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(1)))

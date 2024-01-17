3d2c83893443b2dd77a52aba0ec16083679e35e9
mirayoo <nikita.kopot@yandex.ru>
2024-01-17 23:23:57 +0300

    BJS2-1440: move controller, service and mapper to corresponding packages

---
+++ src/main/java/school/faang/user_service/controller/skill/SkillController.java
@@ -0,0 +1,33 @@
+package school.faang.user_service.controller.skill;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.skill.SkillDto;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.service.skill.SkillService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class SkillController {
+    private final SkillService skillService;
+
+    @PostMapping(value = "/skill")
+    public SkillDto create (SkillDto skill) throws DataValidationException {
+        validateSkill(skill);
+
+        return skillService.create(skill);
+    }
+
+    public List<SkillDto> getUserSkills(long userId) {
+        return skillService.getUserSkills(userId);
+    }
+
+    private void validateSkill (SkillDto skill) {
+        if (skill.getTitle() == null || skill.getTitle().isBlank()) {
+            throw new DataValidationException("Invalid skill name.");
+        }
+    }
+}
---
+++ src/main/java/school/faang/user_service/service/skill/SkillService.java
@@ -0,0 +1,55 @@
+package school.faang.user_service.service.skill;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.skill.SkillDto;
+import school.faang.user_service.entity.Skill;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.mapper.skil.SkillMapper;
+import school.faang.user_service.repository.SkillRepository;
+import school.faang.user_service.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SkillService {
+
+    private final SkillMapper skillMapper;
+    private final SkillRepository skillRepository;
+    private final UserRepository userRepository;
+
+    public SkillDto create (SkillDto skill) {
+        boolean skillExistsInDB = skillRepository.existsByTitle(skill.getTitle());
+
+        if (skillExistsInDB) {
+            throw new DataValidationException("Skill with name " + skill.getTitle() + " already exists in database.");
+        }
+
+        Skill skillEntity = skillMapper.toEntity(skill);
+        List<User> users = userRepository.findAllById(skill.getUserIds());
+        skillEntity.setUsers(users);
+
+        skillEntity = skillRepository.save(skillEntity);
+
+        return skillMapper.toDto(skillEntity);
+    }
+
+    public List<SkillDto> getUserSkills(long userId) {
+
+        List<Skill> skillEntityList = skillRepository.findAllByUserId(userId);
+
+        if (skillEntityList == null) {
+            throw new DataValidationException("No skills found by user id " + userId);
+        }
+
+        for (Skill skill : skillEntityList) {
+            SkillDto skillDto = skillMapper.toDto(skill);
+            List<User> users = userRepository.findAllById(skillDto.getUserIds());
+            skill.setUsers(users);
+        }
+
+        return skillMapper.listToDto(skillEntityList);
+    }
+}

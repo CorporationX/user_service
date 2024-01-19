68b1727783f38b4eaf3c37b73b170df54bd9fa95
fd_shell <dariuskpp@gmail.com>
2024-01-19 11:25:04 +0300

    Remove redundant check, remove DataValidationException.java

--- src/main/java/school/faang/user_service/service/goal/GoalService.java
+++ src/main/java/school/faang/user_service/service/goal/GoalService.java
@@ -2,7 +2,6 @@ package school.faang.user_service.service.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.goal.GoalRepository;
 
 
@@ -13,9 +12,6 @@ public class GoalService {
 
 
     public void deleteGoal(long goalId) {
-        if(!goalRepository.existsById(goalId)){
-            throw new DataValidationException("Такой цели нет");
-        }
         goalRepository.deleteById(goalId);
     }
 }
--- src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
+++ src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
@@ -1,14 +1,11 @@
 package school.faang.user_service.service.goal;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.goal.GoalRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,17 +22,7 @@ class GoalServiceTest {
         //Проверить, что вызывается метод delete у репозитория (если объект найден)
     void testShouldDelete() {
         userId = 1;
-        Mockito.when(goalRepository.existsById(userId)).thenReturn(true);
         goalService.deleteGoal(userId);
         Mockito.verify(goalRepository, Mockito.times(1)).deleteById(userId);
-    }
-
-    @Test
-    void deleteUncorrectedGoalIdTest(){
-        userId = 1;
-        Mockito.when(goalRepository.existsById(userId)).thenReturn(false);
-        DataValidationException dataValidationException = Assert.assertThrows(DataValidationException.class,
-                () -> goalService.deleteGoal(userId));
-        Assertions.assertEquals("Такой цели нет", dataValidationException.getMessage());
     }
 }

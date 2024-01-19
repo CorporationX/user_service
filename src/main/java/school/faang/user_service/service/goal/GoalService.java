5d827b6869cea18360d543e3f8de65cb9a4d6345
Rikaf <fakir207@mail.ru>
2024-01-19 18:29:55 +0500

    1561 delete controllerTest and fix

--- src/main/java/school/faang/user_service/service/goal/GoalService.java
+++ src/main/java/school/faang/user_service/service/goal/GoalService.java
@@ -2,7 +2,6 @@ package school.faang.user_service.service.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.goal.GoalRepository;
 
 

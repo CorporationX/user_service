1ed015394201bce5e5cf705c4b5b4110982af160
Rikaf <fakir207@mail.ru>
2024-01-22 04:44:20 +0500

    1555 fix Added DataValidationException, added method createGoal in GoalController!

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -2,6 +2,8 @@ package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.exceptions.DataValidationException;
 import school.faang.user_service.service.goal.GoalService;
 
 @Controller
@@ -11,10 +13,20 @@ public class GoalController {
     private final GoalService goalService;
 
     public void deleteGoal(Long goalId) {
-        if (goalId != null && goalId > 0) {
-            goalService.deleteGoal(goalId);
-        } else {
-            throw new IllegalArgumentException("Invalid ID: " + goalId);
+        if (goalId == null || goalId <= 0) {
+            throw new DataValidationException("Invalid ID: " + goalId);
         }
+        goalService.deleteGoal(goalId);
+
+    }
+
+    public void createGoal(Long userId, Goal goal) {
+        if (userId == null || userId <= 0) {
+            throw new DataValidationException("Invalid ID: " + userId);
+        }
+        if (goal == null || goal.getTitle() == null || goal.getTitle().isEmpty() || goal.getTitle().isBlank()) {
+            throw new DataValidationException("goal cannot be null or empty");
+        }
+        goalService.createGoal(userId, goal);
     }
 }

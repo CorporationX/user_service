5e5c0b0d89a9b8b4fc902249eca931f5c10c9f95
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2024-01-23 17:54:09 +0300

    fix_3

--- src/main/java/school/faang/user_service/service/goal/GoalService.java
+++ src/main/java/school/faang/user_service/service/goal/GoalService.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.entity.goal.Goal;
 import school.faang.user_service.mapper.goal.GoalMapper;
-import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.service.skill.SkillService;
+import school.faang.user_service.service.user.UserService;
 import school.faang.user_service.validator.GoalValidator;
 
 import java.util.List;
@@ -21,7 +21,7 @@ import java.util.List;
 public class GoalService {
     private final GoalValidator goalValidator;
     private final GoalRepository goalRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final SkillService skillService;
     private final GoalMapper goalMapper;
 
@@ -29,7 +29,7 @@ public class GoalService {
     public void createGoal(Long userId, GoalDto goalDto) {
         goalValidator.validateForCreate(userId, goalDto);
         Goal goal = goalMapper.toEntity(goalDto);
-        goal.setUsers(List.of(userRepository.findById(userId).orElseThrow())); // По хорошему надо через UserService (там же будет обработка исключения)
+        goal.setUsers(List.of(userService.findById(userId)));
         goal.setSkillsToAchieve(goalDto.getSkillIds().stream().map(skillService::findById).toList());
         goalRepository.save(goal);
     }
--- src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
+++ src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
@@ -7,15 +7,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.entity.Skill;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.goal.Goal;
 import school.faang.user_service.mapper.goal.GoalMapper;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.service.skill.SkillService;
+import school.faang.user_service.service.user.UserService;
 import school.faang.user_service.validator.GoalValidator;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,6 +27,8 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class GoalServiceTest {
+    @Mock
+    private UserService userService;
     @Mock
     private GoalValidator goalValidator;
     @Mock
@@ -48,15 +51,21 @@ public class GoalServiceTest {
         Skill skill_2 = new Skill();
         skill_2.setId(2L);
         goal.setSkillsToAchieve(List.of(skill_1, skill_2));
+
         GoalDto goalDto = new GoalDto();
         goalDto.setSkillIds(List.of(1L, 2L));
+
         Long userId = 1L;
+        User user = new User();
+        user.setId(1L);
 
         when(goalMapper.toEntity(goalDto)).thenReturn(goal);
+        when(userService.findById(userId)).thenReturn(user);
+        when(skillService.findById(1L)).thenReturn(skill_1);
+        when(skillService.findById(2L)).thenReturn(skill_2);
 
         goalService.createGoal(userId, goalDto);
 
-        verify(goalRepository, times(1)).create(goal.getTitle(), goal.getDescription(), userId);
-        verify(goalRepository, times(2)).addSkillToGoal(anyLong(), anyLong());
+        verify(goalRepository, times(1)).save(goal);
     }
 }

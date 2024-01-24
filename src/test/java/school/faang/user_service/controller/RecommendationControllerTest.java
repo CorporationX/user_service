d9e6a27457bf592fda0366ff2ecd934fa4bad928
Leilorr <148541216+Leilorr@users.noreply.github.com>
2024-01-25 01:35:01 +0300

    RecommendationControllerTest.java

--- src/test/java/school/faang/user_service/controller/RecommendationControllerTest.java
+++ src/test/java/school/faang/user_service/controller/RecommendationControllerTest.java
@@ -12,9 +12,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.service.RecommendationService;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,39 +19,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 class RecommendationControllerTest {
 
-    private MockMvc mockMvc;
-
     @Mock
     private RecommendationService recommendationService;
 
     @InjectMocks
     private RecommendationController recommendationController;
-  
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(recommendationController).build();
-    }
 
     @Test
-    public void testMethod() {
+    public void testGetAllGivenRecommendations() {
     recommendationController.getAllGivenRecommendations(1L);
         verify(recommendationService, times(1)).getAllGivenRecommendations(1L);
-    }
-
-    @Test
-    void getAllUserRecommendations() throws Exception {
-        long receiverId = 1L;
-        List<RecommendationDto> recommendations = Arrays.asList(
-                new RecommendationDto(receiverId),
-                new RecommendationDto(receiverId)
-        );
-        when(recommendationService.getAllUserRecommendations(receiverId)).thenReturn(recommendations);
-        mockMvc.perform(get("/recommendations/user/{receiverId}", receiverId))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.length()").value(recommendations.size()))
-                .andExpect(jsonPath("$[0].receiverId").value(receiverId))
-                .andExpect(jsonPath("$[1].receiverId").value(receiverId));
     }
 }

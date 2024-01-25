33e142acdce9b62cf765b00d0f84c50074add010
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-01-25 21:25:43 +0300

    fix name class

--- src/test/java/school/faang/user_service/validator/PremiumValidatorTest.java
+++ src/test/java/school/faang/user_service/validator/PremiumValidatorTest.java
@@ -12,17 +12,17 @@ import school.faang.user_service.repository.premium.PremiumRepository;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ValidatorPremiumTest {
+class PremiumValidatorTest {
 
     @Mock
     private PremiumRepository premiumRepository;
 
-    private ValidatorPremium validatorPremium;
+    private PremiumValidator premiumValidator;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        validatorPremium = new ValidatorPremium(premiumRepository);
+        premiumValidator = new PremiumValidator(premiumRepository);
     }
 
     @Test
@@ -30,7 +30,7 @@ class ValidatorPremiumTest {
         long userId = 1L;
         when(premiumRepository.existsByUserId(userId)).thenReturn(false);
 
-        assertDoesNotThrow(() -> validatorPremium.validateUserId(userId));
+        assertDoesNotThrow(() -> premiumValidator.validateUserId(userId));
     }
 
     @Test
@@ -38,7 +38,7 @@ class ValidatorPremiumTest {
         long userId = 1L;
         when(premiumRepository.existsByUserId(userId)).thenReturn(true);
 
-        DataValidationException dataValidationException = assertThrows(DataValidationException.class, () -> validatorPremium.validateUserId(userId));
+        DataValidationException dataValidationException = assertThrows(DataValidationException.class, () -> premiumValidator.validateUserId(userId));
 
         assertEquals("Пользователь уже имеет премиум подписку", dataValidationException.getMessage());
     }
@@ -47,14 +47,14 @@ class ValidatorPremiumTest {
     void validateResponseStatus_whenPaymentSuccess_thenDoNothing() {
         PaymentResponse paymentResponse = new PaymentResponse(PaymentStatus.SUCCESS, 1, 1, null, null, null);
 
-        assertDoesNotThrow(() -> validatorPremium.validateResponseStatus(paymentResponse));
+        assertDoesNotThrow(() -> premiumValidator.validateResponseStatus(paymentResponse));
     }
 
     @Test
     void validateResponseStatus_whenPaymentFailure_thenThrowException() {
         PaymentResponse paymentResponse = new PaymentResponse(PaymentStatus.FAILURE, 1, 1, null, null, null);
 
-        DataValidationException dataValidationException = assertThrows(DataValidationException.class, () -> validatorPremium.validateResponseStatus(paymentResponse));
+        DataValidationException dataValidationException = assertThrows(DataValidationException.class, () -> premiumValidator.validateResponseStatus(paymentResponse));
 
         assertEquals("Ошибка платежа", dataValidationException.getMessage());
 

dfd97a415caaeeb21631a1ae83f2bacf83bf51b8
fd_shell <dariuskpp@gmail.com>
2024-01-25 12:31:20 +0300

    Fix merge from 1565

--- src/main/java/school/faang/user_service/dto/goal/GoalDto.java
+++ src/main/java/school/faang/user_service/dto/goal/GoalDto.java
@@ -2,6 +2,7 @@ package school.faang.user_service.dto.goal;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import school.faang.user_service.entity.goal.GoalStatus;
 import java.util.List;
 

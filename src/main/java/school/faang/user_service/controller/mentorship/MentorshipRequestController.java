3a1c3c7307d74f950b91e5da5cb87bf4b4094fed
javaDevArtem <artemjdev@gmail.com>
2024-01-25 08:38:51 -0500

    BJS2-1488 review fixes

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
@@ -4,6 +4,7 @@ package school.faang.user_service.controller.mentorship;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
+import school.faang.user_service.exception.mentorship.DataValidationException;
 import school.faang.user_service.service.mentorship.MentorshipRequestService;
 
 @Component
@@ -13,6 +14,10 @@ public class MentorshipRequestController {
     private final MentorshipRequestService mentorshipRequestService;
 
     private void requestMentorship(MentorshipRequestDto mentorshipRequestDto) {
+        if (mentorshipRequestDto.getId() == null || mentorshipRequestDto.getRequester() == null
+                || mentorshipRequestDto.getReceiver() == null || mentorshipRequestDto.getDescription() == null) {
+            throw new DataValidationException("Incorrect data");
+        }
         mentorshipRequestService.requestMentorship(mentorshipRequestDto);
     }
 }

5c1424f89d8767c994f205e93004dac075e37960
FD_Shell <dariuskpp@gmail.com>
2024-01-28 17:43:40 +0300

    Fix annotations

--- src/main/java/school/faang/user_service/entity/MentorshipRequest.java
+++ src/main/java/school/faang/user_service/entity/MentorshipRequest.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.entity;
 
 import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

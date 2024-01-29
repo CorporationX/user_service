15b147ef6b30a6a539865c0f584802305c29cf91
Ackerman995 <rauanture@gmail.com>
2024-01-29 20:20:56 +0300

    add Swagger

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
@@ -1,16 +1,18 @@
 package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.MentorshipRequestDto;
 import school.faang.user_service.service.mentorship.MentorshipRequestService;
 @Component
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/mentorship")
 public class MentorshipRequestController {
     private final MentorshipRequestService mentorshipRequestService;
-
-    public MentorshipRequestDto requestMentorship(MentorshipRequestDto mentorshipRequestDto) {
+    @PostMapping("/requestMentorship")
+    public MentorshipRequestDto requestMentorship(@RequestBody MentorshipRequestDto mentorshipRequestDto) {
         return mentorshipRequestService.requestMentorship(mentorshipRequestDto);
     }
 }

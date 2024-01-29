b48a0322b70341b2304a2b4f24e3cbd205bc292f
Ruslan Latyshev <latyshevruslan@ya.ru>
2024-01-29 23:15:21 +0300

    create GlobalExceptionHandler

--- src/main/java/school/faang/user_service/handlers/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/handlers/GlobalExceptionHandler.java
@@ -1,35 +1,65 @@
 package school.faang.user_service.handlers;
 
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.ValidationException;
+import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.webjars.NotFoundException;
+import school.faang.user_service.exception.DataValidationException;
 
-@ControllerAdvice
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler
-    public ResponseEntity<ControllerError> handleIllegalArgumentException(IllegalArgumentException ex) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        return ResponseEntity
-                .status(httpStatus)
-                .body(new ControllerError(httpStatus.value(), ex.getMessage()));
+
+    @ExceptionHandler(DataValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorMessage> handleDataValidationException(DataValidationException e) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                e.getMessage()
+        );
+        return new ResponseEntity<>(message,  HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ControllerError> handleEntityNotFoundException(EntityNotFoundException ex) {
-        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
-        return ResponseEntity
-                .status(httpStatus)
-                .body(new ControllerError(httpStatus.value(), ex.getMessage()));
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach((ex) -> {
+            String fieldName = ex.getField();
+            String errorMessage = ex.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ControllerError> handleConstraintViolationException(ValidationException ex) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        return ResponseEntity
-                .status(httpStatus)
-                .body(new ControllerError(httpStatus.value(), ex.getMessage()));
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException e) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                new Date(),
+                e.getMessage()
+        );
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ResponseEntity<Object> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.METHOD_NOT_ALLOWED.value(),
+                new Date(),
+                e.getMessage()
+        );
+        return new ResponseEntity<>(message, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }

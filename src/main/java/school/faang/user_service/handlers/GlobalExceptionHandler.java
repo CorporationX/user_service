b5a87c94b253b1b2cc5d20b4bf90bc357aed2c49
Ruslan Latyshev <latyshevruslan@ya.ru>
2024-02-04 19:00:44 +0300

    refactor handler

--- src/main/java/school/faang/user_service/handlers/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/handlers/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,7 +16,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataValidationException.class)

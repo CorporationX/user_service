8d0d31f1696ed61b140603537ba1d10fadf6e451
Nikita <nikita.kopot@yandex.ru>
2024-02-04 22:19:42 +0300

    BJS2-1402: update SkillController create method, add tests for SkillController methods

--- src/main/java/school/faang/user_service/controller/skill/SkillController.java
+++ src/main/java/school/faang/user_service/controller/skill/SkillController.java
@@ -1,9 +1,12 @@
 package school.faang.user_service.controller.skill;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
 import school.faang.user_service.dto.skill.SkillDto;
@@ -18,7 +21,8 @@ public class SkillController {
     private final SkillService skillService;
 
     @PostMapping("/skill")
-    public SkillDto create (SkillDto skill) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public SkillDto create (@RequestBody SkillDto skill) {
         validateSkill(skill.getTitle());
 
         return skillService.create(skill);

7765a22bc3ec59ef64aca19bbc07bd4c45ea7959
fd_shell <dariuskpp@gmail.com>
2024-02-09 19:12:28 +0300

    Add cleanupService method

--- src/main/java/school/faang/user_service/service/PremiumCleanupService.java
+++ src/main/java/school/faang/user_service/service/PremiumCleanupService.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.repository.premium.PremiumRepository;
@@ -8,15 +9,28 @@ import school.faang.user_service.entity.premium.Premium;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
 public class PremiumCleanupService {
     private final PremiumRepository premiumRepository;
+    @Value("${premium_cleanup.premium_batch_size}")
+    private int premiumBatchSize;
 
     @Scheduled(cron = "${premium_cleanup.time}", zone = "${premium_cleanup.zone}")
+
     public void cleanExpiredPremiums() {
         List<Premium> expiredPremiums = premiumRepository.findAllByEndDateBefore(LocalDateTime.now());
+        if (expiredPremiums.size() > premiumBatchSize) {
+            effectiveCleanExpiredPremiums(expiredPremiums);
+        }
         premiumRepository.deleteAll(expiredPremiums);
+    }
+
+    private void effectiveCleanExpiredPremiums(List<Premium> expiredPremiums) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
     }
 }

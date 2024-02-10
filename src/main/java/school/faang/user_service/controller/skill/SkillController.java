c94d4c4c58f52eddb8f576673440e9c5770577ce
Nikita <nikita.kopot@yandex.ru>
2024-02-11 01:34:29 +0300

    BJS2-1402: add RequestMapping

--- src/main/java/school/faang/user_service/controller/skill/SkillController.java
+++ src/main/java/school/faang/user_service/controller/skill/SkillController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class SkillController {
     private final SkillService skillService;
 

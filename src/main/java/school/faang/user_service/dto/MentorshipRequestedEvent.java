7416eb0d43417ff9e4b25e0f55452373b3c54ff0
fd_shell <dariuskpp@gmail.com>
2024-02-15 11:58:08 +0300

    Create redis mentorshiprequest publisher

---
+++ src/main/java/school/faang/user_service/dto/MentorshipRequestedEvent.java
@@ -0,0 +1,16 @@
+package school.faang.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MentorshipRequestedEvent {
+    private long requesterId;
+    private long receiverId;
+    private LocalDateTime timestamp;
+}

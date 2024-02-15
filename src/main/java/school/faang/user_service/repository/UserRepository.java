c5ebf9e5a21bde33e61a9841d2ce534024724c6c
phoen1x <106927036+vladmeniailov@users.noreply.github.com>
2024-02-15 23:56:00 +0300

    Repair scheduler and fixed SQL query.

--- src/main/java/school/faang/user_service/repository/UserRepository.java
+++ src/main/java/school/faang/user_service/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.entity.User;
 
 import java.time.LocalDate;
@@ -29,11 +28,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
             """)
     Stream<User> findPremiumUsers();
 
-    @Transactional
     @Modifying
     @Query(nativeQuery = true, value = """
             DELETE FROM users u
-            WHERE u.active = false AND u.updatedAt < ?1
+            WHERE u.active = false AND u.updated_at < ?1
             """)
     void deleteAllInactiveUsersAndUpdatedAtOverMonths(LocalDate time);
 }

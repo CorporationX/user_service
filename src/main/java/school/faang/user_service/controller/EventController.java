3b9cc715aabd5afb86bb943c876dc645c0c3ccc7
phoen1x <106927036+vladmeniailov@users.noreply.github.com>
2024-02-18 16:23:00 +0300

    Repair scheduler and fixed SQL query.

--- src/main/java/school/faang/user_service/controller/EventController.java
+++ src/main/java/school/faang/user_service/controller/EventController.java
@@ -1,7 +1,14 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.event.EventFilterDto;
 import school.faang.user_service.service.EventService;
@@ -16,7 +23,7 @@ public class EventController {
     private final EventService eventService;
     private final EventValidator eventValidator;
 
-    @GetMapping("/byFilter/")
+    @GetMapping("/filter")
     public List<EventDto> getEventsByFilter(@RequestBody EventFilterDto filterDto) {
         eventValidator.checkFilterNotNull(filterDto);
         return eventService.getEventsByFilter(filterDto);
@@ -28,7 +35,7 @@ public class EventController {
         return eventService.create(eventDto);
     }
 
-    @GetMapping("/getAll/{userId}")
+    @GetMapping("/owned/{userId}")
     public List<EventDto> getOwnedEvents(@PathVariable long userId) {
         return eventService.getOwnedEvents(userId);
     }

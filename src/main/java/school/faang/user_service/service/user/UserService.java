29574228df8c4f24c3ba09d9819508cf2727866b
Edisolt Khatsiev <Edisolt Khatsiev>
2024-02-25 08:08:41 +0300

    @Transactional

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.entity.User;
@@ -21,11 +22,13 @@ public class UserService {
     private final UserMapper userMapper;
     private final CsvOfPeopleToUserParser csvParser;
 
+    @Transactional(readOnly = true)
     public User getExistingUserById(long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("User with id = " + id + " is not found in database"));
     }
 
+    @Transactional
     public List<UserDto> generateUsersFromCsv(MultipartFile csvFile) throws IOException {
         List<User> savedUsers = csvParser.parse(csvFile);
         userRepository.saveAll(savedUsers);

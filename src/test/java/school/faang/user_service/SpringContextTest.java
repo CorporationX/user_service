77d0631208cf1e05dc60d2948294829323a1ac55
Marina-Zu <2311.mv@gmail.com>
2024-02-27 00:47:22 +0300

    Добавить тест на проверку того, что Spring Context успешно поднимается

---
+++ src/test/java/school/faang/user_service/SpringContextTest.java
@@ -0,0 +1,16 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringContextTest {
+
+    @Test
+    public void contextLoads() {
+        // Проверяем, что контекст Spring успешно загружен без ошибок
+    }
+}

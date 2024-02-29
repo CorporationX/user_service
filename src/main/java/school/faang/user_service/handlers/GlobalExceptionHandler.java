0361d9003eb63936e76ee65d0672f71b1c19f986
Edisolt Khatsiev <Edisolt Khatsiev>
2024-02-29 05:19:22 +0300

    log

--- src/main/java/school/faang/user_service/handlers/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/handlers/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package school.faang.user_service.handlers;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,34 +11,45 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.io.IOException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        String message = ex.getMessage();
+        log.error("IllegalArgumentException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleEntityNotFoundException(EntityNotFoundException ex) {
-        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        String message = ex.getMessage();
+        log.error("EntityNotFoundException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), message);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleConstraintViolationException(ValidationException ex) {
-        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    public ErrorResponse handleValidationException(ValidationException ex) {
+        String message = ex.getMessage();
+        log.error("EntityNotFoundException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIOException(IOException ex) {
-        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        String message = ex.getMessage();
+        log.error("IOException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleIOException(RuntimeException ex) {
-        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+    public ErrorResponse handleRuntimeException(RuntimeException ex) {
+        String message = ex.getMessage();
+        log.error("RuntimeException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), message);
     }
 }

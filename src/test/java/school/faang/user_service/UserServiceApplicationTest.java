df9f021eeb3246703ade42a9a25f354bc9321865
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-03-01 00:20:59 +0300

    тест на поднятие спринг контекста

--- src/test/java/school/faang/user_service/UserServiceApplicationTest.java
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -2,11 +2,12 @@ package school.faang.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.BootstrapWith;
 
 
 @SpringBootTest
-@BootstrapWith
+@ActiveProfiles("test")
 class UserServiceApplicationTest {
 
     @Test

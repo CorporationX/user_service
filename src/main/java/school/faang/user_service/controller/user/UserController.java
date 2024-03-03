51a0dbc522cf1724f3176718cf94911d9487c71f
Nikita <soul2009.89@gmail.com>
2024-03-03 23:34:04 +0500

    3

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -13,7 +13,7 @@ import school.faang.user_service.service.user.UserService;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 

9d402df6f55ac0bb42b06873122854be80fc4a6d
Nikita <soul2009.89@gmail.com>
2024-03-03 20:04:42 +0500

    2

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -3,7 +3,6 @@ package school.faang.user_service.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;

7dd6fb1c021e2a9806cc900520e788babe9dae00
Bukevsky | Kirill <bykaplay@gmail.com>
2024-03-05 23:47:30 +0300

    deleted my added method on repositories

--- src/main/java/school/faang/user_service/repository/goal/GoalRepository.java
+++ src/main/java/school/faang/user_service/repository/goal/GoalRepository.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.repository.goal;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;

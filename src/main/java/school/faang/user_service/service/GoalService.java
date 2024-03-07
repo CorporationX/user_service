2ed25ebc9a5bf4cf8d17dc1c58410307251ec479
InnMish <inn.mishustina@gmail.com>
2024-03-07 18:22:45 +0300

    refactor

--- src/main/java/school/faang/user_service/service/GoalService.java
+++ src/main/java/school/faang/user_service/service/GoalService.java
@@ -15,7 +15,6 @@ import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.exception.EntityNotFoundException;
 import school.faang.user_service.filter.goal.GoalFilter;
 import school.faang.user_service.mapper.GoalMapper;
-import school.faang.user_service.mapper.goal.GoalMapper;
 import school.faang.user_service.publisher.GoalCompletedEventPublisher;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.validator.GoalValidator;
--- src/test/java/school/faang/user_service/service/GoalServiceTest.java
+++ src/test/java/school/faang/user_service/service/GoalServiceTest.java
@@ -22,8 +22,6 @@ import school.faang.user_service.filter.goal.GoalStatusFilter;
 import school.faang.user_service.filter.goal.GoalTitleFilter;
 import school.faang.user_service.mapper.GoalMapper;
 import school.faang.user_service.mapper.GoalMapperImpl;
-import school.faang.user_service.mapper.goal.GoalMapper;
-import school.faang.user_service.mapper.goal.GoalMapperImpl;
 import school.faang.user_service.publisher.GoalCompletedEventPublisher;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.validator.GoalValidator;

eb7d9b94a6e3b53a21b9c39331c4fdf389680800
Sergey Ryabov <mre@mail.ru>
2024-03-10 21:25:16 +0800

    subscription service WITH NO TEST

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -2,8 +2,12 @@ package school.faang.user_service.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.SubscriptionService;
+
+import java.util.List;
 
 
 @Component
@@ -16,5 +20,28 @@ public class SubscriptionController {
             throw new DataValidationException("You can not follow yourself!");
         }
         subscriptionService.followUser(followerId, followeeId);
+    }
+
+    public void unfollowUser(long followerId, long followeeId) {
+        if (followerId == followeeId) {
+            throw new DataValidationException("You can not unfollow yourself!");
+        }
+        subscriptionService.unfollowUser(followerId, followeeId);
+    }
+
+    public List<UserDto> getFollowers(long followeeId, UserFilterDto filter) {
+        return subscriptionService.getFollowers(followeeId, filter);
+    }
+
+    public int getFollowersCount(long followeeId) {
+        return subscriptionService.getFollowersCount(followeeId);
+    }
+
+    public List<UserDto> getFollowing(long followeeId, UserFilterDto filter) {
+        return subscriptionService.getFollowing(followeeId, filter);
+    }
+
+    public int getFollowingCount(long followerId) {
+        return subscriptionService.getFollowingCount(followerId);
     }
 }
--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -2,13 +2,23 @@ package school.faang.user_service.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.UserFilterDto;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.SubscriptionRepository;
+import school.faang.user_service.service.filters.UserFilter;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
 public class SubscriptionService {
     private final SubscriptionRepository subscriptionRepository;
+    private final UserMapper userMapper;
+    private final List<UserFilter> userFilters;
 
     public void followUser(long followerId, long followeeId) {
         if (!subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, followeeId)) {
@@ -16,6 +26,36 @@ public class SubscriptionService {
         }
         subscriptionRepository.followUser(followerId, followeeId);
 
+    }
+
+    public void unfollowUser(long followerId, long followeeId) {
+        subscriptionRepository.unfollowUser(followerId, followeeId);
+
+    }
+
+    public List<UserDto> getFollowers(long followeeId, UserFilterDto filter) {
+        Stream<User> users = subscriptionRepository.findByFolloweeId(followeeId);
+        return filterUser(users, filter);
+    }
+
+    public int getFollowersCount(long followeeId) {
+        return subscriptionRepository.findFollowersAmountByFolloweeId(followeeId);
+    }
+
+    public List<UserDto> getFollowing(long followerId, UserFilterDto filter) {
+        Stream<User> users = subscriptionRepository.findByFolloweeId(followerId);
+        return filterUser(users, filter);
+    }
+
+    public int getFollowingCount(long followerId) {
+        return subscriptionRepository.findFolloweesAmountByFollowerId(followerId);
+    }
+
+    private List<UserDto> filterUser(Stream<User> users, UserFilterDto filters) {
+        userFilters.stream()
+                .filter(filter -> filter.isApplicable(filters))
+                .forEach(filter -> filter.apply(users, filters));
+        return userMapper.toDto(users.toList());
     }
 
 }

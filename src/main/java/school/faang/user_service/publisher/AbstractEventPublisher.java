61804d965fde5f948d4e2df62e65c144d8dd90b2
Ruslan Latyshev <latyshevruslan@ya.ru>
2024-03-10 13:55:36 +0300

    fix after review

--- src/main/java/school/faang/user_service/publisher/AbstractEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/AbstractEventPublisher.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Data
-public class AbstractEventPublisher<T> {
+public abstract class AbstractEventPublisher<T> {
 
     private final ObjectMapper objectMapper;
     private final RedisTemplate<String, Object> redisTemplate;

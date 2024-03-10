1533be7addd32dfecb1f68f88d25872f55e82c0a
PavelAP96 <pakhomkin96@mail.ru>
2024-03-10 18:17:33 +0300

    fix

--- src/test/java/school/faang/user_service/SpringContextTest.java
+++ src/test/java/school/faang/user_service/SpringContextTest.java
@@ -1,17 +1,35 @@
 package school.faang.user_service;
 
+import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
 public class SpringContextTest {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @Container
+    public static RedisContainer redis = new RedisContainer(
+            DockerImageName.parse("redis:5.0.3-alpine")
+    );
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+    }
 
     @Test
     void contextLoads() {

608b0968e03d08496ac5795f110a92a7aeb2e7ae
DadyaVolodya <pl4stmass@yandex.ru>
2024-03-17 12:39:38 +0300

    По замечаниям

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -1,9 +1,15 @@
 package school.faang.user_service.service.user;
 
+import school.faang.user_service.dto.user.UserDto;
+import school.faang.user_service.dto.user.UserFilterDto;
 import school.faang.user_service.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.mapper.user.UserMapper;
 import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.service.user.filter.UserFilter;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +18,14 @@ public class UserService {
 
     public User getUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("id is not found"));
+    }
+    public List<UserDto> getPremiumUsers(UserFilterDto filters) {
+        List<User> premiumUsers = userRepository.findPremiumUsers().toList();
+        if (!userFilters.isEmpty()) {
+            userFilters.stream()
+                    .filter(userFilter -> userFilter.isApplicable(filters))
+                    .forEach(userFilter -> userFilter.apply(premiumUsers, filters));
+        }
+        return userMapper.toDto(premiumUsers);
     }
 }

ec7e97fc8bd73811fec28d4e93bc00927e77ab7b
kirillgutorov <gutorovvvvv@gmail.ru>
2024-03-18 09:43:42 +0700

    Become a Mentor (resolved conflicts and add Web API annotations)

--- src/main/java/school/faang/user_service/controller/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/MentorshipController.java
@@ -1,31 +1,40 @@
 package school.faang.user_service.controller;
 
-import lombok.Data;
-import org.springframework.stereotype.Controller;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.service.MentorshipService;
-
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@Validated
 public class MentorshipController {
-    MentorshipService mentorshipService;
+    private final MentorshipService mentorshipService;
 
-
-    public List<UserDto> getMentees(long mentorId) {
+    @GetMapping("/get/mentees/{mentorId}")
+    public List<UserDto> getMentees(@PathVariable @Min(0) long mentorId) {
         return mentorshipService.getMentees(mentorId);
     }
 
-    public List<UserDto> getMentors(long menteeId) {
+    @GetMapping("/get/mentors/{menteeId}")
+    public List<UserDto> getMentors(@PathVariable @Min(0) long menteeId) {
         return mentorshipService.getMentors(menteeId);
     }
 
-    public void deleteMentee(long mentorId, long menteeId) {
+    @PutMapping("delete/mentee")
+    public void deleteMentee(@RequestParam long mentorId, @RequestParam long menteeId) {
         mentorshipService.deleteMentee(mentorId, menteeId);
     }
 
-    public void deleteMentor(long menteeId, long mentorId) {
+    @PutMapping("delete/mentor")
+    public void deleteMentor(@RequestParam long menteeId, long mentorId) {
         mentorshipService.deleteMentor(menteeId, mentorId);
     }
 }

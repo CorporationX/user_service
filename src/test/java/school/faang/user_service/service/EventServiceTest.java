956175cf494c2f9fca5f2053092b622c8c8f2d6e
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-03-21 10:38:42 +0300

    fix

--- src/test/java/school/faang/user_service/service/EventServiceTest.java
+++ src/test/java/school/faang/user_service/service/EventServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import school.faang.user_service.repository.event.EventParticipationRepository;
 import school.faang.user_service.repository.event.EventRepository;
 
 import static org.mockito.Mockito.times;
@@ -14,25 +13,14 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 class EventServiceTest {
     @Mock
-    private EventParticipationRepository eventParticipationRepository;
-    @Mock
     private EventRepository eventRepository;
     @InjectMocks
     private EventService eventService;
 
     @Test
-    void deleteAllParticipatedEventsByUserId() {
-        long userId = 1;
-        eventService.deleteAllParticipatedEventsByUserId(userId);
-        verify(eventParticipationRepository, times(1))
-                .deleteAllParticipatedEventsByUserId(userId);
-
-    }
-
-    @Test
     void deleteALLEventByUserId() {
         long userId = 1;
         eventService.deleteALLEventByUserId(userId);
-        verify(eventRepository,times(1)).deleteAllByUserId(userId);
+        verify(eventRepository, times(1)).deleteAllByUserId(userId);
     }
 }
--- src/test/java/school/faang/user_service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -10,15 +10,14 @@ import org.springframework.web.client.RestTemplate;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.UserProfilePic;
+import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.entity.goal.Goal;
 import school.faang.user_service.exception.EntityNotFoundException;
 import school.faang.user_service.mapper.UserMapperImpl;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.service.s3.S3Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
@@ -123,7 +122,7 @@ class UserServiceTest {
                 () -> verify(userMapper, times(1)).toEntity(userDto),
                 () -> verify(s3Service, times(1)).uploadFile(any(), any()),
                 () -> verify(userMapper, times(1)).toDto((User) any()),
-                () -> verify(userRepository, times(1)).save((User) any())
+                () -> verify(userRepository, times(1)).save(any())
         );
     }
 
@@ -134,10 +133,14 @@ class UserServiceTest {
         userService.setGoalService(goalServiceMock); // не знаю, почему, но через аннотацию не работало. Наверное из-за способа внедрения
         userService.setMentorshipService(mentorshipService1);
         long userId = 1;
+        List<Event> events = new ArrayList<>(Arrays.asList(new Event(),new Event()));
         List<Goal> goals = new ArrayList<>();
         List<User> mentors = new ArrayList<>();
         List<User> mentees = new ArrayList<>();
-        User user = User.builder().mentees(mentees).goals(goals).build();
+        User user = User.builder()
+                .mentees(mentees)
+                .goals(goals)
+                .participatedEvents(events).build();
         User mentee = User.builder().mentors(mentors).build();
         mentors.add(user);
         mentees.add(mentee);
@@ -149,11 +152,9 @@ class UserServiceTest {
         goals.add(goal11);
 
         when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        when(goalServiceMock.countingUsersCompletingGoal(anyLong())).thenReturn(1);
 
         userService.deactivateProfile(userId);
 
-        verify(goalServiceMock, times(2)).countingUsersCompletingGoal(anyLong());
         verify(goalServiceMock, times(2)).deleteGoal(anyLong());
         assertFalse(user.isActive());
         verify(mentorshipService1, times(1)).stopMentoring(user, mentee);

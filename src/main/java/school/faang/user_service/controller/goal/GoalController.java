15ee2ea9fd95f715cc42a189571e652c4666eb9e
Dmitriy Goncharov <153752474+hhzktoeto@users.noreply.github.com>
2024-03-22 03:39:12 +0500

    GoalController refactor
    reorganized methods to match CRUD
    minor changes

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.dto.goal.GoalFilterDto;
@@ -20,37 +21,38 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Goals", description = "Endpoints for managing goals")
+@RequestMapping("/goals")
 public class GoalController {
 
     private final GoalService goalService;
 
     @Operation(summary = "Create user's goal by user id")
-    @PostMapping("/goal")
-    public GoalDto createGoal(@RequestHeader("userId") Long userId, @RequestBody GoalDto goalDto) {
+    @PostMapping
+    public GoalDto createGoal(@RequestHeader("x-user-id") Long userId, @RequestBody GoalDto goalDto) {
         return goalService.createGoal(userId, goalDto);
     }
 
+    @Operation(summary = "Get user's goals by user id and filters")
+    @PostMapping("/filtered")
+    public List<GoalDto> getGoalsByUser(@RequestHeader("x-user-id") Long userId, @RequestBody GoalFilterDto filters) {
+        return goalService.getGoalsByUser(userId, filters);
+    }
+
+    @Operation(summary = "Get goal subtasks by goal id and filters")
+    @PostMapping("/{goalId}/subtasks")
+    public List<GoalDto> findSubtasksByGoalId(@PathVariable @Min(1) Long goalId, @RequestBody GoalFilterDto filters) {
+        return goalService.findSubtasksByGoalId(goalId, filters);
+    }
+
     @Operation(summary = "Update goal by goal id")
-    @PutMapping("/goal/{goalId}")
-    public GoalDto updateGoal(@PathVariable Long goalId, @RequestBody GoalDto goalDto) {
+    @PutMapping("/{goalId}")
+    public GoalDto updateGoal(@PathVariable @Min(1) Long goalId, @RequestBody GoalDto goalDto) {
         return goalService.updateGoal(goalId, goalDto);
     }
 
     @Operation(summary = "Delete goal by goal id")
-    @DeleteMapping("/goal/{goalId}")
+    @DeleteMapping("/{goalId}")
     public void deleteGoal(@PathVariable @Min(1) Long goalId) {
         goalService.deleteGoal(goalId);
-    }
-
-    @Operation(summary = "Get goal subtasks by goal id and filters")
-    @PostMapping("/goal/{goalId}/subtasks")
-    public List<GoalDto> findSubtasksByGoalId(@PathVariable Long goalId, @RequestBody GoalFilterDto filters) {
-        return goalService.findSubtasksByGoalId(goalId, filters);
-    }
-
-    @Operation(summary = "Get user's goals by user id and filters")
-    @PostMapping("/goals")
-    public List<GoalDto> getGoalsByUser(@RequestHeader("userId") Long userId, @RequestBody GoalFilterDto filters) {
-        return goalService.getGoalsByUser(userId, filters);
     }
 }

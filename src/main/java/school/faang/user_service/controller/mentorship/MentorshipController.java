4930e348e8ddcf3923f9ad38af7e98502dae9c22
Dmitriy Goncharov <153752474+hhzktoeto@users.noreply.github.com>
2024-03-22 00:37:19 +0500

    MentorshipController refactor
    added endpoints and swagger annotations

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,7 +1,13 @@
 package school.faang.user_service.controller.mentorship;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
@@ -10,22 +16,32 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/mentorship")
+@Tag(name = "Mentorship", description = "Endpoints for managing mentorship system")
 public class MentorshipController {
     private final MentorshipService mentorshipService;
 
-    public List<UserDto> getMentees(long userId) {
+    @Operation(summary = "Get list of mentees by mentorId")
+    @GetMapping("/mentees/{userId}")
+    public List<UserDto> getMentees(@PathVariable long userId) {
         return mentorshipService.getMentees(userId);
     }
 
-    public List<UserDto> getMentors(long userId) {
+    @Operation(summary = "Get list of mentors by menteeId")
+    @GetMapping("/mentors/{userId}")
+    public List<UserDto> getMentors(@PathVariable long userId) {
         return mentorshipService.getMentors(userId);
     }
 
-    public void deleteMentee(long menteeId, long mentorId) {
+    @Operation(summary = "Delete mentee from mentor's list of mentees")
+    @DeleteMapping("/mentee/{menteeId}/mentor/{mentorId}")
+    public void deleteMentee(@PathVariable long menteeId, @PathVariable long mentorId) {
         mentorshipService.deleteMentee(menteeId, mentorId);
     }
 
-    public void deleteMentor(long menteeId, long mentorId) {
+    @Operation(summary = "Delete mentor from mentee's list of mentors")
+    @DeleteMapping("/mentor/{mentorId}/mentee/{menteeId}")
+    public void deleteMentor(@PathVariable long menteeId, @PathVariable long mentorId) {
         mentorshipService.deleteMentor(menteeId, mentorId);
     }
 }

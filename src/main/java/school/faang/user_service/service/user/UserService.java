fa0493000cdfd986cf2aa0cd62405b79b6cc43c7
Bukevsky | Kirill <bykaplay@gmail.com>
2024-03-23 02:06:03 +0300

    complete task and add create user

---
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -0,0 +1,42 @@
+package school.faang.user_service.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.UserProfilePic;
+import school.faang.user_service.mapper.user.UserMapper;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.validator.user.UserValidator;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserMapper userMapper;
+    private final UserValidator userValidator;
+    private final UserRepository userRepository;
+    @Value("${dicebear.avatar}")
+    private String avatarUrl;
+    @Value("${dicebear.small_avatar}")
+    private String smallAvatarUrl;
+
+    public UserDto create(UserDto userDto) {
+        userValidator.validatePassword(userDto);
+        User user = userMapper.toEntity(userDto);
+        user.setUserProfilePic(getRandomAvatar());
+        user.setActive(true);
+        User createdUser = userRepository.save(user);
+        return userMapper.toDto(createdUser);
+    }
+
+    private UserProfilePic getRandomAvatar(){
+        UUID uuid = UUID.randomUUID();
+        return UserProfilePic.builder()
+                .fileId(avatarUrl + uuid)
+                .smallFileId(smallAvatarUrl + uuid)
+                .build();
+    }
+}

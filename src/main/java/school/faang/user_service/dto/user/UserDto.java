d33f9efc6f7a1a32d9f70e12d4586f5b3387968b
Dmitriy Goncharov <153752474+hhzktoeto@users.noreply.github.com>
2024-03-25 15:43:18 +0500

    change after cross-review

--- src/main/java/school/faang/user_service/dto/user/UserDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,5 +29,6 @@ public class UserDto {
     private boolean active;
     private boolean isPremium;
     @NotNull(message = "CountryID can't be empty")
+    @Positive(message = "CountryID can't be less than 1")
     private Long countryId;
 }

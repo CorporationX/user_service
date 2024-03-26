24305194ea628819a2649fcc69cca7c541990f4b
kirillgutorov <gutorovvvvv@gmail.ru>
2024-03-26 20:23:30 +0700

    After review changes

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -2,7 +2,6 @@ package school.faang.user_service.service.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;

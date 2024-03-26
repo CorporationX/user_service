ba80bbec2ed6f064640b865c365196a7be1cd0b8
kirillgutorov <gutorovvvvv@gmail.ru>
2024-03-26 10:37:12 +0700

    reverted changes

---
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package school.faang.user_service.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
+public interface UserMapper {
+    @Mapping(source = "mentees", target = "menteesIds", qualifiedByName = "mapToIds")
+    @Mapping(source = "mentors", target = "mentorsIds", qualifiedByName = "mapToIds")
+    UserDto toDto(User user);
+
+    List<UserDto> toDto(List<User> users);
+
+    @Mapping(target = "mentees", ignore = true)
+    @Mapping(target = "mentors", ignore = true)
+    User toEntity(UserDto userDto);
+
+    @Named("mapToIds")
+    default List<Long> mapToIds(List<User> users) {
+        return users.stream().map(User::getId).toList();
+    }
+
+}

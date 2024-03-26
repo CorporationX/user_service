dc311d70348be7f7374d66cd39312e86c43cb577
Viktor Dyukov <snailrake@mail.ru>
2024-03-26 12:57:17 +0400

    refactor: minor changes

--- src/main/java/school/faang/user_service/controller/premium/PremiumController.java
+++ src/main/java/school/faang/user_service/controller/premium/PremiumController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import school.faang.user_service.dto.premium.PremiumDto;
 import school.faang.user_service.entity.premium.PremiumPeriod;
 import school.faang.user_service.service.premium.PremiumService;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Premium", description = "Endpoints for managing premium access")

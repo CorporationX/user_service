89cd487333b7a2b7a964f1b0aafc54e4e9f32cda
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-03-28 16:52:37 +0600

    Spring context test for  user service

--- src/test/java/school/faang/user_service/SpringContextTest.java
+++ src/test/java/school/faang/user_service/SpringContextTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
 @SpringBootTest(classes = UserServiceApplication.class)
+@Testcontainers
 public class SpringContextTest {
     @Autowired
     private ApplicationContext applicationContext;
@@ -18,6 +19,6 @@ public class SpringContextTest {
     @Test
     @DisplayName("Spring context test check")
     public void contextLoads() {
-        assertNotNull(applicationContext);
+        assertNotNull( applicationContext );
     }
 }

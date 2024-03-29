2969e3e17a5227b0475edbb8419683c3dbcc36e2
kirillgutorov <gutorovvvvv@gmail.ru>
2024-03-29 09:00:42 +0700

    Hot fix for master branch

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import school.faang.user_service.mapper.StringToLongMapper;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
@@ -18,7 +16,6 @@ import school.faang.user_service.mapper.StringToLongMapper;
                 title = "User Service",
                 version = "1.0.0")
 )
-@ComponentScan("school.faang.user_service.mapper") // Сканирование пакета с вашим маппером
 public class UserServiceApplication {
 
     public static void main(String[] args) {

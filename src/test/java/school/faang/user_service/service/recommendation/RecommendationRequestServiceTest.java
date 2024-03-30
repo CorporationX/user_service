3dc6303d143e3b432b8296458dcd83acdddd1890
Sam Migachev <sammigachev@gmail.com>
2024-03-30 20:53:03 +0300

    test

--- src/test/java/school/faang/user_service/service/recommendation/RecommendationRequestServiceTest.java
+++ src/test/java/school/faang/user_service/service/recommendation/RecommendationRequestServiceTest.java
@@ -2,8 +2,10 @@ package school.faang.user_service.service.recommendation;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.dto.recommendation.RejectionDto;
@@ -15,7 +17,7 @@ import school.faang.user_service.repository.recommendation.RecommendationRequest
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class RecommendationRequestServiceTest {
     private RecommendationRequestDto recommendationRequestDto;
     private RecommendationRequest recommendationRequest;

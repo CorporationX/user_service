17acdea0c03acc0f6af9f04a8328a300759dffa3
Sam Migachev <sammigachev@gmail.com>
2024-03-30 20:09:06 +0300

    testing

--- src/test/java/school/faang/user_service/testFail/TestPipline.java
+++ src/test/java/school/faang/user_service/testFail/TestPipline.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class TestPipline {
     @Test
     public void testFail(){
-        int result = 2+3;
+        int result = 3+2;
         Assert.assertEquals(5, result);
     }
 }

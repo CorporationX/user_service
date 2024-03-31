6c4baf1c9d320290751f9a65bf456a2aba9bc520
Ikhsanov Nail <ikhsanovn17@gmail.com>
2024-03-31 21:06:28 +0500

    Исправил ошибку на 3 строчке в файле UserDto

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,9 +1,7 @@
 package school.faang.user_service.dto;
 
-=======
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 

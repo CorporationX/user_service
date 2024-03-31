cd198265edd3a21323a4c4fc96ee405b56e7a5b6
Sam Migachev <sammigachev@gmail.com>
2024-03-31 18:52:52 +0300

    resolve conflict and push

--- src/test/java/school/faang/user_service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -61,7 +61,6 @@ class UserServiceTest {
         ment.setMentors(new ArrayList<>(List.of(u)));
 
         Event ev = new Event();
-
         ev.setId(1L);
         ev.setOwner(u);
         ev.setStatus(EventStatus.PLANNED);

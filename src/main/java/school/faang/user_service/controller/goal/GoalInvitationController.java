40fc9d1a2231934e7c507db51278840dd07850d4
Ikhsanov Nail <ikhsanovn17@gmail.com>
2024-04-04 20:39:52 +0500

    Fixed bugs

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -1,7 +1,9 @@
 package school.faang.user_service.controller.goal;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,30 +18,31 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/invitations")
+@Validated
+@RequestMapping("/api")
 public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
 
     @PostMapping()
     @Operation(description = "Creating an invitation")
-    public GoalInvitationDto createInvitation(@RequestBody GoalInvitationDto invitation) {
+    public GoalInvitationDto createInvitation(@Valid @RequestBody GoalInvitationDto invitation) {
         return goalInvitationService.createInvitation(invitation);
     }
 
-    @GetMapping("/accept/{id}")
+    @GetMapping("/invitations/accept/{id}")
     @Operation(description = "Creating an invitation")
     public GoalInvitationDto acceptGoalInvitation(@PathVariable long id) {
         return goalInvitationService.acceptGoalInvitation(id);
     }
 
 
-    @GetMapping("/reject/{id}")
+    @GetMapping("/invitations/reject/{id}")
     @Operation(description = "Decline the invitation")
     public GoalInvitationDto rejectGoalInvitation(@PathVariable long id) {
         return goalInvitationService.rejectGoalInvitation(id);
     }
 
-    @PostMapping("/get")
+    @PostMapping("/invitations/get")
     @Operation(description = "Filter out invitations")
     public List<GoalInvitationDto> getInvitations(@RequestBody GoalInvitationFilterDto filter) {
         return goalInvitationService.getFilteredInvitations(filter);

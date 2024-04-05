a8acf4806ad7308b2e221d4e85506a39681371de
Dmitriy Goncharov <incoom@yandex.ru>
2024-04-05 11:30:33 +0500

    minor change

--- src/main/java/school/faang/user_service/mapper/jira/JiraAccountMapper.java
+++ src/main/java/school/faang/user_service/mapper/jira/JiraAccountMapper.java
@@ -2,10 +2,11 @@ package school.faang.user_service.mapper.jira;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.jira.JiraAccountDto;
 import school.faang.user_service.entity.jira.JiraAccount;
 
-@Mapper
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface JiraAccountMapper {
 
     @Mapping(source = "user.id", target = "userId")

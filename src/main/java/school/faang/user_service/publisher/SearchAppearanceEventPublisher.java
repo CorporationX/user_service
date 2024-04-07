bf96edf97b78a0928108917c0c2f364bc64e8d28
phoen1x <106927036+vladmeniailov@users.noreply.github.com>
2024-04-07 23:50:36 +0300

    Added in UserRepo "find followersIds"

--- src/main/java/school/faang/user_service/publisher/SearchAppearanceEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/SearchAppearanceEventPublisher.java
@@ -1,8 +1,6 @@
 package school.faang.user_service.publisher;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.event.SearchAppearanceEventDto;
 

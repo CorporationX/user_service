bda32710fcbbae308f9ef1dc8cf0682a0efa0d77
Sam Migachev <sammigachev@gmail.com>
2024-04-20 17:50:10 +0300

    поправил

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -41,9 +41,9 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(objectMapper, Object.class));
         return redisTemplate;

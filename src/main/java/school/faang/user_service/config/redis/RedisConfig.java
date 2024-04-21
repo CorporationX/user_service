52f1bdbeaa6ed290c849bdf34e8df788ce90e0f6
Ikhsanov Nail <ikhsanov.nail@outlook.com>
2024-04-21 17:30:14 +0500

    Minor corrections

--- src/main/java/school/faang/user_service/config/redis/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.config;
+package school.faang.user_service.config.redis;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;

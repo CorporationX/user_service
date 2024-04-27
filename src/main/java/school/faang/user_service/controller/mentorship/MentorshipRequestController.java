89432fae5491a46565aa67a7034dc39f1dba29f7
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-04-27 15:29:15 +0300

    correct

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
@@ -1,8 +1,9 @@
 package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
+import school.faang.user_service.dto.recommendation.RejectionDto;
 import school.faang.user_service.dto.recommendation.RequestFilterDto;
 import school.faang.user_service.service.mentorship.MentorshipRequestService;
 
@@ -10,17 +11,23 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/mentorshipRequest")
 public class MentorshipRequestController {
     private final MentorshipRequestService mentorshipRequestService;
-
-    public void requestMentorship(MentorshipRequestDto mentorshipRequestDto) {
+    @PostMapping("/create")
+    public void requestMentorship(@RequestBody MentorshipRequestDto mentorshipRequestDto) {
         mentorshipRequestService.requestMentorship(mentorshipRequestDto);
     }
-    public List<MentorshipRequestDto> getRequest(RequestFilterDto requestFilterDto){
+    @GetMapping("/get")
+    public List<MentorshipRequestDto> getRequest(@RequestBody RequestFilterDto requestFilterDto){
         return mentorshipRequestService.getRequest(requestFilterDto);
     }
-
-    public MentorshipRequestDto acceptRequest(long id) {
+    @PutMapping("/accept/{id}")
+    public MentorshipRequestDto acceptRequest(@PathVariable long id) {
         return mentorshipRequestService.acceptRequest(id);
+    }
+    @PutMapping("/reject/{id}")
+    public MentorshipRequestDto rejectRequest(@PathVariable long id,@RequestBody RejectionDto rejection){
+        return mentorshipRequestService.rejectRequest(id,rejection);
     }
 }

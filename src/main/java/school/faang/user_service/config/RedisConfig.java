a3d8c35eef67259921877c5abb49fd2a044b82b5
Sam Migachev <sammigachev@gmail.com>
2024-04-29 16:44:34 +0300

    внес правки

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -25,11 +25,11 @@ public class RedisConfig {
     private String host;
     @Value("${spring.data.redis.port}")
     private int port;
-    @Value("${spring.data.redis.channel.recommendation_channel}")
+    @Value("${spring.data.redis.channel.recommendation_channel.name}")
     private String recommendationChannel;
     @Value("${topic.user_ban}")
     private String userBanTopic;
-    @Value("${spring.data.redis.channel.skill_channel}")
+    @Value("${spring.data.redis.channel.skill_channel.name}")
     private String skillChannel;
     private final UsersBanListener usersBanListener;
 

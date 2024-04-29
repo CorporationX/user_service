ac5b201d561bf57d5ac13ab40ccdb888be74a609
Sam Migachev <sammigachev@gmail.com>
2024-04-29 17:05:44 +0300

    добавил логирование в абстрактный класс

--- src/main/java/school/faang/user_service/publisher/AbstractMessagePublisher.java
+++ src/main/java/school/faang/user_service/publisher/AbstractMessagePublisher.java
@@ -2,9 +2,11 @@ package school.faang.user_service.publisher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @RequiredArgsConstructor
+@Slf4j
 public abstract class AbstractMessagePublisher<T> {
     private final RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper objectMapper;
@@ -13,6 +15,7 @@ public abstract class AbstractMessagePublisher<T> {
         try {
             String jsonObject = objectMapper.writeValueAsString(topic);
             redisTemplate.convertAndSend(topic, jsonObject);
+            log.info("Event published to topic " + topic);
         } catch (Exception e) {
             e.printStackTrace();
         }

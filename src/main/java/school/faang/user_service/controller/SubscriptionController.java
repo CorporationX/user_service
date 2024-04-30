288402a24388bf7dc1369a6eebe6183d147edce6
Th0rn-dev <solnek33@gmail.com>
2024-04-30 11:53:56 +0300

    added annotation to controller and his method

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,16 +1,23 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import school.faang.user_service.exceptions.DataValidationException;
 import school.faang.user_service.service.SubscriptionService;
 
 @Controller
+@RequestMapping("/follow")
 @RequiredArgsConstructor
 public class SubscriptionController {
 
     private final SubscriptionService service;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     void followUser(long followerId, long followeeId) {
         if (followerId == followeeId) {
             throw new DataValidationException("The user " + followeeId +

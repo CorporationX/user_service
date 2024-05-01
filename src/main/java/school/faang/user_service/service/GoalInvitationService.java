5326929061c33e8aa89a3989de4c8cfea13f8e53
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-05-01 17:24:25 +0300

    Дополнил GoalInvitationController, GoalInvitationService

--- src/main/java/school/faang/user_service/service/GoalInvitationService.java
+++ src/main/java/school/faang/user_service/service/GoalInvitationService.java
@@ -2,18 +2,28 @@ package school.faang.user_service.service;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.entity.RequestStatus;
+import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.entity.goal.GoalInvitation;
 import school.faang.user_service.mapper.GoalInvitationMapper;
 import school.faang.user_service.repository.goal.GoalInvitationRepository;
+import school.faang.user_service.repository.goal.GoalRepository;
+
+import java.util.List;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class GoalInvitationService {
-    GoalInvitationRepository goalInvitationRepository;
-    GoalInvitationMapper goalInvitationMapper;
+    final GoalInvitationRepository goalInvitationRepository;
+    final GoalInvitationMapper goalInvitationMapper;
+    final GoalRepository goalRepository;
+    GoalInvitation goalInvitation;
+    RuntimeException runtimeException = new RuntimeException();
 
     public void createInvitation(GoalInvitationDto goalInvitationDto) {
         if (goalInvitationDto.getInviterId() != null && goalInvitationDto.getInvitedUserId() != null) {
@@ -21,13 +31,51 @@ public class GoalInvitationService {
                 if (goalInvitationRepository.existsById(goalInvitationDto.getInviterId()) && goalInvitationRepository.existsById(goalInvitationDto.getInvitedUserId())) {
                     goalInvitationRepository.save(goalInvitationMapper.toEntity(goalInvitationDto));
                 } else {
-                    throw new RuntimeException("There is no such user in database");
+                    throw runtimeException;
                 }
             } else {
-                throw new RuntimeException("InviterId equals InvitedUserId");
+                throw runtimeException;
             }
         } else {
-            throw new RuntimeException("Input is null");
+            throw runtimeException;
         }
     }
+
+    public RequestStatus acceptGoalInvitation(long id) {
+        if (goalInvitationRepository.findById(id).isPresent()) {
+            goalInvitation = goalInvitationRepository.findById(id).get();
+            if (goalRepository.findById(goalInvitation.getGoal().getId()).isPresent()) {
+                Goal goal = goalRepository.findById(goalInvitation.getGoal().getId()).get();
+                List<Goal> setGoals = goalInvitation.getInvited().getSetGoals();
+                if (setGoals.size() < 3) {
+                    if (!setGoals.contains(goal)) {
+                        goalInvitation.getInvited().getGoals().add(goal);
+                        return RequestStatus.ACCEPTED;
+                    } else {
+                        throw runtimeException;
+                    }
+                } else {
+                    throw runtimeException;
+                }
+            } else {
+                throw runtimeException;
+            }
+        } else {
+            throw runtimeException;
+        }
+    }
+
+    public RequestStatus rejectGoalInvitation(long id) {
+        if (goalInvitationRepository.findById(id).isPresent()) {
+            goalInvitation = goalInvitationRepository.findById(id).get();
+            if (goalRepository.findById(goalInvitation.getGoal().getId()).isPresent()) {
+                return RequestStatus.REJECTED;
+            } else {
+                throw runtimeException;
+            }
+        } else {
+            throw runtimeException;
+        }
+    }
+
 }

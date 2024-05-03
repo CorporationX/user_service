60affe9a728c16477b745868d2d39cb20ab39ac9
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-05-03 22:19:05 +0300

    BJS2-6302

--- src/main/java/school/faang/user_service/dto/goal/GoalInvitationDto.java
+++ src/main/java/school/faang/user_service/dto/goal/GoalInvitationDto.java
@@ -1,18 +1,25 @@
 package school.faang.user_service.dto.goal;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import school.faang.user_service.entity.RequestStatus;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class GoalInvitationDto {
-    private Long id;
+    Long id;
 
-    private Long inviterId;
+    Long inviterId;
 
-    private Long invitedUserId;
+    Long invitedUserId;
 
-    private Long goalId;
+    Long goalId;
 
-    private RequestStatus status;
+    RequestStatus status;
 }

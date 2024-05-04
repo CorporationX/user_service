0d804427dd9f03eed216b921c341535652511446
Sam Migachev <sammigachev@gmail.com>
2024-05-04 16:05:25 +0300

    correct

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -1,76 +1,76 @@
-package school.faang.user_service.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import school.faang.user_service.subscriber.UsersBanListener;
-
-@Configuration
-@RequiredArgsConstructor
-public class RedisConfig {
-    private final ObjectMapper objectMapper;
-    @Value("${spring.data.redis.host}")
-    private String host;
-    @Value("${spring.data.redis.port}")
-    private int port;
-    @Value("${spring.data.redis.channels.recommendation_channel.name}")
-    private String recommendationChannel;
-    @Value("${spring.data.redis.channels.recommendation_request_channel.name")
-    private String recommendationRequestChannel;
-    @Value("${topic.user_ban}")
-    private String userBanTopic;
-    private final UsersBanListener usersBanListener;
-
-    @Bean
-    public JedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
-        return new JedisConnectionFactory(config);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
-
-    @Bean
-    public ChannelTopic userBanTopic(){
-        return new ChannelTopic(userBanTopic);
-    }
-
-
-    @Bean
-    public MessageListenerAdapter userBanMessageListenerAdapter(){
-        return new MessageListenerAdapter(usersBanListener);
-    }
-
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(redisConnectionFactory);
-        container.addMessageListener(userBanMessageListenerAdapter(), userBanTopic());
-        return container;
-    }
-
-    @Bean
-    public ChannelTopic recommendationTopic() {
-        return new ChannelTopic(recommendationChannel);
-    }
-    @Bean
-    public ChannelTopic recommendationRequestTopic(){
-        return new ChannelTopic(recommendationRequestChannel);
-    }
-}
+//package school.faang.user_service.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.listener.ChannelTopic;
+//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+//import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//import school.faang.user_service.subscriber.UsersBanListener;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class RedisConfig {
+//    private final ObjectMapper objectMapper;
+//    @Value("${spring.data.redis.host}")
+//    private String host;
+//    @Value("${spring.data.redis.port}")
+//    private int port;
+//    @Value("${spring.data.redis.channels.recommendation_channel.name}")
+//    private String recommendationChannel;
+//    @Value("${spring.data.redis.channels.recommendation_request_channel.name")
+//    private String recommendationRequestChannel;
+//    @Value("${topic.user_ban}")
+//    private String userBanTopic;
+//    private final UsersBanListener usersBanListener;
+//
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+//        return new JedisConnectionFactory(config);
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//        return redisTemplate;
+//    }
+//
+//    @Bean
+//    public ChannelTopic userBanTopic(){
+//        return new ChannelTopic(userBanTopic);
+//    }
+//
+//
+//    @Bean
+//    public MessageListenerAdapter userBanMessageListenerAdapter(){
+//        return new MessageListenerAdapter(usersBanListener);
+//    }
+//
+//    @Bean
+//    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+//        container.setConnectionFactory(redisConnectionFactory);
+//        container.addMessageListener(userBanMessageListenerAdapter(), userBanTopic());
+//        return container;
+//    }
+//
+//    @Bean
+//    public ChannelTopic recommendationTopic() {
+//        return new ChannelTopic(recommendationChannel);
+//    }
+//    @Bean
+//    public ChannelTopic recommendationRequestTopic(){
+//        return new ChannelTopic(recommendationRequestChannel);
+//    }
+//}

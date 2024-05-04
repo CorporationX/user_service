578ccc3ecc83d6d5553bfd4af98044ea7a41c2fd
Murzin34 <murzinvolg@yandex.ru>
2024-05-04 11:53:52 +0300

    Create methods getMentees and getMentors

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
 import java.util.List;
@@ -13,13 +14,15 @@ import java.util.List;
 @RequestMapping("/mentorship")
 @RequiredArgsConstructor
 public class MentorshipController {
-    private MentorshipService mentorshipService;
-/*
-    @GetMapping("/{mentorId}/mentees")
-    public List<User> getMentees(@PathVariable int mentorId) {
+    private final MentorshipService mentorshipService;
 
+    @GetMapping("/{mentorId}/mentees")
+    public List<UserDto> getMentees(@PathVariable long mentorId) {
+        return mentorshipService.getMentees(mentorId);
     }
 
- */
-
+    @GetMapping("/{menteeId}/mentors")
+    public List<UserDto> getMentors(@PathVariable long menteeId) {
+        return mentorshipService.getMentors(menteeId);
+    }
 }
--- src/main/java/school/faang/user_service/service/mentorship/MentorshipService.java
+++ src/main/java/school/faang/user_service/service/mentorship/MentorshipService.java
@@ -1,14 +1,47 @@
 package school.faang.user_service.service.mentorship;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 @RequiredArgsConstructor
 public class MentorshipService {
-    private MentorshipRepository mentorshipRepository;
+    private final MentorshipRepository mentorshipRepository;
+    private final UserMapper userMapper;
 
+    @Transactional(readOnly = true)
+    public List<UserDto> getMentees(long userId) {
+        User user = getUserById(userId);
+        return user == null ? Collections.emptyList() :
+                user.getMentees()
+                        .stream()
+                        .map(userMapper::toDto)
+                        .collect(Collectors.toList());
+    }
 
+    @Transactional(readOnly = true)
+    public List<UserDto> getMentors(long userId) {
+        User user = getUserById(userId);
+        return user == null ? Collections.emptyList() :
+                user.getMentors()
+                        .stream()
+                        .map(userMapper::toDto)
+                        .collect(Collectors.toList());
+    }
+
+    // Утилитный private метод
+    private User getUserById(long id) {
+        return mentorshipRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
 }

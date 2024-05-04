3b965c9df91c52ba7ec8cb1e71b04ec8ac432dc9
Murzin34 <murzinvolg@yandex.ru>
2024-05-04 10:29:06 +0300

    Create UserDto

---
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -0,0 +1,20 @@
+package school.faang.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private String aboutMe;
+    private String country;
+    private String city;
+    private Integer experience;
+}

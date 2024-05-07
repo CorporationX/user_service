9335644040adcaaa6040375ba5c2559ce99ebf34
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-05-07 22:08:22 +0300

    test pipeline with failed test

--- src/test/java/school/faang/user_service/service/TestPipeline.java
+++ src/test/java/school/faang/user_service/service/TestPipeline.java
@@ -2,10 +2,12 @@ package school.faang.user_service.service;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestPipeline {
 
     @Test
     void successTest() {
-
+        fail();
     }
 }

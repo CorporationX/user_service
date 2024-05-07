f779f77c1c3bd0b10f31a476eaaf9458e041bd1c
ethiqque <danielpyld.work@gmail.com>
2024-05-07 06:59:00 +0300

    Добавил аннотации @GetMapping, @PutMapping, @PostMapping, @DeleteMapping в RecommendationController

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
@@ -1,6 +1,12 @@
 package school.faang.user_service.controller.recommendation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
@@ -11,31 +17,36 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("recommendations")
+@RequestMapping("/recommendations")
 @RequiredArgsConstructor
 public class RecommendationController {
     private final RecommendationService recommendationService;
     private final RecommendationValidator validator;
 
-    public RecommendationDto giveRecommendation(RecommendationDto recommendation) {
+    @PostMapping
+    public RecommendationDto giveRecommendation(@RequestBody RecommendationDto recommendation) {
         validator.validate(recommendation);
         return recommendationService.create(recommendation);
     }
 
-    public RecommendationDto updateRecommendation(RecommendationDto updated) {
+    @PutMapping("/{id}")
+    public RecommendationDto updateRecommendation(@RequestBody RecommendationDto updated) {
         validator.validate(updated);
         return recommendationService.create(updated);
     }
 
-    public void deleteRecommendation(long id) {
+    @DeleteMapping("/{id}")
+    public void deleteRecommendation(@PathVariable long id) {
         recommendationService.delete(id);
     }
 
-    public List<RecommendationDto> getAllUserRecommendations(long receiverId) {
+    @GetMapping("/receiver/{receiverId}")
+    public List<RecommendationDto> getAllUserRecommendations(@PathVariable long receiverId) {
         return recommendationService.getAllUserRecommendations(receiverId);
     }
 
-    public List<RecommendationDto> getAllGivenRecommendations(long authorId) {
+    @GetMapping("/author/{authorId}")
+    public List<RecommendationDto> getAllGivenRecommendations(@PathVariable long authorId) {
         return recommendationService.getAllGivenRecommendations(authorId);
     }
 }

2cb5809ba9fa339171b412c330459a267665c223
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-05-08 16:22:15 +0400

    becomeMentor

--- src/main/java/school/faang/user_service/dto/user/UserDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserDto.java
@@ -1,9 +1,13 @@
 package school.faang.user_service.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
     private long id;
--- src/test/java/school/faang/user_service/service/controller/mentorship/MentorshipControllerTest.java
+++ src/test/java/school/faang/user_service/service/controller/mentorship/MentorshipControllerTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.controller.mentorship.MentorshipController;
-import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.service.MentorshipService;
 
 import java.util.List;

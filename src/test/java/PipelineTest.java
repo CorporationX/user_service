59e3ff91f977caa503888664af34c8d6b6353fce
ekoregin <evgkoregin@gmail.com>
2024-05-09 15:20:34 +0300

    BJS2-6453: Failed test for pipeline

---
+++ src/test/java/PipelineTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PipelineTest {
+
+    @Test
+    public void failTest() {
+        fail("I must fail!");
+    }
+}

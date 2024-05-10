636ebeea2a5ea4a487f135b28446cede347d7920
Tem <tipokruto014@gmail.com>
2024-05-10 14:29:02 +0300

    fix: изменил директорию для SkillMapper

--- src/main/java/school/faang/user_service/mapper/event/EventMapper.java
+++ src/main/java/school/faang/user_service/mapper/event/EventMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.entity.event.Event;
+import school.faang.user_service.mapper.skill.SkillMapper;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
         injectionStrategy = InjectionStrategy.CONSTRUCTOR, uses = SkillMapper.class)

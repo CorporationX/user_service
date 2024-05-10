b4cdb0979cefa9ae7b6b9dfe4a854d2f7b099421
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-05-10 16:31:52 +0400

    проверка того, что Spring Context успешно поднимается

--- src/test/java/school/faang/user_service/service/SpringContextTest.java
+++ src/test/java/school/faang/user_service/service/SpringContextTest.java
@@ -2,11 +2,17 @@ package school.faang.user_service.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import school.faang.user_service.UserServiceApplication;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class SpringContextTest {
 
     @Test
-    public void testSpringContextInitialization() {
+    public void testSpringContextInitialization(ApplicationContext applicationContext) {
+        assertNotNull(applicationContext);
+        assertNotNull(applicationContext.getBean(UserServiceApplication.class));
     }
 }

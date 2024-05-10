c831121a774f035fbfa0321523f056d571abb5ba
nadir <den4ik.strelok@gmail.com>
2024-05-10 21:21:25 +0300

    Создал тест, который всегда падает

---
+++ src/test/java/school/faang/user_service/FailedTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FailedTest {
+    @Test
+    void failingTest() {
+        fail();
+    }
+}

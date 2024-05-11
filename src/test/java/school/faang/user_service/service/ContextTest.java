84b926584decfc13a4e8fc4824f747e628e594d5
ekoregin <evgkoregin@gmail.com>
2024-05-11 09:33:30 +0300

    BJS2-6450: Test Spring context

---
+++ src/test/java/school/faang/user_service/service/ContextTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContextTest {
+
+    @Test
+    void contextLoads() {
+    }
+}

ef9d8c95aafa292a1ba0b8294f6a91d86cd22131
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-05-12 13:24:49 +0300

    resolved conflicts and add controller

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -1,11 +1,21 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.service.user.UserService;
 
 @RestController
-@RequestMapping("uesrs")
+@RequestMapping("users")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+    
+    @PostMapping("deactivate/{id}")
+    public void deactivateUser(@PathVariable Long id) {
+        userService.deactivateUserById(id);
+    }
 }

feeb1b08a617db8988fb21e419c1759fd29ea6e2
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-05-14 09:22:49 +0300

    add swagger description

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -1,5 +1,8 @@
 package school.faang.user_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +13,14 @@ import school.faang.user_service.service.user.UserService;
 @RestController
 @RequestMapping("users")
 @RequiredArgsConstructor
+@Tag(name = "Users")
 public class UserController {
 
     private final UserService userService;
-    
+
+    @Operation(summary = "Deactivate user")
     @PostMapping("deactivate/{id}")
-    public void deactivateUser(@PathVariable Long id) {
+    public void deactivateUser(@Parameter @PathVariable Long id) {
         userService.deactivateUserById(id);
     }
 }

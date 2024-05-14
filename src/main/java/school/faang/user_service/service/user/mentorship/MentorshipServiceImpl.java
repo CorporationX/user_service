6d73478398a64a83a96bb8e70623cd3332c8eca5
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-05-14 09:20:38 +0300

    fix mentee goals

--- src/main/java/school/faang/user_service/service/user/mentorship/MentorshipServiceImpl.java
+++ src/main/java/school/faang/user_service/service/user/mentorship/MentorshipServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.repository.goal.GoalRepository;
 
 import java.util.List;
 
@@ -11,10 +12,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MentorshipServiceImpl implements MentorshipService {
 
+    private final GoalRepository goalRepository;
+
     @Override
     @Transactional
     public void deleteMentorFromMentee(User mentor) {
         List<User> mentees = mentor.getMentees();
-        mentees.forEach(mentee -> mentee.getMentors().remove(mentor));
+        mentees.forEach(mentee -> {
+            mentee.getMentors().remove(mentor);
+            mentee.getSetGoals().stream()
+                    .filter(goal -> goal.getMentor().equals(mentor))
+                    .forEach(goal -> {
+                        goal.setMentor(mentee);
+                        goalRepository.save(goal);
+                    });
+        });
     }
 }

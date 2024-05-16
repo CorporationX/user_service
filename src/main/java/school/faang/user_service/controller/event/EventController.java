5f0d4b87cdb674667869d1e2c46b0e2d179cd623
Unforgiven <mail.unforgiven@gmail.com>
2024-05-16 16:41:28 -0600

    Added @RequestMapping("/events") annotation.

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.event.EventFilterDto;
@@ -16,6 +17,7 @@ import school.faang.user_service.validator.event.EventValidator;
 import java.util.List;
 
 @RestController
+@RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
     private final EventValidator eventValidator;

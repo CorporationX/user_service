fd648461fcb3ab922e788b562d45006d6490b261
Aleksey <lekha.bolt2004@gmail.com>
2024-05-19 20:44:01 +0300

    Добавил аннотации

--- src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
+++ src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
@@ -1,30 +1,35 @@
 package school.faang.user_service.controller.event;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.service.event.EventParticipationService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/events")
 public class EventParticipationController {
 
     private final EventParticipationService eventParticipationService;
 
+    @PostMapping("/register")
     public UserDto registerParticipant(long eventId, long userId) {
         return eventParticipationService.registerParticipant(eventId, userId);
     }
 
+    @DeleteMapping("/unregister")
     public void unregisterParticipant(long eventId, long userId) {
         eventParticipationService.unregisterParticipant(eventId, userId);
     }
 
+    @GetMapping("/{eventId}/participants")
     public List<UserDto> getParticipants(long eventId) {
         return eventParticipationService.getParticipants(eventId);
     }
 
+    @GetMapping("/events/{eventId}/participants/count")
     public int getParticipantsCount(long eventId) {
         return eventParticipationService.getParticipantsCount(eventId);
     }

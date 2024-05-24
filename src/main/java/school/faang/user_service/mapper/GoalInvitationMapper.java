48fd9a79c6ec4af39768bb223b4a4d1f6f420179
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-05-24 18:20:07 +0300

    Убрал @Component с мапперов, убрал unmappedTargetPolicy с UserMapper

--- src/main/java/school/faang/user_service/mapper/GoalInvitationMapper.java
+++ src/main/java/school/faang/user_service/mapper/GoalInvitationMapper.java
@@ -3,13 +3,12 @@ package school.faang.user_service.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
 import school.faang.user_service.entity.goal.GoalInvitation;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-@Component
 public interface GoalInvitationMapper {
+
     GoalInvitation toEntity(GoalInvitationDto goalInvitationDto);
 
 
--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -1,13 +1,10 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
-import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.entity.User;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-@Component
+@Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
 }

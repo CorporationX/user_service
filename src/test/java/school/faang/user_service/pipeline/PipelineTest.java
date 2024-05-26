4b5ff2804b1ba11285b04faeab2ff23536e48d54
Aleksey <lekha.bolt2004@gmail.com>
2024-05-26 15:11:46 +0300

    проба

---
+++ src/test/java/school/faang/user_service/pipeline/PipelineTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.pipeline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PipelineTest {
+    @Test
+    void failingTest() {
+        fail("This test is meant to fail");
+    }
+}

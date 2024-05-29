9b93be5dda1fab9d74fb52af3f458dfa3d153292
ethiqque <danielpyld.work@gmail.com>
2024-05-29 13:27:21 +0300

    fix

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -1,12 +1,8 @@
 package school.faang.user_service.mapper;
 
-import com.json.student.Person;
-import com.json.student.PersonSchemaV2;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
-import org.mapstruct.factory.Mappers;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.entity.User;
 

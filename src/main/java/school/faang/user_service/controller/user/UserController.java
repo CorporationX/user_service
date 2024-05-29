def7aaa9b301ad9e6b105dd8946ef60f605094ba
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-05-29 15:57:53 +0300

    Добавил аннотацию min

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -1,7 +1,9 @@
 
 package school.faang.user_service.controller.user;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,12 @@ import school.faang.user_service.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class UserController {
     private final UserService userService;
 
     @GetMapping("/users/{userId}")
-    UserDto getUser(@PathVariable long userId) {
+    UserDto getUser(@Min(1) @PathVariable long userId) {
         return userService.getUser(userId);
     }
 }

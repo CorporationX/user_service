a8fd1fdbcd1eccaa705e2611d4d9238910aa7cd8
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-06-04 12:29:38 +0300

    Добавил аннотацию на UserController

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -3,9 +3,11 @@ package school.faang.user_service.controller.user;
 
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.service.UserService;
@@ -17,6 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/users/{userId}")
+    @ResponseStatus(HttpStatus.OK)
     UserDto getUser(@Min(1) @PathVariable long userId) {
         return userService.getUser(userId);
     }

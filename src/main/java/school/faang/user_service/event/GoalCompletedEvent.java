8043fc93175401dd6597d5c9db714815664eaf16
Tem <tipokruto014@gmail.com>
2024-06-05 22:36:56 +0300

    serialization fixes

--- src/main/java/school/faang/user_service/event/GoalCompletedEvent.java
+++ src/main/java/school/faang/user_service/event/GoalCompletedEvent.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 public class GoalCompletedEvent {
     private Long userId;
     private Long goalId;
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime completedAt;
 }

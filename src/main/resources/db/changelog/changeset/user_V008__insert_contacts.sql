9a43e7d4414d0fd96396cfa5ce14c2c2fca0a353
Muhammad <dzhabrailov3.muhammad@yandex.ru>
2024-06-10 03:37:04 +0300

    Заполнены все таблицы мок данными

---
+++ src/main/resources/db/changelog/changeset/user_V008__insert_contacts.sql
@@ -0,0 +1,12 @@
+ INSERT INTO contact (user_id, contact, "type")
+ VALUES
+     (1, 'https://github.com/john-doe', 0),
+     (2, 'https://web.telegram.org/JaneSmith', 1),
+     (3, 'https://vk.com/id55243223', 2),
+     (4, 'https://facebook.com/EmilyDavis', 3),
+     (5, 'https://facebook.com/WilliamTaylor', 3),
+     (6, 'https://facebook.com/OliviaAnderson', 3),
+     (7, 'https://www.instagram.com/JamesWilson', 4),
+     (8, 'https://www.whatsapp.com/id45525200034', 5),
+     (9, 'https://github.com/benjamin-thompson', 0),
+     (10, 'https://custom.org/ava-harris', 6);
---
+++ src/main/resources/db/changelog/changeset/user_V022__insert_skill_requests.sql
@@ -0,0 +1,9 @@
+INSERT INTO skill_request (request_id,skill_id) VALUES
+	 (10,13),
+	 (11,29),
+	 (12,2),
+	 (13,17),
+	 (14,19),
+	 (15,6),
+	 (16,24),
+	 (17,5);

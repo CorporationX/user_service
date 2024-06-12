77678b88589ccbb06bb82fc81172d3764ee9d2ca
Murzin34 <murzinvolg@yandex.ru>
2024-06-12 13:44:03 +0300

    resolve conflicts

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,4 +1,5 @@
 package school.faang.user_service.dto;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;

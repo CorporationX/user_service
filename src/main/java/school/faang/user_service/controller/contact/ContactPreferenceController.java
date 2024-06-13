27e83fa383f7919eb8a80f331eb279d1b28e746a
Tem <tipokruto014@gmail.com>
2024-06-14 01:09:05 +0300

    change mapping

--- src/main/java/school/faang/user_service/controller/contact/ContactPreferenceController.java
+++ src/main/java/school/faang/user_service/controller/contact/ContactPreferenceController.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.contact.ContactPreferenceDto;
 import school.faang.user_service.service.contact.ContactPreferenceService;
 
 @Tag(name = "Contacts")
-@RestController("/contact")
+@RestController
+@RequestMapping("/contacts/preferences")
 @RequiredArgsConstructor
 public class ContactPreferenceController {
 

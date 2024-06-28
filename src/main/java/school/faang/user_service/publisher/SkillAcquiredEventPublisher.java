3ef1babcf018f2f22e99369693b97b6d3098be4d
ekoregin <evgkoregin@gmail.com>
2024-06-28 14:34:22 +0300

    BJS2-17065: Fix bag in user_service

--- src/main/java/school/faang/user_service/publisher/SkillAcquiredEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/SkillAcquiredEventPublisher.java
@@ -4,10 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.SkillAcquiredEvent;
 
 @Slf4j
 @Service
-public class SkillAcquiredEventPublisher extends AbstractPublisher {
+public class SkillAcquiredEventPublisher extends AbstractPublisher<SkillAcquiredEvent> {
 
     public SkillAcquiredEventPublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic skillTopic) {
         super(redisTemplate);

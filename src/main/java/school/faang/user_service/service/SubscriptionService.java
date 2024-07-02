252b9ec1b41482e7fd60c61f3b427fd994fcbbaf
Kirill Maltsev <opp.2211@gmail.com>
2024-07-03 00:58:21 +0500

    test: Подписаться на пользователя

--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -16,8 +16,9 @@ public class SubscriptionService {
     public void followUser(long followerId, long followeeId) {
         boolean isAlreadyFollow = subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, followeeId);
         if (isAlreadyFollow) {
-            throw new DataValidationException("Follower is already exists");
+            throw new DataValidationException("Following is already exists");
         }
+        // нужно ли проверять существуют ли вообще такие пользователи? типо userRepository.existsById
 
         subscriptionRepository.followUser(followerId, followeeId);
     }

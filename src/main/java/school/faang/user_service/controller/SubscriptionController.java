356062c935dd66555fdfb020d915b22a909ca97d
Kirill Maltsev <opp.2211@gmail.com>
2024-07-03 16:44:51 +0500

    fix: controller mapping

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -5,19 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.SubscriptionService;
 
 @RestController
+@RequestMapping("/users/{userId}/subscriptions")
 @RequiredArgsConstructor
 @Validated
 public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
 
-    @PostMapping("/subscribe/{followerId}/{followeeId}")
-    public void followUser(@PathVariable @Positive long followerId, @PathVariable @Positive long followeeId) {
+    @PostMapping("/{followerId}")
+    public void followUser(@PathVariable @Positive long followerId, @PathVariable("userId") @Positive long followeeId) {
         if (followerId == followeeId) {
             throw new DataValidationException("Unable to follow yourself");
         }

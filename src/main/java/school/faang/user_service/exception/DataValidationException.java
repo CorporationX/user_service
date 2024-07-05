207eab0680fecb116dc80dc566979c09ffdd7f60
Grisha Tkach <grisatkac@mail.ru>
2024-07-05 19:44:37 +0300

    add custom exception

---
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class DataValidationException extends RuntimeException{
+
+    public DataValidationException(String message) {
+        super(message);
+    }
+}

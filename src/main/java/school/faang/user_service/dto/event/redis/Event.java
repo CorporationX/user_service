8c606b7f4f58b28799c01daa89efb78e84ccc4fe
Rick <RickHammerson@ya.ru>
2024-07-06 01:40:11 +0300

    Небольшой рефактор

--- src/main/java/school/faang/user_service/dto/event/redis/Event.java
+++ src/main/java/school/faang/user_service/dto/event/redis/Event.java
@@ -2,14 +2,13 @@ package school.faang.user_service.dto.event.redis;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public abstract class Event {
     private Long id;
-    private LocalDateTime createdAt;
-    
-    public Event() {
-        createdAt = LocalDateTime.now();
-    }
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
--- src/main/java/school/faang/user_service/dto/event/redis/SubscriptionEvent.java
+++ src/main/java/school/faang/user_service/dto/event/redis/SubscriptionEvent.java
@@ -1,10 +1,14 @@
 package school.faang.user_service.dto.event.redis;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubscriptionEvent extends Event {
     private Long projectId;
     private Long followerId;

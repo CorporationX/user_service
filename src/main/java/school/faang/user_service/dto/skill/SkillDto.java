e1d8362c2497ae9957aad58f212cde8d6b109ce8
Grisha Tkach <grisatkac@mail.ru>
2024-07-05 19:44:08 +0300

    add skill dto's

---
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -0,0 +1,17 @@
+package school.faang.user_service.dto.skill;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SkillDto {
+
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, message = "Name must be have minimum 3 characters")
+    private String title;
+}

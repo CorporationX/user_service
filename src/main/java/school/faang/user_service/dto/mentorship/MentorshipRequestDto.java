b6bbc54871b6712b9ddd4c17e8c0c1ab0abab201
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-07-07 23:57:10 +0500

    Изменить формат timestamp в DTO

--- src/main/java/school/faang/user_service/dto/mentorship/MentorshipRequestDto.java
+++ src/main/java/school/faang/user_service/dto/mentorship/MentorshipRequestDto.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.dto.mentorship;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import school.faang.user_service.dto.DtoValidationConstraints;
@@ -17,6 +18,8 @@ public class MentorshipRequestDto {
     private Long receiverId;
     private RequestStatus requestStatus;
     private String rejectionReason;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime updatedAt;
 }

6ef9c922c5c15973fe2218c72c12d33e20cc4680
Caveri95 <ura22051995@gmail.com>
2024-07-08 23:13:11 +0300

    test-BJS2-15586
    - Создал провальный тест для проверки срабатывания пайплайна на запуск тестов при PR

---
+++ src/test/java/school/faang/user_service/service/TestPipeline.java
@@ -0,0 +1,13 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestPipeline {
+
+    @Test
+    public void test() {
+        String expected = "expected";
+        Assertions.assertEquals(expected, "fail");
+    }
+}

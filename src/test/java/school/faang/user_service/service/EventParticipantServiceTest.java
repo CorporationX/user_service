dfc41392966483015c1da879c828f1ac99c6ac87
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-07-08 09:52:43 +0300

    12

--- src/test/java/school/faang/user_service/service/EventParticipantServiceTest.java
+++ src/test/java/school/faang/user_service/service/EventParticipantServiceTest.java
@@ -4,12 +4,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import school.faang.user_service.Service.event.EventParticipationService;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.repository.event.EventParticipationRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 public class EventParticipantServiceTest {
+
+    private EventParticipationRepository eventParticipationRepository;
+    private EventParticipationService eventParticipationService;
+
+    @BeforeEach
+    public void setUp(){
+        eventParticipationRepository = mock(EventParticipationRepository.class);
+        eventParticipationService = new EventParticipationService();
+    }
 
     @Test
     void registerPatricipant_UserRegistred_ThrowException() {

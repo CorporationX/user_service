05a46a13adaba11251050903b7aa7224552c0cf8
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-07-11 01:00:28 +0300

    Add RecommendationRequestDto

---
+++ src/main/java/school/faang/user_service/dto/recommendation/RecommendationRequestDto.java
@@ -0,0 +1,31 @@
+package school.faang.user_service.dto.recommendation;
+
+
+import lombok.Data;
+import school.faang.user_service.entity.RequestStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class RecommendationRequestDto {
+    private Long id;
+    private String message;
+    private RequestStatus status;
+    private List<Long> skillIds;
+    private Long requesterId;
+    private Long receiverId;
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+
+    public RecommendationRequestDto(RecommendationRequestDto other) {
+        this.id = other.id;
+        this.message = other.message;
+        this.status = other.status;
+        this.skillIds = List.copyOf(other.skillIds);
+        this.requesterId = other.requesterId;
+        this.receiverId = other.receiverId;
+        this.createAt = other.createAt;
+        this.updatedAt = other.updatedAt;
+    }
+}

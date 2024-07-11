2571cb79a972ccd51482e2ca94f62975a3a58d89
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-07-11 14:16:24 +0300

    Add constructor generation annotation

--- src/main/java/school/faang/user_service/dto/recommendation/RecommendationRequestDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RecommendationRequestDto.java
@@ -1,13 +1,17 @@
 package school.faang.user_service.dto.recommendation;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import school.faang.user_service.entity.RequestStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecommendationRequestDto {
     private Long id;
     private String message;
--- src/main/java/school/faang/user_service/dto/recommendation/RecommendationRequestFilterDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RecommendationRequestFilterDto.java
@@ -1,11 +1,13 @@
 package school.faang.user_service.dto.recommendation;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import school.faang.user_service.entity.RequestStatus;
 
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
 public class RecommendationRequestFilterDto {
     Long requesterId;
     Long receiverId;

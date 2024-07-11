844dd52682944d3c0250c52d1068f809fde7ef33
Grisha Tkach <grisatkac@mail.ru>
2024-07-11 12:31:09 +0300

    add constructors

--- src/main/java/school/faang/user_service/dto/skill/SkillDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -2,11 +2,15 @@ package school.faang.user_service.dto.skill;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SkillDto {
 
     private Long id;

cff4647f44f81c82e175ff8dd8545f5688828d1a
Andrei Kuzmin <Andreytko@mail.ru>
2024-07-12 20:25:51 +0300

    working on mistakes

--- src/main/java/school/faang/user_service/mapper/ListOfSkillsCandidateMapper.java
+++ src/main/java/school/faang/user_service/mapper/ListOfSkillsCandidateMapper.java
@@ -8,9 +8,14 @@ import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.entity.Skill;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ListOfSkillsCandidateMapper {
@@ -18,16 +23,9 @@ public interface ListOfSkillsCandidateMapper {
     SkillMapper skillMapper = Mappers.getMapper(SkillMapper.class);
 
     default List<SkillCandidateDto> listToSkillCandidateDto(List<Skill> skills) {
-        List<SkillDto> skillDtos = skills.stream().map(skill -> skillMapper.toDto(skill)).toList();
-        Set<SkillDto> uniqueSkills = new HashSet<>(skillDtos);
-        List<SkillCandidateDto> candidateDtos = new ArrayList<>();
-        uniqueSkills.forEach(skill -> {
-            long amount = skillDtos.stream().filter(x -> x.equals(skill)).count();
-            SkillCandidateDto skillCandidateDto = new SkillCandidateDto();
-            skillCandidateDto.setSkill(skill);
-            skillCandidateDto.setOffersAmount(amount);
-            candidateDtos.add(skillCandidateDto);
-        });
-        return candidateDtos;
+        Map<SkillDto, Long> dtosAndQuantities =  skills.stream().collect(Collectors.groupingBy(skillMapper::toDto, Collectors.counting()));
+        List<SkillCandidateDto> skillCandidateDtos = new ArrayList<>();
+        dtosAndQuantities.entrySet().stream().forEach(entry->skillCandidateDtos.add(new SkillCandidateDto(entry.getKey(), entry.getValue())));
+        return skillCandidateDtos;
     }
 }

0dceff0cda8ef032ce6f27363ae51de1111f1abc
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-07-13 20:08:02 +0300

    Changing builder annotation

--- src/main/java/school/faang/user_service/dto/recommendation/RecommendationRequestDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RecommendationRequestDto.java
@@ -2,6 +2,7 @@ package school.faang.user_service.dto.recommendation;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import school.faang.user_service.entity.RequestStatus;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class RecommendationRequestDto {
     private Long id;
     private String message;

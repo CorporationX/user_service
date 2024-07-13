96b983f3e9604b642b1070dc9537ada6ca273929
Enemy095 <salam.tahaev@mail.ru>
2024-07-13 09:22:49 +0300

    Creating and configuring skills

---
+++ src/main/java/school/faang/user_service/dto/SkillDto.java
@@ -0,0 +1,19 @@
+package school.faang.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SkillDto {
+    private Long id;
+    private String title;
+    public List<Long> userIds;
+}
---
+++ src/main/java/school/faang/user_service/validator/SkillValidator.java
@@ -0,0 +1,36 @@
+package school.faang.user_service.validator;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.SkillDto;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.repository.SkillRepository;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class SkillValidator {
+    private final SkillRepository skillRepository;
+
+    public void validateSkillTitleIsNotNullAndNotBlank(SkillDto skill) {
+        if (skill.getTitle() == null || skill.getTitle().isBlank()) {
+            log.error("skill title is null or empty");
+            throw new DataValidationException("title is null or empty");
+        }
+    }
+
+    public void validateSkillTitleDosNotExists(SkillDto skill) {
+        if (skillRepository.existsByTitle(skill.getTitle())) {
+            log.error("skill title already exists");
+            throw new DataValidationException("the skill already exists");
+        }
+    }
+
+    public void validateSkillExistenceById(Long id) {
+        if (skillRepository.findById(id).isEmpty()) {
+            log.error("skill does not exist");
+            throw new DataValidationException("the skill does not exist");
+        }
+    }
+}

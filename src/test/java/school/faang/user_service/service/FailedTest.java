246ab3127e50a68fd22db184515c6ba4c67e17a2
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-07-14 05:13:30 +0300

    Add FailedTest

---
+++ src/test/java/school/faang/user_service/service/FailedTest.java
@@ -0,0 +1,15 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FailedTest {
+    @DisplayName("Test to check the functionality of the ci-pipeline")
+    @Test
+    void failedTest(){
+        fail("This test should fail");
+    }
+}
+

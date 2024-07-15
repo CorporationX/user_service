794ad6dfd2e06b190a4af6d0a479c523e33dd9bb
Grisha Tkach <grisatkac@mail.ru>
2024-07-15 09:52:49 +0300

    delete noArgs, AllArgs constructors

--- src/main/java/school/faang/user_service/dto/user/UserFilterDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserFilterDto.java
@@ -1,14 +1,10 @@
 package school.faang.user_service.dto.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserFilterDto {
     private String namePattern;
     private String emailPattern;

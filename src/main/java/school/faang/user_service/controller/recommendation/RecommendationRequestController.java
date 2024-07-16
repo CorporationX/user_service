7b57397e738811994e2c852ca8a758015c71000c
Gevorg <a0964593@gmail.com>
2024-07-16 12:27:25 +0300

    Done task + tests

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.dto.recommendation.RejectionDto;
 import school.faang.user_service.dto.recommendation.RequestFilterDto;
@@ -20,33 +21,29 @@ import java.util.List;
 
 @RequestMapping("/recommendation")
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class RecommendationRequestController {
     public final RecommendationRequestService recommendationRequestService;
 
     @PostMapping
-    @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
     public RecommendationRequestDto requestRecommendation(@RequestBody RecommendationRequestDto recommendationRequestDto) {
         return recommendationRequestService.create(recommendationRequestDto);
     }
 
     @GetMapping("/requests")
-    @ResponseBody
     @ResponseStatus(HttpStatus.FOUND)
     public List<RecommendationRequestDto> getRecommendationRequests(@RequestBody RequestFilterDto filter) {
         return recommendationRequestService.getRequests(filter);
     }
 
     @GetMapping("/request/{id}")
-    @ResponseBody
     @ResponseStatus(HttpStatus.FOUND)
     public RecommendationRequestDto getRecommendationRequest(@PathVariable Long id) {
         return recommendationRequestService.getRequest(id);
     }
 
     @PutMapping("/reject/{requestId}")
-    @ResponseBody
     @ResponseStatus(HttpStatus.ACCEPTED)
     public RecommendationRequestDto rejectRequest(@PathVariable Long requestId, @RequestBody RejectionDto rejection) {
         return recommendationRequestService.rejectRequest(requestId, rejection);

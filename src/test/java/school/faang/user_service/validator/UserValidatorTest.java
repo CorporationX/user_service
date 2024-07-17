aabe53caf82c44a0037bf1d87eab5cc21a5f7abe
Gevorg <a0964593@gmail.com>
2024-07-17 07:43:04 +0300

    Done task + tests

---
+++ src/test/java/school/faang/user_service/validator/UserValidatorTest.java
@@ -0,0 +1,37 @@
+package school.faang.user_service.validator;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import school.faang.user_service.repository.UserRepository;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+
+public class UserValidatorTest {
+    @InjectMocks
+    UserValidator userValidator;
+
+    @Mock
+    UserRepository userRepository;
+
+    private long userId;
+
+    @BeforeEach
+    public void setUp() {
+        userId = 1L;
+
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testValidateUserId() {
+        when(userRepository.existsById(userId)).thenReturn(false);
+
+        assertThrows(EntityNotFoundException.class,
+                () -> userValidator.validateUserId(userId));
+    }
+}

a083bc9e1221ed7d9e72a022f8d7ba0409aefabf
alibaysarov <alibajsarov353@gmail.com>
2024-07-19 00:55:36 +0400

    added tests

---
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -0,0 +1,15 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+@SpringBootTest
+class UserServiceTest {
+    @Test
+    public void testFailure() {
+        fail();
+    }
+}

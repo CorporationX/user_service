ba314b155dd35e8285fefc8dd36a81176b8ac26b
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-07-20 16:43:04 +0300

    BJS2-16730 - получение пользователей

---
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -0,0 +1,7 @@
+package school.faang.user_service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -0,0 +1,7 @@
+package school.faang.user_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}

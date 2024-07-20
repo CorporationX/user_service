160fb4baaf44985d1792e35107376f5f9f87a4fb
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-07-20 17:38:36 +0300

    BJS2-16698 - заменил void методы и нейминг тестов

--- src/main/java/school/faang/user_service/service/UserService.java
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -1,9 +1,13 @@
 package school.faang.user_service.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.mapper.UserMapper;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserMapper userMapper;
+
+
 }

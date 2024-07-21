33229c721c75a37a34913ed45e2bdb3929bb89d4
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-07-21 13:00:35 +0300

    BJS2-16698 - заменил void методы и нейминг тестов

--- src/test/java/school/faang/user_service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -1,2 +1,22 @@
 package school.faang.user_service.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import school.faang.user_service.repository.UserRepository;
+
+@ExtendWith(Extension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @InjectMocks
+    private UserService service;
+
+    @Test
+    public
+}
+

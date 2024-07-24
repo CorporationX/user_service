61498259ae11a4edb869ffe443293af86f07093d
dobrevd <dobrev@ukr.net>
2024-07-24 10:29:42 +0200

    BJS2-16907 revised

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,7 +115,7 @@ jacoco {
     reportsDirectory.set(layout.buildDirectory.dir("$buildDir/reports/jacoco"))
 }
 tasks.test {
-//    finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestReport)
 }
 tasks.jacocoTestReport {
     dependsOn(tasks.test)

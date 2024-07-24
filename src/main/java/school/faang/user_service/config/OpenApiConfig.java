6dd6e117e29d0d8c4865ebabe9de238b710d99e4
Kirill Maltsev <opp.2211@gmail.com>
2024-07-24 05:23:53 +0500

    feat: set up swagger

---
+++ src/main/java/school/faang/user_service/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package school.faang.user_service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User Service Api",
+                //description = "",
+                version = "1.0.0"
+        )
+)
+public class OpenApiConfig {
+}
--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.controller;
 
-import jakarta.validation.constraints.NotNull;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
@@ -18,6 +20,7 @@ import school.faang.user_service.service.SubscriptionService;
 
 import java.util.List;
 
+@Tag(name = "Подписки", description = "Пользователи могут подписываться друг на друга")
 @RestController
 @RequestMapping("/users/{userId}/subscriptions")
 @RequiredArgsConstructor
@@ -26,6 +29,10 @@ public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
 
+    @Operation(summary = "Подписаться на пользователя",
+            description = "Позволяет пользователю с id={followeeId} подписаться на пользователя " +
+                    "с id={{followerId}}"
+    )
     @PostMapping("/{followerId}")
     public void followUser(@PathVariable @Positive(message = "Id should be positive")
                            long followerId,
@@ -36,6 +43,10 @@ public class SubscriptionController {
         subscriptionService.followUser(followerId, followeeId);
     }
 
+    @Operation(summary = "Отписаться от пользователя",
+            description = "Позволяет пользователю с id={followeeId} отписаться от пользователя " +
+                    "с id={{followerId}}"
+    )
     @DeleteMapping("/{followerId}")
     public void unfollowUser(@PathVariable @Positive long followerId, @PathVariable("userId") @Positive long followeeId) {
 
@@ -43,16 +54,28 @@ public class SubscriptionController {
         subscriptionService.unfollowUser(followerId, followeeId);
     }
 
+    @Operation(summary = "Получить всех подписчиков пользователя",
+            description = "Позволяет получить всех подписчиков пользователя с id={followeeId}, " +
+                    "согласно переданному обязательному фильтру"
+    )
     @GetMapping
-    public List<UserDto> getFollowers(@PathVariable("userId") @Positive long followeeId, @NotNull UserFilterDto filter) {
+    public List<UserDto> getFollowers(@PathVariable("userId") @Positive long followeeId, @RequestBody UserFilterDto filter) {
         return subscriptionService.getFollowers(followeeId, filter);
     }
 
+    @Operation(summary = "Получить количество всех подписчиков пользователя",
+            description = "Позволяет получить количество всех подписчиков пользователя с id={followeeId}"
+    )
     @GetMapping("/count")
     public int getFollowersCount(@PathVariable("userId") @Positive long followeeId) {
         return subscriptionService.getFollowersCount(followeeId);
     }
 
+    @Operation(summary = "Получить количество всех подписок пользователя",
+            description = "Позволяет получить количество всех подписок пользователя с " +
+                    "id={followeeId} (количество всех пользователей, на которых подписался " +
+                    "данный пользователь)"
+    )
     @GetMapping("/count-followees")
     public int getFolloweesCount(@PathVariable("userId") @Positive long followerId) {
         return subscriptionService.getFolloweesCount(followerId);

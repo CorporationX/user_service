0defb0838efd5b087a3cf33934b0d881d1cea82c
Kirill Maltsev <opp.2211@gmail.com>
2024-07-25 23:07:51 +0500

    review changes

--- src/main/java/school/faang/user_service/config/OpenApiConfig.java
+++ src/main/java/school/faang/user_service/config/OpenApiConfig.java
@@ -1,22 +1,20 @@
 package school.faang.user_service.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "User Service Api",
-                description = "Сервис работы с пользователями и связанными сущностями",
-                version = "1.0.0"
-        )
-)
+
 @Configuration
 public class OpenApiConfig {
-        @Bean
-        public OpenAPI customOpenAPI() {
-                return new OpenAPI();
-        }
+    @Bean
+    public OpenAPI openAPI(@Value("${application.title}") String title,
+                           @Value("${application.version}") String version) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .version(version));
+    }
 }

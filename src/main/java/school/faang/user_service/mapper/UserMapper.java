e888eb2df2f637c5d879d92454b78aa2ae65c2ff
Alexandra <s7125@mail.ru>
2024-07-26 19:07:40 +0500

    method

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.userDto.UserDto;
 import school.faang.user_service.entity.User;
@@ -10,5 +12,16 @@ import java.util.List;
 public interface UserMapper {
     List<UserDto> toDtoList(List<User> users);
 
+    @Mapping(source = "followers", target = "followersId", qualifiedByName = "mapFollowers")
     UserDto toDto(User user);
+
+
+//    @Mapping(source = "posts", target = "postsId", qualifiedByName = "mapPost")
+//    @Mapping(source = "visibilityUsers", target = "visibilityUsersId", qualifiedByName = "mapVisibilityUsers")
+//    AlbumDto toDto(Album album);
+
+    @Named("mapFollowers")
+    default List<Long> mapFollowers(List<User> followers) {
+        return followers.stream().map(User::getId).toList();
+    }
 }

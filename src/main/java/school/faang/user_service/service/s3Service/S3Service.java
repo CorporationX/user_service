74152d804c1852c1245ce40539d01720cece223d
Grisha Tkach <grisatkac@mail.ru>
2024-07-28 01:36:28 +0300

    add amazon s3 service

---
+++ src/main/java/school/faang/user_service/service/s3Service/S3Service.java
@@ -0,0 +1,61 @@
+package school.faang.user_service.service.s3Service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.exception.s3.FileDownloadException;
+import school.faang.user_service.exception.s3.FileUploadException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3 amazonS3Client;
+    @Value("${aws.bucket-name}")
+    private String bucketName;
+
+    public String uploadFile(File file, String folder) {
+        String key = String.format("%s/%d_%s", folder, System.currentTimeMillis(), file.getName());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        try {
+            objectMetadata.setContentType(Files.probeContentType(file.toPath()));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new FileUploadException(e.getMessage());
+        }
+
+        objectMetadata.setContentLength(file.length());
+
+        try {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, new FileInputStream(file), objectMetadata);
+            amazonS3Client.putObject(putObjectRequest);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new FileUploadException(e.getMessage());
+        }
+
+        return key;
+    }
+
+    public InputStream getFile(String key) {
+        try {
+            S3Object object = amazonS3Client.getObject(bucketName, key);
+            return object.getObjectContent();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new FileDownloadException(e.getMessage());
+        }
+    }
+}

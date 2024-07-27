e2284dd7b5e591398be8b675e58f49325809e8ad
Grisha Tkach <grisatkac@mail.ru>
2024-07-28 01:36:06 +0300

    add exception handlers

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.exception.skill;
+package school.faang.user_service.exception;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,13 +10,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import school.faang.user_service.exception.randomAvatar.AvatarGenerationException;
+import school.faang.user_service.exception.s3.FileDownloadException;
+import school.faang.user_service.exception.s3.FileUploadException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class SkillExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -35,6 +38,21 @@ public class SkillExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DataValidationException.class)
     public ResponseEntity<Object> handleDataValidationException(DataValidationException ex) {
         return new ResponseEntity<>(response(ex), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AvatarGenerationException.class)
+    public ResponseEntity<Object> handleAvatarGenerationException(AvatarGenerationException ex) {
+        return new ResponseEntity<>(response(ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<Object> handleFileUploadException(FileUploadException ex) {
+        return new ResponseEntity<>(response(ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(FileDownloadException.class)
+    public ResponseEntity<Object> handleFileDownloadException(FileDownloadException ex) {
+        return new ResponseEntity<>(response(ex), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private Map<String, String> response(Exception ex) {

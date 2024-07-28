a6e3395fe3b8dd0ecacbad723022d03eb21e1689
Enemy095 <salam.tahaev@mail.ru>
2024-07-28 17:51:05 +0300

    add GlobalExceptionHandler{}

---
+++ src/main/java/school/faang/user_service/controller/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package school.faang.user_service.controller;
+
+import feign.FeignException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import school.faang.user_service.dto.ErrorResponse;
+import school.faang.user_service.exception.DataValidationException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataValidationException(DataValidationException e) {
+        log.error("Data validation exception", e);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,String> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Method argument not valid", e);
+        return getMapWithFieldsThatHaveNotPassedValidation(e);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleEntityNotFoundException(EntityNotFoundException e) {
+        log.error("Entity not found", e);
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleRuntimeException(RuntimeException e) {
+        log.error("Runtime exception", e);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(FeignException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+
+    private HashMap<String, String> getMapWithFieldsThatHaveNotPassedValidation(MethodArgumentNotValidException e) {
+        return new HashMap<>(e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors
+                        .toMap(DefaultMessageSourceResolvable::getDefaultMessage,
+                                error -> Objects.requireNonNullElse(error.getDefaultMessage(), ""))
+                ));
+    }
+}

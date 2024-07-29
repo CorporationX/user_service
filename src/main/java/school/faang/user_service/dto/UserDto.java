07fb85a4e1e6e21ee6b9c94561667d0983a629fa
Andrei Kuzmin <Andreytko@mail.ru>
2024-07-29 23:47:16 +0300

    работа над ошибками + разрешение конфликтов

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -3,10 +3,12 @@ package school.faang.user_service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private Long id;
     private String username;

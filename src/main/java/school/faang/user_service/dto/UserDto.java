5f84480ca31b72475acc1e54cb7b30e5057a2179
Andrei Kuzmin <Andreytko@mail.ru>
2024-07-29 23:44:15 +0300

    работа над ошибками + разрешение конфликтов

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -3,17 +3,13 @@ package school.faang.user_service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import school.faang.user_service.entity.event.Event;
-import school.faang.user_service.entity.goal.Goal;
-
-import java.util.List;
 
 @Builder
 @Data
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+    private String username;
+    private String email;
     private boolean active;
-    private List<Goal> goals;
-    private List<Event> events;
 }
--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -7,6 +7,9 @@ import school.faang.user_service.entity.User;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
+
     UserDto toUserDto(User user);
+
     User toUser(UserDto userDto);
+
 }

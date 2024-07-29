26292fc3b38efa1275a4abba311a13aa1ed695d7
pavlick2745 <Pa4aPV@yandex.ru>
2024-07-29 21:06:35 +0700

    BJS2-15606 - "Интеграция с генератором случайных аватаров юзера" Исправление конфликта слияния

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -1,27 +1,33 @@
 package school.faang.user_service.service.user;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.user.ReadUserDto;
 import school.faang.user_service.dto.user.WriteUserDto;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.mapper.user.UserToReadUserDtoMapper;
 import school.faang.user_service.mapper.user.WriteUserDtoToUserMapper;
 import school.faang.user_service.repository.UserRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
     private final WriteUserDtoToUserMapper writeUserDtoToUserMapper;
     private final UserToReadUserDtoMapper userToReadUserDtoMapper;
     private final AvatarService avatarService;
+
+    public List<UserDto> getUsersDtoByIds(List<Long> ids) {
+        return userMapper.usersToUserDTOs(userRepository.findAllById(ids));
+    }
 
     @Transactional
     public ReadUserDto createUser(WriteUserDto writeUserDto) {
@@ -35,4 +41,6 @@ public class UserService {
                 .map(userToReadUserDtoMapper::map)
                 .orElseThrow();
     }
+
+
 }

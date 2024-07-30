4f6ebc81bbbaca07e851c06c9d9665b4d03aa5ad
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-07-30 11:13:49 +0300

    BJS2-16730 - получаем пользователя и пользователей

--- src/test/java/school/faang/user_service/controller/UserControllerTest.java
+++ src/test/java/school/faang/user_service/controller/UserControllerTest.java
@@ -1,37 +1,50 @@
 package school.faang.user_service.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.service.user.UserService;
+
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
     private static final long INVALID_ID_FOR_USER = -3L;
     private static final long VALID_USER_ID = 1L;
-
-    @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
+    @Mock
     private UserService service;
+    @InjectMocks
+    private UserController controller;
+
+    @BeforeEach
+    public void setUp() {
+        //Arrange
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testUserDtoIsNull() {
+        //Assert
+        assertThrows(
+                RuntimeException.class,
+                () -> controller.deactivatesUserProfile(INVALID_ID_FOR_USER)
+        );
+    }
 
     @Test
     public void testVerifyServiceDeactivatesUserProfile() throws Exception {
@@ -42,7 +55,7 @@ class UserControllerTest {
         dto.setGoalsIds(List.of(VALID_USER_ID));
         dto.setOwnedEventsIds(List.of(VALID_USER_ID));
         //Act
-        Mockito.when(service.deactivatesUserProfile(anyLong())).thenReturn(dto);
+        Mockito.when(service.deactivatesUserProfile(Mockito.anyLong())).thenReturn(dto);
         //Assert
         mockMvc.perform(put("/user/1"))
                 .andExpect(status().isOk())
@@ -50,19 +63,5 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.active").value(true))
                 .andExpect(jsonPath("$.goalsIds", hasSize(1)))
                 .andExpect(jsonPath("$.ownedEventsIds", hasSize(1)));
-    }
-
-    @Test
-    public void testGetUser() throws Exception {
-        UserDto firstUser = new UserDto();
-        firstUser.setId(VALID_USER_ID);
-        firstUser.setUsername("Sasha");
-        firstUser.setEmail("sasha@yandex.ru");
-        Mockito.when(service.getUser(anyLong())).thenReturn(firstUser);
-        mockMvc.perform(get("/users/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(VALID_USER_ID)))
-                .andExpect(jsonPath("$.userName", is("Sasha")))
-                .andExpect(jsonPath("$.email", is("sasha@yandex.ru")));
     }
 }

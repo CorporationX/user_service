63c56f557d1e104d27edb2de59b00012c863d579
Nikita2768 <vechkinnikita22@gmail.com>
2024-07-30 20:30:37 +0500

    Обработчик исключений в user_service fix package

--- src/main/java/school/faang/user_service/handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/handler/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.exception;
+package school.faang.user_service.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -7,6 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import school.faang.user_service.exception.ErrorResponse;
 
 import java.util.Map;
 import java.util.Objects;

44cfe3c54a34f28a4bc596b6dfc89940b5250093
Artem <xotabggg@gmail.com>
2024-08-03 12:07:34 +0700

    BJS2-15609

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -16,16 +16,16 @@ public class UserDto {
 
     private Long id;
 
-    @NotBlank(message = "username should not be blank")
-    @Size(min = 3, max = 64, message = "username length must be between 3 and 64 characters")
+    @NotBlank(message = "username should not be blank", groups = {CreateGroup.class, UpDateGroup.class})
+    @Size(min = 3, max = 64, message = "username length must be between 3 and 64 characters", groups = CreateGroup.class)
     private String username;
 
-    @NotBlank(message = "email should not be blank")
-    @Email
+    @NotBlank(message = "email should not be blank", groups = {CreateGroup.class, UpDateGroup.class})
+    @Email(groups = CreateGroup.class)
     private String email;
 
-    @NotBlank(message = "phone should not be blank")
-    @Size(max = 32, message = "phone length max 32 characters")
+    @NotBlank(message = "phone should not be blank", groups = {CreateGroup.class, UpDateGroup.class})
+    @Size(max = 32, message = "phone length max 32 characters", groups = CreateGroup.class)
     private String phone;
     private boolean isActive;
 }

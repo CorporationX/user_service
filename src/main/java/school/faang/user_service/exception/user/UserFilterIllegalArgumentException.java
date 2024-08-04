4be0926fcb10b91afc7f671e2809a7a9e2f2e6ab
Grisha Tkach <grisatkac@mail.ru>
2024-08-05 02:17:01 +0300

    rename exception

--- src/main/java/school/faang/user_service/exception/user/UserFilterIllegalArgumentException.java
+++
@@ -1,8 +0,0 @@
-package school.faang.user_service.exception.user;
-
-public class UserFilterIllegalArgumentException extends RuntimeException {
-
-    public UserFilterIllegalArgumentException(String message) {
-        super(message);
-    }
-}

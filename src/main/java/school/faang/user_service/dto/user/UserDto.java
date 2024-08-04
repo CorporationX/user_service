7cf13878d45345b877315491878b11007c1b76f6
Grisha Tkach <grisatkac@mail.ru>
2024-08-05 01:31:58 +0300

    add field

--- src/main/java/school/faang/user_service/dto/user/UserDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserDto.java
@@ -1,13 +1,20 @@
 package school.faang.user_service.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private long id;
     @NotBlank
     private String username;
     @NotBlank
     private String email;
+    private boolean active;
 }

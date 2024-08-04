a9bf09ba57ac44e8cc300d2ac0670aa0806ad2fb
Grisha Tkach <grisatkac@mail.ru>
2024-08-05 02:19:58 +0300

    update user service

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -1,13 +1,67 @@
 package school.faang.user_service.service.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserFilterDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.user.DataValidationException;
+import school.faang.user_service.mapper.user.UserMapper;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.service.user.filter.UserFilter;
+import school.faang.user_service.validation.user.filter.UserFilterValidation;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-public interface UserService {
+@Service
+@RequiredArgsConstructor
+public class UserService {
 
-    List<UserDto> getPremiumUsers(UserFilterDto userFilterDto);
-    List<UserDto> getRegularUsers(UserFilterDto userFilterDto);
+    private final UserRepository userRepository;
+    private final List<UserFilter> userFilters;
+    private final UserFilterValidation userFilterValidation;
+    private final UserMapper userMapper;
 
+    @Transactional(readOnly = true)
+    public List<UserDto> getPremiumUsers(UserFilterDto userFilterDto) {
+        if (userFilterValidation.isNullable(userFilterDto)) {
+            throw new DataValidationException("UserFilter is nullable");
+        }
+
+        List<User> premiumUsers = userRepository.findPremiumUsers().toList();
+
+        if (!userFilterValidation.isAnyFilterApplicable(userFilters, userFilterDto)) {
+            return userMapper.toUserDtoList(premiumUsers);
+        }
+
+        return filterUsers(userFilterDto, premiumUsers);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserDto> getRegularUsers(UserFilterDto userFilterDto) {
+        if (userFilterValidation.isNullable(userFilterDto)) {
+            throw new DataValidationException("UserFilter is nullable");
+        }
+
+        List<User> regularUsers = userRepository.findRegularUsers().toList();
+
+        if (!userFilterValidation.isAnyFilterApplicable(userFilters, userFilterDto)) {
+            return userMapper.toUserDtoList(regularUsers);
+        }
+
+        return filterUsers(userFilterDto, regularUsers);
+    }
+
+    private List<UserDto> filterUsers(UserFilterDto userFilterDto, List<User> users) {
+        return userFilters.stream()
+                .filter(filter -> filter.isApplicable(userFilterDto))
+                .reduce(users.stream(),
+                        (stream, filter) -> filter.apply(stream, userFilterDto),
+                        Stream::concat)
+                .map(userMapper::toUserDto)
+                .toList();
+
+    }
 }

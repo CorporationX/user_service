16fe59f293f1ca9c2573211d4e8a1110b6714bac
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-08-05 22:45:21 +0500

    Внес исправления

---
+++ src/main/java/school/faang/user_service/config/async/AsyncConfig.java
@@ -0,0 +1,33 @@
+package school.faang.user_service.config.async;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+    @Value("${spring.task.execution.pool.core-size}")
+    private int coreSize;
+    @Value("${spring.task.execution.pool.max-size}")
+    private int maxSize;
+    @Value("${spring.task.execution.pool.queue-capacity}")
+    private int queueCapacity;
+    @Value("${spring.task.execution.pool.keep-alive}")
+    private int keepAlive;
+    @Value("${spring.task.execution.pool.thread-name-prefix}")
+    private String threadName;
+
+    @Bean(name = "asyncExecutor")
+    public Executor taskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(coreSize);
+        executor.setMaxPoolSize(maxSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setKeepAliveSeconds(keepAlive);
+        executor.setThreadNamePrefix(threadName);
+        return executor;
+    }
+}
--- src/main/java/school/faang/user_service/service/premium/PremiumService.java
+++ src/main/java/school/faang/user_service/service/premium/PremiumService.java
@@ -2,31 +2,23 @@ package school.faang.user_service.service.premium;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.entity.premium.Premium;
-import school.faang.user_service.exception.ExceptionMessages;
 import school.faang.user_service.repository.premium.PremiumRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PremiumService {
-    @Value("${premium.removal.threadPool.count}")
-    private int countThread;
     private final PremiumRepository premiumRepository;
 
     @Transactional
     public void removingExpiredPremiumAccess(int batchSize) {
-        ExecutorService executorService = Executors.newFixedThreadPool(countThread);
-
         List<Premium> premiumList = premiumRepository
                 .findAllByEndDateBefore(LocalDateTime.now());
 
@@ -41,20 +33,14 @@ public class PremiumService {
             int end = Math.min(start + batchSize, premiumList.size());
             List<Premium> batch = premiumList.subList(start, end);
 
-            executorService.submit(() ->
-                    premiumRepository.deleteAllById(batch.stream()
-                            .map(Premium::getId)
-                            .toList()));
+            executeAsyncBatchDelete(batch);
         }
+    }
 
-        executorService.shutdown();
-        try {
-            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
-                executorService.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            log.error(ExceptionMessages.CLOSURE_THREAD_EXCEPTION, e);
-            throw new IllegalStateException(ExceptionMessages.CLOSURE_THREAD_EXCEPTION, e);
-        }
+    @Async("asyncExecutor")
+    public void executeAsyncBatchDelete(List<Premium> batch) {
+        premiumRepository.deleteAllById(batch.stream()
+                .map(Premium::getId)
+                .toList());
     }
 }

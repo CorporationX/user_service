db4e51a0d3824a5e37109cb461e0c2f6aa7391e8
Artem <xotabggg@gmail.com>
2024-08-09 13:53:51 +0700

    BJS2-15642

--- src/main/java/school/faang/user_service/redis/RedisMessageSubscriber.java
+++ src/main/java/school/faang/user_service/redis/RedisMessageSubscriber.java
@@ -1,28 +1,19 @@
 package school.faang.user_service.redis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
-import school.faang.user_service.dto.BanEvent;
-import school.faang.user_service.service.UserBanedService;
 
-import java.io.IOException;
+import school.faang.user_service.service.UserService;
 
 @Component
 @RequiredArgsConstructor
 public class RedisMessageSubscriber implements MessageListener {
-    private final ObjectMapper objectMapper;
-    private final UserBanedService userBanedService;
+    private final UserService userService;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        try {
-            BanEvent banEvent = objectMapper.readValue(message.getBody(), BanEvent.class);
-            userBanedService.banedUser(banEvent.getAuthorId());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        userService.createBanEvent(message);
     }
 }

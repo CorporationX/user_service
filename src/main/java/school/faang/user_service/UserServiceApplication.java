c0635e43ecfd87138f0fc032e97ec01a52bb6909
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-08-10 14:29:58 +0300

    fix

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication

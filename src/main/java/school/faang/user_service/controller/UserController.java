3fdb29799d50b5595bfb16760437f694ca39c6e9
Grisha Tkach <grisatkac@mail.ru>
2024-08-12 06:57:23 +0300

    move endpoints in another controller file

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -2,9 +2,15 @@ package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserFilterDto;
@@ -32,5 +38,20 @@ public class UserController {
     @GetMapping("/exists/{id}")
     public boolean existsById(@PathVariable Long id) {
         return userService.existsById(id);
+    }
+
+    @PostMapping("/{userId}/random-avatar")
+    @ResponseStatus(HttpStatus.OK)
+    public String generateRandomAvatar(@PathVariable Long userId) {
+        return userService.generateRandomAvatar(userId);
+    }
+
+    @GetMapping("{userId}/random-avatar")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<byte[]> getAssignedRandomAvatar(@PathVariable Long userId) {
+        byte[] userRandomAvatar = userService.getUserRandomAvatar(userId);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.valueOf("image/svg+xml"));
+        return new ResponseEntity<>(userRandomAvatar, httpHeaders, HttpStatus.OK);
     }
 }

f4cb269294cce3d9a83699f9b6ca86f3bdeea630
Grisha Tkach <grisatkac@mail.ru>
2024-08-12 06:58:57 +0300

    put another logic to separate method

--- src/main/java/school/faang/user_service/service/randomAvatar/RandomAvatarService.java
+++ src/main/java/school/faang/user_service/service/randomAvatar/RandomAvatarService.java
@@ -4,12 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
-import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.exception.randomAvatar.AvatarGenerationException;
 
 import java.io.File;
@@ -35,13 +32,16 @@ public class RandomAvatarService {
 
     public File getRandomPhoto() {
         String avatarType = avatars.get(new Random().nextInt(avatars.size()));
-        //String uri = String.format("https://api.dicebear.com/9.x/%s/%s",  avatarType.replaceAll(" ", "-").toLowerCase(), fileType);
         String uri = String.format("%s/%s/%s", url, avatarType.replaceAll(" ", "-").toLowerCase(), fileType);
 
         ResponseEntity<byte[]> response = restTemplate.exchange(uri, HttpMethod.GET, null, byte[].class);
 
         File file = new File("avatar");
 
+        return writeResponseToFile(file, response);
+    }
+
+    public File writeResponseToFile(File file, ResponseEntity<byte[]> response) {
         if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
             try (FileOutputStream fileOutputStream = new FileOutputStream(file)){
                 fileOutputStream.write(response.getBody());

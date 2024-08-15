c35e5645b2ea68e1b879a4dab0dbd77dd72a9c2d
Ekaterina Shulenina <shulenina.em@gmail.com>
2024-08-15 20:28:57 +0300

    BJS2-15998

--- src/test/java/school/faang/user_service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -5,14 +5,13 @@ import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.json.student.Address;
 import com.json.student.ContactInfo;
 import com.json.student.Person;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.multipart.MultipartFile;
 import school.faang.user_service.entity.Country;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.mapper.PersonToUserMapper;
@@ -24,9 +23,13 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("UserService CSV Processing Tests")
 public class UserServiceTest {
 
     @Mock
@@ -39,7 +42,7 @@ public class UserServiceTest {
     private CsvMapper csvMapper;
 
     @Mock
-    private PersonToUserMapper userMapper = Mappers.getMapper(PersonToUserMapper.class);
+    private PersonToUserMapper userMapper;
 
     @InjectMocks
     private UserService userService;
@@ -50,14 +53,27 @@ public class UserServiceTest {
     }
 
     @Test
+    @DisplayName("Should successfully process a valid CSV")
     public void testProcessCsv_ValidInput() throws Exception {
+        setupMockBehaviorForValidInput();
+        InputStream inputStream = createMockInputStream();
+        userService.processCsv(inputStream);
+
+        verify(csvMapper, times(1)).readerFor(Person.class);
+    }
+
+    @Test
+    @DisplayName("Should fail while parsing CSV and never save anything to repository")
+    public void testProcessCsv_ExceptionHandling() {
+        InputStream inputStream = createMockInputStream();
+        userService.processCsv(inputStream);
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    private void setupMockBehaviorForValidInput() throws Exception {
         CsvSchema schema = CsvSchema.emptySchema().withHeader();
         when(csvMapper.schemaFor(Person.class)).thenReturn(schema);
 
-        MultipartFile file = mock(MultipartFile.class);
-        when(file.isEmpty()).thenReturn(false);
-        InputStream inputStream = createMockInputStream();
-        when(file.getInputStream()).thenReturn(inputStream);
         Address address = new Address();
         address.setCountry("USA");
 
@@ -66,36 +82,23 @@ public class UserServiceTest {
         contactInfo.setPhone("1234567890");
         contactInfo.setAddress(address);
 
-        Person person = new Person();
-        person.setFirstName("John");
-        person.setLastName("J");
-        person.setContactInfo(contactInfo);
+        Person mockPerson = new Person();
+        mockPerson.setFirstName("John");
+        mockPerson.setLastName("J");
+        mockPerson.setContactInfo(contactInfo);
 
         Country country = new Country();
         country.setId(1L);
         country.setTitle("USA");
 
         User user = new User();
-        user.setUsername(userMapper.combineName(person.getFirstName(), person.getLastName()));
+        user.setUsername(userMapper.combineName(mockPerson.getFirstName(), mockPerson.getLastName()));
         user.setEmail(contactInfo.getEmail());
         user.setPhone(contactInfo.getPhone());
         user.setCountry(country);
         user.setPassword("password");
 
-        when(userMapper.personToUser(person)).thenReturn(user);
-
         var objectReader = Mockito.mock(com.fasterxml.jackson.databind.ObjectReader.class);
         when(csvMapper.readerFor(Person.class)).thenReturn(objectReader);
-
-        var iterator = Mockito.mock(com.fasterxml.jackson.databind.MappingIterator.class);
-        when(objectReader.readValues(any(InputStream.class))).thenReturn(iterator);
-        when(iterator.hasNext()).thenReturn(true, false);
-        when(iterator.next()).thenReturn(person);
-
-        userService.processCsv(file.getInputStream());
-
-        verify(userRepository, times(1)).save(user);
-        verify(countryRepository, times(1)).findAll();
     }
 }
-

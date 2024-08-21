b66415ba5968d8ec239b098408d01e6d3b49f857
Gevorg <a0964593@gmail.com>
2024-08-21 15:36:28 +0300

    done

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import school.faang.user_service.dto.ProfileViewEvent;
 import school.faang.user_service.publisher.ProfileViewEventPublisher;

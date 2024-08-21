16a0b1633346e5cd114403a6002a721b4e337287
Kvitko Aleksandr <kvitok-993@mail.ru>
2024-08-21 10:37:04 +0300

    5.1 commit

--- src/test/java/school/faang/user_service/controller/UserControllerTest.java
+++ src/test/java/school/faang/user_service/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;

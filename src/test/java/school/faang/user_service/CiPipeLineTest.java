4bb7662bc4f260633c53ad00ef4df2a76e4d688e
Sergey Shkaev <shkaev.ss@mail.ru>
2024-09-01 13:19:48 +0300

    сломанный тест

---
+++ src/test/java/school/faang/user_service/CiPipeLineTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CiPipeLineTest {
+    @Test
+    void testIncorrectPL() {
+        assertTrue(0 > 1);
+    }
+}

76009fa30654064327b7792683ff87983700c514
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-02 17:24:19 +0300

    Написал SubscriptionService

--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -1,21 +1,26 @@
 package school.faang.user_service.service;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.exceptions.DataValidationException;
+import school.faang.user_service.filter.UserFilter;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.SubscriptionRepository;
 
 import java.util.List;
 import java.util.stream.Stream;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SubscriptionService {
-    private SubscriptionRepository subscriptionRepository;
+
+    private final SubscriptionRepository subscriptionRepository;
+    private final List<UserFilter> userFilters;
+    private final UserMapper userMapper;
 
     @Transactional
     public void followUser(long followerId, long followeeId) throws DataValidationException {
@@ -49,19 +54,33 @@ public class SubscriptionService {
         }
     }
 
-    public List<UserDto> getFollowers(long followeeId, UserFilterDto filter) {
-        Stream<User> followers = subscriptionRepository.findByFolloweeId(followeeId);
-        return filterUsers(followers, filter)
-                .map(user -> new UserDto(user.getId(), user.getUsername(), user.getEmail()))
+    public List<UserDto> getFollowers(long followeeId, UserFilterDto filters) {
+        Stream<User> followers = subscriptionRepository.findByFollowerId(followeeId);
+        return filterUsers(followers, filters)
+                .map(userMapper::toDto)
                 .toList();
     }
 
-    private Stream<User> filterUsers(Stream<User> users, UserFilterDto filter) {
-        return users.filter(user ->
-                user.getUsername().matches(filter.getNamePattern()) && user.getEmail().matches(filter.getEmailPattern()));
+    public List<UserDto> getFollowing(long followeeId, UserFilterDto filters) {
+        Stream<User> followers = subscriptionRepository.findByFolloweeId(followeeId);
+        return filterUsers(followers, filters)
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    private Stream<User> filterUsers(Stream<User> followers, UserFilterDto filters) {
+        List<UserFilter> applicableFilters = userFilters.stream()
+                .filter(filter -> filter.isApplicable(filters))
+                .toList();
+        return followers
+                .filter(user -> applicableFilters.stream().allMatch(filter -> filter.apply(user, filters)));
     }
 
     public int getFollowersCount(long followerId) {
         return subscriptionRepository.findFollowersAmountByFolloweeId(followerId);
+    }
+
+    public int getFollowingCount(long followerId) {
+        return subscriptionRepository.findFolloweesAmountByFollowerId(followerId);
     }
 }

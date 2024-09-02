e1b2ff9b5fc6bdd8dcc8f115df2b9a26451350f8
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-02 10:33:45 +0300

    Написал SubscriptionService

--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -2,26 +2,62 @@ package school.faang.user_service.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.UserFilterDto;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.exceptions.DataValidationException;
 import school.faang.user_service.repository.SubscriptionRepository;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 @AllArgsConstructor
 public class SubscriptionService {
     private SubscriptionRepository subscriptionRepository;
 
+    @Transactional
     public void followUser(long followerId, long followeeId) throws DataValidationException {
-        validateSubscription(followerId, followeeId);
+        validateUserIds(followerId, followeeId);
+        validateFollowSubscription(followerId, followeeId);
         subscriptionRepository.followUser(followerId, followeeId);
     }
 
-    private void validateSubscription(long followerId, long followeeId) throws DataValidationException {
-        if (followerId == followeeId) {
-            throw new DataValidationException("User %s is trying to subscribe to himself".formatted(followerId));
-        }
-
+    private void validateFollowSubscription(long followerId, long followeeId) throws DataValidationException {
         if (subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, followeeId)) {
             throw new DataValidationException("User %d is already subscribed to user %d.".formatted(followerId, followeeId));
         }
+    }
+
+    @Transactional
+    public void unfollowUser(long followerId, long followeeId) throws DataValidationException {
+        validateUserIds(followerId, followeeId);
+        validateUnfollowSubscription(followerId, followeeId);
+        subscriptionRepository.unfollowUser(followerId, followeeId);
+    }
+
+    private void validateUnfollowSubscription(long followerId, long followeeId) throws DataValidationException {
+        if (!subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, followeeId)) {
+            throw new DataValidationException("User %d is already unsubscribe to user %d.".formatted(followerId, followeeId));
+        }
+    }
+
+    private void validateUserIds(long followerId, long followeeId) throws DataValidationException {
+        if (followerId == followeeId) {
+            throw new DataValidationException("User %s is trying to unsubscribe to himself".formatted(followerId));
+        }
+    }
+
+    public List<UserDto> getFollowers(long followeeId, UserFilterDto filter) {
+        Stream<User> followers = subscriptionRepository.findByFolloweeId(followeeId);
+        return filterUsers(followers, filter)
+                .map(user -> new UserDto(user.getId(), user.getUsername(), user.getEmail()))
+                .toList();
+    }
+
+    private Stream<User> filterUsers(Stream<User> users, UserFilterDto filter) {
+        return users.filter(user ->
+                user.getUsername().matches(filter.getNamePattern()) && user.getEmail().matches(filter.getEmailPattern()));
     }
 }

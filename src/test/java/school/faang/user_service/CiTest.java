c60a1b40c42ca1aa322d716be8f9dac766b76ca4
Sergey Shkaev <shkaev.ss@mail.ru>
2024-09-02 13:34:46 +0300

    сломанный тест

---
+++ src/test/java/school/faang/user_service/CiTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CiTest {
+    @Test
+    void testIncorrectTestMethod() {
+        assertTrue(0 > 1);
+    }
+}

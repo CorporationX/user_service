35ca6aae94a4e108c47d62be945d966170a42dc9
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-05 20:56:48 +0300

    Написал тесты для UserService

---
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -0,0 +1,65 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.repository.event.EventRepository;
+import school.faang.user_service.repository.goal.GoalRepository;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private MentorshipService mentorshipService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private EventRepository eventRepository;
+
+    @Mock
+    private GoalRepository goalRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void deactivateUser_WithValidId() {
+        long id = 1L;
+        boolean active = false;
+        doNothing().when(goalRepository).deleteUnusedGoalsByMentorId(id);
+        doNothing().when(eventRepository).deleteAllByOwnerId(id);
+        doNothing().when(mentorshipService).stopMentorship(id);
+        doNothing().when(userRepository).updateUserActive(id, active);
+
+        userService.deactivateUser(id);
+
+        verify(goalRepository).deleteUnusedGoalsByMentorId(id);
+        verify(eventRepository).deleteAllByOwnerId(id);
+        verify(mentorshipService).stopMentorship(id);
+        verify(userRepository).updateUserActive(id, active);
+    }
+
+    @Test
+    void deactivateUser_WithNull() {
+        long id = 1L;
+        boolean active = false;
+
+        assertThrows(NullPointerException.class,
+                () -> userService.deactivateUser(null));
+
+        verify(goalRepository, never()).deleteUnusedGoalsByMentorId(id);
+        verify(eventRepository, never()).deleteAllByOwnerId(id);
+        verify(mentorshipService, never()).stopMentorship(id);
+        verify(userRepository, never()).updateUserActive(id, active);
+    }
+}

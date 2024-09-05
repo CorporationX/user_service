de306a91339bbe56443a6cb134a71b480dabbf57
Denis Artamonov <dns.artamonov@gmail.com>
2024-09-06 00:40:50 +0300

    feature-BJS2-27185 continue

--- src/main/java/school/faang/user_service/service/recomendation/RecommendationRequestService.java
+++ src/main/java/school/faang/user_service/service/recomendation/RecommendationRequestService.java
@@ -1,10 +1,9 @@
 package school.faang.user_service.service.recomendation;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.recomendation.RecommendationRequestDto;
 import school.faang.user_service.entity.Skill;
-import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.recommendation.RecommendationRequest;
 import school.faang.user_service.entity.recommendation.SkillRequest;
 import school.faang.user_service.mapper.recomendation.RecommendationRequestMapper;
@@ -13,15 +12,11 @@ import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.recommendation.RecommendationRequestRepository;
 import school.faang.user_service.repository.recommendation.SkillRequestRepository;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class RecommendationRequestService {
     private final RecommendationRequestRepository recommendationRequestRepository;
     private final UserRepository userRepository;
@@ -29,58 +24,60 @@ public class RecommendationRequestService {
     private final SkillRequestRepository skillRequestRepository;
     private final SkillRepository skillRepository;
 
+    @Autowired
+    public RecommendationRequestService(RecommendationRequestRepository recommendationRequestRepository, UserRepository userRepository, RecommendationRequestMapper mapper, SkillRequestRepository skillRequestRepository, SkillRepository skillRepository) {
+        this.recommendationRequestRepository = recommendationRequestRepository;
+        this.userRepository = userRepository;
+        this.recommendationRequestMapper = mapper;
+        this.skillRequestRepository = skillRequestRepository;
+        this.skillRepository = skillRepository;
+    }
 
 
     public RecommendationRequestDto create(RecommendationRequestDto recommendationRequestDto) {
-        if (checkUsers(recommendationRequestDto)) {
-            if (checkSkillsInDb(recommendationRequestDto)) {
-                if (requestAllowed(recommendationRequestDto)) {
-                    RecommendationRequest recommendationRequestEntity = recommendationRequestMapper.mapToEntity(recommendationRequestDto);
-                    List<SkillRequest> skillRequests = StreamSupport.stream(skillRequestRepository.findAllById(recommendationRequestDto.getSkillsId()).spliterator(), false).toList();
-                    recommendationRequestEntity.setSkills(skillRequests);
-                    recommendationRequestEntity = recommendationRequestRepository.save(recommendationRequestEntity);
-                    recommendationRequestEntity.getSkills()
-                            .forEach(skillRequest -> skillRequestRepository.create(recommendationRequestDto.getRequesterId(), skillRequest.getId()));
-                    return recommendationRequestMapper.mapToDto(recommendationRequestEntity);
-                } else {
-                    throw new IllegalArgumentException("Request not allowed, there is already such request created less than 6 months ago");
-                }
-            } else {
-                throw new NoSuchElementException("No such skills in database");
-            }
-        } else {
+        if (!userRepository.existsById(recommendationRequestDto.getRequesterId()) || !userRepository.existsById(recommendationRequestDto.getReceiverId())) {
             throw new IllegalArgumentException("Requester id or receiver id is wrong");
         }
+//        if (!isSkillsInDb(recommendationRequestDto)) {
+//            throw new NoSuchElementException("No such skills in database");
+//        }
+        System.out.println("Next");
+        RecommendationRequest recommendationRequestEntity = recommendationRequestMapper.mapToEntity(recommendationRequestDto);
+        List<SkillRequest> skillRequests = StreamSupport.stream(skillRequestRepository.findAllById(recommendationRequestDto.getSkillsId()).spliterator(), false).toList();
+        recommendationRequestEntity.setSkills(skillRequests);
+        recommendationRequestEntity = recommendationRequestRepository.save(recommendationRequestEntity);
+        recommendationRequestEntity.getSkills()
+                .forEach(skillRequest -> skillRequestRepository.create(recommendationRequestDto.getRequesterId(), skillRequest.getId()));
+        return recommendationRequestMapper.mapToDto(recommendationRequestEntity);
     }
 
-    private boolean checkUsers(RecommendationRequestDto recommendationRequestDto) {
-        System.out.println("sdfasdfasdfasdfasd");
-        System.out.println(userRepository.count());
-        System.out.println(recommendationRequestDto.getRequesterId());
-        System.out.println(recommendationRequestDto.getReceiverId());
-        Optional<User> requester = userRepository.findById(recommendationRequestDto.getRequesterId());
-        System.out.println(requester.get());
-        Optional<User> receiver = userRepository.findById(recommendationRequestDto.getRequesterId());
-        receiver.get();
-        System.out.println(requester.isPresent() + " - " + receiver.isPresent());
-        return requester.isPresent() && receiver.isPresent();
-    }
-
-    private boolean requestAllowed(RecommendationRequestDto recommendationRequestDto) {
-        long count = recommendationRequestRepository.findAll().stream()
-                .filter(request -> request.getCreatedAt().isAfter(LocalDateTime.now().minus(6, ChronoUnit.MONTHS)))
-                .filter(request -> request.getRequester().getId().equals(recommendationRequestDto.getRequesterId()))
-                .filter(request -> request.getReceiver().getId().equals(recommendationRequestDto.getReceiverId()))
-                .count();
-        return count == 0;
-    }
-
-    private boolean checkSkillsInDb(RecommendationRequestDto recommendationRequestDto) {
+    public boolean isSkillsInDb(RecommendationRequestDto recommendationRequestDto) {
         List<SkillRequest> skillRequests = (List<SkillRequest>) skillRequestRepository.findAllById(recommendationRequestDto.getSkillsId());
         List<Skill> skills = skillRequests.stream()
-                .map(skillRequest -> skillRequest.getSkill())
+                .map(SkillRequest::getSkill)
                 .toList();
-        System.out.println(skillRepository.existsAllByIdIn(skills));
         return skillRepository.existsAllByIdIn(skills);
     }
+
 }
+
+//    public boolean checkUsers(RecommendationRequestDto recommendationRequestDto) {
+//        List<Long> userIds = List.of(recommendationRequestDto.getRequesterId(), recommendationRequestDto.getReceiverId());
+//        System.out.println(recommendationRequestDto.getRequesterId());
+//        System.out.println(recommendationRequestDto.getReceiverId());
+//
+//        return userRepository.existsAllByIdIn(userIds);
+//    }
+
+//public boolean requestAllowed(RecommendationRequestDto recommendationRequestDto) {
+//    long count = recommendationRequestRepository.findAll().stream()
+//            .filter(request -> request.getCreatedAt().isAfter(LocalDateTime.now().minusMonths(6)))
+//            .filter(request -> request.getRequester().getId().equals(recommendationRequestDto.getRequesterId()))
+//            .filter(request -> request.getReceiver().getId().equals(recommendationRequestDto.getReceiverId()))
+//            .count();
+//    return count == 0;
+//}
+
+
+
+
--- src/test/java/school/faang/user_service/service/recomendation/RecommendationRequestServiceTest.java
+++ src/test/java/school/faang/user_service/service/recomendation/RecommendationRequestServiceTest.java
@@ -1,16 +1,20 @@
 package school.faang.user_service.service.recomendation;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.stereotype.Component;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ContextConfiguration;
 import school.faang.user_service.UserServiceApplication;
 import school.faang.user_service.dto.recomendation.RecommendationRequestDto;
 import school.faang.user_service.entity.RequestStatus;
+import school.faang.user_service.entity.recommendation.RecommendationRequest;
+import school.faang.user_service.entity.recommendation.SkillRequest;
 import school.faang.user_service.mapper.recomendation.RecommendationRequestMapper;
 import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.UserRepository;
@@ -20,10 +24,16 @@ import school.faang.user_service.repository.recommendation.SkillRequestRepositor
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Component
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ContextConfiguration(classes = UserServiceApplication.class)
 @RequiredArgsConstructor
+@ExtendWith(MockitoExtension.class)
 public class RecommendationRequestServiceTest {
+    RecommendationRequest recommendationRequest;
+    @Mock
+    private RecommendationRequestMapper mapper;
     @Mock
     UserRepository userRepository;
     @Mock
@@ -35,25 +45,70 @@ public class RecommendationRequestServiceTest {
     @Mock
     SkillRepository skillRepository;
     @InjectMocks
-    RecommendationRequestService recommendationRequestService = new RecommendationRequestService(recommendationRequestRepository, userRepository, recommendationRequestMapper, skillRequestRepository, skillRepository);
+    RecommendationRequestService recommendationRequestService;
     static RecommendationRequestDto recommendationRequestDto;
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         recommendationRequestDto = new RecommendationRequestDto();
-        recommendationRequestDto.setReceiverId(2L);
         recommendationRequestDto.setId(1L);
-        recommendationRequestDto.setCreatedAt(LocalDateTime.now());
+        recommendationRequestDto.setReceiverId(2L);
         recommendationRequestDto.setRequesterId(5L);
+        recommendationRequestDto.setCreatedAt(LocalDateTime.now());
         recommendationRequestDto.setMessage("testMessage");
         recommendationRequestDto.setStatus(RequestStatus.PENDING);
         recommendationRequestDto.setSkillsId(List.of(1L, 2L));
+
+
+    }
+
+    //Positive
+    @Test
+    @DisplayName("Successfully find users in DB")
+    public void testCheckUsersTrue() {
+        Mockito.when(userRepository.existsById(2L)).thenReturn(true);
+        Mockito.when(userRepository.existsById(5L)).thenReturn(true);
+        userRepository.existsById(2L);
+        userRepository.existsById(5L);
+        assertThrows(IllegalArgumentException.class, () -> {
+            recommendationRequestService.create(recommendationRequestDto);
+        });
     }
 
     @Test
-    @DisplayName("Try to create request with no such skills")
-    public void createNoSuchSkills() {
-
-        recommendationRequestService.create(recommendationRequestDto);
+    public void testExistById () {
+        Mockito.when(userRepository.existsById(2L)).thenReturn(true);
+        assertTrue(userRepository.existsById(2L));
     }
+
+    @Test
+    @DisplayName("Test create")
+    public void testCreate() {
+        Mockito.when(userRepository.existsById(recommendationRequestDto.getRequesterId())).thenReturn(true);
+        Mockito.when(userRepository.existsById(recommendationRequestDto.getReceiverId())).thenReturn(true);
+        Mockito.when(recommendationRequestRepository.save(mapper.mapToEntity(recommendationRequestDto))).thenReturn(recommendationRequest);
+        Mockito.when(mapper.mapToDto(recommendationRequest)).thenReturn(recommendationRequestDto);
+        Mockito.when(recommendationRequest.setSkills(List.of(new SkillRequest(), new SkillRequest())));
+        RecommendationRequestDto result = recommendationRequestService.create(recommendationRequestDto);
+    }
+//    @Test
+//    @DisplayName("Successfully find skills in DB")
+//    public void testIsSkillsInDbTrue() {
+//        when(recommendationRequestService.isSkillsInDb(recommendationRequestDto)).thenReturn(true);
+//        boolean res = recommendationRequestService.isSkillsInDb(recommendationRequestDto);
+//        assertTrue(res);
+//    }
+
+//    //Negative
+//    @Test
+//    @DisplayName("Can't find users in DB")
+//    public void testCheckUsersFalse() {
+//        assertFalse(recommendationRequestService.checkUsers(recommendationRequestDto));
+//    }
+//
+//    @Test
+//    @DisplayName("Can't find skills in DB")
+//    public void testIsSkillsInDbFalse() {
+//        assertFalse(recommendationRequestService.isSkillsInDb(recommendationRequestDto));
+//    }
 }

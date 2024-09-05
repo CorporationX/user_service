2db0152043f719d2cfee7694fb24f8e4c0110a20
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-05 20:56:36 +0300

    Написал UserService

---
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -0,0 +1,31 @@
+package school.faang.user_service.service;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.repository.event.EventRepository;
+import school.faang.user_service.repository.goal.GoalRepository;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+
+    private final MentorshipService mentorshipService;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+    private final GoalRepository goalRepository;
+
+    @Transactional
+    public void deactivateUser(@NonNull Long id) {
+        log.info("deactivating user with id: {}", id);
+        goalRepository.deleteUnusedGoalsByMentorId(id);
+        eventRepository.deleteAllByOwnerId(id);
+        mentorshipService.stopMentorship(id);
+        userRepository.updateUserActive(id, false);
+        log.info("deactivated user with id: {}", id);
+    }
+}

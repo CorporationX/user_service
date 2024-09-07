863c5807b510ebdb9bb4cfc9c8223185fe4db4c5
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-07 18:43:32 +0300

    Написал PremiumController

---
+++ src/main/java/school/faang/user_service/controller/PremiumController.java
@@ -0,0 +1,24 @@
+package school.faang.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.premiun.PremiumDto;
+import school.faang.user_service.dto.premiun.PremiumRequest;
+import school.faang.user_service.service.PremiumService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/premium")
+public class PremiumController {
+
+    private final PremiumService premiumService;
+
+    @PostMapping("/buy/{id}")
+    public PremiumDto buyPremium(@PathVariable long id, @RequestBody PremiumRequest request) {
+        return premiumService.buyPremium(id, request.days(), request.currency());
+    }
+}

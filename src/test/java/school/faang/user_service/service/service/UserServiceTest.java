3fa80303507e5e3d029c7d48fea79b47933f994f
grisatkac <grisatkac@mail.ru>
2024-09-08 23:02:45 +0300

    add tests

--- src/test/java/school/faang/user_service/service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.service.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,25 +7,25 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserFilterDto;
+import school.faang.user_service.entity.Country;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.UserProfilePic;
 import school.faang.user_service.exception.DataValidationException;
-import school.faang.user_service.exception.s3.FileDownloadException;
-import school.faang.user_service.exception.s3.FileUploadException;
+import school.faang.user_service.exception.EntityNotFoundException;
 import school.faang.user_service.mapper.UserMapper;
+import school.faang.user_service.repository.CountryRepository;
 import school.faang.user_service.repository.UserRepository;
-import school.faang.user_service.service.randomAvatar.RandomAvatarService;
+import school.faang.user_service.service.randomAvatar.AvatarService;
 import school.faang.user_service.service.s3Service.S3Service;
 import school.faang.user_service.service.user.UserService;
 import school.faang.user_service.service.user.filter.UserFilter;
 import school.faang.user_service.validator.user.UserFilterValidation;
 import school.faang.user_service.validator.user.UserValidator;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -35,14 +34,16 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,6 +51,9 @@ public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private CountryRepository countryRepository;
 
     private UserFilterValidation userFilterValidation;
 
@@ -61,7 +65,7 @@ public class UserServiceTest {
     private S3Service s3Service;
 
     @Mock
-    private RandomAvatarService randomAvatarService;
+    private AvatarService avatarService;
 
     @Mock
     private UserValidator userValidator;
@@ -73,27 +77,42 @@ public class UserServiceTest {
     private User user;
     private UserDto userDto;
     private UserFilterDto userFilterDto;
-    private File randomPhoto;
+    //private File avatar;
+    private MultipartFile avatar;
+
+    private Country country;
 
     private UserProfilePic userProfilePic;
+
+    private byte[] avatarBytes;
 
     @BeforeEach
     void beforeEachInit() {
         user = new User();
-        userDto = UserDto.builder().build();
+        userDto = UserDto.builder()
+                .id(1L)
+                .countryId(1L)
+                .userProfilePic(UserProfilePic.builder()
+                        .smallFileId("fileId")
+                        .build())
+                .build();
         userFilterDto = UserFilterDto.builder().build();
 
         filters = List.of(nameUserFilter/*, emailUserFilter*/);
         userRepository = Mockito.mock(UserRepository.class);
         userFilterValidation = Mockito.mock(UserFilterValidation.class);
         userMapper = Mockito.mock(UserMapper.class);
-        randomPhoto = new File("randomPhoto.svg");
+        //avatar = new MockMultipartFile("avatar", "avatar", "jpeg", new byte[]{});
+        avatar = mock(MockMultipartFile.class);
+        //avatar = new File("randomPhoto.svg");
         userProfilePic = new UserProfilePic();
         userProfilePic.setFileId("avatarId");
         user.setUserProfilePic(userProfilePic);
 
+        country = Country.builder().id(1L).build();
+        avatarBytes = new byte[100];
 
-        userService = new UserService(userRepository, filters, userFilterValidation, userMapper, s3Service, randomAvatarService, userValidator);
+        userService = new UserService(userRepository, filters, userFilterValidation, userMapper, avatarService, countryRepository, userValidator);
     }
 
     @Test
@@ -205,68 +224,68 @@ public class UserServiceTest {
     }
 
     @Test
-    public void generateRandomAvatarUserNotCurrent() {
-        doThrow(DataValidationException.class).when(userValidator).isCurrentUser(anyLong());
+    public void testCreateUserSuccessWithAvatar() {
+        when(countryRepository.findById(userDto.getCountryId())).thenReturn(Optional.of(country));
+        when(userMapper.toEntity(userDto)).thenReturn(user);
+        when(userRepository.save(any(User.class))).thenReturn(user);
 
-        Assertions.assertThrows(DataValidationException.class, () -> userService.generateRandomAvatar(1L));
+        userService.createUser(userDto, null);
+
+        verify(countryRepository, times(1)).findById(userDto.getCountryId());
+        verify(userRepository, times(2)).save(user);
     }
 
     @Test
-    public void generateRandomAvatarUserNotFound() {
-        doNothing().when(userValidator).isCurrentUser(anyLong());
+    public void testGetByIdUserFound() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(userMapper.toDto(user)).thenReturn(userDto);
+
+        UserDto result = userService.getById(1L);
+
+        verify(userRepository, times(1)).findById(1L);
+        verify(userMapper, times(1)).toDto(user);
+        assertNotNull(result);
+        assertEquals(userDto.getId(), result.getId());
+        assertEquals(userDto, result);
+    }
+
+    @Test
+    public void testGetByIdUserNotFound() {
         when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
 
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+            userService.getById(1L);
+        });
 
-        assertThrows(DataValidationException.class, () -> userService.generateRandomAvatar(1L));
+        assertEquals("User with 1 id doesn't exist", exception.getMessage());
+        verify(userRepository, times(1)).findById(1L);
+        verify(userMapper, never()).toDto(any(User.class));
     }
 
     @Test
-    public void generateRandomAvatarFileUploadFailed() {
-        doNothing().when(userValidator).isCurrentUser(anyLong());
-        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-        when(randomAvatarService.getRandomPhoto()).thenReturn(randomPhoto);
-        when(s3Service.uploadFile(any(File.class), any(String.class))).thenReturn(null);
+    @DisplayName("Получение аватара по userId - пользователь не найден")
+    void testGetAvatarUserNotFound() {
+        // Mock случая, когда пользователь не найден
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
 
-        assertThrows(FileUploadException.class, () -> userService.generateRandomAvatar(1L));
+        // Ожидаем, что будет выброшено исключение
+        assertThrows(EntityNotFoundException.class, () -> userService.getAvatar(1L));
+
+        verify(userRepository, times(1)).findById(1L);
+        verifyNoMoreInteractions(avatarService);
     }
 
     @Test
-    public void generateRandomAvatarSuccess() {
-        doNothing().when(userValidator).isCurrentUser(anyLong());
-        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-        when(randomAvatarService.getRandomPhoto()).thenReturn(randomPhoto);
-        when(s3Service.uploadFile(any(File.class), any(String.class))).thenReturn("avatarId");
+    @DisplayName("Получение аватара по userId - успешно")
+    void testGetAvatarSuccess() {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(userMapper.toDto(user)).thenReturn(userDto);
+        when(avatarService.get(any(String.class))).thenReturn(avatarBytes);
 
-        String avatarId = userService.generateRandomAvatar(1L);
+        byte[] avatar = userService.getAvatar(1L);
 
-        assertEquals("avatarId", avatarId);
-    }
-
-    @Test
-    public void getUserRandomAvatarUserNotFound() {
-        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
-
-        assertThrows(DataValidationException.class, () -> userService.getUserRandomAvatar(1L));
-    }
-
-    @Test
-    public void getUserRandomAvatarAvatarIdNull() {
-        userProfilePic.setFileId(null);
-        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-
-        assertThrows(FileDownloadException.class, () -> userService.getUserRandomAvatar(1L));
-    }
-
-    @Test
-    public void getUserRandomAvatarSuccess() {
-        byte[] avatarBytes = new byte[]{1, 2, 3, 4};
-        InputStream avatarStream = new ByteArrayInputStream(avatarBytes);
-
-        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-        when(s3Service.getFile("avatarId")).thenReturn(avatarStream);
-
-        byte[] result = userService.getUserRandomAvatar(1L);
-
-        assertArrayEquals(avatarBytes, result);
+        assertArrayEquals(avatarBytes, avatar);
+        verify(userRepository, times(1)).findById(1L);
+        verify(avatarService, times(1)).get(any(String.class));
     }
 }

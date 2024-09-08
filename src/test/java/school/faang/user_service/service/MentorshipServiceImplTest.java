cf5c4618d8c1a3301cc26adb414ec64e51674166
Artem Voevoda <voevodacode@gmail.com>
2024-09-08 11:11:24 +0300

    Fix test

--- src/test/java/school/faang/user_service/service/MentorshipServiceImplTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipServiceImplTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MentorshipServiceTest {
+public class MentorshipServiceImplTest {
 
     @Mock
     private MentorshipRepository mentorshipRepository;

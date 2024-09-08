3bf3ca11e7952d53156a49f0f9009cbafae3adae
ifef228 <ifef228@gmail.com>
2024-09-08 13:26:33 +0300

    убрал чужие коммиты

--- src/main/java/school/faang/user_service/filter/UserFilter.java
+++
@@ -1,7 +0,0 @@
-package school.faang.user_service.filter;
-
-import school.faang.user_service.dto.UserFilterDto;
-import school.faang.user_service.entity.User;
-
-public interface UserFilter extends Filter<User, UserFilterDto> {
-}

9fece1ed6d883c919c899f041e053bb3ae41c9e5
SharkB38 <val.wolf.ne@gmail.com>
2024-09-10 05:22:37 +0900

    Added GlobalExceptionHandler

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -1,25 +1,59 @@
 package school.faang.user_service.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import school.faang.user_service.dto.ErrorResponseDto;
+import school.faang.user_service.dto.responses.ConstraintErrorResponse;
+import school.faang.user_service.dto.responses.ErrorResponse;
+import school.faang.user_service.dto.responses.Violation;
+import school.faang.user_service.exception.subscription.SubscriptionAlreadyExistException;
+import school.faang.user_service.exception.subscription.SubscriptionNotFoundException;
+
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({EntityNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponseDto handleException(EntityNotFoundException e) {
-        return new ErrorResponseDto(e.getMessage());
+    @ExceptionHandler({
+            DataValidationException.class,
+            SubscriptionAlreadyExistException.class,
+            IllegalArgumentException.class,
+            EventParticipationRegistrationException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleExceptionWithBadRequest(RuntimeException ex) {
+        log.error(ex.getMessage(), ex);
+        return new ErrorResponse(ex.getMessage());
     }
 
-    @ExceptionHandler({DataValidationException.class})
+    @ExceptionHandler({
+            EntityNotFoundException.class,
+            SubscriptionNotFoundException.class
+    })
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleExceptionWithNotFound(RuntimeException ex) {
+        log.error(ex.getMessage(), ex);
+        return new ErrorResponse(ex.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponseDto handleException(DataValidationException e) {
-        return new ErrorResponseDto(e.getMessage());
+    public ConstraintErrorResponse onConstraintValidationException(ConstraintViolationException ex) {
+        final List<Violation> violations = ex.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getPropertyPath().toString(),
+                                violation.getMessage()
+                        )
+                )
+                .toList();
+        log.error(ex.getMessage(), ex);
+        return new ConstraintErrorResponse(violations);
     }
 }
---
+++ src/main/java/school/faang/user_service/exception/subscription/SubscriptionAlreadyExistException.java
@@ -0,0 +1,4 @@
+package school.faang.user_service.exception.subscription;
+
+public class SubscriptionAlreadyExistException extends RuntimeException {
+}

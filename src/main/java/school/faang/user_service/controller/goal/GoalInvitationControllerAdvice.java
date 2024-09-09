c2d9aa59e9672a720fce896b4ff3ec071822edd9
Sultan <nikowosuka@gmail.com>
2024-09-09 17:23:06 +0500

    second refactoring GoalInvitationControllerAdvice

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationControllerAdvice.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationControllerAdvice.java
@@ -1,20 +1,16 @@
 package school.faang.user_service.controller.goal;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import school.faang.user_service.dto.response.ErrorResponse;
-import school.faang.user_service.exception.goal.invitation.InvitationEntityNotFoundException;
 import school.faang.user_service.exception.goal.invitation.InvitationCheckException;
+import school.faang.user_service.exception.goal.invitation.InvitationEntityNotFoundException;
 
 @Slf4j
 @ControllerAdvice
-@RequiredArgsConstructor
 public class GoalInvitationControllerAdvice {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)

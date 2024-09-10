9930f276a22514c80dab76d07f11c36f32b85b2a
Maksim Kononov <07maks7@mail.ru>
2024-09-10 21:58:17 +0300

    resolve conflict

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -4,7 +4,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.EventUserDto;
 import school.faang.user_service.dto.MentorshipUserDto;
-
 import school.faang.user_service.entity.User;
 
 import java.util.List;
@@ -12,11 +11,12 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
     EventUserDto userToUserDto(User user);
+
     MentorshipUserDto toMentorshipUserDto(User user);
 
     User dtoUserToUser(EventUserDto eventUserDto);
+
     List<MentorshipUserDto> toMentorshipUserDtos(List<User> users);
-}
 
     List<User> userDtosToUsers(List<EventUserDto> eventUserDtos);
 

b1d7bcf37f7446b1501018f1a7b15e11c2ea4a91
TheGreatLucky <danke_villetti@mail.ru>
2024-09-10 18:03:22 +0500

    renamed DTO files

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,9 +1,8 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.UserDTO;
 import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.SubscriptionService;
@@ -23,7 +22,7 @@ public class SubscriptionController {
     subscriptionService.unfollowUser(followerId, followeeId);
   }
 
-  public List<UserDto> getFollowers(long followeeId) {
+  public List<UserDTO> getFollowers(long followeeId) {
     return subscriptionService.getFollowers(followeeId);
   }
 
@@ -31,7 +30,7 @@ public class SubscriptionController {
     return subscriptionService.getFollowersCount(followerId);
   }
 
-  public List<UserDto> getFollowing(long followeeId, UserFilterDto filter) {
+  public List<UserDTO> getFollowing(long followeeId, UserFilterDto filter) {
     return subscriptionService.getFollowing(followeeId, filter);
   }
 
--- src/main/java/school/faang/user_service/dto/UserDTO.java
+++ src/main/java/school/faang/user_service/dto/UserDTO.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDTO {
-    private Long id;
-    private String username;
-    private String email;
+  private Long id;
+  private String username;
+  private String email;
 }

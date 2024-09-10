a45f98b373597078f800f2f288a3a7fd3ead9397
ifef228 <ifef228@gmail.com>
2024-09-10 08:12:51 +0300

    добавил интерфейсы контроллера и сервиса, убрал репозиторий из валидатора, поправил тесты, поправил реализацию валидатора

---
+++ src/main/java/school/faang/user_service/controller/event/EventParticipationControllerImpl.java
@@ -0,0 +1,34 @@
+package school.faang.user_service.controller.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.sevice.event.EventParticipationService;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class EventParticipationControllerImpl implements EventParticipationController {
+    private final EventParticipationService service;
+
+    @Override
+    public void registerParticipant(long eventId, UserDto userDto) {
+        service.registerParticipant(eventId, userDto.id());
+    }
+
+    @Override
+    public void unregisterParticipant(long eventId, UserDto userDto) {
+        service.unregisterParticipant(eventId, userDto.id());
+    }
+
+    @Override
+    public List<UserDto> getParticipant(long eventId) {
+        return service.getParticipant(eventId);
+    }
+
+    @Override
+    public Integer getParticipantsCount(long eventId) {
+        return service.getParticipantsCount(eventId);
+    }
+}

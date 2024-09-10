a34c300a512ec0d98f9615ba663284ad837f90b1
TheGreatLucky <danke_villetti@mail.ru>
2024-09-10 22:15:18 +0500

    Create FAILTEST

--- src/test/java/school/faang/user_service/service/FAILTEST.java
+++ src/test/java/school/faang/user_service/service/FAILTEST.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class FAILTEST {
+
     @Test
     public void thisTestWillFail() {
         fail("This test is intentionally failing");

c722c8a701a638e8352ccdda241d76a640ed0a15
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-09-11 23:34:32 +0500

    Change method UserServiceImpl.getPremiumUsers

--- src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
+++ src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
@@ -5,13 +5,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserFilterDto;
-import school.faang.user_service.entity.User;
 import school.faang.user_service.filter.user.UserFilter;
 import school.faang.user_service.mapper.user.UserMapper;
 import school.faang.user_service.repository.UserRepository;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,14 +22,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserDto> getPremiumUsers(UserFilterDto filters) {
         var premiumUsers = userRepository.findPremiumUsers();
-        return filter(premiumUsers, filters);
 
-    }
-
-    private List<UserDto> filter(Stream<User> users, UserFilterDto filters) {
         return userFilters.stream()
                 .filter(filter -> filter.isApplicable(filters))
-                .flatMap(filter -> filter.apply(users, filters))
+                .reduce(premiumUsers, (stream, filter) -> filter.apply(stream, filters),
+                        (newStream, oldStream) -> newStream)
                 .map(userMapper::toDto)
                 .toList();
     }

0694a0715e3830644e9506f7bdf406b8a2d9ba1d
TheGreatLucky <danke_villetti@mail.ru>
2024-09-11 14:56:41 +0500

    FAILED TEST

---
+++ src/test/java/school/faang/user_service/service/FAILEDTEST.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FAILEDTEST {
+    @Test
+    public void failingTest() {
+        fail("This test is supposed to fail.");
+    }
+}

922840463dfe64645db1ff9b271cdd719389eb05
SharkB38 <val.wolf.ne@gmail.com>
2024-09-11 09:03:16 +0900

    disable test

--- src/test/java/school/faang/user_service/ci/CiFailTest.java
+++ src/test/java/school/faang/user_service/ci/CiFailTest.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.ci;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 class CiFailTest {
 
     @Test
+    @Disabled
     @DisplayName("failed test for ci check")
     void ciTest_checkFailedTest() {
         fail();

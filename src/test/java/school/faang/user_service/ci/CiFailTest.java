6e9317ccedfee60f1c8ab942d6ecb17ab999cbe4
SharkB38 <val.wolf.ne@gmail.com>
2024-09-11 09:54:59 +0900

    check with failed cache

--- src/test/java/school/faang/user_service/ci/CiFailTest.java
+++ src/test/java/school/faang/user_service/ci/CiFailTest.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.ci;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 class CiFailTest {
 
     @Test
-    @Disabled
     @DisplayName("failed test for ci check")
     void ciTest_checkFailedTest() {
         fail();

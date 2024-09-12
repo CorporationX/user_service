c28a9a96b357285bd3ad50db258022f72094b199
Sergei Kochkin <lowerKamaCase@gmail.com>
2024-09-12 15:51:57 +0300

    BJS2-26058: Стать ментором

--- src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
@@ -4,21 +4,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.extension.ExtendWith;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.mapper.user.UserMapper;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -59,14 +59,15 @@ public class MentorshipServiceTest {
             @Test
             @DisplayName("Должен вернуть список менти для ментора")
             void shouldReturnListOfMenteesForMentor() {
-                when(mentorshipRepository.findById(1L)).thenReturn(Optional.of(mentor));
-                when(userMapper.toDto(mentee)).thenReturn(new UserDto(2L, "Mentee Name", "mentee@example.com"));
+                when(mentorshipRepository.findById(mentor.getId())).thenReturn(Optional.of(mentor));
+                when(userMapper.toDto(mentee))
+                        .thenReturn(new UserDto(mentee.getId(), "Mentee Name", "mentee@example.com"));
 
-                List<UserDto> mentees = mentorshipService.getMentees(1L);
+                List<UserDto> mentees = mentorshipService.getMentees(mentor.getId());
 
                 assertEquals(1, mentees.size());
-                assertEquals(2L, mentees.get(0).getId());
-                verify(mentorshipRepository).findById(1L);
+                assertEquals(mentee.getId(), mentees.get(0).getId());
+                verify(mentorshipRepository).findById(mentor.getId());
                 verify(userMapper).toDto(mentee);
             }
         }
@@ -76,15 +77,16 @@ public class MentorshipServiceTest {
         class NegativeTests {
 
             @Test
-            @DisplayName("Должен вернуть пустой список, если ментор не найден")
-            void shouldReturnEmptyListWhenMentorNotFound() {
-                when(mentorshipRepository.findById(1L)).thenReturn(Optional.empty());
+            @DisplayName("Должен выбросить исключение, если ментор не найден")
+            void shouldThrowExceptionWhenMentorNotFound() {
+                when(mentorshipRepository.findById(mentor.getId())).thenReturn(Optional.empty());
 
-                List<UserDto> mentees = mentorshipService.getMentees(1L);
+                EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+                    mentorshipService.getMentees(mentor.getId());
+                });
 
-                assertTrue(mentees.isEmpty());
-                verify(mentorshipRepository).findById(1L);
-                verify(mentorshipRepository, never()).save(any());
+                assertEquals("Ментор с ID 1 не найден", exception.getMessage());
+                verify(mentorshipRepository).findById(mentor.getId());
             }
         }
     }
@@ -100,16 +102,15 @@ public class MentorshipServiceTest {
             @Test
             @DisplayName("Должен удалить менти для ментора")
             void shouldDeleteMenteeForMentor() {
-                when(mentorshipRepository.findById(1L)).thenReturn(Optional.of(mentor));
-                when(mentorshipRepository.findById(2L)).thenReturn(Optional.of(mentee));
-                when(userMapper.toDto(mentee)).thenReturn(new UserDto(2L, "Mentee Name", "mentee@example.com"));
+                when(mentorshipRepository.findById(mentor.getId())).thenReturn(Optional.of(mentor));
+                when(mentorshipRepository.findById(mentee.getId())).thenReturn(Optional.of(mentee));
 
                 assertTrue(mentor.getMentees().contains(mentee));
 
-                Optional<UserDto> deletedMentee = mentorshipService.deleteMentee(1L, 2L);
+                mentorshipService.deleteMentee(mentor.getId(), mentee.getId());
 
-                assertEquals(2L, deletedMentee.get().getId());
                 verify(mentorshipRepository).save(mentor);
+                assertFalse(mentor.getMentees().contains(mentee));
             }
         }
 
@@ -118,16 +119,18 @@ public class MentorshipServiceTest {
         class NegativeTests {
 
             @Test
-            @DisplayName("Должен вернуть пустой Optional, если менти не найден")
-            void shouldReturnEmptyOptionalWhenMenteeNotFound() {
-                when(mentorshipRepository.findById(1L)).thenReturn(Optional.of(mentor));
-                when(mentorshipRepository.findById(2L)).thenReturn(Optional.empty());
+            @DisplayName("Должен выбросить исключение, если менти не найден")
+            void shouldThrowExceptionWhenMenteeNotFound() {
+                when(mentorshipRepository.findById(mentor.getId())).thenReturn(Optional.of(mentor));
+                when(mentorshipRepository.findById(mentee.getId())).thenReturn(Optional.empty());
 
-                Optional<UserDto> result = mentorshipService.deleteMentee(1L, 2L);
+                EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+                    mentorshipService.deleteMentee(mentor.getId(), mentee.getId());
+                });
 
-                assertTrue(result.isEmpty());
-                verify(mentorshipRepository).findById(1L);
-                verify(mentorshipRepository).findById(2L);
+                assertEquals("Пользователь с ID 2 не найден", exception.getMessage());
+                verify(mentorshipRepository).findById(mentor.getId());
+                verify(mentorshipRepository).findById(mentee.getId());
                 verify(mentorshipRepository, never()).save(mentor);
             }
         }
@@ -143,15 +146,16 @@ public class MentorshipServiceTest {
             @Test
             @DisplayName("Должен вернуть список менторов для менти")
             void shouldReturnListOfMentorsForMentee() {
-                when(mentorshipRepository.findById(2L)).thenReturn(Optional.of(mentee));
+                when(mentorshipRepository.findById(mentee.getId())).thenReturn(Optional.of(mentee));
                 mentee.setMentors(new ArrayList<>(List.of(mentor)));
-                when(userMapper.toDto(mentor)).thenReturn(new UserDto(1L, "Mentor Name", "mentor@example.com"));
+                when(userMapper.toDto(mentor))
+                        .thenReturn(new UserDto(mentor.getId(), "Mentor Name", "mentor@example.com"));
 
-                List<UserDto> mentors = mentorshipService.getMentors(2L);
+                List<UserDto> mentors = mentorshipService.getMentors(mentee.getId());
 
                 assertEquals(1, mentors.size());
-                assertEquals(1L, mentors.get(0).getId());
-                verify(mentorshipRepository).findById(2L);
+                assertEquals(mentor.getId(), mentors.get(0).getId());
+                verify(mentorshipRepository).findById(mentee.getId());
                 verify(userMapper).toDto(mentor);
             }
         }
@@ -161,14 +165,16 @@ public class MentorshipServiceTest {
         class NegativeTests {
 
             @Test
-            @DisplayName("Должен вернуть пустой список, если менти не найден")
-            void shouldReturnEmptyListWhenMenteeNotFound() {
-                when(mentorshipRepository.findById(2L)).thenReturn(Optional.empty());
+            @DisplayName("Должен выбросить исключение, если менти не найден")
+            void shouldThrowExceptionWhenMenteeNotFound() {
+                when(mentorshipRepository.findById(mentee.getId())).thenReturn(Optional.empty());
 
-                List<UserDto> mentors = mentorshipService.getMentors(2L);
+                EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+                    mentorshipService.getMentors(mentee.getId());
+                });
 
-                assertTrue(mentors.isEmpty());
-                verify(mentorshipRepository).findById(2L);
+                assertEquals("Менти с ID 2 не найден", exception.getMessage());
+                verify(mentorshipRepository).findById(mentee.getId());
                 verify(userMapper, never()).toDto(any());
             }
         }
@@ -185,17 +191,15 @@ public class MentorshipServiceTest {
             @Test
             @DisplayName("Должен удалить ментора для менти")
             void shouldDeleteMentorForMentee() {
-                when(mentorshipRepository.findById(2L)).thenReturn(Optional.of(mentee));
-                when(mentorshipRepository.findById(1L)).thenReturn(Optional.of(mentor));
-                when(userMapper.toDto(mentor)).thenReturn(new UserDto(1L, "Mentor Name", "mentor@example.com"));
+                when(mentorshipRepository.findById(mentee.getId())).thenReturn(Optional.of(mentee));
+                when(mentorshipRepository.findById(mentor.getId())).thenReturn(Optional.of(mentor));
 
                 assertTrue(mentee.getMentors().contains(mentor));
 
-                Optional<UserDto> deletedMentor = mentorshipService.deleteMentor(2L, 1L);
+                mentorshipService.deleteMentor(mentee.getId(), mentor.getId());
 
-                assertTrue(deletedMentor.isPresent());
-                assertEquals(1L, deletedMentor.get().getId());
                 verify(mentorshipRepository).save(mentee);
+                assertFalse(mentee.getMentors().contains(mentor));
             }
         }
 
@@ -204,16 +208,18 @@ public class MentorshipServiceTest {
         class NegativeTests {
 
             @Test
-            @DisplayName("Должен вернуть пустой Optional, если ментор не найден")
-            void shouldReturnEmptyOptionalWhenMentorNotFound() {
-                when(mentorshipRepository.findById(2L)).thenReturn(Optional.of(mentee));
-                when(mentorshipRepository.findById(1L)).thenReturn(Optional.empty());
+            @DisplayName("Должен выбросить исключение, если ментор не найден")
+            void shouldThrowExceptionWhenMentorNotFound() {
+                when(mentorshipRepository.findById(mentee.getId())).thenReturn(Optional.of(mentee));
+                when(mentorshipRepository.findById(mentor.getId())).thenReturn(Optional.empty());
 
-                Optional<UserDto> result = mentorshipService.deleteMentor(2L, 1L);
+                EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+                    mentorshipService.deleteMentor(mentee.getId(), mentor.getId());
+                });
 
-                assertTrue(result.isEmpty());
-                verify(mentorshipRepository).findById(2L);
-                verify(mentorshipRepository).findById(1L);
+                assertEquals("Пользователь с ID 1 не найден", exception.getMessage());
+                verify(mentorshipRepository).findById(mentee.getId());
+                verify(mentorshipRepository).findById(mentor.getId());
                 verify(mentorshipRepository, never()).save(mentee);
             }
         }

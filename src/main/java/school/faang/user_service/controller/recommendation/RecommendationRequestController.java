a534123445c8b723648454bd1dbc252ff32bb8ab
beksultanramankul <ramankul.beksultan.ramankul@gmail.com>
2024-09-13 16:52:08 +0500

    BJS2-26433 second

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -1,11 +1,20 @@
 package school.faang.user_service.controller.recommendation;
 
-import org.springframework.stereotype.Component;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.RecommendationRequestDto;
+import school.faang.user_service.service.RecommendationRequestService;
 
-@Component
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/recommendation-requests")
 public class RecommendationRequestController {
-    public RecommendationRequestDto requestRecommendation(RecommendationRequestDto recommendationRequest){
-        return null;
+    private final RecommendationRequestService recommendationRequestService;
+
+    @PostMapping
+    public RecommendationRequestDto requestRecommendation(@Valid @RequestBody RecommendationRequestDto recommendationRequest) {
+        recommendationRequestService.create(recommendationRequest);
+        return recommendationRequest;
     }
 }
--- src/main/java/school/faang/user_service/dto/RecommendationRequestDto.java
+++ src/main/java/school/faang/user_service/dto/RecommendationRequestDto.java
@@ -1,23 +1,31 @@
 package school.faang.user_service.dto;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import school.faang.user_service.entity.RequestStatus;
-import school.faang.user_service.entity.User;
-import school.faang.user_service.entity.recommendation.Recommendation;
-import school.faang.user_service.entity.recommendation.SkillRequest;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
 public class RecommendationRequestDto {
+    @Min(value = 0, message = "")
     private long id;
+    @NotNull(message = "")
+    private Long requesterId;
+    @NotNull(message = "")
+    private Long receiverId;
+    @NotBlank(message = "")
     private String message;
     private RequestStatus status;
-    private List<SkillRequest> skills;
-    private Long requesterId;
-    private Long receiverId;
+    @NotEmpty(message = "")
+    private List<Long> skillsId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }

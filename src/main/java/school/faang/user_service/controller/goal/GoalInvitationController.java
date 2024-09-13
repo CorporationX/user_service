5d9c323c6770bc0a6eb7f072f2de1c1d7da6c1ab
Sultan <sultan.inked@gmail.com>
2024-09-14 00:52:11 +0500

    simple refactoring GoalInvitationController after review

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
@@ -45,8 +46,8 @@ public class GoalInvitationController {
         goalInvitationService.rejectGoalInvitation(id);
     }
 
-    @GetMapping
-    public List<GoalInvitationDto> getInvitations(InvitationFilterDto filter) {
+    @PostMapping("/filters")
+    public List<GoalInvitationDto> getInvitations(@RequestBody InvitationFilterDto filter) {
         return goalInvitationService.getInvitations(filter)
                 .stream()
                 .map(goalInvitationMapper::toDto)

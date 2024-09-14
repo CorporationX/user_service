885854fd73eacc28f4464c6387a364ebf2dbb024
beksultanramankul <ramankul.beksultan.ramankul@gmail.com>
2024-09-14 22:48:25 +0500

    BJS2-26433 filter first

--- src/main/java/school/faang/user_service/filter/recommendation/RecommendationRequestFilter.java
+++ src/main/java/school/faang/user_service/filter/recommendation/RecommendationRequestFilter.java
@@ -1,7 +1,8 @@
 package school.faang.user_service.filter.recommendation;
 
+import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.entity.recommendation.RecommendationRequest;
 import school.faang.user_service.filter.Filter;
 
-public interface RecommendationRequestFilter extends Filter<RecommendationRequest, RecommendationRequestFilterDto> {
+public interface RecommendationRequestFilter extends Filter<RecommendationRequest, RecommendationRequestDto> {
 }

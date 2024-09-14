2c78d197b60f09e0e41bd8b565b5ede7f7eb29dc
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-14 10:17:15 +0300

    Внес правки

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
         return userService.getUser(userId);
     }
 
-    @PostMapping("/users")
+    @GetMapping("/users")
     List<UserDto> getUsersByIds(@RequestBody List<Long> ids) {
         return userService.getUsersByIds(ids);
     }

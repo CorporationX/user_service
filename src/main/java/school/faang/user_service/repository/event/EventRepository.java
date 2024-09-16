28d64c230794ac72da482ec2581c973f98d64a31
natalia-dch <natalia.a.doskach@gmail.com>
2024-09-16 09:31:56 +0300

    repo

--- src/main/java/school/faang/user_service/repository/event/EventRepository.java
+++ src/main/java/school/faang/user_service/repository/event/EventRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.event.Event;
+
 import java.util.List;
 
 @Repository

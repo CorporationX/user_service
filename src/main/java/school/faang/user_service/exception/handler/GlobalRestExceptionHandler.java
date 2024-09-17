d181db95c54d2bcac60be99b19c3447c6d693a7a
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-09-17 08:44:23 +0300

    исправлен синтаксис

--- src/main/java/school/faang/user_service/exception/handler/GlobalRestExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/handler/GlobalRestExceptionHandler.java
@@ -7,13 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import school.faang.user_service.exception.SkillAssignmentException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalRestExceptionHandler {
     private static final String BAD_REQUEST_MESSAGE = "Sorry, you've provided an invalid request." +
             " Please check the request format and ensure all required parameters are correct.";
-
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex) {
@@ -25,5 +25,11 @@ public class GlobalRestExceptionHandler {
     public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
         log.error(ex.getMessage(), ex);
         return new ResponseEntity<>(BAD_REQUEST_MESSAGE, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SkillAssignmentException.class)
+    public ResponseEntity<String> handleSkillAssignmentException(SkillAssignmentException ex) {
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 }

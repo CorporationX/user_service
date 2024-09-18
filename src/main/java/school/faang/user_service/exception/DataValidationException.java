a14420311a0bffcdab7ce23455ca90f681fd744b
mamadjonovKO <mamadjonov_k@auca.kg>
2024-09-18 11:11:21 +0600

    fix conflict

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -1,9 +1,6 @@
 package school.faang.user_service.exception;
 
 public class DataValidationException extends RuntimeException {
-    public DataValidationException() {
-    }
-
     public DataValidationException(String message) {
         super(message);
     }

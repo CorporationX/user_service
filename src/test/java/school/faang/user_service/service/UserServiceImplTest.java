25e551b70809b0ec986b886ac4e179c366533c96
Владислав Воронов <iPlesk90@yandex.ru>
2024-09-18 13:47:04 +0300

    ВЫполнил задачу

--- src/test/java/school/faang/user_service/service/UserServiceImplTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceImplTest.java
@@ -4,16 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.mapper.UserMapper;
+import school.faang.user_service.mapper.UserMapperImpl;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.event.EventRepository;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.service.user.UserServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -29,6 +38,9 @@ class UserServiceImplTest {
 
     @Mock
     private GoalRepository goalRepository;
+
+    @Spy
+    private UserMapperImpl userMapper;
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -62,5 +74,42 @@ class UserServiceImplTest {
         verify(eventRepository, never()).deleteAllByOwnerId(id);
         verify(mentorshipService, never()).stopMentorship(id);
         verify(userRepository, never()).updateUserActive(id, active);
+    }
+
+    @Test
+    void testGetUser() {
+        long id = 1L;
+        User userEntity = new User();
+        userEntity.setId(id);
+        userEntity.setUsername("Name");
+        userEntity.setEmail("mail.ru");
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(userEntity));
+
+        UserDto result = userService.getUser(id);
+
+        assertEquals(userEntity.getUsername(), result.username());
+        assertEquals(userEntity.getEmail(), result.email());
+
+        verify(userRepository).findById(id);
+        verify(userMapper).toDto(userEntity);
+    }
+
+    @Test
+    void testGetUsersByIds() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        ids.add(2L);
+
+        List<User> users = new ArrayList<>();
+        User user1 = new User();
+        User user2 = new User();
+        users.add(user1);
+        users.add(user2);
+
+        when(userRepository.findAllById(ids)).thenReturn(users);
+
+        assertDoesNotThrow(() -> userService.getUsersByIds(ids));
+
     }
 }

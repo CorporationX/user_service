1a8825e302d6a168c2719c71774afa659813558e
pgusev <pgusev@fil-it.ru>
2024-09-19 17:10:00 +0300

    BJS2-28952 - Fixed cyclic dependencies

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -3,18 +3,18 @@ package school.faang.user_service.controller.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import school.faang.user_service.service.user.UserService;
+import school.faang.user_service.service.user.UserDeactivationService;
 
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    private final UserDeactivationService userDeactivationService;
 
     @PatchMapping("/{userId}/deactivate")
     @ResponseStatus(HttpStatus.OK)
     public void deactivateUserAccount(@PathVariable Long userId) {
-        userService.deactivateAccount(userId);
+        userDeactivationService.deactivateAccount(userId);
     }
 }

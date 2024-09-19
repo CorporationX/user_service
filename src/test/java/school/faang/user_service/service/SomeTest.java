7623b2669398f377f02b0273de275de451d636aa
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-09-19 10:01:13 +0200

    added failing test

---
+++ src/test/java/school/faang/user_service/service/SomeTest.java
@@ -0,0 +1,15 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class SomeTest {
+
+    @Test
+    @DisplayName("when different visibility exeption must be thrown")
+    public void testValidateSubProjectVisibilityWithNonAppropriateValues() {
+        assertFalse(true);
+    }
+}

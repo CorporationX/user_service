0ebe7c8c6301b72a8c9e7f90843758475a2d0efb
azhibek_a <azhibek.assylkhan@gmail.com>
2024-09-20 15:12:57 +0500

    gradle откат

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.0")
-    implementation("net.coobird:thumbnailator:0.4.1")
+    implementation ("net.coobird:thumbnailator:0.4.1")
 
     /**
      * Test containers
@@ -105,10 +105,10 @@ tasks.bootJar {
  * JaCoCo settings
  */
 val jacocoInclude = listOf(
-        "**/controller/**",
-        "**/service/**",
-        "**/validator/**",
-        "**/mapper/**"
+            "**/controller/**",
+            "**/service/**",
+            "**/validator/**",
+            "**/mapper/**"
 )
 jacoco {
     toolVersion = "0.8.9"
@@ -127,9 +127,9 @@ tasks.jacocoTestReport {
     }
 
     classDirectories.setFrom(
-            sourceSets.main.get().output.asFileTree.matching {
-                include(jacocoInclude)
-            }
+                sourceSets.main.get().output.asFileTree.matching {
+                    include(jacocoInclude)
+                }
     )
 }
 tasks.jacocoTestCoverageVerification {
@@ -137,9 +137,9 @@ tasks.jacocoTestCoverageVerification {
         rule {
             element = "CLASS"
             classDirectories.setFrom(
-                    sourceSets.main.get().output.asFileTree.matching {
-                        include(jacocoInclude)
-                    }
+                        sourceSets.main.get().output.asFileTree.matching {
+                            include(jacocoInclude)
+                        }
             )
             enabled = true
             limit {

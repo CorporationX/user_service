3df15d979538c555a37667d9fe0d9f33e981610d
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-20 19:50:07 +0300

    Убрал лишнее

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -1,26 +1,13 @@
 package school.faang.user_service;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
@@ -31,15 +18,6 @@ import java.nio.file.Path;
 )
 public class UserServiceApplication {
 
-    @Value("${services.s3.accessKey}")
-    private String accessKey;
-
-    @Value("${services.s3.secretKey}")
-    private String secretKey;
-
-    @Value("${services.s3.endpoint}")
-    private String endpoint;
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
@@ -49,28 +27,5 @@ public class UserServiceApplication {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
-    }
-
-    @Bean
-    public AmazonS3 amazonS3() {
-        var credentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
-        var endpointConfiguration = new AwsClientBuilder.EndpointConfiguration("http://127.0.0.1:9000", "us-east-1");
-
-        return AmazonS3Client.builder()
-                .withCredentials(credentialsProvider)
-                .withEndpointConfiguration(endpointConfiguration)
-                .enablePathStyleAccess()
-                .build();
-    }
-
-    @Bean
-    public byte[] defaultAvatar() {
-        try {
-            ClassPathResource resource = new ClassPathResource("image/default-profile-picture.svg");
-            Path path = resource.getFile().toPath();
-            return Files.readAllBytes(path);
-        } catch (IOException e) {
-            throw new RuntimeException("The default avatar was not found" ,e);
-        }
     }
 }

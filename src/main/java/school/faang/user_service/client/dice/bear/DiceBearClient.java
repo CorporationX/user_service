2e450bf5c9031c2634723c1a8532ec082fb4ecb6
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-20 19:45:57 +0300

    Написал DiceBearClient

---
+++ src/main/java/school/faang/user_service/client/dice/bear/DiceBearClient.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.client.dice.bear;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "DiceBear", url = "https://api.dicebear.com/9.x/fun-emoji")
+public interface DiceBearClient {
+
+    @GetMapping("/svg")
+    byte[] getSvgAvatar(@RequestParam String seed, @RequestParam int size);
+}

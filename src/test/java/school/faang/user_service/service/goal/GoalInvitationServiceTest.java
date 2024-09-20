1607144abcbdcf2a91ed37786e48a52ea2e37c25
Nikita Korolev <nk25.dev@gmail.com>
2024-09-20 21:48:09 +0300

    Modified test location, improved Swagger documentation for GoalInvitationService.

--- src/test/java/school/faang/user_service/service/goal/GoalInvitationServiceTest.java
+++ src/test/java/school/faang/user_service/service/goal/GoalInvitationServiceTest.java
@@ -27,7 +27,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
 @ExtendWith(MockitoExtension.class)
 public class GoalInvitationServiceTest {
     @InjectMocks

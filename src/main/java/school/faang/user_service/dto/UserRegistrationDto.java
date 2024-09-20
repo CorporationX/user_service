816399b90f528414704762770670fa5de5699f41
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-20 19:43:07 +0300

    Написал UserRegistrationDto

--- src/main/java/school/faang/user_service/dto/UserRegistrationDto.java
+++ src/main/java/school/faang/user_service/dto/UserRegistrationDto.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
+@Builder
 public record UserRegistrationDto(
 
         @NotBlank

f143c70ba41042b7f59c7892e961656a1264bae6
Nikita Korolev <nk25.dev@gmail.com>
2024-09-20 04:54:01 +0300

    Modified test location, improved Swagger documentation for GoalInvitationService.

--- src/main/java/school/faang/user_service/controller/advice/ApiControllerAdvice.java
+++ src/main/java/school/faang/user_service/controller/advice/ApiControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import school.faang.user_service.controller.goal.GoalInvitationController;
+
 import java.util.stream.Collectors;
 
 @RestControllerAdvice(assignableTypes = {

5b949fa67efe89b3ff3aa6cab5e814e8129bc3f8
Garib_Aghayev <garibaghayev@gmail.com>
2024-09-21 14:20:44 +0300

    without tests

--- src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
+++ src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,12 @@ public class EventParticipationController {
     private final EventParticipationService eventParticipationService;
 
 
-    @PostMapping("/{eventId}/register")
+    @PutMapping("/{eventId}/register")
     public void registerParticipant(@PathVariable Long eventId, @RequestParam Long userId) {
         eventParticipationService.registerParticipant(eventId, userId);
     }
 
-    @PostMapping("/{eventId}/unregister")
+    @PutMapping("/{eventId}/unregister")
     public void unregisterParticipant(@PathVariable Long eventId, @RequestParam Long userId) {
         eventParticipationService.unregisterParticipant(eventId, userId);
     }

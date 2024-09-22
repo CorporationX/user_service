d2bcc1a3e2adb40322276aae6ec711e57e81599e
natalia-dch <natalia.a.doskach@gmail.com>
2024-09-22 19:26:07 +0300

    validation

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,5 +1,7 @@
 package school.faang.user_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @NotBlank
     private long id;
+    @NotBlank
     private String username;
+    @NotBlank
+    @Email
     private String email;
 }

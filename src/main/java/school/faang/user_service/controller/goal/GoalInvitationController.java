37f2fb3dbce431bd89de089dd887955b46789c73
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-09-22 23:13:12 +0500

    Added endpoints into GoalInvitationController

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -1,8 +1,13 @@
 package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
 import school.faang.user_service.dto.goal.InvitationFilterDto;
@@ -11,20 +16,24 @@ import school.faang.user_service.service.goal.GoalInvitationService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/goals_invitations")
 @RequiredArgsConstructor
 public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public GoalInvitationDto createInvitation(GoalInvitationDto invitation) {
         return goalInvitationService.createInvitation(invitation);
     }
 
-    public GoalInvitationDto acceptGoalInvitation(long id) {
+    @PutMapping("/{id}/accepting")
+    public GoalInvitationDto acceptGoalInvitation(@PathVariable long id) {
         return goalInvitationService.acceptInvitation(id);
     }
 
-    public GoalInvitationDto rejectGoalInvitation(long id) {
+    @PutMapping("/{id}/rejection")
+    public GoalInvitationDto rejectGoalInvitation(@PathVariable long id) {
         return goalInvitationService.rejectGoalInvitation(id);
     }
 

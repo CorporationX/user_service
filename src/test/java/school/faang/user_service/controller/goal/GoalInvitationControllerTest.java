036cf0a13d1f96eecced81cdfa289b7268b8c607
zeus <denis@fatum.ru>
2024-09-23 13:25:41 +0300

    BJS2-27218 test

--- src/test/java/school/faang/user_service/controller/goal/GoalInvitationControllerTest.java
+++ src/test/java/school/faang/user_service/controller/goal/GoalInvitationControllerTest.java
@@ -58,7 +58,7 @@ class GoalInvitationControllerTest {
         goalInvitationController.createInvitation(goalInvitationDto);
 
 
-        assertEquals(2, violations.size());
+        assertEquals(1, violations.size());
         assertEquals("не должно равняться null", violations.iterator().next().getMessage());
     }
 

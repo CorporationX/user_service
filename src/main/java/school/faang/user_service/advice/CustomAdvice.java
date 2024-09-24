82b2288f8f686f4980f37f2501209cd473b04fce
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-09-24 16:04:28 +0200

    fixing remarks and resolve conflicts

--- src/main/java/school/faang/user_service/advice/CustomAdvice.java
+++ src/main/java/school/faang/user_service/advice/CustomAdvice.java
@@ -2,20 +2,20 @@ package school.faang.user_service.advice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import school.faang.user_service.annotation.AppExceptionHandler;
 import school.faang.user_service.dto.ResponseDto;
 import school.faang.user_service.exception.DataValidationException;
 
 @Slf4j
-@ControllerAdvice(annotations = AppExceptionHandler.class)
+@RestControllerAdvice(annotations = AppExceptionHandler.class)
 public class CustomAdvice {
     @ExceptionHandler(DataValidationException.class)
-    public ResponseEntity<ResponseDto> handleException(DataValidationException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDto handleDataValidationException(DataValidationException e) {
         log.error("DataValidationException occurred: {}", e.getMessage(), e);
-        ResponseDto response = new ResponseDto(e.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseDto(e.getMessage());
     }
 }

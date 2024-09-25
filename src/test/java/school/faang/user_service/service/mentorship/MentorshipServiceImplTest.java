6e047f3330bcb3b015e187876706d22164325b10
Artem Voevoda <voevodacode@gmail.com>
2024-09-25 03:26:49 +0300

    fix

--- src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceImplTest.java
+++ src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceImplTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import school.faang.user_service.dto.user.UserDto;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.mapper.user.UserMapperImpl;
 import school.faang.user_service.repository.UserRepository;

836c2dcb8e82df168f806a420762af57f879c4d8
dolbahllop <bogdanrazumov125@gmail.com>
2024-09-27 16:23:48 +0300

    поправил отступ, поменял на @Restcontroller, добавил @GetMapping

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -2,22 +2,20 @@ package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.service.UserService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    public List<UserDto> getPremiumUsers (UserFilterDto userFilterDto) {
+    @GetMapping("/premiumUsers")
+    public List<UserDto> getPremiumUsers(UserFilterDto userFilterDto) {
         return userService.getPremiumUsers(userFilterDto);
     }
 

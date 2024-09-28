f5df92c226cd08dc2100f707cb557ca765fdb4b2
ifef228 <ifef228@gmail.com>
2024-09-28 20:03:19 +0300

    новый эндпоинт для фиксации добавления эвента в календарь (добавление в базу в колону айди эвента из календаря)

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.EventDto;
@@ -20,5 +21,11 @@ public class EventController {
     @GetMapping("/{id}")
     public EventDto getEvent(@PathVariable("id") @Positive long id) {
         return service.getEvent(id);
+    }
+
+    @PutMapping("/calendar/event/{eventId}/{calendarEventId}")
+    public void setCalendarEventId(@PathVariable("eventId") @Positive long eventId,
+                                   @PathVariable("calendarEventId") String calendarEventId) {
+        service.setCalendarEventId(eventId, calendarEventId);
     }
 }
--- src/main/java/school/faang/user_service/service/event/EventService.java
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -3,6 +3,7 @@ package school.faang.user_service.service.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.EventDto;
+import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.mapper.EventMapper;
 import school.faang.user_service.repository.event.EventRepository;
 
@@ -14,5 +15,11 @@ public class EventService {
 
     public EventDto getEvent(long id) {
         return mapper.toDto(repository.getReferenceById(id));
+    }
+
+    public void setCalendarEventId(long eventId, String calendarEventId) {
+        Event event = repository.getReferenceById(eventId);
+        event.setCalendarEventId(calendarEventId);
+        repository.save(event);
     }
 }

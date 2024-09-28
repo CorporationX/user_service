292b52ba3e1283fcedbb445bc9b4d264956c8c75
PLSGOTOASS <koteykaarisha@mail.ru>
2024-09-28 16:46:33 +0300

    пофиксил

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -1,37 +1,44 @@
-package school.faang.user_service.exceptionHandler;
+package school.faang.user_service.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import school.faang.user_service.exception.DataValidationException;
-import school.faang.user_service.exceptionDto.ExceptionDto;
+import school.faang.user_service.dto.exception.ExceptionDto;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionDto handleDataValidationException(DataValidationException ex, HttpServletRequest request) {
-        return new ExceptionDto("data validation error", ex.getMessage(), request.getRequestURI(), HttpStatus.BAD_REQUEST.value());
+        String errorMsg = "data validation error";
+        log.error(errorMsg, ex);
+
+        return new ExceptionDto(errorMsg, ex.getMessage(), request.getRequestURI(), HttpStatus.BAD_REQUEST.value(), null);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ExceptionDto handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        String errorMsg = "Method arguments are not valid";
         Map<String, String> errors = new HashMap<>();
+
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        log.error(errorMsg, ex);
+
+        return new ExceptionDto(errorMsg, ex.getMessage(), request.getRequestURI(), HttpStatus.BAD_REQUEST.value(), errors);
     }
 }

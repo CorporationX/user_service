5ac43eaa371080bae906ad5cc5d7263d06612923
OlgaK888 <olgakurilovi4@gmail.com>
2024-09-30 23:57:02 +0300

    Corrected test

--- src/test/java/school/faang/user_service/UserServiceApplicationTest.java
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -13,27 +13,27 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 class UserServiceApplicationTest {
 
-//    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-//            "postgres:13.3"
-//    );
-//
-//    @BeforeAll
-//    static void beforeAll() {
-//        postgres.start();
-//    }
-//
-//    @AfterAll
-//    static void afterAll() {
-//        postgres.stop();
-//    }
-//
-//    @DynamicPropertySource
-//    static void configureProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-//        registry.add("spring.datasource.username", postgres::getUsername);
-//        registry.add("spring.datasource.password", postgres::getPassword);
-//    }
-//
-//    @Test
-//    void contentLoads() {}
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:13.3"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contentLoads() {}
 }

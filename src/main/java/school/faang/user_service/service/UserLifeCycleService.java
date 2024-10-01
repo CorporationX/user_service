18430895b00ac186b79d4f951fb6139ca31f87c7
Robert Oguzov <robertoguzov2@gmail.com>
2024-10-01 23:00:01 +0300

    Изменил UserLifeCycleService и его тесты

--- src/main/java/school/faang/user_service/service/UserLifeCycleService.java
+++ src/main/java/school/faang/user_service/service/UserLifeCycleService.java
@@ -3,9 +3,13 @@ package school.faang.user_service.service;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserRegistrationDto;
 
+import java.util.List;
+
 public interface UserLifeCycleService {
 
     void deactivateUser(Long id);
 
     UserDto registrationUser(UserRegistrationDto userRegistrationDto);
+
+    void banUsersById(List<Long> ids);
 }

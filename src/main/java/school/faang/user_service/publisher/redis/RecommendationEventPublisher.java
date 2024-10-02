63821f2fad7daac96f02f5c18ff7ca6ea3efa5e0
OlgaK888 <olgakurilovi4@gmail.com>
2024-10-02 20:25:59 +0300

    Added @Slf4j to RecommendationEventPublisher

--- src/main/java/school/faang/user_service/publisher/redis/RecommendationEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/redis/RecommendationEventPublisher.java
@@ -1,12 +1,14 @@
 package school.faang.user_service.publisher.redis;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class RecommendationEventPublisher implements MessagePublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
@@ -14,6 +16,8 @@ public class RecommendationEventPublisher implements MessagePublisher {
 
     @Override
     public void publishMessage(String message) {
+
+        log.info("Sending to Redis: {}", message);
         redisTemplate.convertAndSend(recommendationChannel.getTopic(), message);
     }
 }

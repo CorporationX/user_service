7ad9fdb7460f97af99b2e40c96b6657e68d77359
OlgaK888 <olgakurilovi4@gmail.com>
2024-10-02 19:04:33 +0300

    Added @Slf4j to MentorshipAcceptedEventPublisher

--- src/main/java/school/faang/user_service/publisher/redis/MentorshipAcceptedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/redis/MentorshipAcceptedEventPublisher.java
@@ -1,12 +1,14 @@
 package school.faang.user_service.publisher.redis;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MentorshipAcceptedEventPublisher implements MessagePublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
@@ -15,5 +17,6 @@ public class MentorshipAcceptedEventPublisher implements MessagePublisher {
     @Override
     public void publishMessage(String message) {
         redisTemplate.convertAndSend(mentorshipAcceptedChannel.getTopic(), message);
+        log.info("Message in topic \"{}\" published: {}", mentorshipAcceptedChannel.getTopic(), message);
     }
 }

4a69e7fab7d320f1ce5e1853b6314d090ede3e17
Maksim Kononov <07maks7@mail.ru>
2024-10-02 22:00:05 +0300

    final refactoring code (i hope)

--- src/main/java/school/faang/user_service/exception/FileOperationException.java
+++ src/main/java/school/faang/user_service/exception/FileOperationException.java
@@ -2,6 +2,6 @@ package school.faang.user_service.exception;
 
 public class FileOperationException extends RuntimeException {
     public FileOperationException(String message, Throwable cause) {
-        super(message);
+        super(message, cause);
     }
 }

0d835a47a08fb9157c2a925f54e62621fda6f1ca
Bobiktrans <Bobicktrnas@yandex.ru>
2024-10-03 16:22:50 +0300

    BJS2-26244

--- src/main/java/school/faang/user_service/service/RecommendationRequestService.java
+++ src/main/java/school/faang/user_service/service/RecommendationRequestService.java
@@ -3,7 +3,6 @@ package school.faang.user_service.service;
 import school.faang.user_service.dto.*;
 import java.util.List;
 
-
 public interface RecommendationRequestService {
     RecommendationRequestDto rejectRequest(long id, RejectionDto rejectionDto);
     RecommendationRequestDto getRequest(long id);

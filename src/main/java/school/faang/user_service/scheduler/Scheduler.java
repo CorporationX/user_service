2c449be0f290c1f3d21a93fa6bc8a0b173cf7c63
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-10-03 14:07:47 +0300

    Небольшой фикс.

--- src/main/java/school/faang/user_service/scheduler/Scheduler.java
+++ src/main/java/school/faang/user_service/scheduler/Scheduler.java
@@ -1,13 +1,11 @@
 package school.faang.user_service.scheduler;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.service.event.EventService;
 
 @Component
-@Slf4j
 @RequiredArgsConstructor
 public class Scheduler {
     private final EventService eventService;

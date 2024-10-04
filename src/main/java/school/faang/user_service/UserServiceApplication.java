cddc0f79a329ef951b5dd9a4be93c35a4650d6c6
Sultan <sultan.inked@gmail.com>
2024-10-04 20:55:23 +0500

    BJS2-27083 delete @EnableScheduling in UserServiceApplication

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
 @OpenAPIDefinition(

096d76f9498e556ff6e32516ccbb7f2977dd4af8
Rashit Gareyev <gareyevrs@gmail.com>
2024-10-04 21:41:32 +0400

    feature-BJS2-27267 user ban listener

--- src/main/java/school/faang/user_service/mapper/GoalInvitationMapper.java
+++ src/main/java/school/faang/user_service/mapper/GoalInvitationMapper.java
@@ -1,9 +1,11 @@
 package school.faang.user_service.mapper;
 
+import com.json.student.Person;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.goal.GoalInvitation;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)

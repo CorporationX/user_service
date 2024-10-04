9e9084ea9a7d6e1ad8c730a264f265556dfdf52b
Sultan <sultan.inked@gmail.com>
2024-10-04 12:22:16 +0500

    relocated @EnableScheduling in UserServiceApplication

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
 @OpenAPIDefinition(
@@ -17,7 +18,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
                 title = "User Service",
                 version = "1.0.0")
 )
-@EnableScheduling
 public class UserServiceApplication {
 
     public static void main(String[] args) {

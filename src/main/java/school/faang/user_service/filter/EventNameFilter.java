f59453d2e1a9be86ebd89cf67ae70ea787b9fd0c
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-10-06 22:49:18 +0300

    валидация исправления

--- src/main/java/school/faang/user_service/filter/EventNameFilter.java
+++ src/main/java/school/faang/user_service/filter/EventNameFilter.java
@@ -1,19 +1,16 @@
 package school.faang.user_service.filter;
 
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
 import school.faang.user_service.dto.event.EventFilterDto;
 import school.faang.user_service.entity.event.Event;
 
 import java.util.stream.Stream;
 
 @Component
-@Validated
 public class EventNameFilter implements EventFilter {
 
     @Override
-    public boolean isApplicable(@Valid EventFilterDto eventFilterDto) {
+    public boolean isApplicable(EventFilterDto eventFilterDto) {
         return eventFilterDto.getEventTitle() != null;
     }
 

9cd4012a39f54542ecec3a3185cc3b21ea08de8d
Sultan <sultan.inked@gmail.com>
2024-10-08 18:58:11 +0500

    BJS2-27088 add topic name to yaml

--- src/main/java/school/faang/user_service/config/redis/user/UserRedisConfig.java
+++ src/main/java/school/faang/user_service/config/redis/user/UserRedisConfig.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.config.redis.user;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,11 +10,13 @@ import school.faang.user_service.dto.user.ProfileViewEventDto;
 
 @Configuration
 public class UserRedisConfig {
+    @Value("${app.user-redis-config.profile_view_event_topic}")
+    private String profileViewEventTopic;
 
     @Bean
     @Qualifier("profileViewEventTopic")
     public ChannelTopic profileViewEventTopic() {
-        return new ChannelTopic("profileViewEvent");
+        return new ChannelTopic(profileViewEventTopic);
     }
 
     @Bean

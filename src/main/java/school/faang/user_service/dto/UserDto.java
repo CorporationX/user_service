4335b9315366678175734881dd8fb35156f24ae6
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-10-09 15:50:10 +0300

    BJS2-26966 Обработчик событий

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -3,8 +3,10 @@ package school.faang.user_service.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import school.faang.user_service.entity.contact.PreferredContact;
 
 @Data
 @NoArgsConstructor
@@ -15,4 +17,8 @@ public class UserDto {
     private String username;
     @Email
     private String email;
+    @Size(max = 32, message = "Phone size cant be longer than 32 symbols")
+    private String phone;
+    @Size(max = 32, message = "Preference size cant be longer than 32 symbols")
+    private PreferredContact preference;
 }
--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
@@ -9,8 +10,10 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
+    @Mapping(target = "preference", source = "contactPreference.preference")
     UserDto toDto(User user);
 
+    @Mapping(target = "contactPreference.preference", source = "preference")
     User toEntity(UserDto userDto);
 
     List<UserDto> toDto(List<User> users);

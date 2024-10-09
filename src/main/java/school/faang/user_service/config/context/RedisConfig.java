bc1a434185728aaade6370bc767ac53c6ab530b1
DariaMak <49920983+DariaMak@users.noreply.github.com>
2024-10-09 18:13:33 +0300

    start user part 1

--- src/main/java/school/faang/user_service/config/context/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/context/RedisConfig.java
@@ -5,38 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.Topic;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import school.faang.user_service.listener.EventStartEventListener;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
-                                                   MessageListenerAdapter listenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, eventStartTopic());
-        return container;
-    }
-
-    @Bean
-    public ChannelTopic eventStartTopic() {
-        return new ChannelTopic("eventStartTopic");
-    }
-
-    @Bean
-    public MessageListenerAdapter listenerAdapter(EventStartEventListener eventStartEventListener) {
-        return new MessageListenerAdapter(eventStartEventListener);
-    }
-
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         return template;
+    }
+
+    @Bean
+    public ChannelTopic eventStartTopic() {
+        return new ChannelTopic("eventStartTopic");
     }
 }
--- src/main/java/school/faang/user_service/publisher/EventStartEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/EventStartEventPublisher.java
@@ -4,21 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
-import school.faang.user_service.service.event.EventStartEvent;
 
 import java.util.List;
 
 @Service
 public class EventStartEventPublisher {
 
-    private final RedisTemplate<String, Object> redisTemplate;
-    private final ChannelTopic topic;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
-    public EventStartEventPublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic topic) {
-        this.redisTemplate = redisTemplate;
-        this.topic = topic;
-    }
+    private ChannelTopic topic;
 
     public void publishEventStart(Long eventId, List<Long> participantIds) {
         EventStartEvent event = new EventStartEvent(eventId, participantIds);

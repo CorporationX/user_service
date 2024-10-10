9d6c3b2e029d1a231cdb377aa70f41f2c2ae9b80
Sultan <sultan.inked@gmail.com>
2024-10-10 20:31:05 +0500

    BJS2-27088 fix qualified topic

--- src/main/java/school/faang/user_service/config/redis/user/RedisProfileViewEventPublisher.java
+++ src/main/java/school/faang/user_service/config/redis/user/RedisProfileViewEventPublisher.java
@@ -2,6 +2,7 @@ package school.faang.user_service.config.redis.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
@@ -9,10 +10,16 @@ import school.faang.user_service.dto.user.ProfileViewEventDto;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class RedisProfileViewEventPublisher implements ProfileViewEventPublisher {
     private final RedisTemplate<String, ProfileViewEventDto> redisTemplate;
     private final ChannelTopic channelTopic;
+
+    public RedisProfileViewEventPublisher(RedisTemplate<String, ProfileViewEventDto> redisTemplate,
+                                          @Qualifier("profileViewEventTopic")
+                                          ChannelTopic channelTopic) {
+        this.redisTemplate = redisTemplate;
+        this.channelTopic = channelTopic;
+    }
 
     @Override
     public void publish(ProfileViewEventDto profileViewEventDto) {

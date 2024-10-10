9e80a0cf6049412edd19b196fe070c97b1973255
Sultan <sultan.inked@gmail.com>
2024-10-10 20:26:32 +0500

    BJS2-27088 fix test

--- src/test/java/school/faang/user_service/service/user/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/user/UserServiceTest.java
@@ -35,6 +35,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;

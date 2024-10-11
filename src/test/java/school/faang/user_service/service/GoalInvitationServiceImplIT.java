38db6d9adc8d44e3a434ae95fe647e417ce6aac3
Владислав Воронов <iPlesk90@yandex.ru>
2024-10-11 17:51:37 +0300

    Выполнил задачу

--- src/test/java/school/faang/user_service/service/GoalInvitationServiceImplIT.java
+++ src/test/java/school/faang/user_service/service/GoalInvitationServiceImplIT.java
@@ -29,7 +29,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class GoalInvitationServiceImplTest {
+class GoalInvitationServiceImplIT {
 
     private GoalRepository goalRepository;
     private UserRepository userRepository;

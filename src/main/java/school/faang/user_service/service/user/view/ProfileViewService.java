418834f30aec4ddeb453b96892ace4e8c7bf66e9
Sultan <sultan.inked@gmail.com>
2024-10-13 16:26:46 +0500

    BJS2-27088 refactoring ProfileViewService publishAllProfileViewEvents

--- src/main/java/school/faang/user_service/service/user/view/ProfileViewService.java
+++ src/main/java/school/faang/user_service/service/user/view/ProfileViewService.java
@@ -36,20 +36,17 @@ public class ProfileViewService {
 
     public void publishAllProfileViewEvents() {
         List<ProfileViewEventDto> profileViewEventDtosCopy = new ArrayList<>();
+        ProfileViewEventDto profileViewEventDto;
+        while ((profileViewEventDto = profileViewEventDtos.poll()) != null) {
+            profileViewEventDtosCopy.add(profileViewEventDto);
+        }
+        log.info("Publish profile view events, size: {}", profileViewEventDtos.size());
         try {
-            synchronized (profileViewEventDtos) {
-                log.info("Publish profile view events, size: {}", profileViewEventDtos.size());
-                profileViewEventDtosCopy = new ArrayList<>(profileViewEventDtos);
-                profileViewEventDtos.clear();
-            }
             redisProfileViewEventPublisher.publish(profileViewEventDtosCopy);
         } catch (Exception e) {
             log.error("Profile view events publish failed:", e);
-            synchronized (profileViewEventDtos) {
-                log.info("Save back to main profile view events copy remainder, size: {}",
-                        profileViewEventDtosCopy.size());
-                profileViewEventDtos.addAll(profileViewEventDtosCopy);
-            }
+            log.info("Save back to main profile view events copy remainder, size: {}", profileViewEventDtosCopy.size());
+            profileViewEventDtos.addAll(profileViewEventDtosCopy);
         }
     }
 }

c5e5fa7f1f73dec668ea15282fcafb41dce8703a
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-10-14 17:26:53 +0300

    .

---
+++ src/main/java/school/faang/user_service/config/redis/RedisConfig.java
@@ -0,0 +1,42 @@
+package school.faang.user_service.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import school.faang.user_service.publisher.RecommendationRequestedEventPublisher;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.channels.recommendation-request-channel.name}")
+    private String recommendationRequestTopic;
+
+    @Bean
+    public ChannelTopic recommendationRequestTopic() {
+        return new ChannelTopic(recommendationRequestTopic);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RecommendationRequestedEventPublisher recommendationRequestedPublisher(
+            RedisTemplate<String, RecommendationRequestEvent> redisTemplate, ChannelTopic recommendationRequestTopic) {
+        return new RecommendationRequestedEventPublisher(redisTemplate, recommendationRequestTopic);
+    }
+
+}

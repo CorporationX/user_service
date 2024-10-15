9f51525bbe5f27e5be572804a1c8656ca3cfc0c3
Андреев Александр Сергеевич <AAndreev13@alfabank.ru>
2024-10-15 21:48:17 +0300

    исправлен скрипт миграции

--- src/main/resources/db/changelog/changeset/user_V011_BJS2-27267_add_version_banned_columns.sql
+++ src/main/resources/db/changelog/changeset/user_V011_BJS2-27267_add_version_banned_columns.sql
@@ -1,5 +1,5 @@
 ALTER TABLE users
-ADD COLUMN version INT NOT NULL DEFAULT 0;
+    ADD COLUMN if not exists version INT NOT NULL DEFAULT 0;
 
 ALTER TABLE users
-ADD COLUMN banned BOOLEAN NOT NULL DEFAULT false;
+    ADD COLUMN if not exists banned BOOLEAN NOT NULL DEFAULT false;

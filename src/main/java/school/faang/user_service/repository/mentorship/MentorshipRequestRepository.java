661e3e555f4d776451958e8cf919df7961d417e9
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-10-15 22:46:13 +0500

    Refactoring

--- src/main/java/school/faang/user_service/repository/mentorship/MentorshipRequestRepository.java
+++ src/main/java/school/faang/user_service/repository/mentorship/MentorshipRequestRepository.java
@@ -13,7 +13,7 @@ public interface MentorshipRequestRepository extends JpaRepository<MentorshipReq
 
     @Query(nativeQuery = true, value = """
             INSERT INTO mentorship_request (requester_id, receiver_id, description, status, created_at, updated_at)
-            VALUES (:requesterId, :receiverId, :description, 0, NOW(), NOW())
+            VALUES (?1, ?2, ?3, 0, NOW(), NOW())
             RETURNING *
             """)
     MentorshipRequest create(long requesterId, long receiverId, String description);

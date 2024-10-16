7c4f436917907a062f33ebf273dd380ee852cddd
KarabbasBarabas <lkn_popov@mail.ru>
2024-10-16 19:09:15 +0300

    BJS2-27527 поправены методы контроллера

--- src/main/java/school/faang/user_service/controller/subscription/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/subscription/SubscriptionController.java
@@ -17,12 +17,12 @@ import school.faang.user_service.service.subscription.SubscriptionService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users/subscriptions")
+@RequestMapping("/users/{followeeId}/subscriptions")
 @RequiredArgsConstructor
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
-    @PostMapping("/follower/{followerId}/followee/{followeeId}")
+    @PostMapping("/followers/{followerId}")
     public SuccessResponse followUser(@PathVariable Long followerId,
                                       @PathVariable Long followeeId) {
         subscriptionService.followUser(followerId, followeeId);

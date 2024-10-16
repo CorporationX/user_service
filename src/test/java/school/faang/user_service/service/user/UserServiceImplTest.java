84942dab5d97c634097fb09df6b3ff645786e611
Maksim Kononov <07maks7@mail.ru>
2024-10-16 16:29:50 +0300

    resolve ci conflict

--- src/test/java/school/faang/user_service/service/user/UserServiceImplTest.java
+++ src/test/java/school/faang/user_service/service/user/UserServiceImplTest.java
@@ -198,7 +198,7 @@ public class UserServiceImplTest {
         ByteArrayOutputStream outputStream = mock(ByteArrayOutputStream.class);
         String fileName = "fileName";
 
-        String result = (String) ReflectionTestUtils.invokeMethod (
+        String result = (String) ReflectionTestUtils.invokeMethod(
                 service, "uploadFile", userId, outputStream, fileName);
 
         assertNotNull(result);

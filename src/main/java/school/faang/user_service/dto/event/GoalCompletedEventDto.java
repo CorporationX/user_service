1b229f9d7e500a49714e5805f1f8613354bffcdf
Maksim Kononov <07maks7@mail.ru>
2024-10-16 16:16:42 +0300

    delete another task code

--- src/main/java/school/faang/user_service/dto/event/GoalCompletedEventDto.java
+++
@@ -1,9 +0,0 @@
-package school.faang.user_service.dto.event;
-
-import java.time.LocalDateTime;
-
-public record GoalCompletedEventDto(
-        long userId,
-        long eventId,
-        LocalDateTime timestamp) {
-}

d4213770a86a07e06b6726f6b786c9467853fc5a
Robert Oguzov <robertoguzov2@gmail.com>
2024-10-17 18:37:19 +0300

    Добавил новый столбец в users таблицу

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 public class UserDto {
     private Long id;
     private String username;
+    private Long telegramId;
     private String email;
     private String phone;
     private Locale locale;

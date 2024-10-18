57c8a3fa88c662578a64dc7e3540266636e6d1e0
Denis Alginin <AlgininDK@gmail.com>
2024-10-18 13:24:59 +0500

    Добавил тесты

--- src/test/java/school/faang/user_service/publis/publisher/FollowerEventPublisherTest.java
+++ src/test/java/school/faang/user_service/publis/publisher/FollowerEventPublisherTest.java
@@ -1,4 +1,68 @@
 package school.faang.user_service.publis.publisher;
 
-public class FollowerEventPublisherTest {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import school.faang.user_service.config.redis.RedisProperties;
+import school.faang.user_service.dto.follower.FollowerEventDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.mapper.follower.FollowerEventMapper;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class FollowerEventPublisherTest {
+    @Mock
+    private RedisProperties redisProperties;
+    @Mock
+    private StringRedisTemplate redisTemplate;
+    @Mock
+    private ObjectMapper objectMapper;
+    @Mock
+    private FollowerEventMapper followerEventMapper;
+
+    @InjectMocks
+    private FollowerEventPublisher followerEventPublisher;
+
+    private final User follower = new User();
+    private final Long followeeId = 1L;
+    private final FollowerEventDto followerEventDto = new FollowerEventDto();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFollowerEventPublisher() throws JsonProcessingException {
+        String channelName = "followerEventChannel";
+        String message = "{\"followerId\":1,\"followeeId\":2}";
+
+        when(redisProperties.getFollowerEventChannelName()).thenReturn(channelName);
+        when(followerEventMapper.toEventDto(follower, followeeId)).thenReturn(followerEventDto);
+        when(objectMapper.writeValueAsString(followerEventDto)).thenReturn(message);
+
+        followerEventPublisher.publishFollower(follower, followeeId);
+
+        verify(redisTemplate).convertAndSend(channelName, message);
+        verify(followerEventMapper).toEventDto(follower, followeeId);
+        verify(objectMapper).writeValueAsString(followerEventDto);
+    }
+
+    @Test
+    void testFollowerEventPublisher_FailFormat() throws JsonProcessingException {
+        when(followerEventMapper.toEventDto(follower, followeeId)).thenReturn(followerEventDto);
+        when(objectMapper.writeValueAsString(followerEventDto)).
+                thenThrow(new JsonProcessingException("ERROR") {});
+
+        assertThrows(RuntimeException.class, () -> followerEventPublisher.publishFollower(follower, followeeId));
+
+        verify(redisTemplate, never()).convertAndSend(anyString(), anyString());
+    }
 }
--- src/test/java/school/faang/user_service/service/subscription/SubscriptionServiceTest.java
+++ src/test/java/school/faang/user_service/service/subscription/SubscriptionServiceTest.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;

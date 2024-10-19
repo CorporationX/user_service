86386f8e287e081a6f9959656b9bc4633f4f89ea
Anton Martynenko <ant.martynenko@gmail.com>
2024-10-19 18:35:45 +0200

    Уведомление о просмотре профиля

--- src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
+++ src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
@@ -2,15 +2,20 @@ package school.faang.user_service.service.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserFilterDto;
+import school.faang.user_service.dto.redisevent.ProfileViewEvent;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.exception.UserNotFoundException;
 import school.faang.user_service.filter.UserFilter;
 import school.faang.user_service.mapper.UserMapper;
+import school.faang.user_service.publisher.EventPublisher;
+import school.faang.user_service.publisher.RedisTopics;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.premium.PremiumRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
@@ -22,6 +27,8 @@ public class UserServiceImpl implements UserService {
     private final PremiumRepository premiumRepository;
     private final List<UserFilter> filters;
     private final UserMapper userMapper;
+    private final UserContext userContext;
+    private final EventPublisher eventPublisher;
 
     @Override
     public List<UserDto> getPremiumUsers(UserFilterDto userFilterDto) {
@@ -34,9 +41,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto getUser(long userId) {
-        return userRepository.findById(userId)
-                .map(userMapper::toDto)
+        UserDto userDto = userRepository.findById(userId).map(userMapper::toDto)
                 .orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
+        ProfileViewEvent profileViewEvent = new ProfileViewEvent(
+                userDto.getId(),
+                userContext.getUserId(),
+                LocalDateTime.now()
+        );
+        eventPublisher.publishToTopic(RedisTopics.PROFILE_VIEW.getTopicName(), profileViewEvent);
+        return userDto;
     }
 
     @Override

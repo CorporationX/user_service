1676a8e44f2a5c3c9c669dd1a431a44df79e536c
Rashit Gareyev <gareyevrs@gmail.com>
2024-10-20 22:21:18 +0400

    feature-BJS2-27322 fix after merge

--- src/main/java/school/faang/user_service/publisher/ProfileViewEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/ProfileViewEventPublisher.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import school.faang.user_service.model.event.ProfileViewEvent;
 
 @Component
-public class ProfileViewEventPublisher extends RedisEventPublisher<ProfileViewEvent>{
+public class ProfileViewEventPublisher extends AbstractEventPublisher<ProfileViewEvent>{
     public ProfileViewEventPublisher(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper,
                                      @Qualifier("profileViewTopic") ChannelTopic channelTopic) {
         super(redisTemplate, objectMapper, channelTopic);

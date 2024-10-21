eadee0ebfe7ecf60c180b58b8007fa01f6254ea3
Okabe <mathew.mackonoha@gmail.com>
2024-10-21 18:00:50 +0300

    Готово.

--- src/main/java/school/faang/user_service/config/redis/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/redis/RedisConfig.java
@@ -6,10 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import school.faang.user_service.listener.UserBanListener;
 
 @Configuration
@@ -29,8 +32,16 @@ public class RedisConfig {
         return new MessageListenerAdapter(userBanListener);
     }
     @Bean
-    ChannelTopic profileViewTopic() {
+    ChannelTopic profileViewEventTopic() {
         return new ChannelTopic(redisProperties.getChannels().get("profile-view-channel"));
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 
     @Bean
--- src/main/java/school/faang/user_service/publisher/ProfileViewEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/ProfileViewEventPublisher.java
@@ -10,12 +10,13 @@ import school.faang.user_service.dto.ProfileViewEvent;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ProfileViewPublisher {
+public class ProfileViewEventPublisher implements EventPublisher<ProfileViewEvent> {
     private final RedisTemplate<String, Object> redisTemplate;
     private final RedisProperties redisProperties;
 
+    @Override
     public void publish(ProfileViewEvent event) {
-        String channelName = redisProperties.getChannels().get("project-event-channel");
+        String channelName = redisProperties.getChannels().get("profile_view_event");
         redisTemplate.convertAndSend(channelName, event);
         log.info("Published event {}", event);
     } // метод не используется, по-скольку нет сервиса реализующего просмотр профиля пользователя

e7050190a3103cf947aca33d3bb9e97a4f4b805b
Aleksey <aleksei.zuk86@gmail.com>
2024-11-07 22:49:01 +0300

    CI Pipline Test

---
+++ src/test/java/school/faang/user_service/service/TestFail.java
@@ -0,0 +1,14 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestFail {
+
+    @Test
+    public void thatTestFail() {
+
+        fail("This Test is supported3 to fail");
+    }
+}

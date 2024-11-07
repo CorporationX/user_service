5e6397c7d0170de9357ab118fac1fd4fe1d25c7c
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-11-07 14:29:22 +0300

    BJS2-40768 - (test) CI pipeline для user_service

---
+++ src/test/java/school/faang/user_service/service/SomeFailedTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SomeFailedTest {
+    @Test
+    public void testFailed() {
+        fail("Fail");
+    }
+}

d3b9316d99601f58f2a45299eb281513da77e057
Maria Sahno <sahnomyu@m2.ru>
2024-12-22 01:52:23 +0500

    clean up

--- src/main/java/school/faang/user_service/repository/SkillRepository.java
+++ src/main/java/school/faang/user_service/repository/SkillRepository.java
@@ -4,13 +4,11 @@ import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.Skill;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
 
     boolean existsByTitle(String title);
--- src/main/java/school/faang/user_service/repository/UserSkillGuaranteeRepository.java
+++ src/main/java/school/faang/user_service/repository/UserSkillGuaranteeRepository.java
@@ -2,10 +2,8 @@ package school.faang.user_service.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.UserSkillGuarantee;
 
-@Repository
 public interface UserSkillGuaranteeRepository extends CrudRepository<UserSkillGuarantee, Long> {
     @Query(nativeQuery = true, value = "INSERT INTO user_skill_guarantee (user_id, skill_id, guarantor_id) VALUES (?1, ?2, ?3) returning id")
     Long addSkillGuarantee(long userId, long skillId, long guarantorId);

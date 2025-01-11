eb28fdee34fa19524e40eb24430861fbd676dc78
Erik <147136021+alterality@users.noreply.github.com>
2025-01-11 16:54:39 +0600

    Update GlobalExceptionHandler.java

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
 import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;

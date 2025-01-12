b002a4d89828623d5243f06d3d16238f671ed7d1
Alexey Rogoza <rgz.ttt@mail.ru>
2025-01-12 18:10:10 +0700

    Added second realisation

--- src/main/java/school/faang/user_service/contrroller/event/EventParticipationController.java
+++ src/main/java/school/faang/user_service/contrroller/event/EventParticipationController.java
@@ -1,16 +1,22 @@
 package school.faang.user_service.contrroller.event;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.RegisterParticipantDto;
 import school.faang.user_service.service.event.EventParticipationService;
 
+@RestController("/Participation")
 @Component
 @RequiredArgsConstructor
 public class EventParticipationController {
+
     private final EventParticipationService eventParticipationService;
 
-    public void registerReplicant(){
 
+    public void registerParticipant(RegisterParticipantDto registerParticipantDto) {
+        long userId = registerParticipantDto.getUserId();
+        long eventId = registerParticipantDto.getEventId();
+        eventParticipationService.registerParticipant(userId, eventId);
     }
 }

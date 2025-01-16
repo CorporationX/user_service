a53248118baabde27c1910222c7f1edc4403e798
RGZ_TTT <rgz.ttt@mail.ru>
2025-01-16 22:07:39 +0700

    Добавлены следующие этапы для реализации

--- src/main/java/school/faang/user_service/service/event/EventParticipationService.java
+++ src/main/java/school/faang/user_service/service/event/EventParticipationService.java
@@ -3,32 +3,67 @@ package school.faang.user_service.service.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import school.faang.user_service.dto.event.participant.RegisterParticipantDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.event.EventParticipationRepository;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventParticipationService {
     private final EventParticipationRepository eventParticipationRepository;
-    private final Map<Long, Long> particaptionDtoMap = new HashMap<>();
 
-    @Transactional
-    public void registerParticipant(RegisterParticipantDto registerParticipantDto) {
-        long eventId = registerParticipantDto.getEventId();
-        long userId = registerParticipantDto.getUserId();
+    public void eventValidationExist(long eventID) throws DataValidationException {
+        int eventCountById = eventParticipationRepository.countParticipants(eventID);
+        if (eventCountById == 0) {
+            throw new DataValidationException("Событие не найдено!");
+        }
+    }
 
-        particaptionDtoMap.put(eventId, userId);
-        eventParticipationRepository.register(particaptionDtoMap.get(eventId), userId);
+    public void userValidationExist(long userID) throws DataValidationException {
+        List<User> participationList = eventParticipationRepository.findAllParticipantsByEventId(userID);
+        boolean userWasRegister = participationList.stream().anyMatch(user -> user.getId() == userID);
+        if (userWasRegister) {
+            throw new DataValidationException("Пользователь уже был зарегистрирован на событии!");
+        }
+    }
+
+    public void userNotFound(long eventId) throws DataValidationException {
+        if (eventParticipationRepository.findAllParticipantsByEventId(eventId) == null) {
+            throw new DataValidationException("Пользователи не найдены!");
+        }
     }
 
     @Transactional
-    EventParticipationService eventParticipationService() {
-        return new EventParticipationService(eventParticipationRepository);
+    public void registerParticipant(long eventID, long userID) throws DataValidationException {
+        eventValidationExist(eventID);
+        userValidationExist(userID);
+        eventParticipationRepository.register(eventID, userID);
     }
 
+    @Transactional
+    public void unregisterParticipant(long eventID, long userID) throws DataValidationException {
+        eventValidationExist(eventID);
+        List<User> participationList = eventParticipationRepository.findAllParticipantsByEventId(eventID);
+        boolean userWasRegister = participationList.stream().anyMatch(user -> user.getId() == userID);
+        if (!userWasRegister) {
+            throw new DataValidationException("Пользователь не был зарегистрирован на событии!");
+        }
+        eventParticipationRepository.unregister(eventID, userID);
+    }
 
+    @Transactional
+    public List<User> getParticipant(long eventId) {
+        eventValidationExist(eventId);
+        userNotFound(eventId);
+        return eventParticipationRepository.findAllParticipantsByEventId(eventId);
+    }
+
+    @Transactional
+    public int getParticipantCounts(long eventId) {
+        eventValidationExist(eventId);
+        userNotFound(eventId);
+        return eventParticipationRepository.countParticipants(eventId);
+    }
 }

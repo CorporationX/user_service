197b75ac59ee017438aeb56eb59b2437922bf393
User Novozhilova <nov2303@mail,ru>
2025-01-16 10:10:02 +0500

    BJS2-52606 | Запрос на менторство

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class DataValidationException extends RuntimeException{
+
     public DataValidationException(String message) {
         super(message);
     }

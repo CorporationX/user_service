5663ec42db8b10c22c77c7558568f213be408384
RGZ_TTT <rgz.ttt@mail.ru>
2025-01-17 13:24:18 +0700

    Добавлены следующие этапы для реализации

---
+++ src/test/java/school/faang/user_service/service/event/EventParticipationServiceTest.java
@@ -0,0 +1,43 @@
+package school.faang.user_service.service.event;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventParticipationServiceTest {
+
+    private long userId = 1L;
+    private long eventId = 2L;
+
+    @Mock
+    EventParticipationService eventParticipationService;
+
+    @Test
+    void eventValidationExist() {
+    }
+
+    @Test
+    void userValidationExist() {
+    }
+
+    @Test
+    void userNotFound() {
+    }
+
+    @Test
+    void registerParticipant() {
+    }
+
+    @Test
+    void unregisterParticipant() {
+    }
+
+    @Test
+    void getParticipant() {
+    }
+
+    @Test
+    void getParticipantCounts() {
+    }
+}

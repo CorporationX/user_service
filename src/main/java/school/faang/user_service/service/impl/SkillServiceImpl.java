ca68349fa28d2b9376fadb07db5b7c74b510d907
zhagot <zhagot@mail.ru>
2025-01-20 01:44:48 +0300

    BJS2-52603 fix after pull from master

--- src/main/java/school/faang/user_service/service/impl/SkillServiceImpl.java
+++ src/main/java/school/faang/user_service/service/impl/SkillServiceImpl.java
@@ -2,11 +2,11 @@ package school.faang.user_service.service.impl;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.entity.Skill;
+import school.faang.user_service.dto.entity.UserSkillGuarantee;
+import school.faang.user_service.dto.entity.recommendation.SkillOffer;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
 import school.faang.user_service.dto.skill.SkillDto;
-import school.faang.user_service.entity.Skill;
-import school.faang.user_service.entity.UserSkillGuarantee;
-import school.faang.user_service.entity.recommendation.SkillOffer;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.mapper.SkillMapper;
 import school.faang.user_service.repository.SkillRepository;

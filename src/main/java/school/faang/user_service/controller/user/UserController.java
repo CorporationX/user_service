a257a8da0bb2873423b1bcb6e47188f29a5b27cf
porchy21 <ogogo.mt@gmail.com>
2025-01-20 22:34:08 +0500

    fix conflicts in UserService and UserController

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.controller.user;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;

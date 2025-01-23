704361113feecf477588424a9e7b7e6d2c5e8dab
Eugene <@evgeniy_kim>
2025-01-23 10:06:20 +0500

    BJS2-52705 | Покупка премиум доступа

--- src/main/java/school/faang/user_service/mapper/PremiumMapper.java
+++ src/main/java/school/faang/user_service/mapper/PremiumMapper.java
@@ -3,8 +3,8 @@ package school.faang.user_service.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import school.faang.user_service.dto.entity.premium.Premium;
 import school.faang.user_service.dto.premium.PremiumDto;
+import school.faang.user_service.entity.premium.Premium;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PremiumMapper {

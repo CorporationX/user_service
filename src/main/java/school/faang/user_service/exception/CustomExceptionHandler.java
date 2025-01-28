31752fceb4073c0d86e9018894baf393c2a2df6a
Eugene <@evgeniy_kim>
2025-01-28 11:01:07 +0500

    BJS2-52827 | Система постов

--- src/main/java/school/faang/user_service/exception/CustomExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/CustomExceptionHandler.java
@@ -2,15 +2,11 @@ package school.faang.user_service.exception;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.nio.charset.StandardCharsets;
 
 @RestControllerAdvice
 @Slf4j
@@ -19,9 +15,6 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<String> dataNotFound(DataNotFoundException e) {
-        HttpHeaders headers = new HttpHeaders();
-        MediaType mediaType = MediaType.TEXT_PLAIN;
-        headers.setContentType(new MediaType(mediaType.getType(), mediaType.getSubtype(), StandardCharsets.UTF_8));
-        return new ResponseEntity<>(e.getMessage(), headers, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 }

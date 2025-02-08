5155e4b2e79d486244bffa71403d00b197536d75
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2025-02-07 21:47:18 -0800

    should fail checkstyle and tests

--- src/test/java/school/faang/user_service/service/ValidationTest.java
+++ src/test/java/school/faang/user_service/service/ValidationTest.java
@@ -7,9 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ValidationTest {
 
     @Test
-    void shouldBeSuccess() {
+    public void shouldBeSuccess() {
         String t1 = "hh";
         String k2 = "hh";
         assertEquals(t1, k2);
+    }
+
+    @Test
+    public void checkstyleShoulfail() {
+        if (true){
+            System.out.println("it's true");
+        }
+        if( 3 < 5 ) {
+            System.out.println("no space after if");
+        }
+    }
+
+    @Test
+    public void shouldBeFailedTest() {
+        assertEquals(2, 3);
     }
 }

4e271c1e77b2f17bde598a1b84d30f52be1c4631
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2025-02-16 00:28:17 +0300

    small fix(удалил неиспользуемый импорт и неиспользуемый класс)

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.exception.EntityNotFoundException;
 import school.faang.user_service.filters.user.UserFilter;
 import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.UserRepository;

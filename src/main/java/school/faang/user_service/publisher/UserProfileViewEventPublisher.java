88460ece411a836a4bc6e6f20fc9bdde43e0dbd2
Alexey Gorokhov <almigor@mail.ru>
2025-02-26 18:15:56 +0300

    Правки после мержа

--- src/main/java/school/faang/user_service/publisher/UserProfileViewEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/UserProfileViewEventPublisher.java
@@ -3,10 +3,12 @@ package school.faang.user_service.publisher;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserProfileViewEventPublisher {
@@ -14,14 +16,16 @@ public class UserProfileViewEventPublisher {
     private final RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper objectMapper;
 
-    @Value("${redis.topic.user-profile-view}")
+    @Value("${spring.data.redis.channel.profile-view}")
     private final String redisTopic;
 
     public void publish(Object message) {
         try {
             String json = objectMapper.writeValueAsString(message);
             redisTemplate.convertAndSend(redisTopic, json);
+            log.info("Message {} was sent to redis topic {}", message, redisTopic);
         } catch (JsonProcessingException e) {
+            log.error("Error sending message {} to redis topic {}", message, redisTopic);
             throw new RuntimeException(e);
         }
     }

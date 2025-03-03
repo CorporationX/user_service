0cea77437112560cc4c20b17dfdcae7b4013e28f
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-03-03 21:43:13 +0300

    исправил ошибку mapper

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -4,8 +4,9 @@ import org.mapstruct.Mapper;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
     User toEntity(UserDto userDto);
+
     UserDto toDto(User user);
 }

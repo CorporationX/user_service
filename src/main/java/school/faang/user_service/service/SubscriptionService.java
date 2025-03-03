57978f4a86db86d5940fd86778fd2b149b480848
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-03-03 16:58:50 +0300

    замена конструктора с помощью lombok

--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -1,18 +1,15 @@
 package school.faang.user_service.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.SubscriptionRepository;
 
+@RequiredArgsConstructor
 @Service
 public class SubscriptionService {
     private final SubscriptionRepository subscriptionRepository;
-
-    @Autowired
-    public SubscriptionService(SubscriptionRepository subscriptionRepository) {
-        this.subscriptionRepository = subscriptionRepository;
-    }
 
     public void followUser(long followerId, long followeeId) {
         if (subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, followeeId)) {

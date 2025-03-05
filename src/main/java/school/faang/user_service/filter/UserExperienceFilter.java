d40d787ff8f802c225ee66cb6bff9a8f8a7812d1
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-03-05 13:59:57 +0300

    Исправлена ошибка в фильтрах

--- src/main/java/school/faang/user_service/filter/UserExperienceFilter.java
+++ src/main/java/school/faang/user_service/filter/UserExperienceFilter.java
@@ -1,10 +1,12 @@
 package school.faang.user_service.filter;
 
+import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.entity.User;
 
 import java.util.stream.Stream;
 
+@Component
 public class UserExperienceFilter implements UserFilter {
     @Override
     public boolean isApplicable(UserFilterDto userFilterDto) {

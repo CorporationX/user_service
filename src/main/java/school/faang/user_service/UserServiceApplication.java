1ddb11e53d0cf093404738f03e6d9bf0f6242200
vtroshin <vtroshin@rvision.ru>
2025-03-06 17:48:44 +0300

    BJS2-65996 Джоба для удаления истекших премиумов

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -6,14 +6,20 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import school.faang.user_service.schedule.PremiumExpiredScheduleTask;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
+@EnableScheduling
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(UserServiceApplication.class, args);
+        PremiumExpiredScheduleTask premiumExpiredScheduleTask = applicationContext.getBean(PremiumExpiredScheduleTask.class);
+        premiumExpiredScheduleTask.searchAndDeleteExpiredPremiums();
     }
 
     @Bean

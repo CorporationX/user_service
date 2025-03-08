2bcf290e0691043d629a8360bc83a4e475eef70f
vtroshin <vtroshin@rvision.ru>
2025-03-08 19:13:26 +0300

    BJS2-65996 Убрал лишний код из main

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -6,11 +6,9 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import school.faang.user_service.schedule.PremiumExpiredScheduleTask;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
@@ -18,9 +16,7 @@ import school.faang.user_service.schedule.PremiumExpiredScheduleTask;
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(UserServiceApplication.class, args);
-        PremiumExpiredScheduleTask premiumExpiredScheduleTask = applicationContext.getBean(PremiumExpiredScheduleTask.class);
-        premiumExpiredScheduleTask.searchAndDeleteExpiredPremiums();
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
     @Bean

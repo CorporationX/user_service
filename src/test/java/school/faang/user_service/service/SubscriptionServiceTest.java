0dc9be5bdee317f20fc67ee7fa88a9182f524db9
vtroshin <vtroshin@rvision.ru>
2025-03-13 20:16:38 +0300

    BJS2-66745 Add test + static import

--- src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
+++ src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
@@ -19,6 +19,9 @@ import school.faang.user_service.repository.SubscriptionRepository;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 public class SubscriptionServiceTest {
     private final UserFilter phoneFilter = new UserPhoneFilterTest();
@@ -41,53 +44,62 @@ public class SubscriptionServiceTest {
 
     @Test
     public void testFollowUserWithEqualsId() {
-        Assertions.assertThrows(DataValidationException.class,
+        assertThrows(DataValidationException.class,
                 () -> subscriptionService.followUser(1L, 1L));
     }
 
     @Test
-    public void testFollowUserWithExistSubscription() {
-        Mockito.when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
+    public void testFollowUserWithAlreadyExistFollowing() {
+        when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
                 .thenReturn(true);
 
-        Assertions.assertThrows(DataValidationException.class,
+        assertThrows(DataValidationException.class,
+                () -> subscriptionService.followUser(1L, 2L));
+    }
+
+    @Test
+    public void testFollowUserWithExistSubscription() {
+        when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
+                .thenReturn(true);
+
+        assertThrows(DataValidationException.class,
                 () -> subscriptionService.followUser(1L, 2L));
     }
 
     @Test
     public void testFollowUser() {
-        Mockito.when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
+        when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
                 .thenReturn(false);
 
         subscriptionService.followUser(1L, 2L);
 
-        Mockito.verify(subscriptionRepository, Mockito.times(1))
+        verify(subscriptionRepository, times(1))
                 .followUser(1L, 2L);
     }
 
     @Test
     public void testUnfollowUserWithEqualsId() {
-        Assertions.assertThrows(DataValidationException.class,
+        assertThrows(DataValidationException.class,
                 () -> subscriptionService.unfollowUser(1L, 1L));
     }
 
     @Test
     public void testUnfollowUserWithNotExistSubscription() {
-        Mockito.when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
+        when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
                 .thenReturn(false);
 
-        Assertions.assertThrows(DataValidationException.class,
+        assertThrows(DataValidationException.class,
                 () -> subscriptionService.unfollowUser(1L, 2L));
     }
 
     @Test
     public void testUnfollowUser() {
-        Mockito.when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
+        when(subscriptionRepository.existsByFollowerIdAndFolloweeId(1L, 2L))
                 .thenReturn(true);
 
         subscriptionService.unfollowUser(1L, 2L);
 
-        Mockito.verify(subscriptionRepository, Mockito.times(1))
+        verify(subscriptionRepository, times(1))
                 .unfollowUser(1L, 2L);
     }
 
@@ -96,7 +108,7 @@ public class SubscriptionServiceTest {
         User appliedForFilter = User.builder().phone("+79999999999").username("Takewqa").experience(222).build();
         User notAppliedForFilter = User.builder().phone("+79999999997").username("Aqwekat").experience(222).build();
 
-        Mockito.when(subscriptionRepository.findByFollowerId(1L))
+        when(subscriptionRepository.findByFollowerId(1L))
                 .thenReturn(Stream.of(appliedForFilter, notAppliedForFilter));
 
         List<FollowerResponse> actualFollowings = subscriptionService.getFollowing(1L,
@@ -107,17 +119,17 @@ public class SubscriptionServiceTest {
                         333
                 ));
 
-        Assertions.assertEquals(1, actualFollowings.size());
-        Assertions.assertEquals("Takewqa", actualFollowings.get(0).username());
+        assertEquals(1, actualFollowings.size());
+        assertEquals("Takewqa", actualFollowings.get(0).username());
     }
 
     @Test
     public void testGetFollowingCount() {
-        Mockito.when(subscriptionRepository.findFolloweesAmountByFollowerId(1L)).thenReturn(5);
+        when(subscriptionRepository.findFolloweesAmountByFollowerId(1L)).thenReturn(5);
 
         Integer actualFollowingCount = subscriptionService.getFollowingCount(1L);
 
-        Assertions.assertEquals(5, actualFollowingCount);
+        assertEquals(5, actualFollowingCount);
     }
 
     @Test
@@ -125,7 +137,7 @@ public class SubscriptionServiceTest {
         User appliedForFilter = User.builder().phone("+79999999999").username("Takewqa").experience(222).build();
         User notAppliedForFilter = User.builder().phone("+79999999997").username("Aqwekat").experience(222).build();
 
-        Mockito.when(subscriptionRepository.findByFolloweeId(1L))
+        when(subscriptionRepository.findByFolloweeId(1L))
                 .thenReturn(Stream.of(appliedForFilter, notAppliedForFilter));
 
         List<FollowerResponse> actualFollowers = subscriptionService.getFollowers(1L,
@@ -136,16 +148,16 @@ public class SubscriptionServiceTest {
                         333
                 ));
 
-        Assertions.assertEquals(1, actualFollowers.size());
-        Assertions.assertEquals("Takewqa", actualFollowers.get(0).username());
+        assertEquals(1, actualFollowers.size());
+        assertEquals("Takewqa", actualFollowers.get(0).username());
     }
 
     @Test
     public void testGetFollowersCount() {
-        Mockito.when(subscriptionRepository.findFollowersAmountByFolloweeId(1L)).thenReturn(4);
+        when(subscriptionRepository.findFollowersAmountByFolloweeId(1L)).thenReturn(4);
 
         Integer actualFollowersCount = subscriptionService.getFollowersCount(1L);
 
-        Assertions.assertEquals(4, actualFollowersCount);
+        assertEquals(4, actualFollowersCount);
     }
 }

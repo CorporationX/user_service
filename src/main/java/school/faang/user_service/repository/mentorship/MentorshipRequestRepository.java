e6f7b87fe8fd19c4dced0491a6d1c2f58c44dc3e
Artpyak <112198749+Artpyak@users.noreply.github.com>
2025-03-16 12:58:17 +0300

    Update MentorshipRequestRepository.java

--- src/main/java/school/faang/user_service/repository/mentorship/MentorshipRequestRepository.java
+++ src/main/java/school/faang/user_service/repository/mentorship/MentorshipRequestRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import school.faang.user_service.entity.MentorshipRequest;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MentorshipRequestRepository extends CrudRepository<MentorshipRequest, Long> {

47de22fa1f0bf7a48fa5655c94d825aa49fcae2d
Anton Kapshukov <kapshukov.04@mail.ru>
2025-03-17 11:12:00 +0300

    BJS2-66141: исправил косяк не отсносящийся к задаче

--- src/main/java/school/faang/user_service/dto/RequestFilterDto.java
+++ src/main/java/school/faang/user_service/dto/RequestFilterDto.java
@@ -3,7 +3,6 @@ package school.faang.user_service.dto;
 import lombok.Data;
 import school.faang.user_service.entity.RequestStatus;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.entity.recommendation.SkillRequest;
 
 import java.time.LocalDateTime;
 import java.util.List;

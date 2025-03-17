9a5c003c7eb503d3f9b83fc4fa4fb88d27135548
Sergei Ananev <sa.ananyev@yandex.ru>
2025-03-17 21:55:34 +0300

    BJS2-65875 code style fix

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
-public class UserServiceApplication{
+public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
--- src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
+++ src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
@@ -3,6 +3,5 @@ package school.faang.user_service.repository.mentorship;
 import org.springframework.data.repository.CrudRepository;
 import school.faang.user_service.entity.User;
 
-
 public interface MentorshipRepository extends CrudRepository<User, Long> {
 }

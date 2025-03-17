10aa571f1835e8fcf92deca0c87f2a169b6ee853
Anton Kapshukov <kapshukov.04@mail.ru>
2025-03-17 10:00:43 +0300

    BJS2-66141: реализовал AvatarService

--- src/main/java/school/faang/user_service/service/avatar/AvatarService.java
+++ src/main/java/school/faang/user_service/service/avatar/AvatarService.java
@@ -1,4 +1,15 @@
 package school.faang.user_service.service.avatar;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class AvatarService {
+    private final String AVATAR_API_URL = "https://api.dicebear.com/5.x/avataaars/svg";
+
+    public String generateRandomAvatar() {
+        String url = AVATAR_API_URL + "?seed=" + System.currentTimeMillis();
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class);
+    }
 }

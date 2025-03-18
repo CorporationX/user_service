e6b661bf06aba73e42582c2cfbe591f2dc1cca4f
Savchenko <savchenko.ra@yandex.ru>
2025-03-18 10:31:46 +0300

    BJS2-66803 ref.

--- src/main/java/school/faang/user_service/service/implementation/SkillServiceImpl.java
+++ src/main/java/school/faang/user_service/service/implementation/SkillServiceImpl.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.service.skill.implementation;
+package school.faang.user_service.service.implementation;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;

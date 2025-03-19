61e14f858a9fac23fd65a90c9fdb5d3d605df546
Anton Kapshukov <kapshukov.04@mail.ru>
2025-03-19 18:14:10 +0300

    BJS2-66141: добавил зависимость MinIO

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,11 @@ dependencies {
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.464")
 
     /**
+     * MinIO
+     */
+    implementation("io.minio:minio:8.4.6")
+
+    /**
      * Utils & Logging
      */
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")

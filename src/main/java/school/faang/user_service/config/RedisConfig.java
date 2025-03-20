6df91fc5db0b3affba8bda3019efcdb4f7af9300
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-03-20 21:00:03 +0300

    Вынесен редис конфиг из папки leaderboard

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.config.leaderboard;
+package school.faang.user_service.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;

e26c5cb77eadac680d21a91434f5a2093e8ea0bd
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-03-21 22:24:25 +0300

    добавил @RequestMapping("/recommendation")

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -2,15 +2,13 @@ package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.service.RecommendationService;
@@ -20,10 +18,11 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/recommendation")
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    @PostMapping("/recommendation")
+    @PostMapping()
     public RecommendationDto giveRecommendation(@RequestBody RecommendationDto recommendationDto) {
         log.info("The process of transmitting the recommendation to the user begins");
         RecommendationDto returnedRecommendation = recommendationService.create(recommendationDto);
@@ -31,7 +30,7 @@ public class RecommendationController {
         return returnedRecommendation;
     }
 
-    @PutMapping("/recommendation")
+    @PutMapping()
     public RecommendationDto updateRecommendation(@RequestBody RecommendationDto recommendationDto) {
         log.info("Starting update of recommendation");
         RecommendationDto updatedRecommendationDto = recommendationService.update(recommendationDto);
@@ -39,20 +38,20 @@ public class RecommendationController {
         return updatedRecommendationDto;
     }
 
-    @DeleteMapping("/recommendation/{id}")
+    @DeleteMapping("/{id}")
     public void deleteRecommendation(@PathVariable Long id) {
         log.info("Starting to delete the recommendation");
         recommendationService.delete(id);
         log.info("The recommendation has been removed");
     }
 
-    @GetMapping("/recommendation/{authorId}")
+    @GetMapping("/{authorId}")
     public List<RecommendationDto> getAllGivenRecommendation(@PathVariable Long authorId) {
         outputOfRecommendations(recommendationService.getAllGivenRecommendation(authorId));
         return recommendationService.getAllGivenRecommendation(authorId);
     }
 
-    @GetMapping("/recommendation/{receiverId}")
+    @GetMapping("/{receiverId}")
     public List<RecommendationDto> getAllUserRecommendations(@PathVariable Long receiverId) {
         outputOfRecommendations(recommendationService.getAllUserRecommendations(receiverId));
         return recommendationService.getAllGivenRecommendation(receiverId);

4cde338dadee989f75695f8da69f8a08156b12e1
Anton Kapshukov <kapshukov.04@mail.ru>
2025-03-21 17:15:20 +0300

    BJS2-66141: создал маппинг для аватара

--- src/main/java/school/faang/user_service/controller/avatar/UserController.java
+++ src/main/java/school/faang/user_service/controller/avatar/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.service.avatar.AvatarService;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/avatar")
 public class UserController {
     private static final String JSON_RESPONSE_TEMPLATE = "{\"userId\":\"%s\", \"avatarUrl\":\"%s\"}";
     private static final String RECEIVED_REQUEST_LOG = "Received registration request%s";

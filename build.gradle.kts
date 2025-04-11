c2d386f070ab1e4d44a34efad1a3819fd12db0d1
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2025-04-11 21:39:12 +0400

    BJS2-53274 | Настроила контейнер MinIO для интеграционных тестов

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
-    testImplementation("org.testcontainers:kafka:1.17.6")
 
     /**
      * Tests
--- src/test/java/school/faang/user_service/AbstractIntegrationTest.java
+++ src/test/java/school/faang/user_service/AbstractIntegrationTest.java
@@ -1,6 +1,10 @@
 package school.faang.user_service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,7 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -22,6 +27,10 @@ import org.testcontainers.utility.DockerImageName;
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
+    private static final String S3_DOCKER_IMAGE = "minio/minio:latest";
+    private static final int S3_EXPOSED_PORT = 9000;
+    private static final String S3_USER = "user";
+    private static final String S3_PASSWORD = "password";
 
     @Autowired protected MockMvc mockMvc;
 
@@ -35,6 +44,17 @@ public abstract class AbstractIntegrationTest {
     private static final RedisContainer REDIS_CONTAINER =
             new RedisContainer(DockerImageName.parse("redis/redis-stack:latest"));
 
+    @Container
+    private static final GenericContainer<?> S3_CONTAINER
+            = new GenericContainer<>(DockerImageName.parse(S3_DOCKER_IMAGE))
+            .withExposedPorts(S3_EXPOSED_PORT)
+            .withEnv("MINIO_ROOT_USER", S3_USER)
+            .withEnv("MINIO_ROOT_PASSWORD", S3_PASSWORD)
+            .withCommand("server", "/data")
+            .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                    new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(S3_EXPOSED_PORT),
+                            new ExposedPort(S3_EXPOSED_PORT)))));
+
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
@@ -43,5 +63,9 @@ public abstract class AbstractIntegrationTest {
 
         registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
         registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+
+        registry.add("S3_PORT", S3_CONTAINER::getFirstMappedPort);
+        registry.add("S3_ACCESS_KEY", () -> S3_USER);
+        registry.add("S3_SECRET_KEY", () -> S3_PASSWORD);
     }
 }

3daeed9f4e742544353c0c68328cc281847fe96e
Buga Dan <danbil.942@gmail.com>
2025-05-08 20:25:58 +0300

    Fixed few issues with wrappers.

--- src/main/java/school/faang/user_service/dto/skill/SkillCandidateDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillCandidateDto.java
@@ -7,5 +7,5 @@ import lombok.Data;
 @AllArgsConstructor
 public class SkillCandidateDto {
     private SkillDto skill;
-    private long offersAmount;
+    private Long offersAmount;
 }
--- src/main/java/school/faang/user_service/dto/skill/SkillDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class SkillDto {
-    private long id;
+    private Long id;
     private String title;
 }

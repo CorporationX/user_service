d83145295cdca4aaee323514198d299526840d0e
Valeria <valeriia.shurupina@yahoo.com>
2025-05-11 20:58:44 +0200

    BJS2-77413 add unit test for getMentors

--- src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.MenteeDto;
+import school.faang.user_service.dto.MentorDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.mapper.MentorshipMapper;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
@@ -47,5 +48,27 @@ public class MentorshipServiceTest {
         List<MenteeDto> result = mentorshipService.getMentees(userId);
 
         assertEquals(menteeDtos, result);
+    }
+
+    @Test
+    public void testGetMentors() {
+        long userId = 2L;
+
+        User mentor = new User();
+        mentor.setId(1L);
+        List<User> mentors = List.of(mentor);
+
+        MentorDto mentorDto = new MentorDto();
+        mentorDto.setId(1L);
+        List<MentorDto> mentorDtos = List.of(mentorDto);
+
+        Mockito.when(mentorshipRepository.findMentorsByMenteeId(userId))
+                .thenReturn(mentors);
+        Mockito.when(mentorshipMapper.mentorsToMentorsDtos(mentors))
+                .thenReturn(mentorDtos);
+
+        List<MentorDto> result = mentorshipService.getMentors(userId);
+
+        assertEquals(mentorDtos, result);
     }
 }

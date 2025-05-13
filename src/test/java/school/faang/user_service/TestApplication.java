6597305706f2fdf6ef682dddd22a08e7e342547c
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-05-13 12:14:57 +0300

    BJS2-77187 create UserService pipeline. test fail action

--- src/test/java/school/faang/user_service/TestApplication.java
+++ src/test/java/school/faang/user_service/TestApplication.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestApplication {
     @Test
     public void testFailAction() {
+        // add comment
         assertTrue(false);
     }
 }

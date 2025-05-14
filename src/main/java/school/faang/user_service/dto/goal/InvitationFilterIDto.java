fe65991fec34d940da1e9c66ac64eda25b30b23d
Aleksandr <alexandr.sidorkin@gmail.com>
2025-05-14 18:03:01 +0200

    Task 77371 removeGoal updated with invitation.goal sets null, so far invitation operations are nullsafe for this field. DTOs annotations and access modifiers updated.

--- src/main/java/school/faang/user_service/dto/goal/InvitationFilterIDto.java
+++ src/main/java/school/faang/user_service/dto/goal/InvitationFilterIDto.java
@@ -1,15 +1,13 @@
 package school.faang.user_service.dto.goal;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import school.faang.user_service.entity.RequestStatus;
 
-@Builder
-@RequiredArgsConstructor
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvitationFilterIDto {
-    private final Long inviterId;
-    private final Long invitedId;
-    private final RequestStatus status;
+    private Long inviterId;
+    private Long invitedId;
+    private RequestStatus status;
 }

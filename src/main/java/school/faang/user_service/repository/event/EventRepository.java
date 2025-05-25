f5817d42b11b3b2b956c43f2c719faf7a1828adc
Kirill <kirill551@yandex.ru>
2025-05-25 21:50:38 +0200

    "Убрать лишние классы и пакеты из PR"

---
+++ src/main/java/school/faang/user_service/repository/event/EventRepository.java
@@ -0,0 +1,28 @@
+package school.faang.user_service.repository.event;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import school.faang.user_service.entity.event.Event;
+import school.faang.user_service.entity.promotion.enums.Plan;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT e.* FROM event e
+            WHERE e.user_id = :userId
+            """)
+    List<Event> findAllByUserId(long userId);
+
+    @Query(nativeQuery = true, value = """
+            SELECT e.* FROM event e
+            JOIN user_event ue ON ue.event_id = e.id
+            WHERE ue.user_id = :userId
+            """)
+    List<Event> findParticipatedEventsByUserId(long userId);
+}

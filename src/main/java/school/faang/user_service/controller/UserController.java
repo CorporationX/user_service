1508bc6b44fb4f13489f37eb8b93350296c16ca3
Anatolii Izotov <izotov.anatoly.e@gmail.com>
2025-05-29 22:03:50 +0200

    refactor(controller): removed the old controller and add a new method [BJS2-77455]

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.controller.user;
+package school.faang.user_service.controller;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -7,16 +7,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.service.UserDataProcessingService;
 import school.faang.user_service.service.user.UserService;
 
-@Slf4j
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
+    private final UserDataProcessingService userDataProcessingService;
     private final UserService userService;
+
+    @PostMapping
+    public List<UserDto> getUsersByIds(@RequestBody List<Long> ids) {
+        if (ids == null || ids.isEmpty()) {
+            log.info("No user IDs provided. Returning empty list.");
+            return Collections.emptyList();
+        }
+        log.info("Received request to fetch users with IDs: {}", ids);
+        return userDataProcessingService.fetchUsers(ids, 0, ids.size());
+    }
 
     @GetMapping("/{userId}/exists")
     public ResponseEntity<Void> checkUserExists(@PathVariable @NotNull @Positive Long userId) {

7b883ff84bc9eb5b073726bb2ece55cc62a0deb1
Kirill <kirill551@yandex.ru>
2025-06-03 22:38:47 +0200

    "Настроить конфигурацию feign client для запросов к сервису оплаты через service discovery"

--- src/main/java/school/faang/user_service/client/PaymentServiceClient.java
+++ src/main/java/school/faang/user_service/client/PaymentServiceClient.java
@@ -14,11 +14,9 @@ import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 
 @FeignClient(
-        name = "paymentClient",
-        url = "${service.payment.url}",
+        name = "${service.payment.name}",
         configuration = FeignConfig.class
 )
-
 @Retryable(
         retryFor = { SocketTimeoutException.class,
                 ConnectException.class,

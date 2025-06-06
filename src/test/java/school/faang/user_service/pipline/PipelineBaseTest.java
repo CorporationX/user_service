fc57dce3a92f62131efdc72c2e738092b799e72d
v888.rus <v888.rus@gmail.com>
2025-06-06 06:18:57 +0000

    PieplineTest(2.5). 77818.

--- src/test/java/school/faang/user_service/pipline/PipelineBaseTest.java
+++ src/test/java/school/faang/user_service/pipline/PipelineBaseTest.java
@@ -1,10 +1,12 @@
 package school.faang.user_service.pipline;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 public class PipelineBaseTest {
     @Test
     public void test() {
-
+        fail("Unconditional fail...");
     }
 }

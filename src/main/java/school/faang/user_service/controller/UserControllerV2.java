7f3866091e37f53fba79a8ec7b143fe4ab64e2a7
v888.rus <v888.rus@gmail.com>
2025-06-18 11:28:01 +0000

    Fix(4). Moved controller v2 to the corect package

--- src/main/java/school/faang/user_service/controller/UserControllerV2.java
+++ src/main/java/school/faang/user_service/controller/UserControllerV2.java
@@ -1,4 +1,4 @@
-package school.faang.user_service;
+package school.faang.user_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

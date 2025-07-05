dcc0c0a734feea1795c1a343e33cc1136e48e9cb
Raidanmark <Onidanil@Gmail.com>
2025-07-05 22:34:41 +0200

    refactor: Replace KafkaTopics with KafkaTopicsProperties in premium-related services and tests

--- src/main/java/school/faang/user_service/kafka/events/PremiumBoughtEvent.java
+++ src/main/java/school/faang/user_service/kafka/events/PremiumBoughtEvent.java
@@ -10,7 +10,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
-//@Builder
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class PremiumBoughtEvent extends Event {
--- src/main/java/school/faang/user_service/service/premium/PremiumService.java
+++ src/main/java/school/faang/user_service/service/premium/PremiumService.java
@@ -11,7 +11,7 @@ import school.faang.user_service.entity.transaction.TransactionStatus;
 import school.faang.user_service.kafka.events.AnalyticsEventType;
 import school.faang.user_service.kafka.events.PremiumBoughtEvent;
 import school.faang.user_service.kafka.producer.KafkaDataSenderImpl;
-import school.faang.user_service.kafka.producer.KafkaTopics;
+import school.faang.user_service.kafka.producer.KafkaTopicsProperties;
 import school.faang.user_service.mapper.TransactionMapper;
 import school.faang.user_service.service.transaction.TransactionService;
 import school.faang.user_service.service.utils.PremiumServiceUtils;
@@ -25,7 +25,7 @@ public class PremiumService {
     private final TransactionMapper transactionMapper;
     private final UserContext userContext;
     private final KafkaDataSenderImpl kafkaDataSender;
-    private final KafkaTopics kafkaTopics;
+    private final KafkaTopicsProperties kafkaTopics;
 
     public TransactionResultDto buyPremium(Long userId, Integer durationDays) {
         premiumServiceUtils.checkUserHasNoPremium(userId);
@@ -35,11 +35,7 @@ public class PremiumService {
                 .equals(TransactionStatus.SETTLED)) {
             premiumServiceUtils.assignPremiumToUser(userId, durationDays);
         }
-//        PremiumBoughtEvent premiumBoughtEvent = PremiumBoughtEvent.builder()
-//                .paymentAmount(transaction.getAmount())
-//                .subscriptionDuration(durationDays)
-//                .sentAt(transaction.getCreatedAt())
-//                .build();
+
         PremiumBoughtEvent premiumBoughtEvent = new PremiumBoughtEvent();
         premiumBoughtEvent.setPaymentAmount(transaction.getAmount());
         premiumBoughtEvent.setSubscriptionDuration(durationDays);

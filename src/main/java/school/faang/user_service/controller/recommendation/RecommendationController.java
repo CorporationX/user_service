35ee3b50c9dbae49d09f5f00690134d89b9b566f
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-07-08 22:47:51 +0300

    More and better tests. Now with filters!

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
@@ -3,6 +3,7 @@ package school.faang.user_service.controller.recommendation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,7 @@ import java.util.List;
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    @PostMapping("/create")
+    @PostMapping
     public RecommendationDto create(@RequestBody @Valid CreateRecommendationDto recommendationDto) {
         return recommendationService.create(recommendationDto);
     }
@@ -37,12 +38,12 @@ public class RecommendationController {
         return recommendationService.update(recommendationId, recommendationDto);
     }
 
-    @PostMapping("/delete")
-    public void delete(long recommendationId) {
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") long recommendationId) {
         recommendationService.delete(recommendationId);
     }
 
-    @PostMapping("/getbyfilters")
+    @PostMapping("/filter")
     public List<RecommendationDto> getByFilters(@RequestBody @Valid RecommendationFilterDto filters) {
         return recommendationService.getByFilters(filters);
     }

2bcc69012265df8b2e518eaa0cf7acf4e622265f
White <ilyakrivitskiyn@gmail.com>
2025-08-02 18:12:19 +0300

    Rebase

--- src/test/java/school/faang/user_service/service/MentorshipRequestServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipRequestServiceTest.java
@@ -10,6 +10,10 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.dto.mentorship.CreateMentorshipRequestDto;
+import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
+import school.faang.user_service.dto.mentorship.MentorshipRequestFilterDto;
+import school.faang.user_service.dto.mentorship.RejectionDto;
+import school.faang.user_service.entity.RequestStatus;
 import school.faang.user_service.entity.user.MentorshipRequest;
 import school.faang.user_service.entity.user.User;
 import school.faang.user_service.exception.DataValidationException;
@@ -45,27 +49,6 @@ public class MentorshipRequestServiceTest {
     private UserRepository userRepository;
     @Mock
     private UserContext userContext;
-
-
-    @DisplayName("Should delete mentor from his mentees")
-    public void deleteMentorFromMentees() {
-        User mentor = User.builder()
-                .id(1L)
-                .build();
-        User mentee = User.builder()
-                .id(2L)
-                .mentors(new ArrayList<>() {
-                    {
-                        add(mentor);
-                    }
-                })
-                .build();
-        mentor.setMentees(List.of(mentee));
-        when(userRepository.getByIdOrThrow(1L)).thenReturn(mentor);
-
-        mentorshipRequestService.deactivateMentor(mentor.getId());
-        assertEquals(new ArrayList<>(), mentee.getMentors());
-    }
 
     @Test
     @DisplayName("Testing when requester is mentor")
@@ -198,5 +181,151 @@ public class MentorshipRequestServiceTest {
 
         verify(userRepository).save(mentee);
         verify(userRepository).save(mentor);
+    }
+
+    @Test
+    @DisplayName("Testing set status ACCEPTED in accept method")
+    public void setStatusAccepted() {
+        User mentee = new User();
+        mentee.setMentors(new ArrayList<>());
+        mentee.setMentees(new ArrayList<>());
+        Long menteeId = 1L;
+        mentee.setId(menteeId);
+
+
+        User mentor = new User();
+        mentor.setMentees(new ArrayList<>());
+        mentor.setMentors(new ArrayList<>());
+        Long mentorId = 2L;
+        mentor.setId(mentorId);
+
+        long requestId = 1L;
+        MentorshipRequest request = new MentorshipRequest();
+        request.setId(requestId);
+        request.setRequester(mentee);
+        request.setReceiver(mentor);
+
+        when(mentorshipRequestRepository
+                .findById(requestId))
+                .thenReturn(Optional.of(request));
+
+        mentorshipRequestService.accept(requestId);
+
+        assertEquals(RequestStatus.ACCEPTED, request.getStatus());
+    }
+
+    @Test
+    @DisplayName("Testing save request in accept method")
+    public void saveRequest() {
+        User mentee = new User();
+        mentee.setMentors(new ArrayList<>());
+        mentee.setMentees(new ArrayList<>());
+        Long menteeId = 1L;
+        mentee.setId(menteeId);
+
+        User mentor = new User();
+        mentor.setMentees(new ArrayList<>());
+        mentor.setMentors(new ArrayList<>());
+        Long mentorId = 2L;
+        mentor.setId(mentorId);
+
+        long requestId = 1L;
+        MentorshipRequest request = new MentorshipRequest();
+        request.setId(requestId);
+        request.setRequester(mentee);
+        request.setReceiver(mentor);
+
+        when(mentorshipRequestRepository
+                .findById(requestId))
+                .thenReturn(Optional.of(request));
+
+        mentorshipRequestService.accept(requestId);
+
+        verify(mentorshipRequestRepository, times(1)).save(request);
+    }
+
+    @Test
+    @DisplayName("Testing throws EntityNotFound in reject method")
+    public void throwsEntityNotFoundExceptionReject() {
+        long requestId = 1L;
+        RejectionDto dto = new RejectionDto("");
+        MentorshipRequest request = new MentorshipRequest();
+        request.setId(requestId);
+
+        when(mentorshipRequestRepository
+                .findById(requestId))
+                .thenThrow(EntityNotFoundException.class);
+
+        assertThrows(EntityNotFoundException.class, () -> mentorshipRequestService.reject(requestId, dto));
+    }
+
+    @Test
+    @DisplayName("Testing set status Rejected in reject method")
+    public void setRejectedStatus() {
+        long requestId = 1L;
+        RejectionDto dto = new RejectionDto("");
+        MentorshipRequest request = new MentorshipRequest();
+        request.setId(requestId);
+
+        when(mentorshipRequestRepository
+                .findById(requestId))
+                .thenReturn(Optional.of(request));
+
+        mentorshipRequestService.reject(requestId, dto);
+
+        assertEquals(RequestStatus.REJECTED, request.getStatus());
+    }
+
+    @Test
+    @DisplayName("Testing save to repository in reject method")
+    public void saveRejectedRequest() {
+        long requestId = 1L;
+        RejectionDto dto = new RejectionDto("");
+        MentorshipRequest request = new MentorshipRequest();
+        request.setId(requestId);
+
+        when(mentorshipRequestRepository
+                        .findById(requestId))
+                .thenReturn(Optional.of(request));
+
+        mentorshipRequestService.reject(requestId, dto);
+
+        verify(mentorshipRequestRepository, times(1)).save(request);
+    }
+
+    @Test
+    @DisplayName("Testing filter")
+    public void getByFilter() {
+        User firstUser = new User();
+        firstUser.setId(1L);
+        User secondUser = new User();
+        secondUser.setId(2L);
+        User thirstUser = new User();
+        thirstUser.setId(3L);
+
+        MentorshipRequest firstRequest = new MentorshipRequest();
+        firstRequest.setId(1L);
+        firstRequest.setRequester(firstUser);
+        firstRequest.setReceiver(secondUser);
+        firstRequest.setStatus(RequestStatus.ACCEPTED);
+
+        MentorshipRequest secondRequest = new MentorshipRequest();
+        secondRequest.setId(2L);
+        secondRequest.setRequester(secondUser);
+        secondRequest.setReceiver(thirstUser);
+        secondRequest.setStatus(RequestStatus.REJECTED);
+
+        List<MentorshipRequest> requests = List.of(firstRequest, secondRequest);
+
+        MentorshipRequestFilterDto filterDto = new MentorshipRequestFilterDto(
+                1L, null, null);
+
+        List<MentorshipRequestDto> attackedFilteredRequests = new ArrayList<>();
+        attackedFilteredRequests.add(mentorshipRequestMapper.toMentorshipRequestDto(firstRequest));
+
+        when(mentorshipRequestRepository.findAll()).thenReturn(requests);
+        List<MentorshipRequestDto> defencedFilteredRequests = mentorshipRequestService.getByFilters(filterDto);
+
+        assertEquals(attackedFilteredRequests, defencedFilteredRequests);
     }
 }

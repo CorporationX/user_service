4fbe0df739733d24fa0bc4702e2b2ade27076f1d
Roman Chipura <connor.ueyn@gmail.com>
2025-10-08 12:44:45 +0500

    Валидация проверяла доступ, айди пользователя и айди распорядка.
    Теперь сравниваются два юзер айди.

--- src/main/java/school/faang/user_service/service/workschedule/WorkScheduleServiceImpl.java
+++ src/main/java/school/faang/user_service/service/workschedule/WorkScheduleServiceImpl.java
@@ -25,11 +25,11 @@ public class WorkScheduleServiceImpl implements WorkScheduleService {
     @Override
     public WorkScheduleDto addWorkSchedule(long userId, WorkScheduleDto workScheduleDto) {
         validateWorkSchedule(workScheduleDto);
-        validateUserAccess(userId, workScheduleDto.id());
 
         User user = userRepository.getByIdOrThrow(workScheduleDto.id());
 
         WorkSchedule workSchedule = workScheduleMapper.toWorkSchedule(workScheduleDto);
+        validateUserAccess(userId, workSchedule.getUser().getId());
         workSchedule.setUser(user);
         WorkSchedule savedWorkSchedule = workScheduleRepository.save(workSchedule);
 
@@ -38,11 +38,11 @@ public class WorkScheduleServiceImpl implements WorkScheduleService {
 
     public WorkScheduleDto updateWorkSchedule(long userId, long workScheduleId, WorkScheduleDto workScheduleDto) {
         validateWorkSchedule(workScheduleDto);
-        validateUserAccess(userId, workScheduleDto.id());
 
         WorkSchedule workSchedule = workScheduleRepository.getByIdOrThrow(workScheduleId);
 
         WorkSchedule newWorkSchedule = workScheduleMapper.toWorkSchedule(workScheduleDto);
+        validateUserAccess(userId, workSchedule.getUser().getId());
         newWorkSchedule.setUser(workSchedule.getUser());
         WorkSchedule updatedWorkSchedule = workScheduleRepository.save(newWorkSchedule);
 

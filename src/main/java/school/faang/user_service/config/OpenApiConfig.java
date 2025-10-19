35484043d293a1aa2176f43f027e36fed21c4165
Bimcore <vzeten@gmail.com>
2025-10-20 00:39:15 +0200

    Поправил чекстайл openAPI на openApi Перенес длинные строки

--- src/main/java/school/faang/user_service/config/OpenApiConfig.java
+++ src/main/java/school/faang/user_service/config/OpenApiConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openApi() {
         SecurityScheme headerAuth = new SecurityScheme()
                 .type(SecurityScheme.Type.APIKEY)
                 .in(SecurityScheme.In.HEADER)

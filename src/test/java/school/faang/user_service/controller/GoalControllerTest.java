a28bc932fbd263c692fa9812cc20213db59360b1
Oleg_Korovin <olegk.99@mail.ru>
2025-10-19 16:05:05 +0300

    refactor: добавить тесты для метода update, оптимизировать тесты через @ParameterizedTest

--- src/test/java/school/faang/user_service/controller/GoalControllerTest.java
+++ src/test/java/school/faang/user_service/controller/GoalControllerTest.java
@@ -2,16 +2,20 @@ package school.faang.user_service.controller;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.controller.goal.GoalController;
 import school.faang.user_service.dto.goal.CreateGoalDto;
 import school.faang.user_service.dto.goal.GoalDto;
+import school.faang.user_service.dto.goal.UpdateGoalDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.goal.GoalService;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,10 +32,12 @@ class GoalControllerTest {
     @InjectMocks
     private GoalController goalController;
 
-    @Test
-    public void testGoalCreationTitleIsNull() {
-        CreateGoalDto createGoalDto = new CreateGoalDto(
-                null,
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "     ", "\t", "\n"})
+    public void testGoalCreationTitleIsInValid(String title) {
+        final CreateGoalDto createGoalDto = new CreateGoalDto(
+                title,
                 "Some description",
                 null,
                 null,
@@ -42,11 +48,13 @@ class GoalControllerTest {
         verify(goalService, never()).create(createGoalDto);
     }
 
-    @Test
-    public void testGoalCreationTitleIsBlank() {
-        CreateGoalDto createGoalDto = new CreateGoalDto(
-                "    ",
-                "Some description",
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "     ", "\t", "\n"})
+    public void testGoalCreationDescriptionIsInvalid(String description) {
+        final CreateGoalDto createGoalDto = new CreateGoalDto(
+                "Some title",
+                description,
                 null,
                 null,
                 List.of(1L));
@@ -56,56 +64,16 @@ class GoalControllerTest {
         verify(goalService, never()).create(createGoalDto);
     }
 
-    @Test
-    public void testGoalCreationDescriptionIsNull() {
-        CreateGoalDto createGoalDto = new CreateGoalDto(
-                "Some title",
-                null,
-                null,
-                null,
-                List.of(1L));
-
-        assertThrows(DataValidationException.class, () -> goalController.create(createGoalDto));
-
-        verify(goalService, never()).create(createGoalDto);
-    }
-
-    @Test
-    public void testGoalCreationDescriptionIsBlank() {
-        CreateGoalDto createGoalDto = new CreateGoalDto(
-                "Some title",
-                "     ",
-                null,
-                null,
-                List.of(1L));
-
-        assertThrows(DataValidationException.class, () -> goalController.create(createGoalDto));
-
-        verify(goalService, never()).create(createGoalDto);
-    }
-
-    @Test
-    public void testGoalCreationUserIdsIsNull() {
-        CreateGoalDto createGoalDto = new CreateGoalDto(
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    public void testGoalCreationUserIdsIsInvalid(List<Long> userIds) {
+        final CreateGoalDto createGoalDto = new CreateGoalDto(
                 "Some title",
                 "Some description",
                 null,
                 null,
-                null);
-
-        assertThrows(DataValidationException.class, () -> goalController.create(createGoalDto));
-
-        verify(goalService, never()).create(createGoalDto);
-    }
-
-    @Test
-    public void testGoalCreationUserIdsIsEmpty() {
-        CreateGoalDto createGoalDto = new CreateGoalDto(
-                "Some title",
-                "Some description",
-                null,
-                null,
-                Collections.emptyList());
+                userIds);
 
         assertThrows(DataValidationException.class, () -> goalController.create(createGoalDto));
 
@@ -114,7 +82,7 @@ class GoalControllerTest {
 
     @Test
     public void testGoalCreationSuccess() {
-        CreateGoalDto createGoalDto = new CreateGoalDto(
+        final CreateGoalDto createGoalDto = new CreateGoalDto(
                 "Some title",
                 "Some description",
                 null,
@@ -138,5 +106,67 @@ class GoalControllerTest {
         assertEquals(createGoalDto.userIds(), goalDto.userIds());
 
         verify(goalService).create(createGoalDto);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "     ", "\t", "\n"})
+    public void testGoalUpdateTitleIsInvalid(String title) {
+        final UpdateGoalDto updateGoalDto = new UpdateGoalDto(
+                title,
+                "Some description",
+                null,
+                null,
+                null);
+        final long goalId = 5L;
+
+        assertThrows(DataValidationException.class, () -> goalController.update(goalId, updateGoalDto));
+
+        verify(goalService, never()).update(goalId, updateGoalDto);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "     ", "\t", "\n"})
+    public void testGoalUpdateDescriptionIsInvalid(String description) {
+        final UpdateGoalDto updateGoalDto = new UpdateGoalDto(
+                "Some title",
+                description,
+                null,
+                null,
+                null);
+        final long goalId = 5L;
+
+        assertThrows(DataValidationException.class, () -> goalController.update(goalId, updateGoalDto));
+
+        verify(goalService, never()).update(goalId, updateGoalDto);
+    }
+
+    @Test
+    public void testGoalUpdateSuccess() {
+        final UpdateGoalDto updateGoalDto = new UpdateGoalDto(
+                "Some title",
+                "Some description",
+                null,
+                null,
+                null);
+        final long goalId = 5L;
+        when(goalService.update(goalId, updateGoalDto))
+                .thenReturn(new GoalDto(
+                        "Some title",
+                        "Some description",
+                        null,
+                        null,
+                        null,
+                        null,
+                        null));
+
+        GoalDto goalDto = goalController.update(goalId, updateGoalDto);
+
+        assertNotNull(goalDto);
+        assertEquals(updateGoalDto.title(), goalDto.title());
+        assertEquals(updateGoalDto.description(), goalDto.description());
+
+        verify(goalService).update(goalId, updateGoalDto);
     }
 }

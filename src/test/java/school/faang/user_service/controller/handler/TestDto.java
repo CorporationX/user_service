b16334b60650444e4f330e8c4d892908e1c61345
DeniAkhmadov <daglossteckin@gmail.com>
2025-11-07 17:14:25 +0300

    Внёс правки по check_style

--- src/test/java/school/faang/user_service/controller/handler/TestDto.java
+++ src/test/java/school/faang/user_service/controller/handler/TestDto.java
@@ -3,7 +3,7 @@ package school.faang.user_service.controller.handler;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-record TestDto (
+record TestDto(
         @NotBlank String name,
         @NotNull String aboutMe
 ) {}
